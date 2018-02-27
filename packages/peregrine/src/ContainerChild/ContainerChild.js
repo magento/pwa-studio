e17e89ac90a9b17d6c724c5c4bd2976015de9b0a
Andrew Levine <DrewML@users.noreply.github.com>
2018-02-27 15:13:00 -0600

    Remove "processing" flag and logic from ContainerChild (#25)

--- packages/peregrine/src/ContainerChild/ContainerChild.js
+++ packages/peregrine/src/ContainerChild/ContainerChild.js
@@ -1,39 +1,11 @@
 import { createElement, Component } from 'react';
-import { func, string, bool } from 'prop-types';
+import { func, string } from 'prop-types';
 
 export default class ContainerChild extends Component {
     static propTypes = {
         id: string.isRequired,
-        render: func.isRequired,
-        /**
-         * `processed` prop is injected by @magento/pwa-buildpack
-         */
-        processed: bool,
-        /**
-         * `targetContainer` prop is injected by @magento/pwa-buildpack
-         */
-        targetContainer: string
+        render: func.isRequired
     };
-
-    /**
-     * When the PWA Studio build tooling visits and processes a
-     * ContainerChild, it adds the `processed` prop. This enables
-     * catching any case where this component was used incorrectly
-     */
-    componentDidMount() {
-        if (process.env.NODE_ENV === 'production') return;
-        const { processed, targetContainer, id } = this.props;
-
-        if (!processed) {
-            console.warn(
-                '"ContainerChild" component was not preprocessed in the build. ' +
-                    `This means all extensions targeting "${
-                        id
-                    }" within Container "${targetContainer}" ` +
-                    'were not injected. Please see the "ContainerChild" component docs for further info.'
-            );
-        }
-    }
 
     render() {
         return this.props.render();
--- packages/peregrine/src/ContainerChild/__tests__/ContainerChild.test.js
+++ packages/peregrine/src/ContainerChild/__tests__/ContainerChild.test.js
@@ -5,22 +5,6 @@ import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
-test('Console warning when ContainerChild was not processed by buildpack', () => {
-    jest.spyOn(console, 'warn').mockImplementation(() => {});
-    shallow(<ContainerChild id="foo.bar" render={() => {}} />);
-    expect(console.warn).toHaveBeenCalledWith(
-        expect.stringMatching('not preprocessed')
-    );
-    console.warn.mockRestore();
-});
-
-test('No console warning when ContainerChild was processed by buildpack', () => {
-    jest.spyOn(console, 'warn').mockImplementation(() => {});
-    shallow(<ContainerChild id="foo.bar" render={() => {}} processed={true} />);
-    expect(console.warn).not.toHaveBeenCalled();
-    console.warn.mockRestore();
-});
-
 test('Renders content from render prop', () => {
     const wrapper = shallow(
         <ContainerChild
