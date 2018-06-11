dcfe5acfec06c8ecdb37dbcc204fd9ebf3fbe064
Andrew Levine <theandrewtizer@gmail.com>
2018-06-11 12:46:14 -0500

    Export Price component from root index.js

--- packages/peregrine/src/index.js
+++ packages/peregrine/src/index.js
@@ -3,3 +3,4 @@ export { default as ContainerChild } from './ContainerChild';
 export { default as List, Items, Item } from './List';
 export { default as Router } from './Router';
 export { default as Simulators } from './Simulators';
+export { default as Price } from './Price';
