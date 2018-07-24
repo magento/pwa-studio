16f9be84ea832e44b82b854767752827e2c54164
Artyom Kozinets <akozinets@comwrap.com>
2018-07-24 23:30:37 +0200

    Pwa-studio #99 Shop by Category component

--- packages/venia-concept/src/RootComponents/CMS/CMS.js
+++ packages/venia-concept/src/RootComponents/CMS/CMS.js
@@ -1,7 +1,18 @@
 import { createElement, Component } from 'react';
 
+import PropTypes from 'prop-types';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import Page from 'src/components/Page';
+import CategoryList from 'src/components/CategoryList';
+
 export default class CMS extends Component {
     render() {
-        return <div>CMS Page Stub</div>;
+
+        return (
+            <Page>
+                <CategoryList title={'Shop by category'} id={2}></CategoryList>
+            </Page>
+        );
     }
 }
