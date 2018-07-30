332cb41c17d1f89190745b60c91354f31f0ccb00
artKozinets <art.kozinets@gmail.com>
2018-07-30 15:36:10 +0200

    Category list component (#171)
    * Pwa-studio #99 Shop by Category component
    
    * Add comment
    
    * Code refactor
    
    * Makeup
    
    * Review changes

--- packages/venia-concept/src/RootComponents/CMS/CMS.js
+++ packages/venia-concept/src/RootComponents/CMS/CMS.js
@@ -1,7 +1,13 @@
 import { createElement, Component } from 'react';
+import Page from 'src/components/Page';
+import CategoryList from 'src/components/CategoryList';
 
 export default class CMS extends Component {
     render() {
-        return <div>CMS Page Stub</div>;
+        return (
+            <Page>
+                <CategoryList title="Shop by category" id={2} />
+            </Page>
+        );
     }
 }
