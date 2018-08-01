c01c470c1d98a21747a25fdf9255663e222f5d15
zetlen <jzetlen@adobe.com>
2018-08-01 07:14:56 -0500

    test(ci): report results to CircleCI via JUnit
    And configure DangerCI to consume them
    
    fix(ci): use github API to patch CI env

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
+const testPathRE = /(^\/packages\/[^\/]+\/|\.spec|\/__tests?__)/g;
+const testPathToFilePath = filepath => filepath.replace(testPathRE, '');
 module.exports = {
     projects: [
         'packages/peregrine',
@@ -5,5 +7,22 @@ module.exports = {
         'packages/venia-concept'
     ],
     collectCoverage: true,
-    collectCoverageFrom: ['src/**/*.js', '!**/__stories__/**']
+    collectCoverageFrom: ['src/**/*.js', '!**/__stories__/**'],
+    reporters: [
+        'default',
+        [
+            'jest-junit',
+            {
+                suiteName: 'Jest unit and functional tests',
+                output: './test-results/jest/results.xml',
+                suiteNameTemplate: ({ displayName, filepath }) =>
+                    `${displayName}: ${testPathToFilePath(
+                        testPathToFilePath(filepath)
+                    )}`,
+                classNameTemplate: ({ classname, title }) =>
+                    classname !== title ? classname : '',
+                titleTemplate: '{title}'
+            }
+        ]
+    ]
 };
