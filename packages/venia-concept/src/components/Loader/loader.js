1dcca9c9474552777fcb5cce629cb05bf75b13d1
pcvonz@gmail.com <pcvonz@gmail.com>
2018-08-16 16:36:43 -0700

    Make height of loader configurable property

--- packages/venia-concept/src/components/Loader/loader.js
+++ packages/venia-concept/src/components/Loader/loader.js
@@ -1,26 +1,31 @@
 import { Component, createElement } from 'react';
 import defaultClasses from './loader.css';
 import classify from 'src/classify';
-import { string, shape } from 'prop-types';
+import { string, shape, number } from 'prop-types';
 import logo from 'src/shared/logo.svg';
 
 class Loader extends Component {
     static propTypes = {
         classes: shape({
             root: string,
-            loaderAnimation: string
-        })
+            loaderAnimation: string,
+        }),
+        height: number
+    };
+
+    static defaultProps = {
+        height: 128
     };
 
     render() {
-        const { classes } = this.props;
+        const { classes, height } = this.props;
         return (
             <div className={classes.root}>
                 <img
                     className={classes.loaderAnimation}
                     alt="logo"
                     src={logo}
-                    height="128"
+                    height={height}
                 />
                 <p className={classes.loaderAnimation}>loading...</p>
             </div>
