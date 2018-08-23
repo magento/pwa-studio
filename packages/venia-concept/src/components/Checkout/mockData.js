b9118197d1a73751723a1e16ac44963203887f0d
cooper <serunde@gmail.com>
2018-08-23 10:08:56 -0700

    Add replicated mock data of shippingMethods to the Checkout component

---
+++ packages/venia-concept/src/components/Checkout/mockData.js
@@ -0,0 +1,16 @@
+export const shippingMethods = [
+  {
+    "carrier_code":"flatrate",
+    "method_code":"flatrate",
+    "carrier_title":"Flat Rate",
+    "method_title":"Fixed",
+    "amount":5,
+    "base_amount":5,
+    "available":true,
+    "error_message":"",
+    "price_excl_tax":5,
+    "price_incl_tax":5,
+    "code":"Flat Rate",
+    "title":"Flat Rate"
+  }
+]
