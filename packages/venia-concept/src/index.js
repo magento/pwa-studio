2b0d825c443e32fe087cd5eeba085902edffdd07
Jonathan Stein <jonathanstein@live.com>
2018-09-10 15:19:03 -0700

    add auth header login_token to graphql queries

--- packages/venia-concept/src/index.js
+++ packages/venia-concept/src/index.js
@@ -1,24 +1,49 @@
 import { createElement } from 'react';
 import ReactDOM from 'react-dom';
 import bootstrap from '@magento/peregrine';
-import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import reducer from 'src/reducers/app';
 import './index.css';
 
+const urlBase = new URL('/graphql', location.origin).toString();
+
 const { Provider, store } = bootstrap({
-    apiBase: new URL('/graphql', location.origin).toString(),
+    apiBase: urlBase,
     __tmp_webpack_public_path__: __webpack_public_path__
 });
 
 store.addReducer('app', reducer);
 
-const apolloClient = new ApolloClient();
+const httpLink = createHttpLink({
+    uri: urlBase,
+    __tmp_webpack_public_path__: __webpack_public_path__,
+});
+
+const authLink = setContext((_, { headers }) => {
+    // get the authentication token from local storage if it exists
+    const token = localStorage.getItem('login_token');
+    // return the headers to the context so httpLink can read them
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : "",
+        }
+    }
+});
+
+const apolloClient = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
+});
 
 ReactDOM.render(
     <ApolloProvider client={apolloClient}>
-        <Provider />
+    <Provider />
     </ApolloProvider>,
     document.getElementById('root')
 );
@@ -26,13 +51,13 @@ ReactDOM.render(
 if (process.env.SERVICE_WORKER && 'serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker
-            .register(process.env.SERVICE_WORKER)
-            .then(registration => {
-                console.log('Service worker registered: ', registration);
-            })
-            .catch(error => {
-                console.log('Service worker registration failed: ', error);
-            });
+        .register(process.env.SERVICE_WORKER)
+        .then(registration => {
+            console.log('Service worker registered: ', registration);
+        })
+        .catch(error => {
+            console.log('Service worker registration failed: ', error);
+        });
     });
 }
 
