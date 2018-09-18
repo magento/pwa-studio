f39361c28507c63220bf3778416d5d1382caf298
James Zetlen <zetlen@gmail.com>
2018-09-18 05:14:31 -0500

    fix(dev): MagentoRootComponentsPlugin dedupe fix (#221)
    * fix(dev): MagentoRootComponentsPlugin dedupe fix
    
    - Fix MagentoRootComponentsPlugin detection of entry points so that only
    user-defined entry points are considered, and not entry points issued by
    downstream plugins (this fixes resource duplication in named chunks)
    - Add phase detection for MagentoRootComponentsPlugin so it uses the
    right strategy based on development/production mode
    - Configure UglifyPlugin to correctly strip out
    MagentoRootComponentsPlugin imports hack
    
    * fixup: prettier
    
    * fix: PR feedback

--- packages/venia-concept/webpack.config.js
+++ packages/venia-concept/webpack.config.js
@@ -22,7 +22,14 @@ const themePaths = {
 };
 
 // mark dependencies for vendor bundle
-const libs = ['react', 'react-dom', 'react-redux', 'react-router-dom', 'redux'];
+const libs = [
+    'apollo-boost',
+    'react',
+    'react-dom',
+    'react-redux',
+    'react-router-dom',
+    'redux'
+];
 
 module.exports = async function(env) {
     const { phase } = env;
@@ -93,7 +100,7 @@ module.exports = async function(env) {
             }
         }),
         plugins: [
-            new MagentoRootComponentsPlugin(),
+            new MagentoRootComponentsPlugin({ phase }),
             new webpack.NoEmitOnErrorsPlugin(),
             new webpack.DefinePlugin({
                 'process.env.NODE_ENV': JSON.stringify(phase),
@@ -151,7 +158,13 @@ module.exports = async function(env) {
             new webpack.optimize.CommonsChunkPlugin({
                 names: ['vendor']
             }),
-            new UglifyPlugin()
+            new UglifyPlugin({
+                parallel: true,
+                uglifyOptions: {
+                    ecma: 8,
+                    keep_fnames: true
+                }
+            })
         );
     } else {
         throw Error(`Unsupported environment phase in webpack config: `);
