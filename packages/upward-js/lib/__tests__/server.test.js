159732b7f51b1429874363f1f981c78ae6ea758c
zetlen <jzetlen@adobe.com>
2018-10-03 12:51:42 -0500

    feat(stage): add HTTPS to staging server

--- packages/upward-js/lib/__tests__/server.test.js
+++ packages/upward-js/lib/__tests__/server.test.js
@@ -1,7 +1,7 @@
 const { Server } = require('http');
 const supertest = require('supertest');
 const { getScenarios } = require('@magento/upward-spec');
-const upwardServer = require('../server');
+const createUpwardServer = require('../createUpwardServer');
 
 let upwardPath;
 beforeAll(async () => {
@@ -11,13 +11,13 @@ beforeAll(async () => {
 });
 
 test('returns app alone if bindLocal is false', async () => {
-    const { app, server } = await upwardServer({ upwardPath });
+    const { app, server } = await createUpwardServer({ upwardPath });
     expect(app).toBeTruthy();
     expect(server).not.toBeDefined();
 });
 
 test('returns app and server if bindLocal is true', async () => {
-    const { app, server, close } = await upwardServer({
+    const { app, server, close } = await createUpwardServer({
         upwardPath,
         bindLocal: true
     });
@@ -27,7 +27,7 @@ test('returns app and server if bindLocal is true', async () => {
 });
 
 test('responds to requests based on UPWARD config', async () => {
-    const { app } = await upwardServer({ upwardPath });
+    const { app } = await createUpwardServer({ upwardPath });
     const response = await supertest(app).get('/article?articleId=1');
     expect(response.statusCode).toBe(200);
     expect(response.text).toEqual('Hello World!!');
---
+++ packages/upward-js/lib/createUpwardServer.js
@@ -0,0 +1,91 @@
+/**
+ * Standalone server script which creates an Express server, configures and
+ * binds it according to configuration, and applies the upward-js middleware.
+ */
+
+const { resolve } = require('path');
+const express = require('express');
+const middleware = require('./middleware');
+const debugErrorMiddleware = require('debug-error-middleware').express;
+const morgan = require('morgan');
+
+/**
+ * Create an upward-js standalone server and optionally bind it to a local
+ * address. Configure logging and debugging middleware depending on standard
+ * detection of environment: `process.env.NODE_ENV` === 'production'.
+ *
+ * ### `createUpwardServer` Configuration Options
+ *
+ * | Property | Type | Default | Description
+ * | -------- | ---- | ------- | -----------
+ * |`upwardPath` | `string` |  | Path, relative to the current directory, of a YML file with an UPWARD configuration. **Required**.
+ * |`bindLocal` | `boolean` | `false` | Create and  bind an HTTP server before returning.
+ * |`port` | `number` | `0` | Specify the port to be bound. `0` means the first open port.
+ * |`host` | `string` | `0.0.0.0` | Specify the host on which to listen. `0.0.0.0` means all local IPv4 requests.
+ * |`https` | `object` |  | To bind an HTTPS server instead of HTTP, pass a valid `{ key, cert }` object here.
+ * |`logUrl` | `boolean` | `false` | Log the bound URL to stdout (mostly used for testing against upward-spec)
+ *
+ * Returns a plain object with the following properties:
+ * an `app` property. This is an Express
+ *
+ *
+ * @param {object} config Configuration object.
+ * @return Promise `{ app, server?, close? }`
+ */
+async function createUpwardServer({
+    bindLocal = false,
+    port = 0,
+    host = '0.0.0.0',
+    https,
+    logUrl = false,
+    upwardPath
+}) {
+    if (!upwardPath) {
+        throw new Error(`upwardPath is required`);
+    }
+    const app = express();
+    const upward = await middleware(resolve(upwardPath));
+    if (process.env.NODE_ENV === 'production') {
+        app.use(morgan('combined'));
+        app.use(upward);
+    } else {
+        app.use(morgan('dev'));
+        app.use(upward);
+        app.use(debugErrorMiddleware());
+    }
+    if (bindLocal) {
+        return new Promise((resolve, reject) => {
+            try {
+                const protocol = https ? 'https' : 'http';
+                const server = https
+                    ? require('https').createServer(https, app)
+                    : require('http').createServer(app);
+
+                server.listen(port, host);
+
+                server.on('listening', () => {
+                    if (logUrl) {
+                        console.log(
+                            `${protocol}://${host}:${server.address().port}/`
+                        );
+                    }
+                    resolve({
+                        app,
+                        server,
+                        close() {
+                            return new Promise(resolve => {
+                                server.on('close', resolve);
+                                server.close();
+                            });
+                        }
+                    });
+                });
+            } catch (e) {
+                reject(e);
+            }
+        });
+    }
+    return { app };
+}
+
+module.exports = createUpwardServer;
