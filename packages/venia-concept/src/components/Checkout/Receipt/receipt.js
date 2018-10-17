c77a6992f57f4ff79773063d965c3e64cee4cfa6
AlexeyKaryka <karikasv9805@gmail.com>
2018-10-17 14:37:16 +0300

    fix/move-dark-button-theme-to-default-button-folder

--- packages/venia-concept/src/components/Checkout/Receipt/receipt.js
+++ packages/venia-concept/src/components/Checkout/Receipt/receipt.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { func, shape, string, number } from 'prop-types';
 
 import classify from 'src/classify';
-import ReceiptButton from './ReceiptButton';
+import Button, { darkThemeClasses } from 'src/components/Button';
 import defaultClasses from './receipt.css';
 
 class Receipt extends Component {
@@ -27,22 +27,30 @@ class Receipt extends Component {
         return (
             <div className={classes.root}>
                 <div className={classes.body}>
-                    <h2>Thank you for your purchase!</h2>
+                    <h2 className={classes.header}>
+                        Thank you for your purchase!
+                    </h2>
                     <div className={classes.textBlock}>
                         Your order # is{' '}
                         <span className={classes.orderId}>{orderId}</span>
                         <br />
-                        We\'ll email you an order confirmation with details and
+                        We'll email you an order confirmation with details and
                         tracking info
                     </div>
-                    <ReceiptButton onClick={resetCheckout} type="reset">
+                    <Button
+                        classes={darkThemeClasses}
+                        onClick={resetCheckout}
+                        type="reset"
+                    >
                         Continue Shopping
-                    </ReceiptButton>
+                    </Button>
                     <div className={classes.textBlock}>
                         Track order status and earn rewards for your purchase by
                         creating and account.
                     </div>
-                    <ReceiptButton>Create an Account</ReceiptButton>
+                    <Button classes={darkThemeClasses}>
+                        Create an Account
+                    </Button>
                 </div>
             </div>
         );
