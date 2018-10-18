629b845c2c71e9856c66326dfaa69cf63cade251
vitalics <vitalicset@yandex.ru>
2018-10-18 21:56:07 +0300

    fix prettier issue

--- packages/pwa-e2e/typedoc.js
+++ packages/pwa-e2e/typedoc.js
@@ -1,10 +1,10 @@
 module.exports = {
-  out: './docs',
-  readme: './README.md',
-  media: './media',
-  tsconfig: './tsconfig.json',
-  mode: 'file',
-  includeDeclarations:true,
-  name: 'Meganto PWA testing framework documentation',
-  excludeExternals: true,
+    out: './docs',
+    readme: './README.md',
+    media: './media',
+    tsconfig: './tsconfig.json',
+    mode: 'file',
+    includeDeclarations: true,
+    name: 'Meganto PWA testing framework documentation',
+    excludeExternals: true
 };
