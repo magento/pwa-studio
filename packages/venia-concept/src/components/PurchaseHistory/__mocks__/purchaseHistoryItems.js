05fc10df388459da889e88f58d3b7e99df6a0e08
AlexeyKaryka <karikasv9805@gmail.com>
2018-10-23 16:11:35 +0300

    Add purchase history component (#392)

---
+++ packages/venia-concept/src/components/PurchaseHistory/__mocks__/purchaseHistoryItems.js
@@ -0,0 +1,32 @@
+const mockPurchaseHistory = [
+    {
+        id: 1,
+        imageSrc: 'dress.png',
+        title: 'Lorem ipsum dolor sit amet',
+        date: new Date(2018, 3, 20),
+        link: '/prettyDress/1/'
+    },
+    {
+        id: 2,
+        imageSrc: 'dress.png',
+        title: 'Duis aute irure dolor in reprehenderit in voluptate',
+        date: new Date(2017, 2, 12),
+        link: '/prettyDress/2/'
+    },
+    {
+        id: 3,
+        imageSrc: 'dress.png',
+        title: 'Ut enim ad minima',
+        date: new Date(2016, 1, 1),
+        link: '/prettyDress/3/'
+    },
+    {
+        id: 4,
+        imageSrc: 'dress.png',
+        title: 'Quis autem vel',
+        date: new Date(),
+        link: '/prettyDress/4/'
+    }
+];
+
+export default mockPurchaseHistory;
