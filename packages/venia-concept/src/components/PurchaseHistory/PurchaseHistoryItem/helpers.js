9abc215577090fd20066f09d520389ecbcdd9c64
AlexeyKaryka <karikasv9805@gmail.com>
2018-10-23 16:11:35 +0300

    Add purchase history component (#392)

---
+++ packages/venia-concept/src/components/PurchaseHistory/PurchaseHistoryItem/helpers.js
@@ -0,0 +1,3 @@
+export const processDate = date => {
+    return date.toLocaleString('en-US', { year: 'numeric', day: 'numeric', month: 'long' })
+}
---
+++ packages/venia-concept/src/components/PurchaseHistory/PurchaseHistoryItem/purchaseHistoryItem.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { number, shape, string, date } from 'prop-types';
+import { Link } from 'react-router-dom';
+
+import Icon from 'src/components/Icon';
+import classify from 'src/classify';
+import defaultClasses from './purchaseHistoryItem.css';
+import { processDate } from './helpers';
+
+class PurchaseHistoryItem extends Component {
+    static propTypes = {
+        classes: shape({
+            body: string,
+            textBlock: string,
+            textBlockTitle: string,
+            textBlockDate: string,
+            chevronContainer: string
+        }),
+        item: shape({
+            id: number,
+            imageSrc: string,
+            title: string,
+            date: date,
+            link: string
+        })
+    };
+
+    render() {
+        const {
+            classes,
+            item: { imageSrc, title, date, link }
+        } = this.props;
+
+        return (
+            <Link className={classes.body} to={'/'}>
+                <img
+                    src={imageSrc}
+                    alt="clothes"
+                />
+                <div className={classes.textBlock}>
+                    <div className={classes.textBlockTitle}>{title}</div>
+                    <div className={classes.textBlockDate}>{processDate(date)}</div>
+                </div>
+                <div className={classes.chevronContainer}>
+                    <Icon name="chevron-right" />
+                </div>
+            </Link>
+        );
+    }
+}
+
+export default classify(defaultClasses)(PurchaseHistoryItem);
