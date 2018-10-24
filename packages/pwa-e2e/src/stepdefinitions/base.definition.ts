0928bf35a1c018a10ee968f41bef376e3219f990
Vitalics <vitalicset@yandex.ru>
2018-10-24 22:27:24 +0300

    fix typos with code and readme

--- packages/pwa-e2e/src/stepdefinitions/base.definition.ts
+++ packages/pwa-e2e/src/stepdefinitions/base.definition.ts
@@ -8,21 +8,13 @@ export class BaseDefinition {
 
     @after()
     public async write(scenarioResult: events.ScenarioResultPayload): Promise<void> {
-        // some
         if (scenarioResult.status === Status.FAILED || scenarioResult.status === Status.UNDEFINED) {
             console.dir(scenarioResult.scenario.name + ' failed');
-            const screenShot = await browser.takeScreenshot();
-            writeFile('reports/screenshots/error.png', screenShot, 'base64', err => {
-                // tslint:disable-next-line:no-console
-                console.error(err);
-            });
         }
     }
 
     @before()
     public setUp(): void {
-        // setup
         console.dir('before hook');
-        // console.dir(scenario);
     }
 }
--- packages/pwa-e2e/src/utils/protractor/components.ts
+++ packages/pwa-e2e/src/utils/protractor/components.ts
@@ -1,16 +1,14 @@
-import { Component } from '../../components/abstract.component';
-
 // tslint:disable-next-line:ban-types
-export function components<T extends Function>(locator: string, componenet: T, count: number): PropertyDecorator {
+export function components<T extends Function>(locator: string, component: T, count: number): PropertyDecorator {
     return (target, key) => {
-        const isntances: T[] = [];
+        const instances: T[] = [];
         for (let index = 0; index < count; index++) {
-            const instance = Reflect.construct(componenet, [locator]) as T;
-            isntances.push(instance);
+            const instance = Reflect.construct(component, [locator]) as T;
+            instances.push(instance);
         }
 
         Reflect.defineProperty(target, key, {
-            get: (): T[] => isntances,
+            get: (): T[] => instances,
         });
     };
 }
