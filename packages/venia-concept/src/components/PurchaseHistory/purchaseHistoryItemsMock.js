3ca44fbe3ed1ca085f0961ef7b7e9c62ae1010bb
Artsiom Staratsitarau <starotitorov1997@gmail.com>
2018-10-30 17:13:18 +0300

    Finish item image

--- packages/venia-concept/src/components/PurchaseHistory/purchaseHistoryItemsMock.js
+++ packages/venia-concept/src/components/PurchaseHistory/purchaseHistoryItemsMock.js
@@ -1,28 +1,28 @@
 const mockPurchaseHistory = [
     {
         id: 1,
-        imageSrc: 'dress.png',
+        imageSrc: '/media/catalog/product/m/b/mb04-black-0_alt1.jpg',
         title: 'Lorem ipsum dolor sit amet',
         date: new Date(2018, 3, 20),
         link: '/'
     },
     {
         id: 2,
-        imageSrc: 'dress.png',
+        imageSrc: '/media/catalog/product/m/b/mb04-black-0_alt1.jpg',
         title: 'Duis aute irure dolor in reprehenderit in voluptate',
         date: new Date(2017, 2, 12),
         link: '/'
     },
     {
         id: 3,
-        imageSrc: 'dress.png',
+        imageSrc: '/media/catalog/product/m/b/mb04-black-0_alt1.jpg',
         title: 'Ut enim ad minima',
         date: new Date(2016, 1, 1),
         link: '/'
     },
     {
         id: 4,
-        imageSrc: 'dress.png',
+        imageSrc: '/media/catalog/product/m/b/mb04-black-0_alt1.jpg',
         title: 'Quis autem vel',
         date: new Date(),
         link: '/'
