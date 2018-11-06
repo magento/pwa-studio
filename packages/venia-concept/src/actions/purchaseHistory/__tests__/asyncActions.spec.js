c16a0573bb0deec80b9609b91dcf7e5298ee43a5
Artsiom Staratsitarau <starotitorov1997@gmail.com>
2018-11-06 15:54:28 +0300

    Add async actions tests for purchase history

--- packages/venia-concept/src/actions/purchaseHistory/__tests__/asyncActions.spec.js
+++ packages/venia-concept/src/actions/purchaseHistory/__tests__/asyncActions.spec.js
@@ -1,5 +1,50 @@
+import { dispatch, getState } from 'src/store';
 import { getPurchaseHistory } from '../asyncActions';
+import actions from '../actions';
+import * as api from '../api';
+
+jest.mock('src/store');
+
+jest.mock('../api', () => ({
+    fetchPurchaseHistory: jest.fn()
+}));
+
+const thunkArgs = [dispatch, getState];
+
+afterEach(() => {
+    dispatch.mockClear();
+});
 
 test('getPurchaseHistory() to return a thunk', () => {
     expect(getPurchaseHistory()).toBeInstanceOf(Function);
+});
+
+test('getPurchaseHistory thunk dispatches actions on success', async () => {
+    const response = {
+        items: []
+    };
+
+    api.fetchPurchaseHistory.mockResolvedValueOnce(response);
+
+    await getPurchaseHistory()(...thunkArgs);
+
+    expect(dispatch).toHaveBeenNthCalledWith(
+        1,
+        actions.fetchPurchaseHistoryRequest()
+    );
+    expect(dispatch).toHaveBeenNthCalledWith(2, actions.setItems(response));
+});
+
+test('getPurchaseHistory thunk dispatches actions on error', async () => {
+    const error = new Error();
+
+    api.fetchPurchaseHistory.mockResolvedValueOnce(error);
+
+    await getPurchaseHistory()(...thunkArgs);
+
+    expect(dispatch).toHaveBeenNthCalledWith(
+        1,
+        actions.fetchPurchaseHistoryRequest()
+    );
+    expect(dispatch).toHaveBeenNthCalledWith(2, actions.setItems(error));
 });
