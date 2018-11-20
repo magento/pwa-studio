5480ec6c28a800e24a2ba16ddf582ef7c36c4e05
Artsiom Staratsitarau <starotitorov1997@gmail.com>
2018-11-20 22:21:02 +0300

    Purchase history page component (#501)
    * Add purchase history component (#392)
    
    * Fix link and prop types (#407)
    
    * Finish item image
    
    * Simple test for static part of purchase history page (#417)
    
    * Add simple test for purchase history page and prettier a few files
    
    * Add tests for purchase history item component
    
    * Add some styles to filter component (#448)
    
    * Add semantic tags in list, prettier a few files (#443)
    
    * Convert px to rem
    
    * Address review comments
    
    * Update purchase history styles

--- packages/venia-concept/jest.config.js
+++ packages/venia-concept/jest.config.js
@@ -9,6 +9,7 @@ module.exports = {
         // It would be nice if Venia respected a mock in Peregrine,
         // but it doesn't, so Venia tests will fail without this.
         '^FETCH_ROOT_COMPONENT$': '<rootDir>/__mocks__/virtualModule.js',
+        '\\.(jpg|jpeg|png)$': '<rootDir>/__mocks__/fileMock.js',
         '\\.css$': 'identity-obj-proxy',
         // Mirrors webpack alias to resolve from 'src'
         '^src/(.+)': '<rootDir>/src/$1',
