8b63cbb8645049cf5b0f4ab24fa621cb1cd70418
Crifen <crifenbery@bargreen.com>
2018-11-28 07:33:50 -0800

    Fixing linting issue

--- packages/venia-concept/src/components/SearchBar/__tests__/searchBar.spec.js
+++ packages/venia-concept/src/components/SearchBar/__tests__/searchBar.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { configure, shallow, mount } from 'enzyme';
+import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { SearchBar } from '../searchBar';
 
