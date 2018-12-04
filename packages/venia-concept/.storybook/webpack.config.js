1d65e5f0bbb6fc3592dae1f81e41ed5994c43a3a
pcvonz@gmail.com <pcvonz@gmail.com>
2018-12-04 13:04:27 -0800

    Fix storybook in Venia

--- packages/venia-concept/.storybook/webpack.config.js
+++ packages/venia-concept/.storybook/webpack.config.js
@@ -1,43 +1,9 @@
-const webpack = require('webpack');
-const path = require("path");
-
 const base_config  = require('../webpack.config.js')
 
-const themePaths = {
-    src: path.resolve(__dirname, '../src'),
-    assets: path.resolve(__dirname, '../web'),
-    output: path.resolve(__dirname, '../web/js'),
-    node: path.resolve(__dirname, '../../../'),
-    root: path.resolve(__dirname, '../')
-
-};
-
-const {
-    WebpackTools: {
-        makeMagentoRootComponentsPlugin,
-        MagentoResolver
-    }
-} = require('@magento/pwa-buildpack');
-
-const rootComponentsDirs = ['./src/RootComponents/'];
-
-module.exports = async (storybookBaseConfig, configType) => {
-
+module.exports = async (storybookBaseConfig) => {
     const conf = await base_config();
-
-    storybookBaseConfig.resolve = await MagentoResolver.configure({
-        paths: {
-            root: themePaths.root
-        }
-    });
-
-    storybookBaseConfig.module.rules = conf.module.rules;
-
-    storybookBaseConfig.plugins = [await makeMagentoRootComponentsPlugin({
-        rootComponentsDirs,
-        context: themePaths.root
-    })
-    ]
-
+    storybookBaseConfig.plugins = conf.plugins.concat(storybookBaseConfig.plugins);
+    storybookBaseConfig.module = conf.module;
+    storybookBaseConfig.resolve = conf.resolve;
     return storybookBaseConfig;
 };
