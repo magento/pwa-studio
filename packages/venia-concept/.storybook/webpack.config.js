fed087bd7eab649a6116a0c6216ba95b62332f26
pcvonz@gmail.com <pcvonz@gmail.com>
2018-12-04 13:28:46 -0800

    :sparkles: prettier :100:

--- packages/venia-concept/.storybook/webpack.config.js
+++ packages/venia-concept/.storybook/webpack.config.js
@@ -1,8 +1,10 @@
-const base_config  = require('../webpack.config.js')
+const base_config = require('../webpack.config.js');
 
-module.exports = async (storybookBaseConfig) => {
+module.exports = async storybookBaseConfig => {
     const conf = await base_config();
-    storybookBaseConfig.plugins = conf.plugins.concat(storybookBaseConfig.plugins);
+    storybookBaseConfig.plugins = conf.plugins.concat(
+        storybookBaseConfig.plugins
+    );
     storybookBaseConfig.module = conf.module;
     storybookBaseConfig.resolve = conf.resolve;
     return storybookBaseConfig;
