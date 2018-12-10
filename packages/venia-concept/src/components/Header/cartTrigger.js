6c34695c0f521ec4022bbdf1c22526e4d8b2d1fb
Enno Stuurman <enno@spaarnewebdesign.nl>
2018-12-10 18:10:21 +0100

    617 WIP Add counter to header cart button (icon)

--- packages/venia-concept/src/components/Header/cartTrigger.js
+++ packages/venia-concept/src/components/Header/cartTrigger.js
@@ -16,8 +16,25 @@ class Trigger extends Component {
         toggleCart: PropTypes.func.isRequired
     };
 
+
+    /**
+     * Get items quantity from cart state
+     * This is total number of items in cart 
+     * (e.g. 1 x item A + 2 x item B = 3).
+     * Which is not the same as items count 
+     * (total number of different products).
+     * 
+     * @returns number || null
+     */
+    get itemsQuantity() {
+        const { cart } = this.props;
+        const itemQty = cart.details.items_qty;
+        return itemQty > 0 ? itemQty : null;
+    }
+
     render() {
         const { children, classes, toggleCart } = this.props;
+        const { itemsQuantity } = this;
 
         return (
             <button
@@ -26,6 +43,7 @@ class Trigger extends Component {
                 onClick={toggleCart}
             >
                 {children}
+                <span className={classes.counter}>{itemsQuantity}</span>
             </button>
         );
     }
@@ -35,10 +53,18 @@ const mapDispatchToProps = {
     toggleCart
 };
 
+const mapStateToProps = state => {
+    const { cart } = state;
+
+    return {
+        cart
+    };
+};
+
 export default compose(
     classify(defaultClasses),
     connect(
-        null,
+        mapStateToProps,
         mapDispatchToProps
     )
 )(Trigger);
