91152313c397bae773c2a25fcce74f54625e9b73
AlexeyKaryka <karikasv9805@gmail.com>
2018-12-17 19:19:23 +0300

    Draft changes

--- packages/venia-concept/src/components/PurchaseHistoryPage/PurchaseHistory/purchaseHistory.js
+++ packages/venia-concept/src/components/PurchaseHistoryPage/PurchaseHistory/purchaseHistory.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import { arrayOf, number, shape, string } from 'prop-types';
 import { List } from '@magento/peregrine';
-
-import PurchaseHistoryItem from '../PurchaseHistoryItem';
+import Filter from 'src/components/Filter';
 import classify from 'src/classify';
+import PurchaseHistoryItem from '../PurchaseHistoryItem';
 import defaultClasses from './purchaseHistory.css';
-import Filter from '../Filter';
 
 class PurchaseHistory extends Component {
     static propTypes = {
