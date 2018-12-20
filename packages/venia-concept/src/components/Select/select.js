3b5237bfd7d494905fd083a4f7d01fadc21c52c6
Jimmy Sanford <jimbo@users.noreply.github.com>
2018-12-20 12:15:32 -0700

    Add new field components (#670)
    Also update field styles.

--- packages/venia-concept/src/components/Select/select.js
+++ packages/venia-concept/src/components/Select/select.js
@@ -1,26 +1,38 @@
-import React, { Component } from 'react';
-import { arrayOf, number, oneOfType, shape, string } from 'prop-types';
-import { Option, Select } from 'informed';
+import React, { Component, Fragment } from 'react';
+import { arrayOf, node, number, oneOfType, shape, string } from 'prop-types';
+import { BasicSelect, Option, asField } from 'informed';
+import { compose } from 'redux';
 
 import classify from 'src/classify';
+import { Message } from 'src/components/Field';
+import Icon from 'src/components/Icon';
 import defaultClasses from './select.css';
 
-class SelectList extends Component {
+const iconAttrs = {
+    height: 18,
+    width: 18
+};
+
+class Select extends Component {
     static propTypes = {
         classes: shape({
-            root: string
+            input: string
         }),
         field: string.isRequired,
+        fieldState: shape({
+            value: string
+        }),
         items: arrayOf(
             shape({
                 label: string,
                 value: oneOfType([number, string])
             })
-        )
+        ),
+        message: node
     };
 
     render() {
-        const { classes, items, ...restProps } = this.props;
+        const { classes, fieldState, items, message, ...rest } = this.props;
         const options = items.map(({ label, value }) => (
             <Option key={value} value={value}>
                 {label || (value != null ? value : '')}
@@ -28,11 +40,26 @@ class SelectList extends Component {
         ));
 
         return (
-            <Select {...restProps} className={classes.root}>
-                {options}
-            </Select>
+            <Fragment>
+                <div className={classes.wrapper}>
+                    <BasicSelect
+                        {...rest}
+                        fieldState={fieldState}
+                        className={classes.input}
+                    >
+                        {options}
+                    </BasicSelect>
+                    <span className={classes.icon}>
+                        <Icon name="chevron-down" attrs={iconAttrs} />
+                    </span>
+                </div>
+                <Message fieldState={fieldState}>{message}</Message>
+            </Fragment>
         );
     }
 }
 
-export default classify(defaultClasses)(SelectList);
+export default compose(
+    classify(defaultClasses),
+    asField
+)(Select);
