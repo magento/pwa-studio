7a31884117ca0753e54b0be776c5c758d3a88f5d
Enno Stuurman <enno@spaarnewebdesign.nl>
2019-01-04 12:15:45 +0100

    Move cart icon from header to cartTrigger component
    - so we can add dynamic icon layout (filled if items in cart)
      without having to connect header component

--- packages/venia-concept/src/components/Header/cartCounter.js
+++ packages/venia-concept/src/components/Header/cartCounter.js
@@ -5,13 +5,6 @@ import defaultClasses from './cartCounter.css';
 
 class CartCounter extends Component {
     
-    // get showCounter() {
-    //     const { counter, classes } = this.props;
-    //     return counter > 0 ? (
-    //         <span className={classes.counter}>{counter}</span>
-    //     ) : null;
-    // }
-
     render() {
         const { counter, classes } = this.props;
         return counter > 0 ? (
--- packages/venia-concept/src/components/Header/cartTrigger.js
+++ packages/venia-concept/src/components/Header/cartTrigger.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 
 import { toggleCart } from 'src/actions/cart';
 import CartCounter from './cartCounter';
+import Icon from 'src/components/Icon';
 import classify from 'src/classify';
 import defaultClasses from './cartTrigger.css';
 
@@ -17,8 +18,27 @@ class Trigger extends Component {
         toggleCart: PropTypes.func.isRequired
     };
 
+    get cartIcon() {
+        const { cart } = this.props;
+        const itemsQty = cart.details.items_qty;
+        const svgAttributes = {
+            stroke: 'rgb(var(--venia-text))',
+        };
+    
+        if (itemsQty > 0) {
+            svgAttributes.fill = 'rgb(var(--venia-text))';
+        }
+    
+        return (
+            <Icon
+                name="shopping-cart"
+                attrs={svgAttributes}
+            />
+        );
+    }
+
     render() {
-        const { children, classes, toggleCart, cart } = this.props;
+        const { classes, toggleCart, cart } = this.props;
         const itemsQty = cart.details.items_qty;
 
         return (
@@ -27,7 +47,7 @@ class Trigger extends Component {
                 aria-label="Toggle mini cart"
                 onClick={toggleCart}
             >
-                {children}
+                {this.cartIcon}
                 <CartCounter counter={itemsQty ? itemsQty : 0} />
             </button>
         );
