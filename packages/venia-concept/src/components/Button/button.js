bdeb379f992053ad87c636f3050e0faeb88ec69a
Jimmy Sanford <jimbo@users.noreply.github.com>
2019-01-11 15:58:55 -0700

    Update Button component and styles

--- packages/venia-concept/src/components/Button/button.js
+++ packages/venia-concept/src/components/Button/button.js
@@ -4,24 +4,44 @@ import { oneOf, shape, string } from 'prop-types';
 import classify from 'src/classify';
 import defaultClasses from './button.css';
 
-class Button extends Component {
+const getRootClassName = (contrast, priority) =>
+    `root_priority-${priority}_contrast-${contrast}`;
+
+export class Button extends Component {
     static propTypes = {
         classes: shape({
             content: string,
-            root: string
-        }),
-        type: oneOf(['button', 'reset', 'submit'])
+            root: string,
+            'root_priority-normal_contrast_normal': string,
+            'root_priority-high_contrast_normal': string,
+            'root_priority-normal_contrast_high': string,
+            'root_priority-high_contrast_high': string
+        }).isRequired,
+        contrast: oneOf(['high', 'normal']).isRequired,
+        priority: oneOf(['high', 'normal']).isRequired,
+        type: oneOf(['button', 'reset', 'submit']).isRequired
     };
 
     static defaultProps = {
+        contrast: 'normal',
+        priority: 'normal',
         type: 'button'
     };
 
     render() {
-        const { children, classes, type, ...restProps } = this.props;
+        const {
+            children,
+            classes,
+            contrast,
+            priority,
+            type,
+            ...restProps
+        } = this.props;
+
+        const rootClassName = classes[getRootClassName(contrast, priority)];
 
         return (
-            <button className={classes.root} type={type} {...restProps}>
+            <button className={rootClassName} type={type} {...restProps}>
                 <span className={classes.content}>{children}</span>
             </button>
         );
