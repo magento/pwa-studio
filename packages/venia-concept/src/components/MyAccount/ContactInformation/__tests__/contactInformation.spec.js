202141ee3999c12d94e8488c13b04b8f6a0dc894
Stephen Rugh <rugh@adobe.com>
2019-01-23 14:49:03 -0600

    missed one

--- packages/venia-concept/src/components/MyAccount/ContactInformation/__tests__/contactInformation.spec.js
+++ packages/venia-concept/src/components/MyAccount/ContactInformation/__tests__/contactInformation.spec.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
+import { shallow } from 'enzyme';
 import ContactInformation from '../contactInformation';
-
-configure({ adapter: new Adapter() });
 
 const classes = {
     fullName: 'fullName',
