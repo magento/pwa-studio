427a3a9cbf5d5383d130aa9f7ded4a35a8c86a6c
vitaliyboyko <v.boyko@atwix.com>
2019-01-29 11:30:21 +0200

    PWA-749: added validation to the payment form

--- packages/venia-concept/src/components/Checkout/paymentsForm.js
+++ packages/venia-concept/src/components/Checkout/paymentsForm.js
@@ -1,17 +1,18 @@
 import React, { Component, Fragment } from 'react';
-import { Form, Text } from 'informed';
+import { Form } from 'informed';
 import { array, bool, func, shape, string } from 'prop-types';
 
 import BraintreeDropin from './braintreeDropin';
-import Label from './label';
 import Button from 'src/components/Button';
 import Checkbox from 'src/components/Checkbox';
+import TextInput from 'src/components/TextInput';
 
 import classify from 'src/classify';
 import defaultClasses from './paymentsForm.css';
 
 import isObjectEmpty from 'src/util/isObjectEmpty';
-import { hasLengthAtLeast, isNotEmpty } from 'src/util/formValidators.js';
+import Field from "./addressForm";
+import validators from "./validators";
 
 const DEFAULT_FORM_VALUES = {
     addresses_same: true
@@ -89,65 +90,46 @@ class PaymentsForm extends Component {
     /*
      *  Class Properties.
      */
-    billingAddressFields = formState => {
+    billingAddressFields = () => {
         const { classes } = this.props;
-
-        const getError = fieldName => {
-            if (formState.errors[fieldName]) {
-                return formState.errors[fieldName];
-            }
-        };
 
         return (
             <Fragment>
                 <div className={classes.street0}>
-                    <Label htmlFor={classes.street0}>Street</Label>
-                    <Text
-                        className={classes.textInput}
-                        field="street[0]"
-                        id={classes.street0}
-                        validate={this.validateStreet}
-                    />
-                    <span className={classes.validation}>
-                        {getError('street')}
-                    </span>
+                    <Field label="Street">
+                        <TextInput
+                            id={classes.street0}
+                            field="street[0]"
+                            validate={validators.get('street')}
+                        />
+                    </Field>
                 </div>
                 <div className={classes.city}>
-                    <Label htmlFor={classes.city}>City</Label>
-                    <Text
-                        className={classes.textInput}
-                        field="city"
-                        id={classes.city}
-                        validate={this.validateCity}
-                    />
-                    <span className={classes.validation}>
-                        {getError('city')}
-                    </span>
+                    <Field label="City">
+                        <TextInput
+                            id={classes.city}
+                            field="city"
+                            validate={validators.get('city')}
+                        />
+                    </Field>
                 </div>
                 <div className={classes.region_code}>
-                    <Label htmlFor={classes.region_code}>State</Label>
-                    <Text
-                        className={classes.textInput}
-                        field="region_code"
-                        id={classes.region_code}
-                        validate={this.validateState}
-                        validateOnBlur
-                    />
-                    <span className={classes.validation}>
-                        {getError('region_code')}
-                    </span>
+                    <Field label="State">
+                        <TextInput
+                            id={classes.region_code}
+                            field="region_code"
+                            validate={validators.get('regionCode')}
+                        />
+                    </Field>
                 </div>
                 <div className={classes.postcode}>
-                    <Label htmlFor={classes.postcode}>ZIP</Label>
-                    <Text
-                        className={classes.textInput}
-                        field="postcode"
-                        id={classes.postcode}
-                        validate={this.validatePostcode}
-                    />
-                    <span className={classes.validation}>
-                        {getError('postcode')}
-                    </span>
+                    <Field label="ZIP">
+                        <TextInput
+                            id={classes.postcode}
+                            field="postcode"
+                            validate={validators.get('postcode')}
+                        />
+                    </Field>
                 </div>
             </Fragment>
         );
@@ -179,7 +161,7 @@ class PaymentsForm extends Component {
                         />
                     </div>
                     {!formState.values.addresses_same &&
-                        this.billingAddressFields(formState)}
+                        this.billingAddressFields()}
                 </div>
                 <div className={classes.footer}>
                     <Button className={classes.button} onClick={this.cancel}>
@@ -247,14 +229,6 @@ class PaymentsForm extends Component {
     cancelPaymentNonceRequest = () => {
         this.setState({ isRequestingPaymentNonce: false });
     };
-
-    validateCity = value => isNotEmpty(value);
-
-    validatePostcode = value => isNotEmpty(value);
-
-    validateState = value => hasLengthAtLeast(value, 2);
-
-    validateStreet = value => isNotEmpty(value);
 }
 
 export default classify(defaultClasses)(PaymentsForm);
