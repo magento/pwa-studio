71d3b79038c9b9cb33b5c0539c5dac0fdb601929
Andy Terranova <13182778+supernova-at@users.noreply.github.com>
2019-02-06 12:02:56 -0600

    Add cart mask (#806)

--- packages/venia-concept/src/selectors/cart.js
+++ packages/venia-concept/src/selectors/cart.js
@@ -8,3 +8,5 @@ export const getOrderInformation = (
 
 export const isEmptyCartVisible = ({ cart, checkout: { step } }) =>
     step === 'cart' && (!cart.details.items || cart.details.items.length === 0);
+
+export const isMiniCartMaskOpen = ({ checkout: { step } }) => step === 'form';
