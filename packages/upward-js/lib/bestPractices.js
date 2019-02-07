86bc8ff9c18516876f2bfe7aff4ae8e39a3884d6
Jimmy Sanford <jimbo@users.noreply.github.com>
2019-02-07 10:07:02 -0700

    Make image deps optional

--- packages/upward-js/lib/bestPractices.js
+++ packages/upward-js/lib/bestPractices.js
@@ -1,9 +1,22 @@
 const connect = require('connect');
-const shrinkRay = require('shrink-ray-current');
+let shrinkRay;
+
+try {
+    shrinkRay = require('shrink-ray-current');
+} catch (error) {
+    console.warn(
+        'Missing dependency: `shrink-ray-current`. Ensure additional tools are installed: https://github.com/nodejs/node-gyp#installation'
+    );
+}
+
 // TODO: node-helmet and any other zero-conf best practices should go here.
 function bestPractices() {
     const bestPracticeMiddlewares = connect();
-    bestPracticeMiddlewares.use(shrinkRay());
+
+    if (shrinkRay) {
+        bestPracticeMiddlewares.use(shrinkRay());
+    }
+
     return bestPracticeMiddlewares;
 }
 
