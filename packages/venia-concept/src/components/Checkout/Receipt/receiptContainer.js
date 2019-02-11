282acb6d89814a01827db7697417843e6eb2dbcb
Stephen Rugh <rugh@adobe.com>
2019-02-11 13:20:27 -0600

    Update tests for receipt and receiptContainer

--- packages/venia-concept/src/components/Checkout/Receipt/receiptContainer.js
+++ packages/venia-concept/src/components/Checkout/Receipt/receiptContainer.js
@@ -1,17 +1,25 @@
 import { connect } from 'src/drivers';
 import { compose } from 'redux';
 import { withRouter } from 'react-router-dom';
-import actions from 'src/actions/checkoutReceipt';
+import { reset } from 'src/actions/checkoutReceipt';
 import { continueShopping, createAccount } from 'src/actions/checkout';
 import Receipt from './receipt';
 import { getOrderInformation } from 'src/selectors/checkoutReceipt';
 
-const { reset } = actions;
+const mapStateToProps = state => ({
+    order: getOrderInformation(state)
+});
+
+const mapDispatchToProps = {
+    continueShopping,
+    createAccount,
+    reset
+};
 
 export default compose(
     withRouter,
     connect(
-        state => ({ order: getOrderInformation(state) }),
-        { continueShopping, createAccount, reset }
+        mapStateToProps,
+        mapDispatchToProps
     )
 )(Receipt);
