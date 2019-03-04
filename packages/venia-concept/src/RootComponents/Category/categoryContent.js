fc4b72a89bc65356352cd02fe2f8f637e99fbbac
Adrian Bece <adrian.bece@inchoo.net>
2019-03-04 13:51:37 +0100

    Improving graphql query & code cleanup

--- packages/venia-concept/src/RootComponents/Category/categoryContent.js
+++ packages/venia-concept/src/RootComponents/Category/categoryContent.js
@@ -1,47 +1,10 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Query } from 'react-apollo';
-import gql from 'graphql-tag';
 import classify from 'src/classify';
 import Gallery from 'src/components/Gallery';
 import Pagination from 'src/components/Pagination';
 import FilterModal from 'src/components/FilterModal';
 import defaultClasses from './category.css';
-
-const productsQuery = gql`
-    query category($id: String!, $pageSize: Int!, $currentPage: Int!) {
-        products(
-            pageSize: $pageSize
-            currentPage: $currentPage
-            filter: { category_id: { eq: $id } }
-        ) {
-            items {
-                id
-                name
-                small_image
-                url_key
-                price {
-                    regularPrice {
-                        amount {
-                            value
-                            currency
-                        }
-                    }
-                }
-            }
-            total_count
-            filters {
-                name
-                filter_items_count
-                request_var
-                filter_items {
-                    label
-                    value_string
-                }
-            }
-        }
-    }
-`;
 
 class CategoryContent extends Component {
     static propTypes = {
@@ -64,12 +27,13 @@ class CategoryContent extends Component {
             pageSize,
             openDrawer,
             closeDrawer,
-            isFilterModalOpen,
-            queryVariables
+            isFilterModalOpen
         } = this.props;
 
         const title = data ? data.category.description : null;
         const categoryTitle = data ? data.category.name : null;
+
+        const { items, filters } = data && data.products;
 
         return (
             <article className={classes.root}>
@@ -90,32 +54,17 @@ class CategoryContent extends Component {
                         </button>
                     </div>
                 </h1>
-                <Query query={productsQuery} variables={{ ...queryVariables }}>
-                    {({ loading, error, data }) => {
-                        if (loading || error) return null;
-                        const { products } = data;
-                        const { items, filters } = products;
-                        return (
-                            <Fragment>
-                                <section className={classes.gallery}>
-                                    <Gallery
-                                        data={items}
-                                        title={title}
-                                        pageSize={pageSize}
-                                    />
-                                </section>
-                                <div className={classes.pagination}>
-                                    <Pagination pageControl={pageControl} />
-                                </div>
-                                <FilterModal
-                                    closeModalHandler={closeDrawer}
-                                    isModalOpen={isFilterModalOpen}
-                                    filters={filters}
-                                />
-                            </Fragment>
-                        );
-                    }}
-                </Query>
+                <section className={classes.gallery}>
+                    <Gallery data={items} title={title} pageSize={pageSize} />
+                </section>
+                <div className={classes.pagination}>
+                    <Pagination pageControl={pageControl} />
+                </div>
+                <FilterModal
+                    closeModalHandler={closeDrawer}
+                    isModalOpen={isFilterModalOpen}
+                    filters={filters}
+                />
             </article>
         );
     }
