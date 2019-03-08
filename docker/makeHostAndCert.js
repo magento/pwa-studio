2172a704a5cbb38422bd25523ce97809da13ad31
sharkysharks <althen@adobe.com>
2019-03-08 08:10:35 -0800

    add polling to ensure hot reloading

--- docker/makeHostAndCert.js
+++ docker/makeHostAndCert.js
@@ -2,14 +2,14 @@ const devcert = require('devcert');
 const domain = process.argv[2];
 const fs = require('fs');
 
-let makeFile = (name, content) => {
+const makeFile = (name, content) => {
     fs.writeFile(name, content, err => {
         if (err) throw err;
         console.log(`${name} CREATED SUCCESSFULLY`);
     });
 };
-let ssl = async function (domain) {
-    let s = await devcert.certificateFor(domain);
+const ssl = async function (domain) {
+    const s = await devcert.certificateFor(domain);
     makeFile(`./docker/certs/${domain}.crt`, s.cert);
     makeFile(`./docker/certs/${domain}.key`, s.key);
 };
