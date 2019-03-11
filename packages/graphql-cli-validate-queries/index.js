a9d21e64faa0899c35789ce1c93a59fc6231e054
Andrew Terranova <aterrano@adobe.com>
2019-03-11 15:23:33 -0500

    Adds unit tests to graphql-cli-validate-queries plugin.
    Installs and wires up Jest for unit testing the validate-queries plugin.

--- packages/graphql-cli-validate-queries/index.js
+++ packages/graphql-cli-validate-queries/index.js
@@ -3,27 +3,52 @@
  * TODO: make this unit testable.
  */
 
+const fs = require('fs');
 const eslint = require('eslint');
 
 const PLUGIN_COMMAND = 'validate-queries';
 const PLUGIN_DESCRIPTION =
     'Validate all query files and template literals in a project against a schema.';
+const PLUGIN_SUPPORTED_ARGUMENTS = {
+    clients: {
+        alias: 'c',
+        choices: ['apollo', 'fraql', 'literal', 'lokka', 'relay'],
+        default: ['apollo'],
+        describe: 'GraphQL clients in use in this project.',
+        type: 'array'
+    },
+    filesGlob: {
+        alias: 'f',
+        describe: 'The glob used to target files for validation.',
+        type: 'string'
+    },
+    project: {
+        alias: 'p',
+        describe: 'Project name as specified in graphql config.',
+        type: 'string'
+    }
+};
 
 /**
- * TODO
- * @param {*} context
- * @param {*} argv
+ * Validate GraphQL queries against a schema.
  */
 async function validateQueries(context, argv) {
     try {
         console.log('Validating project queries...');
 
-        const { clients, filesGlob, insecure, project } = argv;
+        const { clients, filesGlob, project } = argv;
 
-        if (insecure) {
-            console.log('Allowing insecure (self-signed) certificates.');
-            process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+        // Ensure the schema exists.
+        context.spinner.start('Locating schema...');
+        const projectConfiguration = await context.getProjectConfig();
+        const { schemaPath } = projectConfiguration.config;
+        const schemaExists = fs.existsSync(schemaPath);
+        if (!schemaExists) {
+            throw new Error(
+                `Could not find a schema at ${schemaPath}.\n Run \`graphql get-schema --project ${project}\` to download the schema before running ${PLUGIN_COMMAND}.`
+            );
         }
+        context.spinner.succeed();
 
         // Validate our queries against that schema.
         context.spinner.start('Finding queries in files...');
@@ -48,43 +73,20 @@ async function validateQueries(context, argv) {
             const formatter = validator.getFormatter();
             console.log(formatter(report.results));
         }
+
+        process.exit(0);
     } catch (e) {
         const message = e.message ? e.message : e;
-        context.spinner.fail(`An error occurred: ${message}`);
+        context.spinner.fail(`An error occurred:\n${message}`);
+        process.exit(1);
     }
 }
 
 /**
- *
- * @param {*} args
+ * Supplies an object containing the arguments that this plugin supports.
  */
 function getSupportedArguments(args) {
-    const supportedArguments = {
-        clients: {
-            alias: 'c',
-            choices: ['apollo', 'fraql', 'literal', 'lokka', 'relay'],
-            default: ['apollo'],
-            describe: 'GraphQL clients in use in this project.',
-            type: 'array'
-        },
-        filesGlob: {
-            alias: 'f',
-            describe: 'The glob used to target files for validation.',
-            type: 'string'
-        },
-        insecure: {
-            alias: 'i',
-            describe: 'Allow insecure (self-signed) certificates.',
-            type: 'boolean'
-        },
-        project: {
-            alias: 'p',
-            describe: 'Project name as specified in graphql config',
-            type: 'string'
-        }
-    };
-
-    return args.options(supportedArguments);
+    return args.options(PLUGIN_SUPPORTED_ARGUMENTS);
 }
 
 function getValidator({ clients, project }) {
@@ -112,5 +114,6 @@ module.exports = {
     builder: getSupportedArguments,
     command: PLUGIN_COMMAND,
     desc: PLUGIN_DESCRIPTION,
-    handler: validateQueries
+    handler: validateQueries,
+    supportedArguments: PLUGIN_SUPPORTED_ARGUMENTS
 };
