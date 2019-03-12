e79438ad1e5a3fa0bac06d58f6ef89e8204ab6ce
Andrew Terranova <aterrano@adobe.com>
2019-03-12 11:28:15 -0500

    Adds a PWA-specific summarization to the end of the validate-queries command.
    Adds a new script summarize-validate-queries.js to venia-concept
    Runs this script at the end of the validate-queries script
    This script uses the compatibility table to clue developers to compatibility issues and how to resolve them

---
+++ packages/venia-concept/summarize-validate-queries.js
@@ -0,0 +1,33 @@
+/**
+ * This file outputs a summary blurb to the console informing the developer
+ * about PWA to Magento version compatibility.
+ * 
+ * This will help developers experiencing compatibility issues to solve them.
+ */
+
+const semver = require('semver');
+
+const compatibilityDefinitions = require('../../magento-compatibility.js');
+const package = require('../../package.json');
+
+const DOCS_COMPAT_TABLE_PATH = 'https://magento-research.github.io/pwa-studio/';
+const currentVersion = package.version;
+
+const pwaVersions = Object.keys(compatibilityDefinitions);
+const matchingPWAVersion = pwaVersions.find(pwaVersion => semver.satisfies(currentVersion, pwaVersion));
+
+let details = '';
+if (matchingPWAVersion) {
+    const compatibleMagentoVersion = compatibilityDefinitions[matchingPWAVersion];
+    details = `
+Your version of PWA is: ${currentVersion}.
+This version of PWA is compatible with version ${compatibleMagentoVersion} of Magento.
+    `;
+}
+
+console.log(`
+If there are errors above, your versions of PWA and Magento may be incompatible.
+${details}
+Please refer to the compatibility table in the PWA documentation for more details:
+${DOCS_COMPAT_TABLE_PATH}.
+`);
