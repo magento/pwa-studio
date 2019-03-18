2e425af49d283c3b6328cac4cb7b44c6343876d3
Andrew Terranova <aterrano@adobe.com>
2019-03-18 14:45:51 -0500

    Adds ARIA best practices to SwatchTooltip

--- packages/venia-concept/src/components/SwatchTooltip/swatchTooltip.js
+++ packages/venia-concept/src/components/SwatchTooltip/swatchTooltip.js
@@ -13,12 +13,67 @@ class SwatchTooltip extends Component {
         text: string
     };
 
+    constructor(...args) {
+        super(...args);
+
+        this.timeoutId = null;
+    }
+
+    state = {
+        isShowing: false
+    };
+
+    /*
+     *  Close the tooltip on the next tick.
+     *  Unfortunately this is necessary because another child of ours may have received focus
+     *  and this blur event is fired before that new focus event.
+     *  @see https://reactjs.org/docs/accessibility.html#mouse-and-pointer-events.
+     */
+    onBlur = () => {
+        this.timeoutId = setTimeout(() => {
+            this.setState({ isShowing: false });
+        }, 0);
+    }
+
+    /*
+     *  The tooltip target has received focus, show the tooltip.
+     */
+    onFocus = () => {
+        // If a child receives focus, do not close the tooltip yet.
+        clearTimeout(this.timeoutId);
+
+        this.setState({ isShowing: true });
+    }
+
+    onKeyDown = event => {
+        if (event.key === 'Escape') {
+            this.setState({ isShowing: false });
+        }
+    }
+    onMouseOver = () => {
+        this.setState({ isShowing: true });
+    }
+    onMouseLeave = () => {
+        this.setState({ isShowing: false });
+    }
+
     render() {
         const { text, children, classes } = this.props;
+        const { isShowing } = this.state;
 
         return (
-            <div className={classes.root}>
-                <span className={classes.tooltip}>{text}</span>
+            <div 
+                className={classes.root}
+                onBlur={this.onBlur}
+                onFocus={this.onFocus}
+                onKeyDown={this.onKeyDown}
+                onMouseOver={this.onMouseOver}
+                onMouseLeave={this.onMouseLeave}
+                tabIndex="1"
+            >
+                {isShowing && (
+                    <div className={classes.tooltip} role="tooltip">{text}</div>
+                )}
                 {children}
             </div>
         );
