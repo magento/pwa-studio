ebb3a4e440b80170839c0fdc0c6d41e80c3bca3f
zetlen <jzetlen@adobe.com>
2019-03-18 18:14:33 -0500

    WIP fiddling with tests

--- packages/venia-concept/src/util/__tests__/makeUrl.spec.js
+++ packages/venia-concept/src/util/__tests__/makeUrl.spec.js
@@ -1,46 +1,109 @@
-import makeUrl from '../makeUrl';
+let makeUrl;
 
-test('returns absolute path unmodified', () => {
-    const absoluteUrls = [
-        'data://example.com/foo.png',
-        'http://example.com/bar.png',
-        'https://example.com/baz.png'
-    ];
+beforeEach(() => {
+    jest.resetModuleRegistry();
+});
 
+const fastly = toggle => {
+    process.env.USE_FASTLY = toggle;
+    makeUrl = require('../makeUrl').default;
+};
+
+const FASTLY_CONSTANT_PARAMETERS = 'auto=webp&format=pjpg';
+
+const JEST_HOST = window.location.href;
+
+const absoluteUrls = [
+    'data://example.com/foo.png',
+    'http://example.com/bar.png',
+    'https://example.com/baz.png'
+];
+const relativeUrl = '/some/path/to/img.jpg';
+const leadingSlash = '/foo.jpg';
+const noLeadingSlash = 'foo.jpg';
+
+test('returns absolute path with no options unmodified', () => {
+    fastly(false);
     absoluteUrls.forEach(url => {
         expect(makeUrl(url)).toBe(url);
     });
 });
 
+test('USE_FASTLY: returns absolute path with no options unmodified', () => {
+    fastly(true);
+    absoluteUrls.forEach(url => {
+        expect(makeUrl(url)).toBe(`${url}?${FASTLY_CONSTANT_PARAMETERS}`);
+    });
+});
+
 test('returns untyped, unresized url unmodified', () => {
-    const relativeUrl = '/some/path/to/img.jpg';
-    expect(makeUrl(relativeUrl)).toBe(relativeUrl);
+    fastly(false);
+    expect(makeUrl(relativeUrl)).toBe(JEST_HOST + relativeUrl);
+});
+
+test('FASTLY: returns untyped, unresized url unmodified', () => {
+    fastly(true);
+    expect(makeUrl(relativeUrl)).toBe(
+        `${JEST_HOST}${relativeUrl}?${FASTLY_CONSTANT_PARAMETERS}`
+    );
 });
 
 test('normalizes slash paths on typed urls', () => {
-    const leadingSlash = '/foo.jpg';
-    const noLeadingSlash = 'foo.jpg';
+    fastly(false);
     expect(makeUrl(leadingSlash, { type: 'image-product' })).toBe(
-        '/media/catalog/product/foo.jpg'
+        JEST_HOST + '/media/catalog/product/foo.jpg'
     );
     expect(makeUrl(noLeadingSlash, { type: 'image-category' })).toBe(
-        '/media/catalog/category/foo.jpg'
+        JEST_HOST + '/media/catalog/category/foo.jpg'
     );
 });
 
-test('errors on unrecognized type', () => {
-    expect(() => makeUrl('url', { type: 'invalid' })).toThrow(
-        'Unrecognized media type invalid'
+test('FASTLY: normalizes slash paths on typed urls', () => {
+    fastly(true);
+    expect(makeUrl(leadingSlash, { type: 'image-product' })).toBe(
+        `${JEST_HOST}/media/catalog/product/foo.jpg?${FASTLY_CONSTANT_PARAMETERS}`
+    );
+    expect(makeUrl(noLeadingSlash, { type: 'image-category' })).toBe(
+        `${JEST_HOST}/media/catalog/category/foo.jpg?${FASTLY_CONSTANT_PARAMETERS}`
+    );
+});
+
+test('resizes but does not prepend path when width is passed but not type', () => {
+    fastly(false);
+    expect(makeUrl(absoluteUrls[0], { width: 100 })).toBe(
+        'data://example.com/foo.png?auto=webp&format=pjpg&width=100'
+    );
+});
+
+test('FASTLY: resizes but does not prepend path when width is passed but not type', () => {
+    fastly(true);
+    expect(makeUrl(absoluteUrls[0], { width: 100 })).toBe(
+        'data://example.com/foo.png?auto=webp&format=pjpg&width=100'
     );
 });
 
 test('resizes urls to specific widths', () => {
+    fastly(false);
     expect(
         makeUrl('some/category.jpg', { type: 'image-category', width: 160 })
     ).toBe(
-        '/img/resize/160?url=%2Fmedia%2Fcatalog%2Fcategory%2Fsome%2Fcategory.jpg'
+        JEST_HOST +
+            'img/resize/160?url=%2Fmedia%2Fcatalog%2Fcategory%2Fsome%2Fcategory.jpg'
     );
-    expect(makeUrl('/some/other/thing.jpg', { width: 480 })).toBe(
-        '/img/resize/480?url=%2Fsome%2Fother%2Fthing.jpg'
+});
+
+test('FASTLY: resizes urls to specific widths', () => {
+    fastly(true);
+    expect(
+        makeUrl('some/category.jpg', { type: 'image-category', width: 160 })
+    ).toBe(
+        `${JEST_HOST}media/catalog/category/some/category.jpg?${FASTLY_CONSTANT_PARAMETERS}&width=160`
+    );
+});
+
+test('errors on unrecognized type', () => {
+    fastly(false);
+    expect(() => makeUrl('url', { type: 'invalid' })).toThrow(
+        'Unrecognized media type invalid'
     );
 });
--- packages/venia-concept/src/util/makeUrl.js
+++ packages/venia-concept/src/util/makeUrl.js
@@ -18,42 +18,51 @@ const mediaPathPrefixes = {
         process.env.MAGENTO_BACKEND_MEDIA_PATH_CATALOG ||
         '/media/catalog/category'
 };
+function ensurePathPrefix(url, type) {
+    // Can't prepend to absolute URLs, and don't know how to prepend if no type.
+    if (absoluteUrl.test(url) || !type) {
+        return url;
+    }
 
-// Should produce `/img/resize` in the default case
-const resizeBase = joinUrls(process.env.IMAGE_SERVICE_PATH || '/img', 'resize');
+    if (!mediaPathPrefixes.hasOwnProperty(type)) {
+        throw new Error(`Unrecognized media type ${type}`);
+    }
+    // "/path/to/jpeg" becomes e.g. "/media/catalog/product/path/to/jpeg"
+    return joinUrls(mediaPathPrefixes[type], url);
+}
 
-// `makeResizedUrl("/path/to/jpeg", { width: 80 })` returns
-// "/img/resize/80?url=%2Fpath%2fto%2fjpeg"
-function makeResizedUrl(url, width) {
-    return `${joinUrls(resizeBase, width.toString())}?url=${encodeURIComponent(
-        url
-    )}`;
+let resizeBase;
+function makeOptimizedUrl(url, width) {
+    if (process.env.USE_FASTLY) {
+        // `makeResizedUrl("/path/to/jpeg?queryParam=1", { width: 80 })` returns
+        // "/path/to/jpeg?queryParam=1&auto=webp&format=pjpg&width=80"
+        const urlObject = new URL(url, window.location.href);
+        const params = new URLSearchParams(urlObject.search);
+        params.set('auto', 'webp');
+        params.set('format', 'pjpg');
+        if (width) {
+            params.set('width', width);
+        }
+        urlObject.search = '?' + params;
+        return urlObject.href;
+    } else {
+        // `makeResizedUrl("/path/to/jpeg", { width: 80 })` returns
+        // "/img/resize/80?url=%2Fpath%2fto%2fjpeg"
+
+        resizeBase =
+            resizeBase ||
+            joinUrls(process.env.IMAGE_SERVICE_PATH || '/img', 'resize/');
+        return width
+            ? `${resizeBase}${width}?url=${encodeURIComponent(url)}`
+            : url;
+    }
 }
 
 // Should fail silently on a null value, so default to the empty string
 // so that string methods work
 function formatUrl(url = '', opts = {}) {
     const { type, width } = opts;
-    if (absoluteUrl.test(url) || !(type || width)) {
-        // Absolute URLs shouldn't be resized
-        // Without a type or a width, we don't know how to transform this url
-        return url;
-    }
-
-    // we need to throw on an unrecognized `type`, so we may have to assign this
-    // in another code branch, hence the "let"
-    let formattedUrl = url;
-
-    if (type) {
-        if (!mediaPathPrefixes.hasOwnProperty(type)) {
-            throw new Error(`Unrecognized media type ${type}`);
-        }
-        // "/path/to/jpeg" becomes e.g. "/media/catalog/product/path/to/jpeg"
-        formattedUrl = joinUrls(mediaPathPrefixes[type], url);
-    }
-
-    // if a width exists, make a resized URL!
-    return width ? makeResizedUrl(formattedUrl, width) : formattedUrl;
+    return makeOptimizedUrl(ensurePathPrefix(url, type), width);
 }
 
 // Each combination of type, url, and width is computed only once and cached.
