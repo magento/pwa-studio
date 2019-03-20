f8798aabb7250cfb2261f0e96b2621b3d029ec1d
Stephen Rugh <sirugh@users.noreply.github.com>
2019-03-20 09:28:58 -0500

    Update packages/venia-concept/src/util/makeUrl.js jsdoc

--- packages/venia-concept/src/util/makeUrl.js
+++ packages/venia-concept/src/util/makeUrl.js
@@ -20,7 +20,25 @@ const resizeBase = joinUrls(
     process.env.IMAGE_SERVICE_PATH || '/img/',
     '/resize/'
 );
-
+/**
+ * Creates an "optimized" url for a provided absolute or relative url based on
+ * requested media type and width.
+ *
+ * If a `type` is provided the `path` will be joined with the associated media
+ * base.
+ *  - "/media/catalog/product/some/path/to/img.jpg"
+ *
+ * If a `width` is provided a "resize url" is returned using the desired width
+ * and original media url.
+ *  - /img/resize/640?url=%2Fmedia%2Fcatalog%2Fproduct%2Fsome%2Fpath%2Fto%2F/image.jpg
+ *
+ * If only `path` is provided it is returned unaltered.
+ *
+ * @param {string} path - absolute or relative url to resource.
+ * @param {Object} props - properties describing desired optimizations
+ * @param {string} props.type - "image-product" or "image-category"
+ * @param {number} props.width - the desired resize width of the image
+ */
 const makeOptimizedUrl = (path, { type, width } = {}) => {
     const { location } = window;
     const urlObject = new URL(path, location.href);
