a3c46b9052f7b0784675acaaa9a9f6169e48205d
Stephen Rugh <rugh@adobe.com>
2019-03-29 14:52:27 -0500

    prettier

--- packages/venia-concept/src/components/PurchaseHistoryPage/PurchaseHistory/purchaseHistoryContainer.js
+++ packages/venia-concept/src/components/PurchaseHistoryPage/PurchaseHistory/purchaseHistoryContainer.js
@@ -3,7 +3,7 @@ import { transformItems } from 'src/selectors/purchaseHistory';
 import PurchaseHistory from './purchaseHistory';
 import actions, { getPurchaseHistory } from 'src/actions/purchaseHistory';
 
-const mapStateToProps = ({ purchaseHistory: { isFetching, items} }) => {
+const mapStateToProps = ({ purchaseHistory: { isFetching, items } }) => {
     return {
         isFetching,
         items: transformItems(items)
