0db92b5a1e6a48ca3d1e1a5294c2c433600c1b18
James Calcaben <calcaben@adobe.com>
2019-04-09 16:44:35 -0500

    Add ability to test specific file

--- pwa-devdocs/scripts/tests/index.js
+++ pwa-devdocs/scripts/tests/index.js
@@ -1,9 +1,22 @@
 const path = require('path');
 const linter = require('../markdown-linter').linter
+const process = require('process');
 
 const config = {
   basePath: path.join(__dirname, '../../src')
 }
 
-// Run linter
-linter(path.join(config.basePath, 'technologies/versioning/index.md'));
+// If a single file is specified, only run tests on that file
+const filepath = process.argv[2];
+if(filepath){
+  const fullPath = path.join(__dirname,'..','..', filepath)
+  runTests(fullPath);
+}
+else {
+// Run tests on all markdown files found under the configured directory
+}
+
+function runTests(filepath){
+  // Run linter
+  linter(filepath);
+}
