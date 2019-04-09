e9b2fab32e45fe1270fb2a2ce95c4199a4ddbba3
James Calcaben <calcaben@adobe.com>
2019-04-09 17:10:38 -0500

    Add ability to run tests on all markdown files in src

--- pwa-devdocs/scripts/tests/index.js
+++ pwa-devdocs/scripts/tests/index.js
@@ -1,22 +1,39 @@
+const fs = require('fs');
 const path = require('path');
-const linter = require('../markdown-linter').linter
+const linter = require('../markdown-linter').linter;
 const process = require('process');
 
 const config = {
-  basePath: path.join(__dirname, '../../src')
-}
+    basePath: path.join(__dirname, '../../src')
+};
 
 // If a single file is specified, only run tests on that file
 const filepath = process.argv[2];
-if(filepath){
-  const fullPath = path.join(__dirname,'..','..', filepath)
-  runTests(fullPath);
-}
-else {
-// Run tests on all markdown files found under the configured directory
+if (filepath) {
+    const fullPath = path.join(__dirname, '..', '..', filepath);
+    testFile(fullPath);
+} else {
+    testDirectory(config.basePath);
 }
 
-function runTests(filepath){
-  // Run linter
-  linter(filepath);
+// Run tests on all markdown files found under the specified directory
+function testDirectory(directoryPath) {
+    fs.promises
+        .readdir(directoryPath, { withFileTypes: true })
+        .then(filenames => {
+            filenames.forEach(file => {
+                const fullPath = path.join(directoryPath, file.name);
+                if (file.isDirectory()) {
+                    testDirectory(fullPath);
+                } else if (file.isFile() && path.extname(fullPath) === '.md') {
+                    testFile(fullPath);
+                }
+            });
+        });
+}
+
+// Run all tests on a single file
+function testFile(filepath) {
+    // Run linter
+    linter(filepath);
 }
