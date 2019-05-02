428fe0d5471c62112336656d5346ff9a7b4090d1
Stephen Rugh <rugh@adobe.com>
2019-05-02 09:26:54 -0500

    Add comment and disable image on desktop

--- packages/venia-concept/src/components/ProductImageCarousel/thumbnail.js
+++ packages/venia-concept/src/components/ProductImageCarousel/thumbnail.js
@@ -20,7 +20,9 @@ class Thumbnail extends Component {
         itemIndex: PropTypes.number,
         onClickHandler: PropTypes.func.isRequired
     };
-
+    // TODO: When we implement hooks/mergeClasses from #1078 we can replace this
+    // with a useWindowSize hook and rerender when the width changes across the
+    // breakpoint. This is important when changing from portrait to landscape.
     isDesktop = () => {
         return window.innerWidth > 640;
     };
@@ -38,10 +40,7 @@ class Thumbnail extends Component {
         } = this.props;
 
         const src = file
-            ? resourceUrl(file, {
-                  type: 'image-product',
-                  width: this.isDesktop() ? 240 : 640
-              })
+            ? resourceUrl(file, { type: 'image-product', width: 240 })
             : transparentPlaceholder;
 
         return (
@@ -49,7 +48,9 @@ class Thumbnail extends Component {
                 onClick={this.onClickHandlerWrapper}
                 className={isActive ? classes.rootSelected : classes.root}
             >
-                <img className={classes.image} src={src} alt={label} />
+                {this.isDesktop() ? (
+                    <img className={classes.image} src={src} alt={label} />
+                ) : null}
             </button>
         );
     }
