a809d7d4e20edb7c1d57fd9562a452a2605a10e8
Andrew Terranova <aterrano@adobe.com>
2019-05-03 09:39:29 -0500

    Product Details Component to Hooks

--- packages/venia-concept/src/components/ProductFullDetail/ProductFullDetail.js
+++ packages/venia-concept/src/components/ProductFullDetail/ProductFullDetail.js
@@ -1,93 +1,143 @@
-import React, { Component, Suspense } from 'react';
+import React, { Suspense, useCallback, useState } from 'react';
 import { arrayOf, bool, func, number, shape, string } from 'prop-types';
 import { Form } from 'informed';
 import { Price } from '@magento/peregrine';
 
-import classify from 'src/classify';
+import defaultClasses from './productFullDetail.css';
+import { mergeClasses } from 'src/classify';
+
 import Button from 'src/components/Button';
 import { loadingIndicator } from 'src/components/LoadingIndicator';
 import Carousel from 'src/components/ProductImageCarousel';
 import Quantity from 'src/components/ProductQuantity';
 import RichText from 'src/components/RichText';
-import defaultClasses from './productFullDetail.css';
+
 import appendOptionsToPayload from 'src/util/appendOptionsToPayload';
 import findMatchingVariant from 'src/util/findMatchingProductVariant';
 import isProductConfigurable from 'src/util/isProductConfigurable';
 
 const Options = React.lazy(() => import('../ProductOptions'));
 
-class ProductFullDetail extends Component {
-    static propTypes = {
-        classes: shape({
-            cartActions: string,
-            description: string,
-            descriptionTitle: string,
-            details: string,
-            detailsTitle: string,
-            imageCarousel: string,
-            options: string,
-            productName: string,
-            productPrice: string,
-            quantity: string,
-            quantityTitle: string,
-            root: string,
-            title: string
-        }),
-        product: shape({
-            __typename: string,
-            id: number,
-            sku: string.isRequired,
-            price: shape({
-                regularPrice: shape({
-                    amount: shape({
-                        currency: string.isRequired,
-                        value: number.isRequired
-                    })
-                }).isRequired
-            }).isRequired,
-            media_gallery_entries: arrayOf(
-                shape({
-                    label: string,
-                    position: number,
-                    disabled: bool,
-                    file: string.isRequired
-                })
-            ),
-            description: string
-        }).isRequired,
-        addToCart: func.isRequired
-    };
+const INITIAL_OPTION_CODES = new Map();
+const INITIAL_OPTION_SELECTIONS = new Map();
+const INITIAL_QUANTITY = 1;
 
-    static getDerivedStateFromProps(props, state) {
-        const { configurable_options } = props.product;
-        const optionCodes = new Map(state.optionCodes);
-
-        // if this is a simple product, do nothing
-        if (!isProductConfigurable(props.product)) {
-            return null;
-        }
-
-        // otherwise, cache attribute codes to avoid lookup cost later
-        for (const option of configurable_options) {
-            optionCodes.set(option.attribute_id, option.attribute_code);
-        }
-
-        return { optionCodes };
+const deriveOptionCodesFromProduct = product => {
+    // If this is a simple product it has no option codes.
+    if (!isProductConfigurable(product)) {
+        return INITIAL_OPTION_CODES;
     }
 
-    state = {
-        optionCodes: new Map(),
-        optionSelections: new Map(),
-        quantity: 1
-    };
+    // Initialize optionCodes based on the options of the product.
+    const initialOptionCodes = new Map();
+    for (const option of product.configurable_options) {
+        initialOptionCodes.set(option.attribute_id, option.attribute_code);
+    }
 
-    setQuantity = quantity => this.setState({ quantity });
+    return initialOptionCodes;
+};
 
-    addToCart = () => {
-        const { props, state } = this;
-        const { optionSelections, quantity, optionCodes } = state;
-        const { addToCart, product } = props;
+const getIsMissingOptions = (product, optionSelections) => {
+    // Non-configurable products can't be missing options.
+    if (!isProductConfigurable(product)) {
+        return false;
+    }
 
+    // Configurable products are missing options if we have fewer
+    // option selections than the product has options.
+    const { configurable_options } = product;
+    const numProductOptions = configurable_options.length;
+    const numProductSelections = optionSelections.size;
+
+    return numProductSelections < numProductOptions;
+};
+
+const getMediaGalleryEntries = (product, optionCodes, optionSelections) => {
+    let value = [];
+
+    const { media_gallery_entries, variants } = product;
+    const isConfigurable = isProductConfigurable(product);
+    const optionsSelected = optionSelections.size > 0;
+
+    if (!isConfigurable || !optionsSelected) {
+        value = media_gallery_entries;
+    } else {
+        const item = findMatchingVariant({
+            optionCodes,
+            optionSelections,
+            variants
+        });
+
+        value = item
+            ? [...item.product.media_gallery_entries, ...media_gallery_entries]
+            : media_gallery_entries;
+    }
+
+    const key = value.reduce((fullKey, entry) => {
+        return `${fullKey},${entry.file}`;
+    }, '');
+
+    return { key, value };
+};
+
+const getProductOptions = (product, optionSelections, setOptionSelections) => {
+    const { configurable_options } = product;
+    const isConfigurable = isProductConfigurable(product);
+
+    if (!isConfigurable) {
+        return null;
+    }
+
+    const handleSelectionChange = useCallback(
+        (optionId, selection) => {
+            // We must create a new Map here so that React knows that the value
+            // of optionSelections has changed.
+            const newOptionSelections = new Map([...optionSelections]);
+            newOptionSelections.set(optionId, Array.from(selection).pop());
+            setOptionSelections(newOptionSelections);
+        },
+        [optionSelections]
+    );
+
+    return (
+        <Suspense fallback={loadingIndicator}>
+            <Options
+                options={configurable_options}
+                onSelectionChange={handleSelectionChange}
+            />
+        </Suspense>
+    );
+};
+
+const ProductFullDetail = props => {
+    // Props.
+    const { addToCart, isAddingItem, product } = props;
+
+    // State.
+    const [quantity, setQuantity] = useState(INITIAL_QUANTITY);
+    const [optionSelections, setOptionSelections] = useState(
+        INITIAL_OPTION_SELECTIONS
+    );
+    const derivedOptionCodes = deriveOptionCodesFromProduct(product);
+    const [optionCodes] = useState(derivedOptionCodes);
+
+    // Members.
+    const { amount: productPrice } = product.price.regularPrice;
+    const classes = mergeClasses(defaultClasses, props.classes);
+    const isMissingOptions = getIsMissingOptions(product, optionSelections);
+    const mediaGalleryEntries = getMediaGalleryEntries(
+        product,
+        optionCodes,
+        optionSelections
+    );
+    const productOptions = getProductOptions(
+        product,
+        optionSelections,
+        setOptionSelections
+    );
+
+    // Event handlers.
+    const handleAddToCart = useCallback(() => {
         const payload = {
             item: product,
             productType: product.__typename,
@@ -99,156 +149,93 @@ class ProductFullDetail extends Component {
         }
 
         addToCart(payload);
-    };
+    }, [addToCart, optionCodes, optionSelections, product, quantity]);
 
-    handleSelectionChange = (optionId, selection) => {
-        this.setState(({ optionSelections }) => ({
-            optionSelections: new Map(optionSelections).set(
-                optionId,
-                Array.from(selection).pop()
-            )
-        }));
-    };
-
-    get fallback() {
-        return loadingIndicator;
-    }
-
-    get productOptions() {
-        const { fallback, handleSelectionChange, props } = this;
-        const { configurable_options } = props.product;
-        const isConfigurable = isProductConfigurable(props.product);
-
-        if (!isConfigurable) {
-            return null;
-        }
-
-        return (
-            <Suspense fallback={fallback}>
-                <Options
-                    options={configurable_options}
-                    onSelectionChange={handleSelectionChange}
-                />
-            </Suspense>
-        );
-    }
-
-    get mediaGalleryEntries() {
-        const { props, state } = this;
-        const { product } = props;
-        const { optionCodes, optionSelections } = state;
-        const { media_gallery_entries, variants } = product;
-
-        const isConfigurable = isProductConfigurable(product);
-
-        if (
-            !isConfigurable ||
-            (isConfigurable && optionSelections.size === 0)
-        ) {
-            return media_gallery_entries;
-        }
-
-        const item = findMatchingVariant({
-            optionCodes,
-            optionSelections,
-            variants
-        });
-
-        if (!item) {
-            return media_gallery_entries;
-        }
-
-        return [
-            ...item.product.media_gallery_entries,
-            ...media_gallery_entries
-        ];
-    }
-
-    get isMissingOptions() {
-        const { product } = this.props;
-
-        // Non-configurable products can't be missing options
-        if (!isProductConfigurable(product)) {
-            return false;
-        }
-
-        // Configurable products are missing options if we have fewer
-        // option selections than the product has options.
-        const { configurable_options } = product;
-        const numProductOptions = configurable_options.length;
-        const numProductSelections = this.state.optionSelections.size;
-
-        return numProductSelections < numProductOptions;
-    }
-
-    render() {
-        const {
-            addToCart,
-            isMissingOptions,
-            mediaGalleryEntries,
-            productOptions,
-            props
-        } = this;
-        const { classes, isAddingItem, product } = props;
-        const { regularPrice } = product.price;
-
-        // We want this key to change whenever mediaGalleryEntries changes.
-        // Make it dependent on a unique value in each entry (file),
-        // and the order.
-        const carouselKey = mediaGalleryEntries.reduce((fullKey, entry) => {
-            return `${fullKey},${entry.file}`;
-        }, '');
-
-        return (
-            <Form className={classes.root}>
-                <section className={classes.title}>
-                    <h1 className={classes.productName}>
-                        <span>{product.name}</span>
-                    </h1>
-                    <p className={classes.productPrice}>
-                        <Price
-                            currencyCode={regularPrice.amount.currency}
-                            value={regularPrice.amount.value}
-                        />
-                    </p>
-                </section>
-                <section className={classes.imageCarousel}>
-                    <Carousel images={mediaGalleryEntries} key={carouselKey} />
-                </section>
-                <section className={classes.options}>{productOptions}</section>
-                <section className={classes.quantity}>
-                    <h2 className={classes.quantityTitle}>
-                        <span>Quantity</span>
-                    </h2>
-                    <Quantity
-                        initialValue={this.state.quantity}
-                        onValueChange={this.setQuantity}
+    return (
+        <Form className={classes.root}>
+            <section className={classes.title}>
+                <h1 className={classes.productName}>{product.name}</h1>
+                <p className={classes.productPrice}>
+                    <Price
+                        currencyCode={productPrice.currency}
+                        value={productPrice.value}
                     />
-                </section>
-                <section className={classes.cartActions}>
-                    <Button
-                        priority="high"
-                        onClick={addToCart}
-                        disabled={isAddingItem || isMissingOptions}
-                    >
-                        <span>Add to Cart</span>
-                    </Button>
-                </section>
-                <section className={classes.description}>
-                    <h2 className={classes.descriptionTitle}>
-                        <span>Product Description</span>
-                    </h2>
-                    <RichText content={product.description} />
-                </section>
-                <section className={classes.details}>
-                    <h2 className={classes.detailsTitle}>
-                        <span>SKU</span>
-                    </h2>
-                    <strong>{product.sku}</strong>
-                </section>
-            </Form>
-        );
-    }
-}
+                </p>
+            </section>
+            <section className={classes.imageCarousel}>
+                <Carousel
+                    images={mediaGalleryEntries.value}
+                    key={mediaGalleryEntries.key}
+                />
+            </section>
+            <section className={classes.options}>{productOptions}</section>
+            <section className={classes.quantity}>
+                <h2 className={classes.quantityTitle}>Quantity</h2>
+                <Quantity initialValue={quantity} onValueChange={setQuantity} />
+            </section>
+            <section className={classes.cartActions}>
+                <Button
+                    priority="high"
+                    onClick={handleAddToCart}
+                    disabled={isAddingItem || isMissingOptions}
+                >
+                    Add to Cart
+                </Button>
+            </section>
+            <section className={classes.description}>
+                <h2 className={classes.descriptionTitle}>
+                    Product Description
+                </h2>
+                <RichText content={product.description} />
+            </section>
+            <section className={classes.details}>
+                <h2 className={classes.detailsTitle}>SKU</h2>
+                <strong>{product.sku}</strong>
+            </section>
+        </Form>
+    );
+};
 
-export default classify(defaultClasses)(ProductFullDetail);
+ProductFullDetail.propTypes = {
+    addToCart: func.isRequired,
+    classes: shape({
+        cartActions: string,
+        description: string,
+        descriptionTitle: string,
+        details: string,
+        detailsTitle: string,
+        imageCarousel: string,
+        options: string,
+        productName: string,
+        productPrice: string,
+        quantity: string,
+        quantityTitle: string,
+        root: string,
+        title: string
+    }),
+    isAddingItem: bool,
+    product: shape({
+        __typename: string,
+        id: number,
+        sku: string.isRequired,
+        price: shape({
+            regularPrice: shape({
+                amount: shape({
+                    currency: string.isRequired,
+                    value: number.isRequired
+                })
+            }).isRequired
+        }).isRequired,
+        media_gallery_entries: arrayOf(
+            shape({
+                label: string,
+                position: number,
+                disabled: bool,
+                file: string.isRequired
+            })
+        ),
+        description: string
+    }).isRequired
+};
+
+export default ProductFullDetail;
