7ae2aa91b7eeaca3c7afdb568092e77f2c05d424
James Calcaben <calcaben@adobe.com>
2019-05-15 12:51:22 -0500

    Fix empty params bug

--- pwa-devdocs/scripts/create-reference-docs/createFunctionDocs.js
+++ pwa-devdocs/scripts/create-reference-docs/createFunctionDocs.js
@@ -9,24 +9,33 @@ let createFunctionDocs = ({ sourcePath, overrides, githubSource }) => {
         let apiData = jsDocs.getTemplateDataSync({
             files: [sourcePath]
         });
-        let description = apiData[0].description;
-        let props = {};
-        apiData[0].params.forEach(param => {
-            props[param.name] = {
-                type: param.type,
-                description: param.description
-            };
-        });
-        fileContent =
-            description +
-            templates.functionTable({
-                githubSource,
-                props,
-                propsOverrides: overrides
-            });
 
-        if (fileContent) {
-            resolve(fileContent);
+        let functionData = apiData[0];
+
+        if (functionData) {
+            let { description, params } = functionData;
+
+            let props = {};
+
+            if (params) {
+                params.forEach(param => {
+                    props[param.name] = {
+                        type: param.type,
+                        description: param.description
+                    };
+                });
+            }
+            fileContent =
+                description +
+                templates.functionTable({
+                    githubSource,
+                    props,
+                    propsOverrides: overrides
+                });
+
+            if (fileContent) {
+                resolve(fileContent);
+            }
         } else {
             reject(Error('Could not generate docs', sourcePath));
         }
