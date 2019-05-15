c0c067b9271793ee7a7baf3fd651cfe0a9b4ba6b
James Calcaben <calcaben@adobe.com>
2019-05-15 17:07:29 -0500

    Add useDropdown topic

--- packages/peregrine/src/hooks/useDropdown.js
+++ packages/peregrine/src/hooks/useDropdown.js
@@ -2,6 +2,14 @@ import { useCallback, useRef, useState } from 'react';
 
 import { useDocumentListener } from './useDocumentListener';
 
+/**
+ * A React Hook for adding dropdown-related logic.
+ *
+ * [ref]: https://reactjs.org/docs/refs-and-the-dom.html
+ * [state hook]: https://reactjs.org/docs/hooks-state.html
+ *
+ * @return {Object} An object containing functions and values to add dropdown logic
+ */
 export const useDropdown = () => {
     const elementRef = useRef(null);
     const [expanded, setExpanded] = useState(false);
@@ -19,6 +27,13 @@ export const useDropdown = () => {
     // add listener to document, as an effect
     useDocumentListener('mousedown', maybeCollapse);
 
+    /**
+     * @typedef ReturnedObject
+     * @type {Object}
+     * @property {Object} elementRef - A [ref][] object for attaching to React elements
+     * @property {bool} expanded - The value of the `expanded` state
+     * @property {function} setExpanded - [State Hook][] function for setting the expanded state
+     */
     return {
         elementRef,
         expanded,
