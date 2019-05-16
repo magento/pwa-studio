33a8731c631087dd03a3d12eb5bd4c40229c8347
James Calcaben <calcaben@adobe.com>
2019-05-16 16:55:56 -0500

    Refactor class docs creation to use default render function

--- pwa-devdocs/scripts/create-reference-docs/createFunctionDocs.js
+++ pwa-devdocs/scripts/create-reference-docs/createFunctionDocs.js
@@ -2,32 +2,16 @@
  * Uses the jsdoc-to-markdown library to generate React hooks docs
  */
 const jsDocs = require('jsdoc-to-markdown');
-const templates = require('./templates');
 
 let createFunctionDocs = ({ sourcePath, githubSource }) => {
-    return jsDocs.getTemplateData({ files: [sourcePath] }).then(apiData => {
+    return jsDocs.render({ files: [sourcePath] }).then(content => {
         return new Promise((resolve, reject) => {
-            // This assumes the first entry is always the main function and
-            // the second one is a type definition for a return object if it
-            // exists
-            let [functionData, returnData] = apiData;
-
-            if (functionData) {
-                let { description, params, returns } = functionData;
-
-                fileContent = templates.functionDocs({
-                    description,
-                    githubSource,
-                    parameters: params,
-                    returnData
-                });
-                if (fileContent) {
-                    resolve(fileContent);
-                } else {
-                    reject(Error('Could not generate docs', sourcePath));
-                }
+            if (content == undefined) {
+                reject(Error('Could not generate content', sourcePath));
             } else {
-                reject(Error('Could not get function data', sourcePath));
+                resolve(
+                    `${content}\n\nFor implementation details [**View Source**](${githubSource}).`
+                );
             }
         });
     });
