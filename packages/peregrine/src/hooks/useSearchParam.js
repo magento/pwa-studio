91af099fe591ecfd3e4ac4bb35e360af0b94a087
James Calcaben <calcaben@adobe.com>
2019-05-16 16:56:19 -0500

    Add more doc pages and doc blocks

--- packages/peregrine/src/hooks/useSearchParam.js
+++ packages/peregrine/src/hooks/useSearchParam.js
@@ -6,7 +6,24 @@ const getSearchParam = (parameter = '', location = window.location) => {
     return params.get(parameter) || '';
 };
 
+/**
+ * A [React Hook]{@link https://reactjs.org/docs/hooks-intro.html} that gets
+ * a search parameter from a URL and calls a provided setter function with
+ * the corresponding value.
+ *
+ * @kind function
+ *
+ * @param {PropObject} props An Object containing the location, parameter, and setter function.
+ */
 export const useSearchParam = props => {
+    /**
+     * @typedef PropObject
+     * @type {object}
+     *
+     * @property {string} location The URL location to search for
+     * @property {string} parameter The parameter to search for in the URL
+     * @property {function} setValue A setter function that is passed the parameter value found in the URL
+     */
     const { location, parameter, setValue } = props;
     const value = getSearchParam(parameter, location);
 
---
+++ pwa-devdocs/src/peregrine/reference/hooks/useWindowSize/index.md
@@ -0,0 +1,18 @@
+---
+title: useWindowSize
+---
+
+<!--
+The reference doc content is generated automatically from the source code.
+To update this section, update the doc blocks in the source code
+-->
+
+{% include auto-generated/peregrine/src/hooks/useWindowSize.md %}
+
+## Examples
+
+```jsx
+<WindowSizeContextProvider>
+    <App />
+</WindowSizeContextProvider>
+```
