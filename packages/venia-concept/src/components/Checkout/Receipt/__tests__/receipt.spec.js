f10119e0ee1602d7577475ca69b68158882e76b3
Stephen Rugh <rugh@adobe.com>
2019-05-16 14:52:08 -0500

    Remove continue shopping button from receipt and add View Order Details button for authed users

--- packages/venia-concept/src/components/Checkout/Receipt/__tests__/receipt.spec.js
+++ packages/venia-concept/src/components/Checkout/Receipt/__tests__/receipt.spec.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import testRenderer from 'react-test-renderer';
+import testRenderer, { act } from 'react-test-renderer';
+import { createTestInstance } from '@magento/peregrine';
 import { shallow } from 'enzyme';
-import Receipt, {
-    CREATE_ACCOUNT_BUTTON_ID,
-    CONTINUE_SHOPPING_BUTTON_ID
-} from '../receipt';
+
+import Receipt, { CREATE_ACCOUNT_BUTTON_ID } from '../receipt';
 
 const classes = {
     header: 'header',
@@ -17,7 +16,6 @@ const userProp = { isSignedIn: false };
 
 test('renders a Receipt component correctly', () => {
     const props = {
-        continueShopping: jest.fn(),
         order: { id: '123' },
         createAccount: jest.fn(),
         reset: jest.fn(),
@@ -29,23 +27,6 @@ test('renders a Receipt component correctly', () => {
     expect(component.toJSON()).toMatchSnapshot();
 });
 
-test('calls `handleContinueShopping` when `Continue Shopping` button is pressed', () => {
-    const handleContinueShoppingMock = jest.fn();
-
-    const wrapper = shallow(
-        <Receipt
-            continueShopping={handleContinueShoppingMock}
-            classes={classes}
-            user={userProp}
-        />
-    ).dive();
-    wrapper
-        .findWhere(el => el.prop('data-id') === CONTINUE_SHOPPING_BUTTON_ID)
-        .first()
-        .simulate('click');
-    expect(handleContinueShoppingMock).toBeCalled();
-});
-
 test('calls `handleCreateAccount` when `Create an Account` button is pressed', () => {
     const handleCreateAccountMock = jest.fn();
 
@@ -55,7 +36,7 @@ test('calls `handleCreateAccount` when `Create an Account` button is pressed', (
             classes={classes}
             user={userProp}
         />
-    ).dive();
+    );
 
     wrapper
         .findWhere(el => el.prop('data-id') === CREATE_ACCOUNT_BUTTON_ID)
@@ -68,11 +49,13 @@ test('calls `handleCreateAccount` when `Create an Account` button is pressed', (
 test('calls `reset` when component was unmounted', () => {
     const resetHandlerMock = jest.fn();
 
-    const wrapper = shallow(
+    const instance = createTestInstance(
         <Receipt reset={resetHandlerMock} classes={classes} user={userProp} />
-    ).dive();
+    );
 
-    wrapper.unmount();
+    act(() => {
+        instance.unmount();
+    });
 
     expect(resetHandlerMock).toBeCalled();
 });
