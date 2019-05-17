dd2326e467856dd924af2955ba511ce47b364881
Stephen Rugh <rugh@adobe.com>
2019-05-17 11:18:06 -0500

    Refactor App to be a functional component

--- packages/venia-concept/src/components/App/app.js
+++ packages/venia-concept/src/components/App/app.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useCallback } from 'react';
 import { array, bool, func, shape, string } from 'prop-types';
 
 import Main from 'src/components/Main';
@@ -9,84 +9,37 @@ import ErrorNotifications from './errorNotifications';
 import renderRoutes from './renderRoutes';
 import errorRecord from 'src/util/createErrorRecord';
 
-class App extends Component {
-    static propTypes = {
-        app: shape({
-            drawer: string,
-            hasBeenOffline: bool,
-            isOnline: bool,
-            overlay: bool.isRequired
-        }).isRequired,
-        closeDrawer: func.isRequired,
-        markErrorHandled: func.isRequired,
-        unhandledErrors: array
-    };
+const App = (props) => {
+    const { renderError } = props;
 
-    static get initialState() {
-        return {
-            renderError: null
-        };
-    }
+    const recoverFromRenderError = useCallback(() => {
+        window.location.reload();
+    }, []);
 
-    get errorFallback() {
-        const { renderError } = this.state;
-        if (renderError) {
-            const errors = [
-                errorRecord(renderError, window, this, renderError.stack)
-            ];
-            return (
-                <Fragment>
-                    <Main isMasked={true} />
-                    <Mask isActive={true} />
-                    <ErrorNotifications
-                        errors={errors}
-                        onDismissError={this.recoverFromRenderError}
-                    />
-                </Fragment>
-            );
-        }
-    }
-
-    get onlineIndicator() {
-        const { app } = this.props;
-        const { hasBeenOffline, isOnline } = app;
-
-        // Only show online indicator when
-        // online after being offline
-        return hasBeenOffline ? <OnlineIndicator isOnline={isOnline} /> : null;
-    }
-
-    // Defining this static method turns this component into an ErrorBoundary,
-    // which can re-render a fallback UI if any of its descendant components
-    // throw an exception while rendering.
-    // This is a common implementation: React uses the returned object to run
-    // setState() on the component. <App /> then re-renders with a `renderError`
-    // property in state, and the render() method below will render a fallback
-    // UI describing the error if the `renderError` property is set.
-    static getDerivedStateFromError(renderError) {
-        return { renderError };
-    }
-
-    recoverFromRenderError = () => window.location.reload();
-
-    state = App.initialState;
-
-    render() {
-        const { errorFallback } = this;
-        if (errorFallback) {
-            return errorFallback;
-        }
+    if (renderError) {
+        const errors = [
+            errorRecord(renderError, window, this, renderError.stack)
+        ];
+        return (
+            <Fragment>
+                <Main isMasked={true} />
+                <Mask isActive={true} />
+                <ErrorNotifications
+                    errors={errors}
+                    onDismissError={recoverFromRenderError}
+                />
+            </Fragment>
+        );
+    } else {
         const {
-            app,
+            app: { drawer, hasBeenOffline, isOnline, overlay },
             closeDrawer,
             markErrorHandled,
             unhandledErrors
-        } = this.props;
-        const { drawer, overlay } = app;
+        } = props;
+
         const navIsOpen = drawer === 'nav';
         const cartIsOpen = drawer === 'cart';
-
-        const { hasBeenOffline, isOnline } = app;
 
         return (
             <Fragment>
@@ -108,5 +61,20 @@ class App extends Component {
         );
     }
 }
+
+App.propTypes = {
+    app: shape({
+        drawer: string,
+        hasBeenOffline: bool,
+        isOnline: bool,
+        overlay: bool.isRequired
+    }).isRequired,
+    closeDrawer: func.isRequired,
+    markErrorHandled: func.isRequired,
+    renderError: shape({
+        stack: string
+    }),
+    unhandledErrors: array,
+};
 
 export default App;
