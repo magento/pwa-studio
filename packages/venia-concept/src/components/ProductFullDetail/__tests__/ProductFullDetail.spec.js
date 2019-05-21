cf77f6f1f14c5164b902ad0ed10cdda1cba0face
Andrew Terranova <aterrano@adobe.com>
2019-05-21 11:35:38 -0500

    Restores media gallery entries test

--- packages/venia-concept/src/components/ProductFullDetail/__tests__/ProductFullDetail.spec.js
+++ packages/venia-concept/src/components/ProductFullDetail/__tests__/ProductFullDetail.spec.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     WindowSizeContextProvider,
     createTestInstance
@@ -6,6 +6,11 @@ import {
 
 import ProductFullDetail from '../ProductFullDetail';
 
+jest.mock('react', () => {
+    const React = jest.requireActual('react');
+
+    return Object.assign(React, { useState: jest.fn(React.useState) });
+});
 jest.mock('src/classify');
 
 const mockConfigurableProduct = {
@@ -101,4 +106,40 @@ test('Configurable Product has correct media gallery image count', async () => {
         productFullDetailComponent.children[0].children[1].children[0];
 
     expect(carouselComponent.props.images).toHaveLength(2);
+});
+
+test('Configurable Product with selections has correct media gallery image count', async () => {
+    useState
+        // window size
+        .mockReturnValueOnce([
+            {
+                innerHeight: 99,
+                innerWidth: 99,
+                outerHeight: 99,
+                outerWidth: 99
+            },
+            jest.fn()
+        ])
+        // quantity
+        .mockReturnValueOnce([1, jest.fn()])
+        // optionSelections
+        .mockReturnValueOnce([new Map([['1', 1]]), jest.fn()]);
+
+    debugger;
+    const { root } = createTestInstance(
+        <WindowSizeContextProvider>
+            <ProductFullDetail
+                product={mockConfigurableProduct}
+                isAddingItem={false}
+                classes={{}}
+                addToCart={jest.fn()}
+            />
+        </WindowSizeContextProvider>
+    );
+
+    const productFullDetailComponent = root.children[0].children[0];
+    const carouselComponent =
+        productFullDetailComponent.children[0].children[1].children[0];
+
+    expect(carouselComponent.props.images).toHaveLength(3);
 });
