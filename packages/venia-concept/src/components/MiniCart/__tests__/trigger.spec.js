4f810fa5ce022cd89f2f69ea59e07346b38101b4
Andrew Terranova <aterrano@adobe.com>
2019-05-22 16:03:51 -0500

    Adds Unit Tests for Trigger component

---
+++ packages/venia-concept/src/components/MiniCart/__tests__/trigger.spec.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { createTestInstance } from '@magento/peregrine';
+
+import Trigger from '../trigger';
+
+test('renders children inside of a button', () => {
+    const props = {
+        children: <span>Hi, I'm a child</span>
+    };
+
+    const tree = createTestInstance(
+        <Trigger {...props} />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+});
+
+test('renders a button with no children when children not supplied', () => {
+    const props = {};
+    
+    const tree = createTestInstance(
+        <Trigger {...props} />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+});
