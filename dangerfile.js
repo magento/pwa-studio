f972e6f81eb7adc515e0c70f1748b3047bd140b9
Stephen Rugh <rugh@adobe.com>
2019-05-24 15:57:06 -0500

    another push

--- dangerfile.js
+++ dangerfile.js
@@ -5,6 +5,7 @@ const labels = require('danger-plugin-labels').default;
  * TODO:
  *  - Replace danger env token in build with reference to aws param store after fixing credentials
  *  - Merge back with pwa-studio-cicd
+ *  - Remove actor_id gate from webhook
  */
 
 /**
