c24c285189a244bbfa066b99acbf1b7fe0c3f1e1
James Calcaben <jcalcaben@users.noreply.github.com>
2019-05-28 16:08:43 -0500

    [Documentation] Peregrine hooks reference v3.0.0 (#1278)
    * Add ability to generate hooks reference docs from code
    
    * Refactor create docs functionality into promises
    * Update type from hook to function
    * Refactor create tables logic into separate files
    * Prettify and add comments
    
    * Create useEventListener topic and update doc blocks in source file
    
    * Add link to source code in auto-generated docs
    
    * Fix empty params bug
    
    * Refactor and simplify generating function docs
    
    * Simplify function for creating docs from React classes
    
    * Improve templates
    
    * Add useDropdown topic
    
    * Refactor class docs creation to use default render function
    
    * Add more doc pages and doc blocks
    
    * Run prettier
    
    * Add useQuery doc blocks and topic
    
    * Add more comments and example
    
    * Update based on PR feedback
    
    * Remove peregrine docs about non-3.0 content

--- pwa-devdocs/scripts/create-reference-docs/config/index.js
+++ pwa-devdocs/scripts/create-reference-docs/config/index.js
@@ -4,6 +4,8 @@ const veniaConceptFiles = require('./venia-concept');
 let files = [].concat(peregrineFiles, veniaConceptFiles);
 
 module.exports = {
+    baseGitHubPath:
+        'https://github.com/magento-research/pwa-studio/blob/master/',
     packagesPath: 'packages',
     includesPath: 'src/_includes/auto-generated',
     files: files
