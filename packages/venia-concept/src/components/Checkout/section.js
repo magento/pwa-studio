f95f5096404fad72509e7bbc01fff2d69308a62b
Stephen Rugh <rugh@adobe.com>
2019-06-07 08:54:07 -0500

    Refactor section component

--- packages/venia-concept/src/components/Checkout/section.js
+++ packages/venia-concept/src/components/Checkout/section.js
@@ -1,54 +1,48 @@
-import React, { Component } from 'react';
-import { bool, node, shape, string } from 'prop-types';
+import React from 'react';
+import { bool, func, node, shape, string } from 'prop-types';
 import EditIcon from 'react-feather/dist/icons/edit-2';
 
-import classify from 'src/classify';
+import { mergeClasses } from 'src/classify';
 import Icon from 'src/components/Icon';
 import defaultClasses from './section.css';
 
-// TODO: move these attributes to CSS.
 const editIconAttrs = {
     color: 'black',
     width: 18
 };
 const EDIT_ICON = <Icon src={EditIcon} attrs={editIconAttrs} />;
 
-class Section extends Component {
-    static propTypes = {
-        classes: shape({
-            content: string,
-            icon: string,
-            label: string,
-            root: string,
-            summary: string
-        }),
-        label: node,
-        showEditIcon: bool
-    };
+const Section = props => {
+    const { children, label, onClick, showEditIcon = false } = props;
 
-    render() {
-        const {
-            children,
-            classes,
-            label,
-            showEditIcon,
-            ...restProps
-        } = this.props;
+    const classes = mergeClasses(defaultClasses, props.classes);
 
-        const icon = showEditIcon ? EDIT_ICON : null;
+    const icon = showEditIcon ? EDIT_ICON : null;
 
-        return (
-            <button className={classes.root} {...restProps}>
-                <span className={classes.content}>
-                    <span className={classes.label}>
-                        <span>{label}</span>
-                    </span>
-                    <span className={classes.summary}>{children}</span>
-                    <span className={classes.icon}>{icon}</span>
+    return (
+        <button className={classes.root} onClick={onClick}>
+            <span className={classes.content}>
+                <span className={classes.label}>
+                    <span>{label}</span>
                 </span>
-            </button>
-        );
-    }
-}
+                <span className={classes.summary}>{children}</span>
+                <span className={classes.icon}>{icon}</span>
+            </span>
+        </button>
+    );
+};
 
-export default classify(defaultClasses)(Section);
+Section.propTypes = {
+    classes: shape({
+        content: string,
+        icon: string,
+        label: string,
+        root: string,
+        summary: string
+    }),
+    label: node,
+    onClick: func,
+    showEditIcon: bool
+};
+
+export default Section;
