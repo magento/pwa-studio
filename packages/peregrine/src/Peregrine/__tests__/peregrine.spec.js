67e1fdaf2b08ba91e094c2c6d74185cb01c56b16
Tommy Wiebell <twiebell@adobe.com>
2019-06-11 09:50:56 -0500

    - Add prop to component to make it configurable - Cover changes with tests

--- packages/peregrine/src/Peregrine/__tests__/peregrine.spec.js
+++ packages/peregrine/src/Peregrine/__tests__/peregrine.spec.js
@@ -3,7 +3,7 @@ import ShallowRenderer from 'react-test-renderer/shallow';
 
 import Peregrine from '../peregrine';
 
-test('render wraps children with peregrine context providers', () => {
+test('render wraps children with all peregrine context providers', () => {
     const shallowRenderer = new ShallowRenderer();
     const tree = shallowRenderer.render(
         <Peregrine>
@@ -12,4 +12,32 @@ test('render wraps children with peregrine context providers', () => {
     );
 
     expect(tree).toMatchSnapshot();
+});
+
+test('render wraps children with single peregrine context provider', () => {
+    const shallowRenderer = new ShallowRenderer();
+    const tree = shallowRenderer.render(
+        <Peregrine providers={['TOASTS']}>
+            <i />
+        </Peregrine>
+    );
+
+    expect(tree).toMatchSnapshot();
+});
+
+test('fails to render with invalid context provider', () => {
+    jest.spyOn(console, 'error').mockImplementation(() => {});
+
+    const shallowRenderer = new ShallowRenderer();
+    shallowRenderer.render(
+        <Peregrine providers={['WAFFLES']}>
+            <i />
+        </Peregrine>
+    );
+
+    expect(console.error).toHaveBeenCalledWith(
+        expect.stringContaining(
+            'Invalid prop `providers[0]` of value `WAFFLES` supplied to `Peregrine`, expected one of ["TOASTS","WINDOW_SIZE"].'
+        )
+    );
 });
