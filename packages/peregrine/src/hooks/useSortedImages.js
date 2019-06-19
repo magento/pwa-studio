b8d666022d5214a994c91c5bcf06b2b72021aeb8
Stephen Rugh <rugh@adobe.com>
2019-06-19 10:08:42 -0500

    Remove backward compat shim from sort

--- packages/peregrine/src/hooks/useSortedImages.js
+++ packages/peregrine/src/hooks/useSortedImages.js
@@ -2,12 +2,6 @@ import { useMemo } from 'react';
 
 export const useSortedImages = images => {
     const sortImages = () =>
-        images
-            .filter(i => !i.disabled)
-            .sort((a, b) => {
-                const aPos = isNaN(a.position) ? 9999 : a.position;
-                const bPos = isNaN(b.position) ? 9999 : b.position;
-                return aPos - bPos;
-            });
+        images.filter(i => !i.disabled).sort((a, b) => a.position - b.position);
     return useMemo(sortImages, [images]);
 };
