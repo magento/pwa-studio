d92bb781dec21da1c882a0f8c4367efe4a1a53b4
Jimmy Sanford <jimbo@users.noreply.github.com>
2019-06-24 13:47:18 -0700

    Remove old comments

--- packages/venia-concept/src/components/LoadingIndicator/indicator.js
+++ packages/venia-concept/src/components/LoadingIndicator/indicator.js
@@ -5,12 +5,8 @@ import { mergeClasses } from 'src/classify';
 
 import logo from '../Logo/logo.svg';
 
-// The <img> tag below has a hardcoded className, because it is
-// sharing its animation styles with the spinner code inlined
-// into the app shell. See templates/critical-style.mst.
 const LoadingIndicator = props => {
     const classes = mergeClasses(defaultClasses, props.classes);
-    // `global` renders a fixed-position full-page indicator.
     const className = props.global ? classes.global : classes.root;
 
     return (
