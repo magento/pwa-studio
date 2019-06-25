5dc74194dd4c9f94923ad4435be7fdcce19e09f7
Revanth Kumar Annavarapu <annavara@revanths-mbp.corp.adobe.com>
2019-06-25 15:35:39 -0500

    Removed describe block.

--- packages/peregrine/src/hooks/__tests__/usePageTitle.spec.js
+++ packages/peregrine/src/hooks/__tests__/usePageTitle.spec.js
@@ -11,29 +11,27 @@ const TestComponent = ({ newTitle }) => {
     return <React.Fragment />;
 };
 
-describe('Unit testing usePageTitle hook.', () => {
-    beforeAll(() => {
-        const div = document.createElement('div');
-        window.domNode = div;
-        document.body.appendChild(div);
-        document.title = 'Old';
-    });
+beforeAll(() => {
+    const div = document.createElement('div');
+    window.domNode = div;
+    document.body.appendChild(div);
+    document.title = 'Old';
+});
 
-    /**
-     * TODO: Skipping because JSDOM/Enzyme is not
-     * allowing us to access title.
-     */
-    it.skip('Should change title from Old to New', () => {
-        const newTitle = 'New';
-        const wrapper = mount(<TestComponent newTitle={newTitle} />, {
-            attachTo: window.domNode
-        });
-        return new Promise(resolve => {
-            setTimeout(() => {
-                resolve();
-            }, 0);
-        }).then(() => {
-            expect(wrapper.find('title').getDOMNode().innerText).toBe(newTitle);
-        });
+/**
+ * TODO: Skipping because JSDOM/Enzyme is not
+ * allowing us to access title.
+ */
+it.skip('Should change title from Old to New', () => {
+    const newTitle = 'New';
+    const wrapper = mount(<TestComponent newTitle={newTitle} />, {
+        attachTo: window.domNode
+    });
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        }, 0);
+    }).then(() => {
+        expect(wrapper.find('title').getDOMNode().innerText).toBe(newTitle);
     });
 });
