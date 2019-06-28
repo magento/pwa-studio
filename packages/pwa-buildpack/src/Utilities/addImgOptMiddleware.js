636e534d2279d713476d88e376b9afc9f2ebcf4d
Stephen Rugh <sirugh@users.noreply.github.com>
2019-06-28 16:45:05 -0500

    [feature]: Add height to `makeUrl` and enforce crop in middleware if provided (#1361)
    * Add height and crop to image middleware and makeUrl
    
    * Fix for fastly
    
    * enforce crop in express-sharp if height and width are provided
    
    * Remove unnecessary check in addImgOptMiddleware
    
    * Remove width checks from toExpressSharlUrl function since it is gated by width check.
    
    * Move trailing to forward slash to prefix

--- packages/pwa-buildpack/src/Utilities/addImgOptMiddleware.js
+++ packages/pwa-buildpack/src/Utilities/addImgOptMiddleware.js
@@ -63,11 +63,7 @@ https://github.com/nodejs/node-gyp#installation`
             const imageUrl = new URL(incomingUrl, backendUrl);
             debug('imageUrl', imageUrl);
 
-            const optParamNames = ['auto', 'format', 'width'];
-
-            const rewritten = new URL(
-                `https://0.0.0.0/resize/${incomingQuery.width}`
-            );
+            const optParamNames = ['auto', 'format', 'width', 'height'];
 
             // Start with the original search params, so
             // we can preserve any non-imageopt parameters
@@ -83,6 +79,24 @@ https://github.com/nodejs/node-gyp#installation`
             if (incomingQuery.auto === 'webp') {
                 params.set('format', 'webp');
             }
+
+            const { width, height } = incomingQuery;
+            let rewrittenUrl = `https://0.0.0.0/resize/${width}`;
+
+            // If we received height and width we should force crop since our
+            // implementation of express sharp defaults fit to "outside" if crop
+            // is falsy. `outside` sizes the image, retaining the aspect ratio
+            // but may fall "outside" the desired height or width. `cover`
+            // retains the aspect ratio like `outside` but clips to fit desired
+            // height and width.
+            //   https://github.com/magento-research/express-sharp/blob/develop/lib/transform.js#L23
+            //   https://sharp.pixelplumbing.com/en/stable/api-resize/
+            if (height) {
+                rewrittenUrl += `/${height}`;
+                params.set('crop', true);
+            }
+
+            const rewritten = new URL(rewrittenUrl);
             rewritten.search = params.toString();
 
             debug({ rewritten });
