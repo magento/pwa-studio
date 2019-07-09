5290331792d1672d4e4f57f1ac0ad8093f43b0b1
zetlen <jzetlen@adobe.com>
2019-07-09 11:59:54 +0200

    fixup remove CI cache rule

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,6 @@
 module.exports = api => {
     const config = { presets: ['@magento/peregrine'] };
-    if (process.env.CODEBUILD_BUILD_ID) {
-        // In AWS CodeBuild CI, don't cache
-        api.cache.never();
-    } else if (api.env() === 'development') {
+    if (api.env() === 'development') {
         // Ignore everything with underscores except stories in dev mode
         config.exclude = [
             /\/__(tests?|mocks|fixtures|helpers|dist|packages\/babel\-preset\-peregrine)__\//
