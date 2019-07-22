a212e9e076dfc5aeefb7ba4bbd9e612fac3fbfb1
James Calcaben <jcalcaben@users.noreply.github.com>
2019-07-22 15:47:25 -0500

    [doc] Revise useEventListener doc blocks (#1442)
    * Revise doc blocks in useEventListener
    
    * Modify config so it builds doc snippet for useEventListener
    
    * Ran Prettier
    
    * Fix wording

--- packages/peregrine/lib/hooks/useEventListener.js
+++ packages/peregrine/lib/hooks/useEventListener.js
@@ -1,13 +1,21 @@
 import { useEffect } from 'react';
 
 /**
- * Attaches an event listener to a target object on mount. Removes the listener
- * on unmount.
+ * A [React Hook]{@link https://reactjs.org/docs/hooks-intro.html} that gives
+ * you the ability to add a callback function when an event is triggered on
+ * an object.
  *
- * @param {Object} target the target to attach the listener to
- * @param {String} type the type of event like 'resize', 'error', etc.
- * @param {Function} listener a callback function to be invoked
- * @param  {...any} rest any other arguments to pass to the function
+ * This function attaches an event listener to a target object on mount
+ * and removes the listener on unmount.
+ *
+ * See [addEventListener()]{@link https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener}
+ *
+ * @kind function
+ *
+ * @param {EventTarget} target The [EventTarget]{@link https://developer.mozilla.org/en-US/docs/Web/API/EventTarget} to attach the listener to
+ * @param {String} type The type of [Event]{@link https://developer.mozilla.org/en-US/docs/Web/Events} to listen for, e.g. 'resize', 'error', etc.
+ * @param {Function} listener A callback function that is invoked when the event is triggered
+ * @param  {...any} rest Any other arguments to pass to the addEventListener() function
  */
 export const useEventListener = (target, type, listener, ...rest) => {
     useEffect(() => {
