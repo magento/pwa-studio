a8cdccb80405996b02700a62bee2a1ed805f3c55
Narendra Vyas <nrenvyas@gmail.com>
2019-08-01 17:31:07 +0530

    Removed unwanted code

--- packages/venia-concept/src/components/ProductQuantity/quantity.js
+++ packages/venia-concept/src/components/ProductQuantity/quantity.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Text } from 'informed';
 import { Plus, Minus } from 'react-feather';
 import classify from 'src/classify';
@@ -84,45 +84,3 @@ Quantity.propTypes = {
 };
 
 export default classify(defaultClasses)(Quantity);
-
-// import React, { Component } from 'react';
-// import { arrayOf, number, shape, string } from 'prop-types';
-
-// import classify from '../../classify';
-// import Select from '../Select';
-// import mockData from './mockData';
-// import defaultClasses from './quantity.css';
-
-// class Quantity extends Component {
-//     static propTypes = {
-//         classes: shape({
-//             root: string
-//         }),
-//         items: arrayOf(
-//             shape({
-//                 value: number
-//             })
-//         )
-//     };
-
-//     static defaultProps = {
-//         selectLabel: "product's quantity"
-//     };
-
-//     render() {
-//         const { classes, selectLabel, ...restProps } = this.props;
-
-//         return (
-//             <div className={classes.root}>
-//                 <Select
-//                     {...restProps}
-//                     field="quantity"
-//                     aria-label={selectLabel}
-//                     items={mockData}
-//                 />
-//             </div>
-//         );
-//     }
-// }
-
-// export default classify(defaultClasses)(Quantity);
