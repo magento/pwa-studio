a87c723a187c6da586a99eea17c9913d52af3315
Andrew Terranova <aterrano@adobe.com>
2019-08-07 16:30:32 -0500

    Support Peregrine List initial selections

--- packages/peregrine/lib/List/items.js
+++ packages/peregrine/lib/List/items.js
@@ -1,47 +1,114 @@
-import React, { Fragment } from 'react';
-import PropTypes from 'prop-types';
+import React, { Fragment, useCallback, useMemo, useState } from 'react';
+import { array, func, object, oneOf, oneOfType, string } from 'prop-types';
 
-import { useListState } from './useListState';
 import iterable from '../validators/iterable';
 import Item from './item';
 
 const Items = props => {
     const {
         getItemKey,
+        initialSelection,
+        items,
+        onSelectionChange,
+        renderItem,
+        selectionModel
+    } = props;
+
+    const initialSelectedKeys = useMemo(() => {
+        if (!initialSelection) {
+            return null;
+        }
+
+        // We store the keys of each item that is initially selected.
+        if (!Array.isArray(initialSelection)) {
+            return [getItemKey(initialSelection)];
+        } else {
+            return initialSelection.map(getItemKey);
+        }
+    }, [getItemKey, initialSelection]);
+
+    const [cursor, setCursor] = useState(null);
+    const [hasFocus, setHasFocus] = useState(false);
+    const [selectedKeys, setSelectedKeys] = useState(
+        new Set(initialSelectedKeys)
+    );
+
+    const handleFocus = useCallback(index => {
+        setCursor(index);
+        setHasFocus(true);
+    }, []);
+
+    const handleClick = useCallback(
+        key => {
+            let newSelectedKeys;
+
+            if (selectionModel === 'radio') {
+                // For radio, only one item can be selected at a time.
+                newSelectedKeys = new Set().add(key);
+            }
+
+            if (selectionModel === 'checkbox') {
+                newSelectedKeys = new Set(selectedKeys);
+
+                if (!newSelectedKeys.has(key)) {
+                    // The item is being selected.
+                    newSelectedKeys.add(key);
+                } else {
+                    // The item is being deselected.
+                    newSelectedKeys.delete(key);
+                }
+            }
+
+            // Set the new state.
+            setSelectedKeys(newSelectedKeys);
+
+            // And notify.
+            if (onSelectionChange) {
+                onSelectionChange(newSelectedKeys);
+            }
+        },
+        [onSelectionChange, selectedKeys, selectionModel]
+    );
+
+    const children = useMemo(() => {
+        return Array.from(items, (item, index) => {
+            const key = getItemKey(item, index);
+
+            return (
+                <Item
+                    hasFocus={hasFocus && cursor === key}
+                    isSelected={selectedKeys.has(key)}
+                    item={item}
+                    itemIndex={index}
+                    key={key}
+                    onBlur={setHasFocus(false)}
+                    onClick={handleClick.bind(this, key)}
+                    onFocus={handleFocus}
+                    render={renderItem}
+                />
+            );
+        });
+    }, [
+        cursor,
+        getItemKey,
+        handleClick,
+        handleFocus,
+        hasFocus,
         items,
         renderItem,
-        selectionModel,
-        onSelectionChange
-    } = props;
-    const [
-        { cursor, hasFocus, selection },
-        { setFocus, removeFocus, updateSelection }
-    ] = useListState({ selectionModel, onSelectionChange });
-    const itemsElements = items.map((item, index) => {
-        const key = getItemKey(item, index);
-        return (
-            <Item
-                key={key}
-                uniqueID={key}
-                item={item}
-                itemIndex={index}
-                render={renderItem}
-                hasFocus={hasFocus && cursor === key}
-                isSelected={selection.has(key)}
-                updateSelection={updateSelection}
-                setFocus={setFocus}
-                onBlur={removeFocus}
-            />
-        );
-    });
-    return <Fragment>{itemsElements}</Fragment>;
+        selectedKeys
+    ]);
+
+    return <Fragment>{children}</Fragment>;
 };
 
 Items.propTypes = {
-    getItemKey: PropTypes.func.isRequired,
+    getItemKey: func.isRequired,
+    initialSelection: oneOfType([array, object]),
     items: iterable.isRequired,
-    renderItem: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
-    selectionModel: PropTypes.oneOf(['checkbox', 'radio'])
+    onSelectionChange: func,
+    renderItem: oneOfType([func, string]),
+    selectionModel: oneOf(['checkbox', 'radio'])
 };
 
 Items.defaultProps = {
