6175da429fbbf693f37976ec674432742848a5cc
Revanth Kumar Annavarapu <revanth0212@gmail.com>
2019-08-13 13:57:51 -0500

    Minor change.

--- packages/peregrine/lib/List/item.js
+++ packages/peregrine/lib/List/item.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import PropTypes from 'prop-types';
 
 import fromRenderProp from '../util/fromRenderProp';
