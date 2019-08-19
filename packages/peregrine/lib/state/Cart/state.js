646d933adfdd60f31c26cc28f09062b522602703
Stephen Rugh <rugh@adobe.com>
2019-08-19 14:37:21 -0500

    Update cart state reducer cases

--- packages/peregrine/lib/state/Cart/state.js
+++ packages/peregrine/lib/state/Cart/state.js
@@ -4,19 +4,43 @@ import withLogger from '../../util/withLogger';
 const initialState = {
     cartId: null,
     details: {},
-    editItem: null,
     paymentMethods: [],
-    shippingMethods: [],
     totals: {}
 };
 
 const reducer = (state, { payload, type }) => {
-    // TODO: add cases
     switch (type) {
+        case 'reset cart': {
+            return {
+                ...initialState
+            };
+        }
         case 'set cart id': {
             return {
                 ...state,
                 cartId: payload
+            };
+        }
+        case 'set details': {
+            return {
+                ...state,
+                details: {
+                    ...payload
+                }
+            };
+        }
+        case 'set payment methods': {
+            return {
+                ...state,
+                paymentMethods: [...payload]
+            };
+        }
+        case 'set totals': {
+            return {
+                ...state,
+                totals: {
+                    ...payload
+                }
             };
         }
         default: {
@@ -29,6 +53,16 @@ const wrappedReducer = withLogger(reducer);
 export const useCartState = () => {
     const [state, dispatch] = useReducer(wrappedReducer, initialState);
 
+    const resetCart = useCallback(
+        payload => {
+            dispatch({
+                payload,
+                type: 'reset cart'
+            });
+        },
+        [dispatch]
+    );
+
     const setCartId = useCallback(
         payload => {
             dispatch({
@@ -38,12 +72,50 @@ export const useCartState = () => {
         },
         [dispatch]
     );
+    const setDetails = useCallback(
+        payload => {
+            dispatch({
+                payload,
+                type: 'set details'
+            });
+        },
+        [dispatch]
+    );
+    const setPaymentMethods = useCallback(
+        payload => {
+            dispatch({
+                payload,
+                type: 'set payment methods'
+            });
+        },
+        [dispatch]
+    );
+    const setTotals = useCallback(
+        payload => {
+            dispatch({
+                payload,
+                type: 'set totals'
+            });
+        },
+        [dispatch]
+    );
     const api = useMemo(
         () => ({
             dispatch,
-            setCartId
+            resetCart,
+            setCartId,
+            setDetails,
+            setPaymentMethods,
+            setTotals
         }),
-        [setCartId, dispatch]
+        [
+            resetCart,
+            setCartId,
+            setDetails,
+            setPaymentMethods,
+            setTotals,
+            dispatch
+        ]
     );
 
     return [state, api];
