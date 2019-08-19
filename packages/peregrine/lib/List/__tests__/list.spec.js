1ace7e2fcbcba4cfe02df0ad28a89d8190e6b09c
Revanth Kumar Annavarapu <35203638+revanth0212@users.noreply.github.com>
2019-08-19 12:53:45 -0500

    Using hooks to refactor list component. (#1515)
    * Using hooks to refactor list component.
    
    * Prettier changes.
    
    * Added snapshots and removed enzyme dependency.
    
    * Prettier changes.
    
    * Added useListState related tests.
    
    * Deleted old commented out code.
    
    * Updated tests to not use react-hooks testing utils.
    
    * Removing react-hooks dependency from all projects.
    
    * Using useMemo for caching Root in item.
    
    * Removing unnecessary useMemo call.
    
    * Minor change.

--- packages/peregrine/lib/List/__tests__/list.spec.js
+++ packages/peregrine/lib/List/__tests__/list.spec.js
@@ -1,7 +1,7 @@
-import React, { Fragment } from 'react';
-import { shallow } from 'enzyme';
+import React from 'react';
 
-import List from '..';
+import List, { Items } from '..';
+import createTestInstance from '../../util/createTestInstance';
 
 const classes = {
     root: 'abc'
@@ -34,94 +34,61 @@ const items = [
     }
 ];
 
-test('renders a div by default', () => {
-    const props = { classes };
-    const wrapper = shallow(<List {...props} />).dive();
+const getItemKey = jest.fn(({ id }) => id);
 
-    expect(wrapper.type()).toEqual('div');
-    expect(wrapper.prop('className')).toEqual(classes.root);
+const renderItem = jest.fn(() => <div />);
+
+const onSelectionChange = jest.fn();
+
+test('snapshot testing List component', () => {
+    const tree = createTestInstance(
+        <List
+            classes={classes}
+            items={items}
+            getItemKey={getItemKey}
+            selectionModel="radio"
+            render="div"
+            renderItem={renderItem}
+            onSelectionChange={onSelectionChange}
+        />
+    );
+
+    expect(tree.toTree()).toMatchSnapshot();
 });
 
-test('renders a provided tagname', () => {
-    const props = { classes, render: 'ul' };
-    const wrapper = shallow(<List {...props} />).dive();
+test('rerenders should not create new root element if props dint change', () => {
+    const renderer = createTestInstance(<List items={items} render="div" />);
+    const firstRoot = renderer.root.children[0];
+    renderer.update(<List items={items} render="div" />);
 
-    expect(wrapper.type()).toEqual('ul');
-    expect(wrapper.prop('className')).toEqual(classes.root);
+    expect(firstRoot).toEqual(renderer.root.children[0]);
 });
 
-test('renders a provided component', () => {
+test('rerenders should create new root element if props change', () => {
+    const renderer = createTestInstance(<List items={items} render="div" />);
+    const firstRoot = renderer.root.children[0];
+    renderer.update(<List items={items} render="ul" />);
+
+    expect(firstRoot).not.toEqual(renderer.root.children[0]);
+});
+
+test('handleSelectionChange is memoized on onSelectionChange prop', () => {
+    const renderer = createTestInstance(
+        <List items={items} onSelectionChange={onSelectionChange} />
+    );
+    const firstHandleSelectionChange = renderer.root.findByType(Items).props
+        .onSelectionChange;
+    renderer.update(<List items={items} onSelectionChange={jest.fn()} />);
+
+    expect(firstHandleSelectionChange).not.toEqual(
+        renderer.root.findByType(Items).props.onSelectionChange
+    );
+});
+
+test('renders the component provided using render prop as the container', () => {
     const Nav = () => <nav />;
-    const props = { render: Nav };
-    const wrapper = shallow(<List {...props} />);
+    const instance = createTestInstance(<List items={items} render={Nav} />)
+        .root;
 
-    expect(wrapper.type()).toEqual(Nav);
-    expect(wrapper.dive().type()).toEqual('nav');
-});
-
-test('passes only rest props to basic `render`', () => {
-    const props = { classes, items, render: 'ul', renderItem: 'li' };
-    const wrapper = shallow(<List {...props} data-id="b" />).dive();
-
-    expect(wrapper.props()).toHaveProperty('data-id');
-    expect(wrapper.props()).not.toHaveProperty('classes');
-    expect(wrapper.props()).not.toHaveProperty('items');
-    expect(wrapper.props()).not.toHaveProperty('onSelectionChange');
-    expect(wrapper.props()).not.toHaveProperty('selectionModel');
-    expect(wrapper.props()).not.toHaveProperty('render');
-    expect(wrapper.props()).not.toHaveProperty('renderItem');
-});
-
-test('passes custom and rest props to composite `render`', () => {
-    const Nav = () => <nav />;
-    const props = { classes, items, render: Nav, renderItem: 'a' };
-    const wrapper = shallow(<List {...props} data-id="b" />);
-
-    expect(wrapper.props()).toHaveProperty('data-id');
-    expect(wrapper.props()).toHaveProperty('classes');
-    expect(wrapper.props()).toHaveProperty('items');
-    expect(wrapper.props()).toHaveProperty('onSelectionChange');
-    expect(wrapper.props()).toHaveProperty('selectionModel');
-    expect(wrapper.props()).not.toHaveProperty('render');
-    expect(wrapper.props()).not.toHaveProperty('renderItem');
-});
-
-test('renders a fragment as `children`', () => {
-    const props = { classes, items };
-    const wrapper = shallow(<List {...props} />);
-
-    expect(
-        wrapper
-            .childAt(0)
-            .dive()
-            .type()
-    ).toEqual(Fragment);
-});
-
-test('passes correct props through to `Items`', () => {
-    const Item = () => <li />;
-    const selectionModel = 'checkbox';
-    const props = { items, renderItem: Item, selectionModel };
-    const wrapper = shallow(<List {...props} />);
-
-    expect(wrapper.childAt(0).props()).toMatchObject(props);
-});
-
-test('calls `onSelectionChange` on selection change', () => {
-    const onSelectionChange = jest.fn();
-    const selection = new Set();
-    const props = { items, onSelectionChange };
-    const wrapper = shallow(<List {...props} />);
-
-    wrapper.instance().handleSelectionChange(selection);
-    expect(onSelectionChange).toHaveBeenCalledWith(selection);
-});
-
-test('does not throw if `onSelectionChange` is not provided', () => {
-    const selection = new Set();
-    const props = { items };
-    const wrapper = shallow(<List {...props} />);
-
-    const cb = () => wrapper.instance().handleSelectionChange(selection);
-    expect(cb).not.toThrow();
+    expect(instance.children[0].type).toEqual(Nav);
 });
