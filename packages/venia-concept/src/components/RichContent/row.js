5a135c5dab4cbc0bcc4ba98d726b995543057582
Dave Macaulay <macaulay@adobe.com>
2019-08-23 15:01:26 -0500

    MC-18995: Page Builder & PWA Studio Compatibility
    - Render background and min-height for row

--- packages/venia-concept/src/components/RichContent/row.js
+++ packages/venia-concept/src/components/RichContent/row.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
-const Row = ({ minHeight, backgroundColor, children }) => {
+const Row = ({minHeight, backgroundColor, desktopImage, mobileImage, backgroundSize, backgroundPosition, backgroundAttachment, backgroundRepeat, children}) => {
+    let image = desktopImage;
+    if (mobileImage && window.matchMedia('(max-width: 768px)').matches) {
+        image = mobileImage;
+    }
+    const dynamicStyles = {
+        minHeight: minHeight,
+        backgroundColor: backgroundColor,
+        backgroundImage: image ? `url(${image})` : null,
+        backgroundSize,
+        backgroundPosition,
+        backgroundAttachment,
+        backgroundRepeat: backgroundRepeat ? 'repeat' : 'no-repeat'
+    };
     return (
-        <div data-content-type="row" style={{minHeight: minHeight, backgroundColor: backgroundColor}}>
+        <div data-content-type="row" style={dynamicStyles}>
             {children}
         </div>
     );
