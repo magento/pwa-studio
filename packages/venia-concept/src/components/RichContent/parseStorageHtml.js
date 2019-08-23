9d92bb20e0c01a6fa49b677a8d3be58955c1576c
Dan Mooney <dmooney@adobe.com>
2019-08-23 09:41:06 -0500

    MC-18995: Page Builder & PWA Studio Compatibility

--- packages/venia-concept/src/components/RichContent/parseStorageHtml.js
+++ packages/venia-concept/src/components/RichContent/parseStorageHtml.js
@@ -1,15 +1,16 @@
+const createContentTypeObject = (contentTypeStr) => {
+    return {
+        contentType: contentTypeStr,
+        children: [],
+    };
+};
+
 /**
  * @param {HTMLElement} rootEl
+ * @param {Object} contentTypeStructureObj
  * @returns {Object}
  */
-const walk = (rootEl, treeArray) => {
-    if (!treeArray) {
-        treeArray = {
-            contentType: "stage",
-            children: []
-        };
-    }
-
+const walk = (rootEl, contentTypeStructureObj) => {
     const tree = document.createTreeWalker(
         rootEl,
         NodeFilter.SHOW_ELEMENT | NodeFilter.SHOW_TEXT
@@ -17,28 +18,33 @@ const walk = (rootEl, treeArray) => {
 
     let currentNode = tree.nextNode();
     while (currentNode) {
-        if (currentNode.nodeType === Node.ELEMENT_NODE) {
-            const contentType = currentNode.getAttribute('data-content-type');
-            if (contentType) {
-                let props = {
-                    type: contentType,
-                    children: [],
-                };
-                if (pageBuilderVisitors[contentType]) {
-                    props = Object.assign(props, pageBuilderVisitors[contentType](currentNode));
-                }
-                treeArray.children.push(props);
-                walk(currentNode, props);
-                currentNode = tree.nextSibling();
-            } else {
-                currentNode = tree.nextNode();
-            }
-        } else {
+        if (currentNode.nodeType !== Node.ELEMENT_NODE) {
             currentNode = tree.nextNode();
+            continue;
         }
+
+        const contentType = currentNode.getAttribute('data-content-type');
+
+        if (!contentType) {
+            currentNode = tree.nextNode();
+            continue;
+        }
+
+        const props = {
+            type: contentType,
+            children: [],
+        };
+
+        if (pageBuilderVisitors[contentType]) {
+            Object.assign(props, pageBuilderVisitors[contentType](currentNode));
+        }
+
+        contentTypeStructureObj.children.push(props);
+        walk(currentNode, props);
+        currentNode = tree.nextSibling();
     }
 
-    return treeArray;
+    return contentTypeStructureObj;
 };
 
 const pageBuilderVisitors = {
@@ -65,7 +71,9 @@ const parseStorageHtml = htmlStr => {
     const container = document.createElement('div');
     container.innerHTML = htmlStr;
 
-    return walk(container);
+    const stageContentType = createContentTypeObject('stage');
+
+    return walk(container, stageContentType);
 };
 
 export default parseStorageHtml;
