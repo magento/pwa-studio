6a6cf5657ffce8739636b41d9a5edb8e2a5abe8b
Dan Mooney <dmooney@adobe.com>
2019-08-26 14:23:44 -0500

    MC-18995: Page Builder & PWA Studio Compatibility
    - Add tabs/tab item

--- packages/venia-concept/src/components/RichContent/PageBuilder/ContentTypes/Tabs/tabs.js
+++ packages/venia-concept/src/components/RichContent/PageBuilder/ContentTypes/Tabs/tabs.js
@@ -1,65 +1,55 @@
 import React from 'react';
-import { Tabs, TabList, Tab, TabPanel } from 'react-tabs';
+import { Tabs as TabWrapper, TabList, Tab as TabHeader } from 'react-tabs';
 import tabsClasses from './tabs.css';
-import RichContent from '../../../richContent';
-import GenericElement from './genericElement';
+import classify from 'src/classify';
+import defaultClasses from "../Column/column.css";
 
-const PageBuilderTabs = ({ data }) => {
-    const tabsProps = data.element.domAttributes;
-    const tabs = data.elements.headers.map((tabHeader, index) => (
-        <Tab
-            {...tabHeader.domAttributes}
-            className={
-                tabHeader.domAttributes.className + ' ' + tabsClasses.tab
-            }
-            key={index}
-        >
-            <RichContent data={tabHeader.children} />
-        </Tab>
-    ));
+const Tabs = ({classes, appearance, verticalAlignment, minHeight, defaultIndex, headers, navigation, border, borderColor, borderWidth, borderRadius, marginTop, marginRight, marginBottom, marginLeft, paddingTop, paddingRight, paddingBottom, paddingLeft, cssClasses, children}) => {
+    const tabWrapperDynamicStyles = {
+        verticalAlignment,
+        minHeight,
+        border,
+        borderColor,
+        borderWidth,
+        borderRadius,
+        marginTop,
+        marginRight,
+        marginBottom,
+        marginLeft,
+        paddingTop,
+        paddingRight,
+        paddingBottom,
+        paddingLeft
+    };
 
-    const tabsNavigation = data.elements.navigation[0];
-    const tabListProps = tabsNavigation.domAttributes;
-    const tabsContent = data.elements.content[0];
+    const tabWrapperProps = {
+        defaultIndex
+    };
 
-    if (!Number.isNaN(data.element.dataAttributes.activeTab)) {
-        tabsProps.defaultIndex = parseInt(
-            data.element.dataAttributes.activeTab,
-            10
-        );
-    }
+    cssClasses.push(classes.root);
 
     return (
-        <Tabs
-            {...tabsProps}
-            className={tabsProps.className + ' ' + tabsClasses.root}
-            disabledTabClassName={tabsClasses.tabDisabled}
-            selectedTabClassName={tabsClasses.tabSelected}
+        <TabWrapper
+            style={tabWrapperDynamicStyles}
+            className={[...cssClasses].join(' ')}
+            disabledTabClassName={classes.tabDisabled}
+            selectedTabClassName={classes.tabSelected}
+            {...tabWrapperProps}
         >
             <TabList
-                {...tabListProps}
-                className={tabListProps.className + ' ' + tabsClasses.tabList}
+                className={[...navigation.cssClasses, classes.tabList].join(' ')}
             >
-                {tabs}
-            </TabList>
-            <GenericElement data={tabsContent}>
-                {tabsContent.children.map((child, index) => (
-                    <TabPanel
-                        {...child.domAttributes}
-                        key={index}
-                        className={
-                            child.domAttributes.className +
-                            ' ' +
-                            tabsClasses.tabPanel
-                        }
-                        selectedClassName={tabsClasses.tabPanelSelected}
+                {headers.map((header, i) => (
+                    <TabHeader
+                        key={i}
                     >
-                        <RichContent data={child.children} />
-                    </TabPanel>
+                        {header}
+                    </TabHeader>
                 ))}
-            </GenericElement>
-        </Tabs>
+            </TabList>
+            {children}
+        </TabWrapper>
     );
 };
 
-export default PageBuilderTabs;
+export default classify(defaultClasses)(Tabs);
