c09a2856bbbeba00ade435a9cae219ce902294af
Revanth Kumar <revanth0212@gmail.com>
2019-08-28 13:54:40 -0500

    Minor.

--- packages/venia-concept/webpack.config.js
+++ packages/venia-concept/webpack.config.js
@@ -58,8 +58,8 @@ module.exports = async env => {
             template: './template.html',
             minify: {
                 collapseWhitespace: true,
-                removeComments: true,
-              }
+                removeComments: true
+            }
         })
     ];
 
