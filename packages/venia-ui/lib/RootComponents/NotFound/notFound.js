ba5d21673f6ab55972c8b523e51f50e4cbf6e5db
Stephen Rugh <rugh@adobe.com>
2019-09-10 11:47:07 -0500

    Use window history instead of withRouter composed

--- packages/venia-ui/lib/RootComponents/NotFound/notFound.js
+++ packages/venia-ui/lib/RootComponents/NotFound/notFound.js
@@ -1,12 +1,10 @@
 import React, { useCallback } from 'react';
 import { mergeClasses } from '../../classify';
 import defaultClasses from './notFound.css';
-import { withRouter } from '@magento/venia-drivers';
-import { compose } from 'redux';
 
 const NotFound = props => {
     const classes = mergeClasses(defaultClasses, props.classes);
-    const { history } = props;
+    const { history } = window;
 
     const handleGoBack = useCallback(() => {
         history.back();
@@ -26,4 +24,4 @@ NotFound.defaultProps = {
     id: 3
 };
 
-export default compose(withRouter)(Category);
+export default NotFound;
