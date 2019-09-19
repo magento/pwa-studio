4dabfc745dd7987d1e087c61bd356e83ea50f79f
Dave Macaulay <macaulay@adobe.com>
2019-09-19 10:38:19 -0500

    PB-4: Master format parser for PWA Studio
    - Include documentation for parser and adding a new content type

--- packages/venia-ui/lib/components/RichContent/PageBuilder/__tests__/config.spec.js
+++ packages/venia-ui/lib/components/RichContent/PageBuilder/__tests__/config.spec.js
@@ -1,6 +1,6 @@
 import { contentTypesConfig } from '../config';
 
-test('all content types contain component and aggregator', () => {
+test('all content types contain component and aggregator and component can be rendered', () => {
     for (const [, config] of Object.entries(contentTypesConfig)) {
         expect(config.configAggregator).toBeDefined();
         expect(config.component).toBeDefined();
