677cfe761323e8858d0a31e3359f7d04c7188979
Stephen Rugh <rugh@adobe.com>
2019-09-23 11:30:48 -0500

    Oops, forgot to commit userchip hook

--- packages/peregrine/lib/mixins/AuthBar/useUserChip.js
+++ packages/peregrine/lib/mixins/AuthBar/useUserChip.js
@@ -1,19 +1,18 @@
 import { useCallback } from 'react';
 
 export const useUserChip = props => {
-  const { showMyAccount, user } = props;
-  const { email, firstname, lastname } = user || {};
-  const fullname = `${firstname} ${lastname}`;
-  const display = fullname.trim() || 'Loading...';
+    const { showMyAccount, user } = props;
+    const { email, firstname, lastname } = user || {};
+    const fullname = `${firstname} ${lastname}`;
+    const display = fullname.trim() || 'Loading...';
 
-  const handleClick = useCallback(() => {
-      showMyAccount();
-  }, [showMyAccount]);
+    const handleClick = useCallback(() => {
+        showMyAccount();
+    }, [showMyAccount]);
 
-  return {
-    display,
-    email,
-    fullname,
-    handleClick,
-  }
+    return {
+        display,
+        email,
+        handleClick
+    };
 };
