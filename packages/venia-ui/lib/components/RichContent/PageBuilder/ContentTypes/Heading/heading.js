14f27472cabde931870e94fe20849ac7cc846b1f
Ievgen Kolesov <ikolesov@adobe.com>
2019-09-24 17:33:57 -0500

    PB-11: Heading content type in Venia UI / PWA Studio

--- packages/venia-ui/lib/components/RichContent/PageBuilder/ContentTypes/Heading/heading.js
+++ packages/venia-ui/lib/components/RichContent/PageBuilder/ContentTypes/Heading/heading.js
@@ -3,6 +3,18 @@ import defaultClasses from './heading.css';
 import { mergeClasses } from '../../../../../classify';
 import { arrayOf, shape, string } from 'prop-types';
 
+/**
+ * Page Builder Heading component.
+ *
+ * This component is part of the Page Builder / PWA integration. It can be consumed without Page Builder.
+ *
+ * @typedef Heading
+ * @kind functional component
+ *
+ * @param {props} props React component props
+ *
+ * @returns {React.Element} A React component that renders Heading with optional styling properties.
+ */
 const Heading = props => {
     const classes = mergeClasses(defaultClasses, props.classes);
     const {
@@ -23,7 +35,7 @@ const Heading = props => {
         paddingLeft,
         cssClasses = []
     } = props;
-    const rootClass = classes[`root_${headingType}`];
+    const headingTypeClass = classes[headingType];
     const HeadingType = `${headingType}`;
     const dynamicStyles = {
         textAlign,
@@ -42,15 +54,46 @@ const Heading = props => {
     };
     cssClasses.push(classes.heading);
     return (
-        <HeadingType style={dynamicStyles} className={rootClass}>
+        <HeadingType
+            style={dynamicStyles}
+            className={[classes.root, headingTypeClass, ...cssClasses].join(
+                ' '
+            )}
+        >
             {text}
         </HeadingType>
     );
 };
 
+/**
+ * Props for {@link Heading}
+ *
+ * @typedef props
+ *
+ * @property {Object} classes An object containing the class names for the Heading
+ * @property {String} classes.root CSS class for the root container element
+ * @property {String} classes.headingTypeClass CSS class representing heading type
+ * @property {String} headingType Level of HTML heading
+ * @property {String} text Heading text
+ * @property {String} textAlign Alignment of the text within the parent container
+ * @property {String} border CSS border property
+ * @property {String} borderColor CSS border color property
+ * @property {String} borderWidth CSS border width property
+ * @property {String} borderRadius CSS border radius property
+ * @property {String} marginTop CSS margin top property
+ * @property {String} marginRight CSS margin right property
+ * @property {String} marginBottom CSS margin bottom property
+ * @property {String} marginLeft CSS margin left property
+ * @property {String} paddingTop CSS padding top property
+ * @property {String} paddingRight CSS padding right property
+ * @property {String} paddingBottom CSS padding bottom property
+ * @property {String} paddingLeft CSS padding left property
+ * @property {Array} cssClasses List of CSS classes to be applied to the component
+ */
 Heading.propTypes = {
     classes: shape({
-        heading: string
+        root: string,
+        headingTypeClass: string
     }),
     headingType: string,
     text: string,
@@ -66,6 +109,7 @@ Heading.propTypes = {
     paddingTop: string,
     paddingRight: string,
     paddingBottom: string,
+    paddingLeft: string,
     cssClasses: arrayOf(string)
 };
 
