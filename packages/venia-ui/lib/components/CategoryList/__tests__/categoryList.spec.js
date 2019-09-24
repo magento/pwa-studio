e3e75f0a05150940e14357cfbed3f9d497564c7b
Stephen Rugh <rugh@adobe.com>
2019-09-24 09:09:36 -0500

    Update tests for use with talons

--- packages/venia-ui/lib/components/CategoryList/__tests__/categoryList.spec.js
+++ packages/venia-ui/lib/components/CategoryList/__tests__/categoryList.spec.js
@@ -1,31 +1,44 @@
 import React from 'react';
-import waitForExpect from 'wait-for-expect';
-import TestRenderer from 'react-test-renderer';
-import { MemoryRouter } from 'react-router-dom';
-import { MockedProvider } from 'react-apollo/test-utils';
+import { createTestInstance } from '@magento/peregrine';
 
 import LoadingIndicator from '../../LoadingIndicator';
 import CategoryTile from '../categoryTile';
 import CategoryList from '../categoryList';
-import getCategoryList from '../../../queries/getCategoryList.graphql';
+import { useCategoryList } from '@magento/peregrine/lib/mixins/CategoryList/useCategoryList';
 
+import { useCategoryTile } from '@magento/peregrine/lib/mixins/CategoryList/useCategoryTile';
+
+jest.mock('@magento/venia-drivers');
 jest.mock('../../../classify');
+jest.mock('@magento/peregrine/lib/mixins/CategoryList/useCategoryTile', () => {
+    return {
+        useCategoryTile: jest.fn()
+    };
+});
 
-const withRouterAndApolloClient = (mocks, renderFn) => (
-    <MemoryRouter initialIndex={0} initialEntries={['/']}>
-        <MockedProvider mocks={mocks} addTypename={false}>
-            {renderFn()}
-        </MockedProvider>
-    </MemoryRouter>
-);
+jest.mock('@magento/peregrine/lib/mixins/CategoryList/useCategoryList', () => {
+    return {
+        useCategoryList: jest.fn()
+    };
+});
+
+useCategoryTile.mockReturnValue({
+    image: {},
+    item: {}
+});
+useCategoryList.mockReturnValue({
+    data: {
+        category: {
+            children: []
+        }
+    },
+    loading: false,
+    error: false
+});
 
 test('renders a header', () => {
     const title = 'foo';
-    const { root } = TestRenderer.create(
-        withRouterAndApolloClient([], () => (
-            <CategoryList id={2} title={title} />
-        ))
-    );
+    const { root } = createTestInstance(<CategoryList id={2} title={title} />);
 
     const list = root.findByProps({ className: 'root' });
     const header = list.findByProps({ className: 'header' });
@@ -35,91 +48,83 @@ test('renders a header', () => {
 });
 
 test('omits the header if there is no title', () => {
-    const { root } = TestRenderer.create(
-        withRouterAndApolloClient([], () => <CategoryList id={2} />)
-    );
+    const { root } = createTestInstance(<CategoryList id={2} />);
 
     expect(root.findAllByProps({ className: 'header' })).toHaveLength(0);
 });
 
-test('renders category tiles', async () => {
-    const mocks = [
-        {
-            request: {
-                query: getCategoryList,
-                variables: {
-                    id: 2
-                }
-            },
-            result: {
-                data: {
-                    category: {
-                        id: 2,
-                        children: [
+test('renders a loading indicator', () => {
+    useCategoryList.mockReturnValueOnce({
+        loading: true
+    });
+
+    const { root } = createTestInstance(<CategoryList id={2} title="foo" />);
+
+    expect(root.findAllByType(LoadingIndicator)).toBeTruthy();
+});
+
+test('renders category tiles', () => {
+    const data = {
+        category: {
+            id: 2,
+            children: [
+                {
+                    id: 15,
+                    name: 'foo',
+                    url_key: 'foo-url.html',
+                    url_path: '/foo-url.html',
+                    children_count: 0,
+                    path: '1/2/15',
+                    image: 'media/foo.png',
+                    productImagePreview: {
+                        items: [
                             {
-                                id: 15,
-                                name: 'foo',
-                                url_key: 'foo-url.html',
-                                url_path: '/foo-url.html',
-                                children_count: 0,
-                                path: '1/2/15',
-                                image: 'media/foo.png',
-                                productImagePreview: {
-                                    items: [
-                                        {
-                                            small_image: {
-                                                url: 'media/foo-product.jpg'
-                                            }
-                                        }
-                                    ]
-                                }
-                            },
-                            {
-                                id: 16,
-                                name: 'bar',
-                                url_key: 'bar-url.html',
-                                url_path: '/bar-url.html',
-                                children_count: 0,
-                                path: '1/2/16',
-                                image: null,
-                                productImagePreview: {
-                                    items: [
-                                        {
-                                            small_image: {
-                                                url: 'media/bar-product.jpg'
-                                            }
-                                        }
-                                    ]
-                                }
-                            },
-                            {
-                                id: 17,
-                                name: 'baz',
-                                url_key: 'baz-url.html',
-                                url_path: '/baz-url.html',
-                                children_count: 0,
-                                path: '1/2/17',
-                                image: null,
-                                productImagePreview: {
-                                    items: []
+                                small_image: {
+                                    url: 'media/foo-product.jpg'
                                 }
                             }
                         ]
                     }
+                },
+                {
+                    id: 16,
+                    name: 'bar',
+                    url_key: 'bar-url.html',
+                    url_path: '/bar-url.html',
+                    children_count: 0,
+                    path: '1/2/16',
+                    image: null,
+                    productImagePreview: {
+                        items: [
+                            {
+                                small_image: {
+                                    url: 'media/bar-product.jpg'
+                                }
+                            }
+                        ]
+                    }
+                },
+                {
+                    id: 17,
+                    name: 'baz',
+                    url_key: 'baz-url.html',
+                    url_path: '/baz-url.html',
+                    children_count: 0,
+                    path: '1/2/17',
+                    image: null,
+                    productImagePreview: {
+                        items: []
+                    }
                 }
-            }
+            ]
         }
-    ];
+    };
 
-    const { root } = TestRenderer.create(
-        withRouterAndApolloClient(mocks, () => (
-            <CategoryList id={2} title="foo" />
-        ))
-    );
-
-    expect(root.findByType(LoadingIndicator)).toBeTruthy();
-
-    await waitForExpect(() => {
-        expect(root.findAllByType(CategoryTile)).toHaveLength(3);
+    useCategoryList.mockReturnValueOnce({
+        data
     });
+
+    const { root } = createTestInstance(<CategoryList id={2} title="foo" />);
+
+    expect(root.findAllByType(CategoryTile)).toHaveLength(3);
 });
