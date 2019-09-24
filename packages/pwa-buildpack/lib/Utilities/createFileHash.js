fbb28958bdd667ba75a0e307da8778548a1c2876
Revanth Kumar <revanth0212@gmail.com>
2019-09-24 12:13:44 -0500

    Refactored error handling in createFileHash.js

--- packages/pwa-buildpack/lib/Utilities/createFileHash.js
+++ packages/pwa-buildpack/lib/Utilities/createFileHash.js
@@ -4,32 +4,29 @@ const crypto = require('crypto');
 const getDefaultRandomString = () => crypto.randomBytes(8).toString('hex');
 
 function createFileHash(filePath) {
-    const result = new Promise(resolve => {
-        try {
-            if (fs.existsSync(filePath)) {
-                const fileStream = fs.createReadStream(filePath);
-                const hash = crypto.createHash('md5');
-                hash.setEncoding('hex');
+    const result = new Promise((resolve, reject) => {
+        if (fs.existsSync(filePath)) {
+            const fileStream = fs.createReadStream(filePath);
+            const hash = crypto.createHash('md5');
+            hash.setEncoding('hex');
 
-                fileStream.on('end', function() {
-                    hash.end();
-                    resolve(hash.read());
-                });
+            fileStream.on('end', function() {
+                hash.end();
+                resolve(hash.read());
+            });
 
-                fileStream.on('error', function() {
-                    hash.end();
-                    console.error(`Unable to read file ${filePath}`);
-                    resolve(getDefaultRandomString());
-                });
+            fileStream.on('error', function() {
+                hash.end();
+                reject(`Error creating hash of ${filePath}`);
+            });
 
-                fileStream.pipe(hash);
-            } else {
-                throw new Error(`Error reading file ${filePath}`);
-            }
-        } catch (err) {
-            console.error(`Unable to create hash of ${filePath}`, err);
-            resolve(getDefaultRandomString());
+            fileStream.pipe(hash);
+        } else {
+            reject(`Error reading file ${filePath}`);
         }
+    }).catch(err => {
+        console.error(err);
+        return getDefaultRandomString()
     });
 
     return result;
