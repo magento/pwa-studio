3047976d01a8e766a738cd82c44eae57fcec671e
Ievgen Kolesov <ikolesov@adobe.com>
2019-09-26 11:35:58 -0500

    PB-34: Implement hide / show functionality for all content types

--- packages/venia-ui/lib/components/RichContent/PageBuilder/ContentTypes/Image/configAggregator.js
+++ packages/venia-ui/lib/components/RichContent/PageBuilder/ContentTypes/Image/configAggregator.js
@@ -1,6 +1,7 @@
 import {
     getBorder,
     getCssClasses,
+    getIsHidden,
     getMargin,
     getPadding,
     getTextAlign
@@ -21,7 +22,8 @@ export default node => {
         ...getMargin(node),
         ...getBorder(imageNode[0]),
         ...getCssClasses(node),
-        ...getTextAlign(node)
+        ...getTextAlign(node),
+        ...getIsHidden(node)
     };
     if (props.desktopImage === props.mobileImage) {
         props.mobileImage = null;
