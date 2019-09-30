f9d3235caee88b739ab7788f06bee90627377f0d
Jimmy Sanford <jimbo@users.noreply.github.com>
2019-09-30 13:43:28 -0700

    Add SearchBar talons (#1786)
    * Add useAutocomplete
    
    * Add useSearchBar
    
    * Add useSearchField
    
    * Add useSuggestions
    
    * Add useSuggestedCategory
    
    * Fix tests
    
    * Fixup
    
    * Move error log to useQuery

---
+++ packages/peregrine/lib/talons/SearchBar/index.js
@@ -0,0 +1,5 @@
+export { useAutocomplete } from './useAutocomplete';
+export { useSearchBar } from './useSearchBar';
+export { useSearchField } from './useSearchField';
+export { useSuggestedCategory } from './useSuggestedCategory';
+export { useSuggestions } from './useSuggestions';
--- packages/venia-ui/lib/components/SearchBar/suggestedCategories.js
+++ packages/venia-ui/lib/components/SearchBar/suggestedCategories.js
@@ -1,33 +1,24 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { arrayOf, func, number, shape, string } from 'prop-types';
-import { Link } from '@magento/venia-drivers';
 
 import { mergeClasses } from '../../classify';
-import getLocation from './getLocation';
+import SuggestedCategory from './suggestedCategory';
 import defaultClasses from './suggestedCategories.css';
 
 const SuggestedCategories = props => {
     const { categories, limit, onNavigate, value } = props;
     const classes = mergeClasses(defaultClasses, props.classes);
 
-    const handleClick = useCallback(() => {
-        if (typeof onNavigate === 'function') {
-            onNavigate();
-        }
-    }, [onNavigate]);
-
     const items = categories
         .slice(0, limit)
         .map(({ label, value_string: categoryId }) => (
             <li key={categoryId} className={classes.item}>
-                <Link
-                    className={classes.link}
-                    to={getLocation(value, categoryId)}
-                    onClick={handleClick}
-                >
-                    <strong className={classes.value}>{value}</strong>
-                    <span>{` in ${label}`}</span>
-                </Link>
+                <SuggestedCategory
+                    categoryId={categoryId}
+                    label={label}
+                    onNavigate={onNavigate}
+                    value={value}
+                />
             </li>
         ));
 
@@ -49,9 +40,7 @@ SuggestedCategories.propTypes = {
     ).isRequired,
     classes: shape({
         item: string,
-        link: string,
-        root: string,
-        value: string
+        root: string
     }),
     limit: number.isRequired,
     onNavigate: func,
