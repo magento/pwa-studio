0af50567271eeec67645a34da61410fc5e83ed8e
Andrew Terranova <aterrano@adobe.com>
2019-09-30 11:18:05 -0500

    Removes getFileHash, which was reverted

--- packages/pwa-buildpack/lib/Utilities/index.js
+++ packages/pwa-buildpack/lib/Utilities/index.js
@@ -2,7 +2,6 @@ module.exports = {
     addImgOptMiddleware: require('./addImgOptMiddleware'),
     configureHost: require('./configureHost'),
     createDotEnvFile: require('./createDotEnvFile'),
-    createFileHash: require('./createFileHash'),
     createProject: require('./createProject'),
     graphQL: require('./graphQL'),
     loadEnvironment: require('./loadEnvironment')
