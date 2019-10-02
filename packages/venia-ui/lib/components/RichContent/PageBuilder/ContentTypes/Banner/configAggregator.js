5f76519da2c4548714d31ec3486897a7d345c9ed
Dave Macaulay <macaulay@adobe.com>
2019-10-02 10:03:06 -0500

    PB-17: Banner content type in Venia UI / PWA Studio
    - Complete configAggregator for banner

--- packages/venia-ui/lib/components/RichContent/PageBuilder/ContentTypes/Banner/configAggregator.js
+++ packages/venia-ui/lib/components/RichContent/PageBuilder/ContentTypes/Banner/configAggregator.js
@@ -1,19 +1,64 @@
-import {getMargin, getBackgroundImages, getBorder, getPadding} from '../../utils';
+import {
+    getMargin,
+    getBackgroundImages,
+    getBorder,
+    getPadding,
+    getTextAlign,
+    getCssClasses
+} from '../../utils';
 
-export default (node, props) => {
-    console.log(props.appearance, node);
-    const wrapper = node.querySelector('[data-element="wrapper"]');
-    const overlay = node.querySelector('[data-element="overlay"]');
-    const response = {
-        minHeight: overlay.style.minHeight ? overlay.style.minHeight : null,
+/**
+ * Determine the button type based on class
+ *
+ * @param node
+ * @returns {string}
+ */
+const getButtonType = node => {
+    if (node.classList.contains('pagebuilder-button-secondary')) {
+        return 'secondary';
+    }
+    if (node.classList.contains('pagebuilder-button-link')) {
+        return 'link';
+    }
+    return 'primary';
+};
+
+export default node => {
+    const wrapperElement = node.querySelector('[data-element="wrapper"]');
+    const overlayElement = node.querySelector('[data-element="overlay"]');
+    const linkElement = node.querySelector('a[data-element="link"]');
+    const buttonElement = node.querySelector('[data-element="button"]');
+    const showButton = node.getAttribute('data-show-button');
+    const showOverlay = node.getAttribute('data-show-overlay');
+
+    return {
+        minHeight: overlayElement.style.minHeight
+            ? overlayElement.style.minHeight
+            : null,
+        ...getBackgroundImages(wrapperElement),
         content: node.querySelector('[data-element="content"]').innerHTML,
-        showButton: node.getAttribute('data-show-button'),
-        showOverlay: node.getAttribute('data-show-overlay'),
-        ...getBackgroundImages(wrapper),
-        ...getBorder(wrapper),
+        link: linkElement ? linkElement.getAttribute('href') : null,
+        linkType: linkElement
+            ? linkElement.getAttribute('data-link-type')
+            : null,
+        showButton,
+        buttonText:
+            buttonElement && showButton !== 'never'
+                ? buttonElement.textContent
+                : null,
+        buttonType:
+            buttonElement && showButton !== 'never'
+                ? getButtonType(buttonElement)
+                : null,
+        showOverlay,
+        overlayColor:
+            overlayElement && showOverlay !== 'never'
+                ? overlayElement.getAttribute('data-overlay-color')
+                : null,
+        ...getTextAlign(wrapperElement),
+        ...getBorder(wrapperElement),
+        ...getCssClasses(node),
         ...getMargin(node),
-        ...getPadding(overlay),
+        ...getPadding(overlayElement)
     };
-    console.log(response);
-    return response;
 };
