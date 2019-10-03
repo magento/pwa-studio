e134bfa51419bf40bfc1dff8e3fc0c4aa3759e5a
Stephen Rugh <rugh@adobe.com>
2019-10-03 14:57:30 -0500

    fix test

--- packages/venia-ui/lib/components/ProductFullDetail/__tests__/productFullDetail.spec.js
+++ packages/venia-ui/lib/components/ProductFullDetail/__tests__/productFullDetail.spec.js
@@ -92,7 +92,7 @@ const mockConfigurableProduct = {
     ]
 };
 
-test('Configurable Product has correct media gallery image count', async () => {
+test('Configurable Product has correct initial media gallery image count', () => {
     const { root } = createTestInstance(
         <WindowSizeContextProvider>
             <ProductFullDetail product={mockConfigurableProduct} classes={{}} />
@@ -103,5 +103,5 @@ test('Configurable Product has correct media gallery image count', async () => {
     const carouselComponent =
         productFullDetailComponent.children[0].children[1].children[0];
 
-    expect(carouselComponent.props.images).toHaveLength(3);
+    expect(carouselComponent.props.images).toHaveLength(2);
 });
