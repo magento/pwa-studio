f3ad78caa1a3d98639323b13d8dd21d08dc03ca1
Dave Macaulay <macaulay@adobe.com>
2019-10-09 10:22:53 -0500

    PB-18: Slider content type in Venia UI / PWA Studio

--- pwa-devdocs/scripts/create-reference-docs/config/venia-concept/index.js
+++ pwa-devdocs/scripts/create-reference-docs/config/venia-concept/index.js
@@ -1,5 +1,9 @@
 module.exports = [
     {
+        target: 'venia-ui/lib/components/RichContent/PageBuilder/ContentTypes/Slider/slider.js',
+        type: 'function'
+    },
+    {
         target: 'venia-ui/lib/components/RichContent/PageBuilder/ContentTypes/Text/text.js',
         type: 'function'
     },
