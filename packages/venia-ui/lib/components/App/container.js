68e58efa37de10f714531555f78844e6d3d61e35
Stephen Rugh <sirugh@users.noreply.github.com>
2019-10-09 12:18:58 -0500

    adds app talon (#1828)
    * adds app talon
    
    * Move creation of error dismisser into talon
    
    * Move isOpen to internal miniCart state instead of prop
    
    * Add comments and improve error return jsx

--- packages/venia-ui/lib/components/App/container.js
+++ packages/venia-ui/lib/components/App/container.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useAppContext } from '@magento/peregrine/lib/context/app';
 import { useErrorContext } from '@magento/peregrine/lib/context/unhandledErrors';
 
 import App from './app';
@@ -7,17 +6,9 @@ import { useErrorBoundary } from './useErrorBoundary';
 
 const AppContainer = () => {
     const ErrorBoundary = useErrorBoundary(App);
-    const [appState, appApi] = useAppContext();
     const [unhandledErrors, errorApi] = useErrorContext();
 
-    return (
-        <ErrorBoundary
-            unhandledErrors={unhandledErrors}
-            app={appState}
-            {...appApi}
-            {...errorApi}
-        />
-    );
+    return <ErrorBoundary unhandledErrors={unhandledErrors} {...errorApi} />;
 };
 
 export default AppContainer;
