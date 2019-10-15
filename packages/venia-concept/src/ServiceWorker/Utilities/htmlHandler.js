af6939537658e3fb1581f4e1e5cbbd52eb463c92
Revanth Kumar <revanth0212@gmail.com>
2019-10-15 12:33:08 -0500

    Implemented HTML content checking.

--- packages/venia-concept/src/ServiceWorker/Utilities/htmlHandler.js
+++ packages/venia-concept/src/ServiceWorker/Utilities/htmlHandler.js
@@ -12,6 +12,20 @@ export const cacheHTMLPlugin = {
     },
     requestWillFetch: async ({ request }) => {
         const newRequest = cloneRequestWithDiffURL(request, '/');
+
         return newRequest;
+    },
+    fetchDidSucceed: async ({ response }) => {
+        const cachedResponseObj = await caches.match('/');
+        if (cachedResponseObj) {
+            const cachedResponse = await cachedResponseObj.text();
+            const clonedResponse = await response.clone().text();
+            if (cachedResponse !== clonedResponse) {
+                console.log('responses are different');
+            } else {
+                console.log('responses are same');
+            }
+        }
+        return response;
     }
 };
