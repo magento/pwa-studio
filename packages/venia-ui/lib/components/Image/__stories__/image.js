a10248f759d87d86bbdc510053d329348e2ff998
Andrew Terranova <aterrano@adobe.com>
2019-10-16 08:15:32 -0500

    Updates Image storybook stories

--- packages/venia-ui/lib/components/Image/__stories__/image.js
+++ packages/venia-ui/lib/components/Image/__stories__/image.js
@@ -1,29 +1,57 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { transparentPlaceholder } from '@magento/peregrine/lib/util/images';
 
-import Image from '../image';
 import '../../../index.css';
-
+import Image from '../image';
 import classes from './image.css';
+
 const stories = storiesOf('Image', module);
 
-stories.add('An Image with a placeholder (throttle me)', () => (
+stories.add('An Image using a direct src', () => (
     <div className={classes.container}>
         <Image
-            alt={'An image with a placeholder'}
+            alt="An Image using a direct src"
             classes={{ root: classes.root }}
-            placeholder={transparentPlaceholder}
             src={'https://www.fillmurray.com/400/600'}
         />
     </div>
 ));
 
-stories.add('An Image without a placeholder', () => (
+stories.add('An Image with a custom placeholder (throttle me)', () => (
     <div className={classes.container}>
         <Image
-            alt={'An image without a placeholder'}
+            alt="An Image with a custom placeholder (throttle me)"
+            classes={{ root: classes.root }}
+            placeholder={'https://via.placeholder.com/400x600'}
             src={'https://www.fillmurray.com/400/600'}
         />
     </div>
 ));
+
+stories.add(
+    'An Image using a Magento resource (resize the viewport + view network)',
+    () => (
+        <div className={classes.container}>
+            <Image
+                alt="An Image with a custom placeholder (throttle me)"
+                classes={{ root: classes.root }}
+                resource="timeless.jpg"
+            />
+        </div>
+    )
+);
+
+stories.add(
+    'An Image using a Magento resource with resource constraints',
+    () => (
+        <div className={classes.container}>
+            <Image
+                alt="An Image with a custom placeholder (throttle me)"
+                classes={{ root: classes.root }}
+                resource="timeless.jpg"
+                resourceHeight="100"
+                resourceWidth="80"
+            />
+        </div>
+    )
+);
