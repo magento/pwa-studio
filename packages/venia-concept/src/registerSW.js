726dad2d482702f3ac98427f424b1958750c7030
Revanth Kumar <revanth0212@gmail.com>
2019-10-17 15:59:19 -0500

    Removed workbox window dependency.

--- packages/venia-concept/src/registerSW.js
+++ packages/venia-concept/src/registerSW.js
@@ -18,27 +18,20 @@ export const registerSW = () => {
         process.env.NODE_ENV === 'production' ||
         process.env.DEV_SERVER_SERVICE_WORKER_ENABLED
     ) {
-        import('workbox-window')
-            .then(({ Workbox }) => {
-                const wb = new Workbox('/sw.js');
-
-                navigator.serviceWorker.addEventListener('message', e => {
-                    const { type, payload } = e.data;
-                    handleMessageFromSW(type, payload, e);
-                });
-
-                wb.register()
-                    .then(() => {
-                        window.console.log('SW Registered');
-                    })
-                    .catch(() => {
-                        window.console.warn('Failed to register SW.');
-                    });
-
-                registerMessageHandlers();
+        navigator.serviceWorker
+            .register('/sw.js')
+            .then(() => {
+                window.console.log('SW Registered');
             })
             .catch(() => {
-                window.console.warn('Failed to load Workbox.');
+                window.console.warn('Failed to register SW.');
             });
+
+        navigator.serviceWorker.addEventListener('message', e => {
+            const { type, payload } = e.data;
+            handleMessageFromSW(type, payload, e);
+        });
+
+        registerMessageHandlers();
     }
 };
