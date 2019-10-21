e93720805d40529daa8691c09afcc6e346e03f6b
Revanth Kumar <revanth0212@gmail.com>
2019-10-21 14:05:20 -0500

    More JSDocs.

--- packages/venia-concept/src/ServiceWorker/Utilities/htmlHandler.js
+++ packages/venia-concept/src/ServiceWorker/Utilities/htmlHandler.js
@@ -10,16 +10,33 @@ const cloneRequestWithDiffURL = (request, url) =>
           })
         : request;
 
+/**
+ * cacheHTMLPlugin is a workbox plugin that will apply request
+ * and reponse manipulations on HTML routes.
+ */ 
 export const cacheHTMLPlugin = {
     cacheKeyWillBeUsed: async () => {
+        /**
+         * For all read and writes that arrive on any HTML route
+         * return `/`.
+         */
         return '/';
     },
     requestWillFetch: async ({ request }) => {
+        /**
+         * Clone the request with url as `/`.
+         */
         const newRequest = cloneRequestWithDiffURL(request, '/');
 
         return newRequest;
     },
     fetchDidSucceed: async ({ request, response }) => {
+        /**
+         * Check if there is a response cached for the request
+         * URL. If yes, compare the content of both the cached
+         * response and the new response. If they are different
+         * send a the `HTML_UPDATE_AVAILABLE` message to window.
+         */
         const cachedResponseObj = await caches.match(
             new URL(request.url).pathname
         );
