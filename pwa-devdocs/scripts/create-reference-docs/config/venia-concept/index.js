8ab053e27a94bcc33d025caf42d6b45e18e45d18
James Calcaben <calcaben@adobe.com>
2019-10-29 11:14:04 -0500

    Add Button component reference docs

--- pwa-devdocs/scripts/create-reference-docs/config/venia-concept/index.js
+++ pwa-devdocs/scripts/create-reference-docs/config/venia-concept/index.js
@@ -44,6 +44,10 @@ module.exports = [
         type: 'function'
     },
     {
+        target: 'venia-ui/lib/components/Button/button.js',
+        type: 'function'
+    },
+    {
         target: 'venia-ui/lib/components/RichContent/PageBuilder/ContentTypes/Video/video.js',
         type: 'function'
     },
---
+++ pwa-devdocs/src/venia-ui/reference/components/Button/index.md
@@ -0,0 +1,10 @@
+---
+title: Button
+---
+
+<!--
+The reference doc content is generated automatically from the source code.
+To update this section, update the doc blocks in the source code
+-->
+
+{% include auto-generated/venia-ui/lib/components/Button/button.md %}
