0512dacbe0ef01ed1021f60babe987f7fe64d00d
Andrew Terranova <aterrano@adobe.com>
2019-11-08 08:38:49 -0600

    Improves logic flow of useProduct talon

--- packages/peregrine/lib/talons/RootComponents/Product/useProduct.js
+++ packages/peregrine/lib/talons/RootComponents/Product/useProduct.js
@@ -1,5 +1,5 @@
-import { useEffect, useMemo } from 'react';
-import { useApolloClient, useLazyQuery } from '@apollo/react-hooks';
+import { useMemo } from 'react';
+import { useApolloClient, useQuery } from '@apollo/react-hooks';
 
 /**
  * A [React Hook]{@link https://reactjs.org/docs/hooks-intro.html} that
@@ -22,30 +22,21 @@ import { useApolloClient, useLazyQuery } from '@apollo/react-hooks';
 export const useProduct = props => {
     const { cachePrefix, fragment, mapProduct, query, urlKey } = props;
 
-    const [runQuery, { loading, error, data }] = useLazyQuery(query, {
-        // We're manually checking the cache here, so useLazyQuery can skip that.
-        fetchPolicy: 'network-only',
-        variables: {
-            onServer: false,
-            urlKey
-        }
-    });
-
     const apolloClient = useApolloClient();
 
+    /*
+     * Look up the product in cache first.
+     *
+     * We may have it from a previous query, but we have to manually tell Apollo cache where to find it.
+     * A single product query (as described by https://github.com/magento/graphql-ce/issues/86)
+     * will alleviate the need to do this manual work.
+     *
+     * If the object with the specified `id` is not in the cache, we get `null`.
+     * If the object is in the cache but doesn't have all the fields we need, an error is thrown.
+     *
+     * @see https://www.apollographql.com/docs/react/caching/cache-interaction/#readfragment.
+     */
     const productFromCache = useMemo(() => {
-        /*
-         * Look up the product in cache first.
-         *
-         * We may have it from a previous query, but we have to manually tell Apollo cache where to find it.
-         * A single product query (as described by https://github.com/magento/graphql-ce/issues/86)
-         * will alleviate the need to do this manual work.
-         *
-         * If the object with the specified `id` is not in the cache, we get `null`.
-         * If the object is in the cache but doesn't have all the fields we need, an error is thrown.
-         *
-         * @see https://www.apollographql.com/docs/react/caching/cache-interaction/#readfragment.
-         */
         try {
             return apolloClient.readFragment({
                 id: `${cachePrefix}:${urlKey}`,
@@ -57,6 +48,24 @@ export const useProduct = props => {
             return null;
         }
     }, [apolloClient, cachePrefix, fragment, urlKey]);
+
+    /*
+     * Fetch the product from the network.
+     *
+     * Note that this always fires, regardless of whether we have a cached product or not:
+     * If we do not have a cached product, we need to go fetch it from the network.
+     * If we do have a cached product, we want to ensure its cache entry doesn't get stale.
+     * In both cases, Apollo will update the cache with the latest data when this returns.
+     */
+    const { loading, error, data } = useQuery(query, {
+        // Once we're able to remove the manual cache lookup,
+        // this fetch policy can change to 'cache-and-network'.
+        fetchPolicy: 'network-only',
+        variables: {
+            onServer: false,
+            urlKey
+        }
+    });
 
     const product = useMemo(() => {
         if (productFromCache) {
@@ -71,20 +80,6 @@ export const useProduct = props => {
         // The product isn't in the cache and we don't have a response from GraphQL yet.
         return null;
     }, [data, mapProduct, productFromCache]);
-
-    useEffect(() => {
-        // Fetch the product from the network.
-        // Note that this always fires, regardless of whether we have a cached product or not:
-        // If we do not have a cached product, we need to go fetch it from the network.
-        // If we do have a cached product, we want to ensure its cache entry doesn't get stale.
-        // In both cases, Apollo will update the cache with the latest data when this returns.
-        runQuery({
-            variables: {
-                onServer: false,
-                urlKey
-            }
-        });
-    }, [runQuery, urlKey]);
 
     return {
         error,
