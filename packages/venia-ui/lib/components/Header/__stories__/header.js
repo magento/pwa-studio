45eeffa7eba433da23327802c2e3123acd265bb3
Andrew Terranova <aterrano@adobe.com>
2019-11-18 11:42:09 -0600

    Fixes Header component stories

--- packages/venia-ui/lib/components/Header/__stories__/header.js
+++ packages/venia-ui/lib/components/Header/__stories__/header.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
+import { useAppContext } from '@magento/peregrine/lib/context/app';
+
 import Header from '../header';
 import defaultClasses from '../header.css';
 
-const stories = storiesOf('Header', module);
-const noop = () => {};
+const stories = storiesOf('App/Header', module);
 
-stories.add('Search Bar Closed', () => (
-    <Header classes={defaultClasses} searchOpen={false} toggleSearch={noop} />
-));
+stories.add('Search Bar Closed', () => <Header classes={defaultClasses} />);
 
-stories.add('Search Bar Open', () => (
-    <Header classes={defaultClasses} searchOpen={true} toggleSearch={noop} />
-));
+stories.add('Search Bar Open', () => {
+    // Stories are wrapped in AppContextProvider in `./storybook/config.js`.
+    const Wrapper = () => {
+        const [, api] = useAppContext();
+        const { toggleSearch } = api;
+
+        // Open the search
+        toggleSearch();
+
+        return <Header classes={defaultClasses} />;
+    };
+
+    return <Wrapper />;
+});
