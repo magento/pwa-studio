1eb9dbc29b22766044c8c78fc0d5949cb0a41103
Shikha Mishra <shikhamishra@cedcoss.com>
2019-11-19 21:42:35 +0530

    Fixed #1919 Product price is not changing while changing configurable options (#1964)
    * Added Price for configurable options
    * Fixed #1919 Product price is not changing while changing configurable options
    * Added config variations price

--- packages/peregrine/lib/talons/ProductFullDetail/useProductFullDetail.js
+++ packages/peregrine/lib/talons/ProductFullDetail/useProductFullDetail.js
@@ -113,6 +113,33 @@ const getBreadcrumbCategoryId = categories => {
     return leafCategory.id || categories[0].id;
 };
 
+const getConfigPrice = (product, optionCodes, optionSelections) => {
+    let value;
+
+    const { variants } = product;
+    const isConfigurable = isProductConfigurable(product);
+
+    const optionsSelected =
+        Array.from(optionSelections.values()).filter(value => !!value).length >
+        0;
+
+    if (!isConfigurable || !optionsSelected) {
+        value = product.price.regularPrice.amount;
+    } else {
+        const item = findMatchingVariant({
+            optionCodes,
+            optionSelections,
+            variants
+        });
+
+        value = item
+            ? item.product.price.regularPrice.amount
+            : product.price.regularPrice.amount;
+    }
+
+    return value;
+};
+
 export const useProductFullDetail = props => {
     const { product } = props;
 
@@ -181,11 +208,16 @@ export const useProductFullDetail = props => {
         [setQuantity]
     );
 
+    const productPrice = useMemo(
+        () => getConfigPrice(product, optionCodes, optionSelections),
+        [product, optionCodes, optionSelections]
+    );
+
     // Normalization object for product details we need for rendering.
     const productDetails = {
         description: product.description,
         name: product.name,
-        price: product.price.regularPrice.amount,
+        price: productPrice,
         sku: product.sku
     };
 
