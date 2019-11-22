8c8d988752198377a1d144c022b58bb17351dc1f
Stephen Rugh <rugh@adobe.com>
2019-11-22 16:33:30 -0600

    IDEATION: Can we do this

--- packages/peregrine/lib/context/cart.js
+++ packages/peregrine/lib/context/cart.js
@@ -1,14 +1,25 @@
-import React, { createContext, useContext, useMemo } from 'react';
+import React, { createContext, useCallback, useContext, useMemo } from 'react';
 import { connect } from 'react-redux';
+
+import { useMutation } from '@apollo/react-hooks';
 
 import actions from '../store/actions/cart/actions';
 import * as asyncActions from '../store/actions/cart/asyncActions';
 import bindActionCreators from '../util/bindActionCreators';
+import gql from 'graphql-tag';
 
 const CartContext = createContext();
 
 const isCartEmpty = cart =>
     !cart || !cart.details.items || cart.details.items.length === 0;
+
+const createCartMutation = gql`
+    # This mutation will return a masked cart id. If a bearer token is provided for
+    # a logged in user it will return the cart id for that user.
+    mutation createCart {
+        cartId: createEmptyCart
+    }
+`;
 
 const CartContextProvider = props => {
     const { actions, asyncActions, cartState, children } = props;
@@ -18,12 +29,22 @@ const CartContextProvider = props => {
         isEmpty: isCartEmpty(cartState)
     };
 
+    const [getCartId] = useMutation(createCartMutation);
+
+    const createCart = useCallback(async () => {
+        const {
+            data: { cartId }
+        } = await getCartId();
+        await asyncActions.setCartId(cartId);
+    }, [asyncActions, getCartId]);
+
     const cartApi = useMemo(
         () => ({
             actions,
-            ...asyncActions
+            ...asyncActions,
+            createCart
         }),
-        [actions, asyncActions]
+        [actions, asyncActions, createCart]
     );
 
     const contextValue = useMemo(() => [derivedCartState, cartApi], [
