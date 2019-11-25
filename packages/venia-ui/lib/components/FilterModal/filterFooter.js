b81f4adb9e79ea8581c09b588111558d5add7aa3
Gaurav Agarwal <37572719+gauravagarwal1001@users.noreply.github.com>
2019-11-25 19:39:22 +0530

    Update changes after running prettier cmd

--- packages/venia-ui/lib/components/FilterModal/filterFooter.js
+++ packages/venia-ui/lib/components/FilterModal/filterFooter.js
@@ -16,7 +16,11 @@ const FilterFooter = props => {
             <Button disabled={!hasFilters} onClick={resetFilters}>
                 {'Reset Filters'}
             </Button>
-            <Button disabled={!touched || !hasFilters} onClick={applyFilters} priority="high">
+            <Button
+                disabled={!touched || !hasFilters}
+                onClick={applyFilters}
+                priority="high"
+            >
                 {'Apply Filters'}
             </Button>
         </div>
