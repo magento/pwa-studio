5cbc333c2dce7c892d75927754186d8c6dd12dd5
Andrew Terranova <aterrano@adobe.com>
2019-11-25 07:44:11 -0600

    Simplifies useForm by removing state and effect hooks

--- packages/peregrine/lib/talons/Checkout/useForm.js
+++ packages/peregrine/lib/talons/Checkout/useForm.js
@@ -1,18 +1,11 @@
-import { useEffect, useState } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 
 export const useForm = props => {
     const { countriesQuery } = props;
 
-    const [countries, setCountries] = useState(null);
+    const { data } = useQuery(countriesQuery);
 
-    const { data: countriesData } = useQuery(countriesQuery);
-
-    useEffect(() => {
-        if (countriesData) {
-            setCountries(countriesData.countries);
-        }
-    }, [countriesData]);
+    const { countries } = data;
 
     return {
         countries
