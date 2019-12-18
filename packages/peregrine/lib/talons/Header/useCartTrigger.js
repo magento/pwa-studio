5e123a0d85a8327d3e68b28930b3810d59e3eb2d
Stephen Rugh <rugh@adobe.com>
2019-12-18 16:41:48 -0600

    spike off spike -- make fetch for specific details within a component

--- packages/peregrine/lib/talons/Header/useCartTrigger.js
+++ packages/peregrine/lib/talons/Header/useCartTrigger.js
@@ -1,15 +1,46 @@
-import { useCallback } from 'react';
-import { useCartContext } from '@magento/peregrine/lib/context/cart';
+import { useCallback, useEffect } from 'react';
+import { useLazyQuery } from '@apollo/react-hooks';
+
 import { useAppContext } from '@magento/peregrine/lib/context/app';
+import { useCartContext } from '@magento/peregrine/lib/context/cart';
+
+import gql from 'graphql-tag';
+
+const GET_ITEM_QUANTITY_GQL = gql`
+    query getCartDetails($cartId: String!) {
+        cart(cart_id: $cartId) {
+            items {
+                id
+                quantity
+            }
+        }
+    }
+`;
+
+const getTotalQuantity = items =>
+    items.reduce((total, item) => total + item.quantity, 0);
 
 export const useCartTrigger = () => {
     const [, { toggleDrawer }] = useAppContext();
-    const [{ derivedDetails }] = useCartContext();
-    const { numItems: itemCount } = derivedDetails;
+    const [{ cartId }] = useCartContext();
+    const [getItemQuantities, { data }] = useLazyQuery(GET_ITEM_QUANTITY_GQL);
+
+    useEffect(() => {
+        if (cartId) {
+            getItemQuantities({
+                variables: { cartId }
+            });
+        }
+    }, [cartId, getItemQuantities]);
 
     const handleClick = useCallback(async () => {
         toggleDrawer('cart');
     }, [toggleDrawer]);
+
+    const itemCount =
+        data && data.cart && data.cart.items
+            ? getTotalQuantity(data.cart.items)
+            : 0;
 
     return {
         handleClick,
