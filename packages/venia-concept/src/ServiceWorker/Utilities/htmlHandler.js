20ffcfa789088e890dafea38e703c3222a00afb8
Revanth Kumar <revanth0212@gmail.com>
2020-01-06 15:26:40 -0600

    Using html parser to validate changes to html.

--- packages/venia-concept/src/ServiceWorker/Utilities/htmlHandler.js
+++ packages/venia-concept/src/ServiceWorker/Utilities/htmlHandler.js
@@ -1,6 +1,118 @@
 import { HTML_UPDATE_AVAILABLE } from '@magento/venia-ui/lib/constants/swMessageTypes';
+import { parse } from 'node-html-parser';
 
 import { sendMessageToWindow } from './messageHandler';
+
+const generateScriptResource = htmlElement => htmlElement.attributes.src;
+
+const generateLinkResource = htmlElement => htmlElement.attributes.href;
+
+const generateStyleResource = htmlElement => htmlElement.text;
+
+/**
+ * Generates list of resources from the parsed HTML object.
+ *
+ * @param {HTMLElement} parsedHTML
+ *
+ * @returns { scripts: [string], links: [string], styles: [string] }
+ */
+const generateResources = parsedHTML => {
+    const resources = {};
+
+    resources.scripts = parsedHTML
+        .querySelectorAll('script')
+        .map(generateScriptResource)
+        .sort();
+
+    resources.links = parsedHTML
+        .querySelectorAll('link')
+        .map(generateLinkResource)
+        .sort();
+
+    resources.styles = parsedHTML
+        .querySelectorAll('style')
+        .map(generateStyleResource)
+        .sort();
+
+    return resources;
+};
+
+/**
+ * Returns true if resouce url/urls have changed.
+ *
+ * @param {HTMLElement} newDOM
+ * @param {HTMLElement} oldDOM
+ *
+ * @returns Boolean
+ */
+const haveResourcesChanged = (newDOM, oldDOM) => {
+    const newResources = generateResources(newDOM);
+    const oldResources = generateResources(oldDOM);
+    const keysToCompare = Object.keys(newResources);
+
+    return !keysToCompare.reduce((acc, key) => {
+        const hasResourceChanged =
+            newResources[key].toString() === oldResources[key].toString();
+
+        return acc && hasResourceChanged;
+    }, true);
+};
+
+/**
+ * Returns true if title has changed.
+ *
+ * @param {HTMLElement} newDOM
+ * @param {HTMLElement} oldDOM
+ *
+ * @returns Boolean
+ */
+const hasTitleChanged = (newDOM, oldDOM) => {
+    const oldTitle = oldDOM.querySelector('title').text;
+    const newTitle = newDOM.querySelector('title').text;
+
+    return newTitle !== oldTitle;
+};
+
+/**
+ * Returns true if media backend url has changed.
+ *
+ * @param {HTMLElement} newDOM
+ * @param {HTMLElement} oldDOM
+ *
+ * @returns Boolean
+ */
+const hasBackendUrlChanged = (newDOM, oldDOM) => {
+    const oldUrl = oldDOM.querySelector('html').attributes[
+        'data-media-backend'
+    ];
+    const newUrl = newDOM.querySelector('html').attributes[
+        'data-media-backend'
+    ];
+
+    return newUrl !== oldUrl;
+};
+
+/**
+ * Array of verfication functions to run to determine
+ * if HTML has changed and needs updation on the UI.
+ */
+const verificationFunctions = [
+    haveResourcesChanged,
+    hasTitleChanged,
+    hasBackendUrlChanged
+];
+
+/**
+ * Runs all verification functions with new and old
+ * HTML to determine if HTML has changed.
+ *
+ * @param {HTMLElement} newDOM
+ * @param {HTMLElement} oldDOM
+ */
+const hasHTMLChanged = (newDOM, oldDOM) =>
+    verificationFunctions.reduce((acc, fn) => {
+        return acc || fn(newDOM, oldDOM);
+    }, false);
 
 const cloneRequestWithDiffURL = (request, url) =>
     url
@@ -35,7 +147,7 @@ export const cacheHTMLPlugin = {
          * Check if there is a response cached for the request
          * URL. If yes, compare the content of both the cached
          * response and the new response. If they are different
-         * send a the `HTML_UPDATE_AVAILABLE` message to window.
+         * send the `HTML_UPDATE_AVAILABLE` message to window.
          */
         const cachedResponseObj = await caches.match(
             new URL(request.url).pathname
@@ -43,7 +155,13 @@ export const cacheHTMLPlugin = {
         if (cachedResponseObj) {
             const cachedResponse = await cachedResponseObj.text();
             const clonedResponse = await response.clone().text();
-            if (cachedResponse !== clonedResponse) {
+
+            if (
+                hasHTMLChanged(
+                    parse(clonedResponse, { style: true }),
+                    parse(cachedResponse, { style: true })
+                )
+            ) {
                 sendMessageToWindow(HTML_UPDATE_AVAILABLE);
             }
         }
