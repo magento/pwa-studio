f010ed9ec9ec0529373b4b5094013b1c5f7d7115
Andrew Terranova <aterrano@adobe.com>
2020-01-07 14:26:44 -0600

    Sticky order summary in desktop view

--- packages/venia-ui/lib/components/CartPage/cartPage.js
+++ packages/venia-ui/lib/components/CartPage/cartPage.js
@@ -8,7 +8,6 @@ import { mergeClasses } from '../../classify';
 import defaultClasses from './cartPage.css';
 
 const CartPage = props => {
-
     const { handleSignIn, isSignedIn } = useCartPage();
 
     const classes = mergeClasses(defaultClasses, props.classes);
@@ -27,22 +26,35 @@ const CartPage = props => {
         <div className={classes.root}>
             <div className={classes.heading_container}>
                 <h1 className={classes.heading}>Cart</h1>
-                { signInDisplay }
+                {signInDisplay}
             </div>
-            <div className={classes.items_container}>
-                <a href="https://jira.corp.magento.com/browse/PWA-238">Items List to be completed by PWA-238</a>.
-            </div>
-            <div className={classes.price_adjustments_container}>
-                <a href="https://jira.corp.magento.com/browse/PWA-241">Price Adjustments to be completed by PWA-241</a>.
-            </div>
-            <div className={classes.summary_container}>
-                <a href="https://jira.corp.magento.com/browse/PWA-240">Order Summary to be completed by PWA-240</a>.
-            </div>
-            <div className={classes.recently_viewed_container}>
-                Recently Viewed TBD. Should show only in Desktop.
+            <div className={classes.body}>
+                <div className={classes.items_container}>
+                    <a href="https://jira.corp.magento.com/browse/PWA-238">
+                        Items List to be completed by PWA-238
+                    </a>
+                    .
+                </div>
+                <div className={classes.price_adjustments_container}>
+                    <a href="https://jira.corp.magento.com/browse/PWA-241">
+                        Price Adjustments to be completed by PWA-241
+                    </a>
+                    .
+                </div>
+                <div className={classes.summary_container}>
+                    <div className={classes.summary_contents}>
+                        <a href="https://jira.corp.magento.com/browse/PWA-240">
+                            Order Summary to be completed by PWA-240
+                        </a>
+                        .
+                    </div>
+                </div>
+                <div className={classes.recently_viewed_container}>
+                    Recently Viewed TBD.
+                </div>
             </div>
         </div>
     );
-}
+};
 
 export default CartPage;
