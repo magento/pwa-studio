30482118bf6ffab9d1571bacb9242849bcdef10f
Andy Terranova <13182778+supernova-at@users.noreply.github.com>
2020-01-08 15:34:27 -0600

    Venia v2 Cart Page (#2084)
    * Adds Cart Page
    
    * Hooks up Sign In button
    
    * Sticky order summary in desktop view
    
    * Unit tests useCartPage talon
    
    * Adds link to JIRA for Recently Viewed component
    
    * Adds a comment to cartPage CSS
    
    * Runs prettier
    
    * Adds useMemo for sign in display
    
    * Updates styles in accordance with styleguide
    
    * Order summary sticks directly underneath header

---
+++ packages/peregrine/lib/talons/CartPage/useCartPage.js
@@ -0,0 +1,20 @@
+import { useCallback } from 'react';
+
+import { useAppContext } from '../../context/app';
+import { useUserContext } from '../../context/user';
+
+export const useCartPage = () => {
+    const [, appApi] = useAppContext();
+    const { toggleDrawer } = appApi;
+    const [{ isSignedIn }] = useUserContext();
+
+    const handleSignIn = useCallback(() => {
+        // TODO: set navigation state to "SIGN_IN". useNavigation:showSignIn doesn't work.
+        toggleDrawer('nav');
+    }, [toggleDrawer]);
+
+    return {
+        handleSignIn,
+        isSignedIn
+    };
+};
