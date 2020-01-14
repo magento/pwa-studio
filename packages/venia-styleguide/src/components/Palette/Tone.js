ee11af794911723b6068d0530333f845ca8868f7
Jimmy Sanford <jimbo@users.noreply.github.com>
2020-01-14 11:34:51 -0700

    Extract contrast utils

--- packages/venia-styleguide/src/components/Palette/Tone.js
+++ packages/venia-styleguide/src/components/Palette/Tone.js
@@ -1,23 +1,8 @@
 import React, { useMemo } from 'react';
 
+import { getHex, getLightness } from '../../util/contrastUtils';
 import finalizeClasses from '../../util/finalizeClasses';
 import classes from './Tone.css';
-
-const getHex = channel => parseInt(channel).toString(16);
-
-const getSRGB = channel => {
-    const factor = channel / 255;
-
-    return factor <= 0.03928
-        ? factor / 12.92
-        : Math.pow((factor + 0.055) / 1.055, 2.4);
-};
-
-const getLightness = channels => {
-    const [r, g, b] = channels.map(getSRGB);
-
-    return r * 0.2126 + g * 0.7152 + b * 0.0722;
-};
 
 const WHITE = getLightness([255, 255, 255]);
 
@@ -25,6 +10,7 @@ const Tone = props => {
     const { hue, tone, value } = props;
     const style = { '--tone': value };
 
+    // determine whether a value is light or dark
     const channels = useMemo(() => value.split(' '), [value]);
     const lightness = getLightness(channels);
     const ratio = WHITE / (lightness + 0.05);
