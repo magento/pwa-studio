f88a2482fae569105b0a5c864e9c3a561b1d1674
Revanth Kumar <revanth0212@gmail.com>
2020-01-14 10:28:28 -0600

    Minor.

--- packages/peregrine/lib/util/objectAccessor.js
+++ packages/peregrine/lib/util/objectAccessor.js
@@ -1,35 +1,35 @@
 function isNull(val) {
-  return val === null;
+    return val === null;
 }
 
 function isUndefined(val) {
-  return val === undefined;
+    return val === undefined;
 }
 
 function or(fn1, fn2) {
-  return function(val) {
-    return fn1(val) || fn2(val);
-  };
+    return function(val) {
+        return fn1(val) || fn2(val);
+    };
 }
 
 function isNullOrUndefined(val) {
-  return or(isNull, isUndefined)(val);
+    return or(isNull, isUndefined)(val);
 }
 
 export function pathOr(backup, keyArray, obj) {
-  const [key, ...rest] = keyArray;
-  const result = obj[key];
-  if (rest.length) {
-    if (result instanceof Object) {
-      return pathOr(backup, rest, result);
+    const [key, ...rest] = keyArray;
+    const result = obj[key];
+    if (rest.length) {
+        if (result instanceof Object) {
+            return pathOr(backup, rest, result);
+        } else {
+            return backup;
+        }
     } else {
-      return backup;
+        return isNullOrUndefined(result) ? backup : result;
     }
-  } else {
-    return isNullOrUndefined(result) ? backup : result;
-  }
 }
 
 export function propOr(backup, key, obj) {
-  return pathOr(backup, [key], obj);
+    return pathOr(backup, [key], obj);
 }
