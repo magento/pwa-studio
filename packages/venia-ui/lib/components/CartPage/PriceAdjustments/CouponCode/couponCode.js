b66be3a8a6a897059ccc9c4f4961b9a9fbb10227
Stephen Rugh <rugh@adobe.com>
2020-01-22 09:33:13 -0600

    Remove unused styles
    Signed-off-by: Stephen Rugh <rugh@adobe.com>

--- packages/venia-ui/lib/components/CartPage/PriceAdjustments/CouponCode/couponCode.js
+++ packages/venia-ui/lib/components/CartPage/PriceAdjustments/CouponCode/couponCode.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import gql from 'graphql-tag';
 
@@ -78,8 +78,8 @@ const CouponCode = props => {
     if (data.cart.applied_coupons) {
         const codes = data.cart.applied_coupons.map(({ code }) => {
             return (
-                <span className={classes.appliedCoupon} key={code}>
-                    <span className={classes.code}>{code}</span>
+                <Fragment key={code}>
+                    <span>{code}</span>
                     <button
                         className={classes.removeButton}
                         disabled={removingCoupon}
@@ -89,11 +89,11 @@ const CouponCode = props => {
                     >
                         Remove
                     </button>
-                </span>
+                </Fragment>
             );
         });
 
-        return <div className={classes.root}>{codes}</div>;
+        return <div>{codes}</div>;
     } else {
         return (
             <Form className={classes.entryForm} onSubmit={handleApplyCoupon}>
