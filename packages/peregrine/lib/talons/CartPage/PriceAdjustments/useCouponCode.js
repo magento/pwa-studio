4ffd33b766a5258ee9aef928ae4e10b74de2cf8a
Stephen <sirugh@users.noreply.github.com>
2020-01-29 10:23:08 -0600

    [feature]: Cart Coupons (#2108)
    * Inital coupon code impl
    
    Signed-off-by: Stephen Rugh <rugh@adobe.com>
    
    * Use fragments in requeest body instead of refetchQueries to prevent overfetch
    
    Signed-off-by: Stephen Rugh <rugh@adobe.com>
    
    * Use empty space for correct sizing in desktop mode
    
    Signed-off-by: Stephen Rugh <rugh@adobe.com>
    
    * Create coupon code talon
    
    Signed-off-by: Stephen Rugh <rugh@adobe.com>
    
    * add default field spacing
    
    Signed-off-by: Stephen Rugh <rugh@adobe.com>
    
    * Adjust accordion section padding
    
    * Remove unused styles
    
    Signed-off-by: Stephen Rugh <rugh@adobe.com>
    
    * Add tests for coupon code
    
    Signed-off-by: Stephen Rugh <rugh@adobe.com>
    
    * Address UX feedback: Remove parens around negative summary values. Use venia text variable for removal button.
    
    Signed-off-by: Stephen Rugh <rugh@adobe.com>
    
    * Fix snaps
    
    Signed-off-by: Stephen Rugh <rugh@adobe.com>
    
    * Add applied coupons to cart page fragment
    
    Signed-off-by: Stephen Rugh <rugh@adobe.com>
    
    Co-authored-by: Jimmy Sanford <jimbo@users.noreply.github.com>
    Co-authored-by: Devagouda <40405790+dpatil-magento@users.noreply.github.com>

---
+++ packages/peregrine/lib/talons/CartPage/PriceAdjustments/useCouponCode.js
@@ -0,0 +1,81 @@
+import { useCallback, useEffect } from 'react';
+import { useLazyQuery, useMutation } from '@apollo/react-hooks';
+import { useCartContext } from '@magento/peregrine/lib/context/cart';
+
+export const useCouponCode = props => {
+    const {
+        applyCouponMutation,
+        getAppliedCouponsQuery,
+        removeCouponMutation
+    } = props;
+
+    const [{ cartId }] = useCartContext();
+    const [fetchAppliedCoupons, { data, error: fetchError }] = useLazyQuery(
+        getAppliedCouponsQuery,
+        {
+            fetchPolicy: 'cache-and-network'
+        }
+    );
+
+    const [
+        applyCoupon,
+        { error: applyError, loading: applyingCoupon }
+    ] = useMutation(applyCouponMutation);
+
+    const [removeCoupon, { loading: removingCoupon }] = useMutation(
+        removeCouponMutation
+    );
+
+    const handleApplyCoupon = useCallback(
+        async ({ couponCode }) => {
+            if (!couponCode) return;
+            try {
+                await applyCoupon({
+                    variables: {
+                        cartId,
+                        couponCode
+                    }
+                });
+            } catch (err) {
+                console.error(err);
+            }
+        },
+        [applyCoupon, cartId]
+    );
+
+    const handleRemoveCoupon = useCallback(
+        async couponCode => {
+            try {
+                await removeCoupon({
+                    variables: {
+                        cartId,
+                        couponCode
+                    }
+                });
+            } catch (err) {
+                console.error(err);
+            }
+        },
+        [cartId, removeCoupon]
+    );
+
+    useEffect(() => {
+        if (cartId) {
+            fetchAppliedCoupons({
+                variables: {
+                    cartId
+                }
+            });
+        }
+    }, [cartId, fetchAppliedCoupons]);
+
+    return {
+        applyError,
+        applyingCoupon,
+        data,
+        fetchError,
+        handleApplyCoupon,
+        handleRemoveCoupon,
+        removingCoupon
+    };
+};
--- packages/venia-ui/lib/components/CartPage/cartPageFragments.js
+++ packages/venia-ui/lib/components/CartPage/cartPageFragments.js
@@ -2,14 +2,17 @@ import gql from 'graphql-tag';
 
 import { ProductListingFragment } from './ProductListing/productListingFragments';
 import { PriceSummaryFragment } from './PriceSummary/priceSummaryFragments';
+import { AppliedCouponsFragment } from './PriceAdjustments/CouponCode/couponCodeFragments';
 
 export const CartPageFragment = gql`
     fragment CartPageFragment on Cart {
         id
         total_quantity
+        ...AppliedCouponsFragment
         ...ProductListingFragment
         ...PriceSummaryFragment
     }
+    ${AppliedCouponsFragment}
     ${ProductListingFragment}
     ${PriceSummaryFragment}
 `;
