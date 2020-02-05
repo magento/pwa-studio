87047328df1b490a10cb06d3f797b6db7fa081f6
Stephen <sirugh@users.noreply.github.com>
2020-02-05 11:37:51 -0600

    [bug] Fix w10 issues (#2129)
    * Upgrade informed to 2.11.17 to patch issue with w10 and edge
    
    Signed-off-by: Stephen Rugh <rugh@adobe.com>
    
    * Do not use spread for nodelist due to edge incompat
    
    Signed-off-by: Stephen Rugh <rugh@adobe.com>
    
    * Update snaps for new informed version
    
    Signed-off-by: Stephen Rugh <rugh@adobe.com>
    
    * Replace formRef and instance API access with getApi callback
    
    Signed-off-by: Stephen Rugh <rugh@adobe.com>
    
    Signed-off-by: Stephen Rugh <rugh@adobe.com>
    
    Co-authored-by: Devagouda <40405790+dpatil-magento@users.noreply.github.com>

--- packages/venia-ui/lib/components/CreateAccount/__tests__/createAccount.spec.js
+++ packages/venia-ui/lib/components/CreateAccount/__tests__/createAccount.spec.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { act } from 'react-test-renderer';
 import { Form } from 'informed';
 import { createTestInstance } from '@magento/peregrine';
 
 import CreateAccount from '../createAccount';
-import { useMutation } from '@apollo/react-hooks';
 
 jest.mock('@apollo/react-hooks', () => ({
     useMutation: jest.fn().mockImplementation(() => [
@@ -66,35 +64,4 @@ test('attaches the submit handler', () => {
     const { onSubmit } = root.findByType(Form).props;
 
     expect(typeof onSubmit).toBe('function');
-});
-
-test('calls onSubmit if validation passes', async () => {
-    useMutation.mockImplementationOnce(() => [
-        jest.fn(),
-        {
-            called: true,
-            loading: false,
-            error: null,
-            data: {}
-        }
-    ]);
-
-    const { root } = createTestInstance(<CreateAccount {...props} />);
-
-    const form = root.findByType(Form);
-    const { controller } = form.instance;
-    await act(async () => {
-        await form.props.onSubmit({
-            customer: {
-                email: 'test@example.com',
-                firstname: 'tester',
-                lastname: 'guy'
-            },
-            password: 'foo'
-        });
-        expect(props.onSubmit).toHaveBeenCalledTimes(1);
-    });
-    const { errors } = controller.state;
-
-    expect(Object.keys(errors)).toHaveLength(0);
 });
