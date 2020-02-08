4333d80060da25af01df748db7889061e34f93b3
Harald Deiser <h.deiser@techdivision.com>
2020-02-08 15:17:51 +0100

    Added swatch color resolves #1633

--- packages/venia-ui/lib/components/ProductOptions/swatch.js
+++ packages/venia-ui/lib/components/ProductOptions/swatch.js
@@ -15,7 +15,6 @@ import { Check as CheckIcon } from 'react-feather';
 
 import defaultClasses from './swatch.css';
 
-import { memoizedGetRandomColor } from '../../util/getRandomColor';
 import { useSwatch } from '@magento/peregrine/lib/talons/ProductOptions/useSwatch';
 
 const getClassName = (name, isSelected, hasFocus) =>
@@ -25,7 +24,7 @@ const Swatch = props => {
     const {
         hasFocus,
         isSelected,
-        item: { label, value_index },
+        item: { label, value_index, swatch_data },
         onClick,
         style
     } = props;
@@ -43,18 +42,13 @@ const Swatch = props => {
 
     const classes = mergeClasses(defaultClasses, props.classes);
 
-    // TODO: use the colors from graphQL when they become available.
-    //   https://github.com/magento/graphql-ce/issues/196
-    //   https://github.com/magento/pwa-studio/issues/1633
-    const randomColor = memoizedGetRandomColor(value_index);
-
     // We really want to avoid specifying presentation within JS.
     // Swatches are unusual in that their color is data, not presentation,
     // but applying color *is* presentational.
     // So we merely provide the color data here, and let the CSS decide
     // how to use that color (e.g., background, border).
     const finalStyle = Object.assign({}, style, {
-        '--venia-swatch-bg': randomColor
+        '--venia-swatch-bg': swatch_data.value
     });
 
     const className = classes[getClassName('root', isSelected, hasFocus)];
