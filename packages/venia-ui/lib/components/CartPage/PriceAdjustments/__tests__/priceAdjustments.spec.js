61f6f22e1600f7aab870ca3a27a59369c89321d4
Andy Terranova <13182778+supernova-at@users.noreply.github.com>
2020-02-10 12:46:56 -0600

    Keeps current Accordion sections open (#2147)
    * Keeps current Accordion sections open
    
    * Mock GiftCardSection to avoid conflicts between CE/EE snapshots
    
    * Adds back closing all price adjustment sections initially

--- packages/venia-ui/lib/components/CartPage/PriceAdjustments/__tests__/priceAdjustments.spec.js
+++ packages/venia-ui/lib/components/CartPage/PriceAdjustments/__tests__/priceAdjustments.spec.js
@@ -3,7 +3,6 @@ import { createTestInstance } from '@magento/peregrine';
 
 import PriceAdjustments from '../priceAdjustments';
 
-jest.mock('../CouponCode/couponCode', () => 'CouponCode');
 jest.mock('../giftCardSection', () => 'GiftCardSection');
 
 test('it renders Venia price adjustments', () => {
