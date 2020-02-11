53eb4b273ac18aeb1f6bee7b2a029e5bce1cfa53
James Zetlen <jzetlen@adobe.com>
2020-02-11 11:37:15 -0600

    fixup test mocks

--- packages/venia-ui/lib/components/RichContent/__tests__/richContent.spec.js
+++ packages/venia-ui/lib/components/RichContent/__tests__/richContent.spec.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { createTestInstance } from '@magento/peregrine';
 import RichContent from '../richContent';
 
+// mock the part of the build where venia-ui/targets/intercept.js adds
+// plainHtmlRenderer through its own richContentRenderers target
+jest.mock('../richContentRenderers', () => [require('../plainHtmlRenderer')]);
+
 test('renders a RichContent component', () => {
     const component = createTestInstance(<RichContent />);
 
