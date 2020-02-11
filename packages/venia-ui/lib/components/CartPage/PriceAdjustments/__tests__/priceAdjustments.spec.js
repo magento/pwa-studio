5596146d749963fadbe09d70e1c865c26c7cd185
Tommy Wiebell <twiebell@adobe.com>
2020-02-11 08:53:05 -0600

    - Fixup unit test after merge

--- packages/venia-ui/lib/components/CartPage/PriceAdjustments/__tests__/priceAdjustments.spec.js
+++ packages/venia-ui/lib/components/CartPage/PriceAdjustments/__tests__/priceAdjustments.spec.js
@@ -3,7 +3,6 @@ import { createTestInstance } from '@magento/peregrine';
 
 import PriceAdjustments from '../priceAdjustments';
 
-jest.mock('../ShippingMethods/shippingMethods', () => 'ShippingMethods');
 jest.mock('../giftCardSection', () => 'GiftCardSection');
 
 test('it renders Venia price adjustments', () => {
