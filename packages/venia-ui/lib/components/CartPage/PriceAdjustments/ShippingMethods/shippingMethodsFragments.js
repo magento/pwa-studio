fa035d1f317af5e58afac29be2ce3406527aa1fa
Stephen Rugh <rugh@adobe.com>
2020-02-20 11:42:14 -0600

    adds a helpful comment
    Signed-off-by: Stephen Rugh <rugh@adobe.com>

--- packages/venia-ui/lib/components/CartPage/PriceAdjustments/ShippingMethods/shippingMethodsFragments.js
+++ packages/venia-ui/lib/components/CartPage/PriceAdjustments/ShippingMethods/shippingMethodsFragments.js
@@ -1,5 +1,11 @@
 import gql from 'graphql-tag';
 
+/**
+ * WARNING: This fragment can cause slowdowns on the network request because it
+ * causes the backend to make remote queries to carriers for real-time data.
+ * It is intentionally not included in the CartPageFragment. Make sure you are
+ * conscious about the side effects when including it in queries and mutations.
+ */
 export const AvailableShippingMethodsFragment = gql`
     fragment AvailableShippingMethodsFragment on Cart {
         id
