34744fad61153cbba4870bafbe21a6c511037115
Revanth Kumar <revanth0212@gmail.com>
2020-02-24 11:18:12 -0600

    Removed render if.

--- packages/venia-ui/lib/components/CheckoutPage/checkoutPage.js
+++ packages/venia-ui/lib/components/CheckoutPage/checkoutPage.js
@@ -1,4 +1,4 @@
-import React, { useCallback, Fragment } from 'react';
+import React, { Fragment } from 'react';
 
 import { useCheckoutPage } from '@magento/peregrine/lib/talons/CheckoutPage/useCheckoutPage';
 
@@ -14,7 +14,6 @@ import OrderConfirmationPage from './OrderConfirmationPage';
 import CREATE_CART_MUTATION from '../../queries/createCart.graphql';
 import GET_CART_DETAILS_QUERY from '../../queries/getCartDetails.graphql';
 
-import { renderIf } from './utilities';
 import { mergeClasses } from '../../classify';
 
 import defaultClasses from './checkoutPage.css';
@@ -91,177 +90,103 @@ export default props => {
 
     const classes = mergeClasses(defaultClasses, propClasses);
 
-    const renderIfGuestCheckout = useCallback(renderIf(isGuestCheckout), [
-        isGuestCheckout
-    ]);
-    const renderIfCartNotEmpty = useCallback(renderIf(!isCartEmpty), [
-        isCartEmpty
-    ]);
-    const renderIfShippingInformationDone = useCallback(
-        renderIf(shippingInformationDone),
-        [shippingInformationDone]
-    );
-    const renderIfShippingMethodDone = useCallback(
-        renderIf(shippingMethodDone),
-        [shippingMethodDone]
-    );
-    const renderIfPaymentInformationNotDone = useCallback(
-        renderIf(!paymentInformationDone),
-        [paymentInformationDone]
-    );
-    const renderIfOrderPlaced = useCallback(renderIf(orderPlaced), [
-        orderPlaced
-    ]);
-
     return (
         <div className={classes.root}>
             <Title>{`Checkout - ${STORE_NAME}`}</Title>
-            {/**
-             * Lazily evaluating checkout page components only
-             * if the cart is not empty.
-             */
-            renderIfCartNotEmpty(
-                () => (
-                    <Fragment>
-                        {/**
-                         * Lazily evaluating Guest checkout options
-                         * only if use is not signed in.
-                         */
-                        renderIfGuestCheckout(() => (
-                            <GuestCheckoutOptions
-                                classes={classes}
-                                handleSignIn={handleSignIn}
-                            />
-                        ))}
-                        <GreetingMessage
-                            isGuestCheckout={isGuestCheckout}
+            {!isCartEmpty ? (
+                <Fragment>
+                    {isGuestCheckout && (
+                        <GuestCheckoutOptions
                             classes={classes}
+                            handleSignIn={handleSignIn}
                         />
-                        <div className={classes.body}>
-                            <div
-                                className={
-                                    classes.shipping_information_container
-                                }
-                            >
-                                <ShippingInformation
-                                    onSave={setShippingInformationDone}
-                                    doneEditing={shippingInformationDone}
+                    )}
+                    <GreetingMessage
+                        isGuestCheckout={isGuestCheckout}
+                        classes={classes}
+                    />
+                    <div className={classes.body}>
+                        <div className={classes.shipping_information_container}>
+                            <ShippingInformation
+                                onSave={setShippingInformationDone}
+                                doneEditing={shippingInformationDone}
+                            />
+                        </div>
+                        {shippingInformationDone && (
+                            <div className={classes.shipping_method_container}>
+                                <ShippingMethod
+                                    onSave={setShippingMethodDone}
+                                    doneEditing={shippingMethodDone}
                                 />
                             </div>
-                            {/**
-                             * Lazily evaluating Shipping method component
-                             * only if shipping information component is
-                             * done editing.
-                             */
-                            renderIfShippingInformationDone(() => (
-                                <Fragment>
-                                    <div
-                                        className={
-                                            classes.shipping_method_container
-                                        }
-                                    >
-                                        <ShippingMethod
-                                            onSave={setShippingMethodDone}
-                                            doneEditing={shippingMethodDone}
-                                        />
-                                    </div>
-                                    {/**
-                                     * Lazily evaluating Payment information and price
-                                     * adjustments components if the shipping method
-                                     * component is done editing.
-                                     */
-                                    renderIfShippingMethodDone(() => (
-                                        <Fragment>
-                                            <div
-                                                className={
-                                                    classes.payment_information_container
-                                                }
-                                            >
-                                                <PaymentInformation
-                                                    doneEditing={
-                                                        paymentInformationDone
-                                                    }
-                                                />
-                                            </div>
-                                            {renderIfPaymentInformationNotDone(
-                                                () => (
-                                                    <Fragment>
-                                                        <div
-                                                            className={
-                                                                classes.price_adjustments_container
-                                                            }
-                                                        >
-                                                            <PriceAdjustments />
-                                                        </div>
-
-                                                        <Button
-                                                            onClick={
-                                                                setPaymentInformationDone
-                                                            }
-                                                            priority="high"
-                                                            className={
-                                                                classes.review_order_button
-                                                            }
-                                                        >
-                                                            {'Review Order'}
-                                                        </Button>
-                                                    </Fragment>
-                                                ),
-                                                () => (
-                                                    <Fragment>
-                                                        <div
-                                                            className={
-                                                                classes.items_review_container
-                                                            }
-                                                        >
-                                                            <ItemsReview />
-                                                        </div>
-                                                        <Button
-                                                            onClick={placeOrder}
-                                                            priority="high"
-                                                            className={
-                                                                classes.place_order_button
-                                                            }
-                                                        >
-                                                            {'Place Order'}
-                                                        </Button>
-                                                    </Fragment>
-                                                )
-                                            )}
-                                        </Fragment>
-                                    ))}
-                                </Fragment>
-                            ))}
-                            <div className={classes.summary_container}>
-                                <div className={classes.summary_contents}>
-                                    <PriceSummary />
+                        )}
+                        {shippingInformationDone && shippingMethodDone && (
+                            <Fragment>
+                                <div
+                                    className={
+                                        classes.payment_information_container
+                                    }
+                                >
+                                    <PaymentInformation
+                                        doneEditing={paymentInformationDone}
+                                    />
                                 </div>
+                                {paymentInformationDone ? (
+                                    <Fragment>
+                                        <div
+                                            className={
+                                                classes.items_review_container
+                                            }
+                                        >
+                                            <ItemsReview />
+                                        </div>
+                                        <Button
+                                            onClick={placeOrder}
+                                            priority="high"
+                                            className={
+                                                classes.place_order_button
+                                            }
+                                        >
+                                            {'Place Order'}
+                                        </Button>
+                                    </Fragment>
+                                ) : (
+                                    <Fragment>
+                                        <div
+                                            className={
+                                                classes.price_adjustments_container
+                                            }
+                                        >
+                                            <PriceAdjustments />
+                                        </div>
+
+                                        <Button
+                                            onClick={setPaymentInformationDone}
+                                            priority="high"
+                                            className={
+                                                classes.review_order_button
+                                            }
+                                        >
+                                            {'Review Order'}
+                                        </Button>
+                                    </Fragment>
+                                )}
+                            </Fragment>
+                        )}
+                        <div className={classes.summary_container}>
+                            <div className={classes.summary_contents}>
+                                <PriceSummary />
                             </div>
                         </div>
-                    </Fragment>
-                ),
-                () => (
-                    <Fragment>
-                        {renderIfOrderPlaced(
-                            () => (
-                                /**
-                                 * Once an order has been placed, cart will be
-                                 * empty and orderPlaced flag will be true.
-                                 *
-                                 * Rendering Order Placed Summary page.
-                                 */
-                                <OrderConfirmationPage />
-                            ),
-                            /**
-                             * Rendering empty cart component if the cart is emtpy.
-                             */
-                            <EmptyCartMessage
-                                classes={classes}
-                                isGuestCheckout={isGuestCheckout}
-                            />
-                        )}
-                    </Fragment>
-                )
+                    </div>
+                </Fragment>
+            ) : orderPlaced ? (
+                <OrderConfirmationPage />
+            ) : (
+                <EmptyCartMessage
+                    classes={classes}
+                    isGuestCheckout={isGuestCheckout}
+                />
             )}
         </div>
     );
