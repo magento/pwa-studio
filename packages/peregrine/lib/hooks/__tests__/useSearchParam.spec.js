44d1e899108745a0944b7d0a96d88174ddec8a68
Stephen <sirugh@users.noreply.github.com>
2020-02-24 12:03:36 -0600

    [bugfix]: search trigger focus (#2165)
    * Use hooks for location and history in search bar
    
    Signed-off-by: Stephen Rugh <rugh@adobe.com>
    
    * Focuses search input on button click
    
    Signed-off-by: Stephen Rugh <rugh@adobe.com>
    
    * Fix tests
    
    Signed-off-by: Stephen Rugh <rugh@adobe.com>
    
    * Unnecessary existence check
    
    Signed-off-by: Stephen Rugh <rugh@adobe.com>
    
    * feedback
    
    Signed-off-by: Stephen Rugh <rugh@adobe.com>
    
    * Revert old change
    
    Co-authored-by: Revanth Kumar Annavarapu <35203638+revanth0212@users.noreply.github.com>
    Co-authored-by: Jimmy Sanford <jimbo@users.noreply.github.com>
    Co-authored-by: Devagouda <40405790+dpatil-magento@users.noreply.github.com>

--- packages/peregrine/lib/hooks/__tests__/useSearchParam.spec.js
+++ packages/peregrine/lib/hooks/__tests__/useSearchParam.spec.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 import { useSearchParam } from '../useSearchParam';
 import createTestInstance from '../../util/createTestInstance';
 
 const log = jest.fn();
 const setValue = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+    useLocation: jest.fn(() => ({ search: '?a=b&c=d' }))
+}));
 
 const Component = props => {
     const hookProps = useSearchParam(props);
@@ -15,41 +20,28 @@ const Component = props => {
 };
 
 test('returns undefined', () => {
-    createTestInstance(
-        <Component
-            location={{ search: '?a=b&c=d' }}
-            parameter="a"
-            setValue={setValue}
-        />
-    );
+    createTestInstance(<Component parameter="a" setValue={setValue} />);
 
     expect(log).toHaveBeenCalledTimes(1);
     expect(log).toHaveBeenNthCalledWith(1, void 0);
 });
 
 test('calls `setValue` as an effect', () => {
-    createTestInstance(
-        <Component
-            location={{ search: '?a=b&c=d' }}
-            parameter="a"
-            setValue={setValue}
-        />
-    );
+    createTestInstance(<Component parameter="a" setValue={setValue} />);
 
     expect(setValue).toHaveBeenCalledTimes(1);
     expect(setValue).toHaveBeenNthCalledWith(1, 'b');
 });
 
 test("sets value to empty string if `parameter` isn't found", () => {
-    createTestInstance(
-        <Component location={{ search: '?a=b&c=d' }} setValue={setValue} />
-    );
+    createTestInstance(<Component setValue={setValue} />);
 
     expect(setValue).toHaveBeenCalledTimes(1);
     expect(setValue).toHaveBeenNthCalledWith(1, '');
 });
 
 test("uses `window.location` if `location` isn't provided", () => {
+    useLocation.mockReturnValueOnce(undefined);
     window.history.pushState({}, '', '/search.html?a=b&c=d');
 
     createTestInstance(<Component parameter="c" setValue={setValue} />);
--- packages/peregrine/lib/talons/SearchBar/useSearchField.js
+++ packages/peregrine/lib/talons/SearchBar/useSearchField.js
@@ -1,10 +1,14 @@
-import { useCallback } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 import { useFieldState, useFormApi } from 'informed';
 
+import { useAppContext } from '../../context/app';
 /**
  * Returns props necessary to render a SearchField component.
  */
 export const useSearchField = () => {
+    const [{ searchOpen }] = useAppContext();
+    const inputRef = useRef();
+
     const { value } = useFieldState('search_query');
     const formApi = useFormApi();
 
@@ -12,7 +16,15 @@ export const useSearchField = () => {
         formApi.reset();
     }, [formApi]);
 
+    // When the search field is opened focus on the input.
+    useEffect(() => {
+        if (searchOpen && inputRef.current) {
+            inputRef.current.focus();
+        }
+    }, [searchOpen]);
+
     return {
+        inputRef,
         resetForm,
         value
     };
