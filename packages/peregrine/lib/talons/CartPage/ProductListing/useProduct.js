a236f52d9ad59c24a07375b8f96d06e3f30c0c00
Andrew Terranova <aterrano@adobe.com>
2020-02-26 11:57:47 -0600

    Updates unit tests

--- packages/peregrine/lib/talons/CartPage/ProductListing/useProduct.js
+++ packages/peregrine/lib/talons/CartPage/ProductListing/useProduct.js
@@ -1,4 +1,4 @@
-import { useCallback, useRef, useState } from 'react';
+import { useCallback, useState } from 'react';
 import { useMutation } from '@apollo/react-hooks';
 
 import { useCartContext } from '../../../context/cart';
