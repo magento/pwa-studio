47dd63e778e8576be33ef27c626cdf492a022deb
Andrew Terranova <aterrano@adobe.com>
2020-02-27 09:29:59 -0600

    Changes to useMemo instead of useState for error message

--- packages/peregrine/lib/talons/CartPage/ProductListing/useProduct.js
+++ packages/peregrine/lib/talons/CartPage/ProductListing/useProduct.js
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import { useMutation } from '@apollo/react-hooks';
 
 import { useCartContext } from '../../../context/cart';
@@ -13,12 +13,27 @@ export const useProduct = props => {
 
     const flatProduct = flattenProduct(item);
     const [removeItem] = useMutation(removeItemMutation);
-    const [updateItemQuantity] = useMutation(updateItemQuantityMutation);
+    const [updateItemQuantity, { error: updateError }] = useMutation(updateItemQuantityMutation);
 
     const [{ cartId }] = useCartContext();
 
     const [isFavorite, setIsFavorite] = useState(false);
-    const [updateItemErrorMessage, setUpdateItemErrorMessage] = useState(null);
+
+    const updateItemErrorMessage = useMemo(() => {
+        if (!updateError) return null;
+
+        if (updateError.graphQLErrors) {
+            // Apollo prepends "GraphQL Error:" onto the message,
+            // which we don't want to show to an end user.
+            // Build up the error message manually without the prepended text.
+            return updateError.graphQLErrors
+                .map(({ message }) => message)
+                .join(', ');
+        }
+
+        // A non-GraphQL error occurred.
+        return updateError.message;
+    }, [updateError]);
 
     const handleToggleFavorites = useCallback(() => {
         setIsFavorite(!isFavorite);
@@ -61,22 +76,8 @@ export const useProduct = props => {
                         quantity
                     }
                 });
-
-                // The update was successful, clear out the error.
-                setUpdateItemErrorMessage(null);
-            } catch (updateItemError) {
-                if (!updateItemError.graphQLErrors) {
-                    setUpdateItemErrorMessage(updateItemError.message);
-                } else {
-                    // Apollo prepends "GraphQL Error:" onto the message,
-                    // which we don't want to show to an end user.
-                    // We build up the error message manually without the prepended text.
-                    const message = updateItemError.graphQLErrors
-                        .map(({ message }) => message)
-                        .join(', ');
-
-                    setUpdateItemErrorMessage(message);
-                }
+            } catch (err) {
+                // Do nothing. The error message is handled above.
             } finally {
                 setIsUpdating(false);
             }
