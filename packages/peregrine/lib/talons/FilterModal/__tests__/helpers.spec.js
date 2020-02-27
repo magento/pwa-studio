d9b636e7bdadd18159cd82af3de0eb5aedd1f361
Stephen Rugh <rugh@adobe.com>
2020-02-27 16:36:44 -0600

    adds tests for helper
    Signed-off-by: Stephen Rugh <rugh@adobe.com>

--- packages/peregrine/lib/talons/FilterModal/__tests__/helpers.spec.js
+++ packages/peregrine/lib/talons/FilterModal/__tests__/helpers.spec.js
@@ -1,4 +1,4 @@
-import { getFiltersFromSearch } from '../helpers';
+import { getFiltersFromSearch, getFilterInput } from '../helpers';
 
 describe('#getFiltersFromSearch', () => {
     test('returns filter values from a search string', () => {
@@ -19,5 +19,79 @@ describe('#getFiltersFromSearch', () => {
         const filters = getFiltersFromSearch(searchString);
 
         expect(filters.size).toBe(0);
+    });
+});
+
+describe('#getFilterInput', () => {
+    describe('for FilterEqualTypeInput', () => {
+        const TYPE = 'FilterEqualTypeInput';
+        it('returns an "eq" filter when the provided value set\'s size is 1', () => {
+            const filterInput = getFilterInput(new Set(['foo,1']), TYPE);
+
+            const expected = {
+                eq: '1'
+            };
+
+            expect(filterInput).toEqual(expected);
+        });
+        it('returns an "in" filter when the provided value set\'s size is > 1', () => {
+            const filterInput = getFilterInput(
+                new Set(['foo,1', 'bar,2']),
+                TYPE
+            );
+
+            const expected = {
+                in: ['1', '2']
+            };
+
+            expect(filterInput).toEqual(expected);
+        });
+    });
+
+    describe('FilterMatchTypeInput', () => {
+        const TYPE = 'FilterMatchTypeInput';
+        it('returns a "match" filter', () => {
+            const filterInput = getFilterInput(new Set(['foo,1']), TYPE);
+
+            const expected = {
+                match: '1'
+            };
+
+            expect(filterInput).toEqual(expected);
+        });
+    });
+
+    describe('FilterRangeTypeInput', () => {
+        const TYPE = 'FilterRangeTypeInput';
+        it('returns a filter with "to" and "from" props', () => {
+            const filterInput = getFilterInput(new Set(['foo,1_100']), TYPE);
+
+            const expected = {
+                from: '1',
+                to: '100'
+            };
+
+            expect(filterInput).toEqual(expected);
+        });
+
+        it('returns a filter with just a "to" prop if lower value is "*"', () => {
+            const filterInput = getFilterInput(new Set(['foo,*_100']), TYPE);
+
+            const expected = {
+                to: '100'
+            };
+
+            expect(filterInput).toEqual(expected);
+        });
+
+        it('returns a filter with just a "from" prop if higher value is "*"', () => {
+            const filterInput = getFilterInput(new Set(['foo,100_*']), TYPE);
+
+            const expected = {
+                from: '100'
+            };
+
+            expect(filterInput).toEqual(expected);
+        });
     });
 });
