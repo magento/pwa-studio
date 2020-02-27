8cda85a44cd748381008d649d0ce6b181928c1cb
Revanth Kumar <revanth0212@gmail.com>
2020-02-27 11:55:26 -0600

    Removing unnecessary css.

--- packages/venia-ui/lib/components/CheckoutPage/PaymentInformation/paymentInformation.js
+++ packages/venia-ui/lib/components/CheckoutPage/PaymentInformation/paymentInformation.js
@@ -6,14 +6,12 @@ export default props => {
     const { showContent, doneEditing } = props;
 
     return showContent ? (
-        <div className={defaultClasses.body}>
-            <div className={defaultClasses.container}>
-                <div>
-                    Payment Information Will be handled in PWA-183 and PWA-185
-                </div>
-                <div className={defaultClasses.text_content}>
-                    {doneEditing ? 'In Read Only Mode' : 'In Edit Mode'}
-                </div>
+        <div className={defaultClasses.container}>
+            <div>
+                Payment Information Will be handled in PWA-183 and PWA-185
+            </div>
+            <div className={defaultClasses.text_content}>
+                {doneEditing ? 'In Read Only Mode' : 'In Edit Mode'}
             </div>
         </div>
     ) : (
