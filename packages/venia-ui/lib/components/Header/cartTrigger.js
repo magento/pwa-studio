48aa999d9e4bb5dd91e79f62be867f7c8e86972d
roettigl <l.roettig@techdivision.com>
2020-03-05 19:34:08 +0100

    Allow to change the cart trigger color

--- packages/venia-ui/lib/components/Header/cartTrigger.js
+++ packages/venia-ui/lib/components/Header/cartTrigger.js
@@ -10,32 +10,33 @@ import CREATE_CART_MUTATION from '../../queries/createCart.graphql';
 import GET_CART_DETAILS_QUERY from '../../queries/getCartDetails.graphql';
 import defaultClasses from './cartTrigger.css';
 
-const CART_ICON_FILLED = (
-    <Icon
-        src={ShoppingCartIcon}
-        attrs={{
-            fill: 'rgb(var(--venia-text))',
-            stroke: 'rgb(var(--venia-text))'
-        }}
-    />
-);
-const CART_ICON_EMPTY = (
-    <Icon
-        src={ShoppingCartIcon}
-        attrs={{
-            stroke: 'rgb(var(--venia-text))'
-        }}
-    />
-);
-
 const CartTrigger = props => {
     const { handleClick, itemCount } = useCartTrigger({
         createCartMutation: CREATE_CART_MUTATION,
         getCartDetailsQuery: GET_CART_DETAILS_QUERY
     });
 
+    const iconColor = props.iconColor;
     const classes = mergeClasses(defaultClasses, props.classes);
-    const cartIcon = itemCount > 0 ? CART_ICON_FILLED : CART_ICON_EMPTY;
+
+    const cartIcon =
+        itemCount > 0 ? (
+            <Icon
+                src={ShoppingCartIcon}
+                attrs={{
+                    fill: iconColor,
+                    stroke: iconColor
+                }}
+            />
+        ) : (
+            <Icon
+                src={ShoppingCartIcon}
+                attrs={{
+                    stroke: iconColor
+                }}
+            />
+        );
+
     const buttonAriaLabel = `Toggle mini cart. You have ${itemCount} items in your cart.`;
 
     const itemCounter = itemCount ? (
@@ -55,9 +56,14 @@ const CartTrigger = props => {
 };
 
 CartTrigger.propTypes = {
+    iconColor: string,
     classes: shape({
         root: string
     })
+};
+
+CartTrigger.defaultProps = {
+    iconColor: 'rgb(var(--venia-text))'
 };
 
 export default CartTrigger;
