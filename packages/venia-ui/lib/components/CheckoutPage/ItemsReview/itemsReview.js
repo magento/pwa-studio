451955fa6c2062b4246bc99b6892b5b33a14419f
Revanth Kumar <revanth0212@gmail.com>
2020-03-17 16:15:35 -0500

    Probably the final minor.

--- packages/venia-ui/lib/components/CheckoutPage/ItemsReview/itemsReview.js
+++ packages/venia-ui/lib/components/CheckoutPage/ItemsReview/itemsReview.js
@@ -5,12 +5,17 @@ import { useItemsReview } from '@magento/peregrine/lib/talons/CheckoutPage/Items
 import Item from './item';
 import ShowAllItemsFooter from './showAllItemsFooter';
 import LoadingIndicator from '../../LoadingIndicator';
+import { mergeClasses } from '../../../classify';
 
 import LIST_OF_PRODUCTS_IN_CART_QUERY from './itemsReviewQuery';
 
 import defaultClasses from './itemsReview.css';
 
 const ItemsReview = props => {
+    const { classes: propClasses } = props;
+
+    const classes = mergeClasses(defaultClasses, propClasses);
+
     const talonProps = useItemsReview({
         queries: {
             getItemsInCart: LIST_OF_PRODUCTS_IN_CART_QUERY
@@ -26,11 +31,7 @@ const ItemsReview = props => {
     } = talonProps;
 
     const items = itemsInCart.map(item => (
-        <Item
-            key={item.id}
-            {...item}
-            classes={props.classes && props.classes.item}
-        />
+        <Item key={item.id} {...item} classes={classes.item} />
     ));
 
     const showAllItemsFooter = !showAllItems ? (
@@ -44,10 +45,10 @@ const ItemsReview = props => {
     }
 
     return (
-        <div className={defaultClasses.items_review_container}>
-            <div className={defaultClasses.items_container}>
+        <div className={classes.items_review_container}>
+            <div className={classes.items_container}>
                 <div
-                    className={defaultClasses.total_quantity}
+                    className={classes.total_quantity}
                 >{`(${totalQuantity}) Items in your order`}</div>
                 {items}
             </div>
