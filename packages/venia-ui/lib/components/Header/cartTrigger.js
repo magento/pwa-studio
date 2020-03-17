b17f476b531dd7b1cf83f97f5859292af75a1326
Tommy Wiebell <twiebell@adobe.com>
2020-03-17 15:59:38 -0500

    Minor cleanup

--- packages/venia-ui/lib/components/Header/cartTrigger.js
+++ packages/venia-ui/lib/components/Header/cartTrigger.js
@@ -4,10 +4,10 @@ import { ShoppingCart as ShoppingCartIcon } from 'react-feather';
 
 import { useCartTrigger } from '@magento/peregrine/lib/talons/Header/useCartTrigger';
 
-import Icon from '../Icon';
 import { mergeClasses } from '../../classify';
 import CREATE_CART_MUTATION from '../../queries/createCart.graphql';
 import GET_CART_DETAILS_QUERY from '../../queries/getCartDetails.graphql';
+import Icon from '../Icon';
 import defaultClasses from './cartTrigger.css';
 import { GET_ITEM_COUNT_QUERY } from './cartTrigger.gql';
 
@@ -22,26 +22,13 @@ const CartTrigger = props => {
         }
     });
 
-    const iconColor = props.iconColor;
+    const { iconColor } = props;
     const classes = mergeClasses(defaultClasses, props.classes);
 
-    const cartIcon =
-        itemCount > 0 ? (
-            <Icon
-                src={ShoppingCartIcon}
-                attrs={{
-                    fill: iconColor,
-                    stroke: iconColor
-                }}
-            />
-        ) : (
-            <Icon
-                src={ShoppingCartIcon}
-                attrs={{
-                    stroke: iconColor
-                }}
-            />
-        );
+    const cartIconAttributes = {
+        fill: itemCount ? iconColor : 'none',
+        stroke: iconColor
+    };
 
     const buttonAriaLabel = `Toggle mini cart. You have ${itemCount} items in your cart.`;
 
@@ -55,7 +42,7 @@ const CartTrigger = props => {
             aria-label={buttonAriaLabel}
             onClick={handleClick}
         >
-            {cartIcon}
+            <Icon src={ShoppingCartIcon} attrs={cartIconAttributes} />
             {itemCounter}
         </button>
     );
