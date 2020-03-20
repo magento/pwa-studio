a0b781b186489947c2ec2a25e463f17426689e14
christopher_brabender <cbrabender@absolunet.com>
2020-03-20 16:27:13 -0400

    - Fixing merge translations files issue

--- packages/venia-concept/src/i18n.js
+++ packages/venia-concept/src/i18n.js
@@ -1,12 +1,21 @@
 import i18n from 'i18next';
 import merge from 'lodash/merge';
 import resourcesVeniaUI from '@alienfast/i18next-loader!@magento/venia-ui/lib/locales/index.js';
-import resources from '@alienfast/i18next-loader!./locales/index.js';
+import resourcesLocal from '@alienfast/i18next-loader!./locales/index.js';
 import { Util } from '@magento/peregrine';
 const { BrowserPersistence } = Util;
 
+/**
+ * Initialize the i18n object for our app
+ * Also loads our translation files
+ */
 export const initi18n = () => {
-  console.log({ resources, resourcesVeniaUI}, merge(resourcesVeniaUI,resources));
+  /**
+   * Merge Venia UI and Local Resources together
+   * We can add additional resources here (maybe from peregrine for example)
+   * Would want to look at exposing this to the extensibility work also
+   */
+  const resources = merge(resourcesVeniaUI,resourcesLocal);
 
   const storage = new BrowserPersistence();
   const storeView = storage.getItem('store_view');
