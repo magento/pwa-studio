35ddc07d8f0b389e874ff56b7750046f57537573
Revanth Kumar Annavarapu <35203638+revanth0212@users.noreply.github.com>
2020-03-26 18:13:38 -0500

    Checkout Items Review Component (#2257)
    * Added initial UI.
    * Added total quantity header.
    * Added show all items footer.
    * Minor css change.
    * Addressed PR comments.
    * Added useItemsReview tests.
    * Added ItemsReview UI component tests.
    * Define click handler locally
    * Restore rem
    * UX changes.
    * Snapshot updates.
    * Added css animation.
    * Reducing gap from 1 rem to 0.5 rem for product options
    * show -> isHidden
    * Removed grid gap and using margin-top for items to handle hidden items spacing issue.
    * Update packages/venia-ui/lib/components/CheckoutPage/ItemsReview/item.css
    * Update packages/venia-ui/lib/components/CheckoutPage/ItemsReview/item.css
    
    Co-authored-by: Devagouda <40405790+dpatil-magento@users.noreply.github.com>
    Co-authored-by: Jimmy Sanford <jimbo@users.noreply.github.com>
    Co-authored-by: Tommy Wiebell <twiebell@adobe.com>

--- packages/venia-ui/lib/components/CheckoutPage/ItemsReview/itemsReview.js
+++ packages/venia-ui/lib/components/CheckoutPage/ItemsReview/itemsReview.js
@@ -1,18 +1,58 @@
 import React from 'react';
 
+import { useItemsReview } from '@magento/peregrine/lib/talons/CheckoutPage/ItemsReview/useItemsReview';
+
+import Item from './item';
+import ShowAllButton from './showAllButton';
+import LoadingIndicator from '../../LoadingIndicator';
+import { mergeClasses } from '../../../classify';
+
+import LIST_OF_PRODUCTS_IN_CART_QUERY from './itemsReview.gql';
+
 import defaultClasses from './itemsReview.css';
 
-const ItemsReview = () => {
-    /**
-     * Use talon to get information about items in cart
-     * to show a review of them.
-     */
+const ItemsReview = props => {
+    const { classes: propClasses } = props;
+
+    const classes = mergeClasses(defaultClasses, propClasses);
+
+    const talonProps = useItemsReview({
+        queries: {
+            getItemsInCart: LIST_OF_PRODUCTS_IN_CART_QUERY
+        }
+    });
+
+    const {
+        items: itemsInCart,
+        totalQuantity,
+        showAllItems,
+        setShowAllItems,
+        isLoading
+    } = talonProps;
+
+    const items = itemsInCart.map((item, index) => (
+        <Item key={item.id} {...item} isHidden={!showAllItems && index >= 2} />
+    ));
+
+    const showAllItemsFooter = !showAllItems ? (
+        <ShowAllButton onClick={setShowAllItems} />
+    ) : null;
+
+    if (isLoading) {
+        return (
+            <LoadingIndicator>{`Fetching Items in your Order`}</LoadingIndicator>
+        );
+    }
+
     return (
-        <div className={defaultClasses.container}>
-            <div>Items review will be handled in PWA-406</div>
-            <div className={defaultClasses.text_content}>
-                Review of Items in Cart
+        <div className={classes.items_review_container}>
+            <div className={classes.items_container}>
+                <div
+                    className={classes.total_quantity}
+                >{`(${totalQuantity}) Items in your order`}</div>
+                {items}
             </div>
+            {showAllItemsFooter}
         </div>
     );
 };
