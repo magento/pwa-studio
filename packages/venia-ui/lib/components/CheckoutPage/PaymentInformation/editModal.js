a02abcd772e385aee814d2cc94c647e11b6c4f64
Revanth Kumar <revanth0212@gmail.com>
2020-03-30 17:25:30 -0500

    Added edit modal component base.

---
+++ packages/venia-ui/lib/components/CheckoutPage/PaymentInformation/editModal.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { X as CloseIcon } from 'react-feather';
+
+import Icon from '../../Icon';
+import { Modal } from '../../Modal';
+import { mergeClasses } from '../../../classify';
+
+import defaultClasses from './editModal.css';
+
+const EditModal = props => {
+    const { classes: propClasses } = props;
+    const [isOpen, setIsOpen] = useState(true);
+
+    const classes = mergeClasses(defaultClasses, propClasses);
+    const rootClass = isOpen ? classes.root_open : classes.root;
+
+    return (
+        <Modal>
+            <aside className={rootClass}>
+                <div className={classes.header}>
+                    <span className={classes.headerText}>
+                        Edit Shipping Information
+                    </span>
+                    <button
+                        className={classes.closeButton}
+                        onClick={() => setIsOpen(false)}
+                    >
+                        <Icon src={CloseIcon} />
+                    </button>
+                </div>
+                <div className={classes.body}>
+                    <div> Edit Form goes here </div>
+                </div>
+            </aside>
+        </Modal>
+    );
+};
+
+export default EditModal;
