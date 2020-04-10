78e6ec917054b2574b2d4d2d33e6ae3aad699c7b
Andrew Terranova <aterrano@adobe.com>
2020-04-10 14:19:49 -0500

    Updating the shipping address refreshes list of available methods

--- packages/venia-ui/lib/components/CheckoutPage/ShippingInformation/EditForm/editForm.gql.js
+++ packages/venia-ui/lib/components/CheckoutPage/ShippingInformation/EditForm/editForm.gql.js
@@ -1,6 +1,7 @@
 import gql from 'graphql-tag';
 
 import { ShippingInformationFragment } from '../shippingInformationFragments.gql';
+import { ShippingMethodsFragment } from '@magento/venia-ui/lib/components/CheckoutPage/ShippingMethod/shippingMethodFragments.gql';
 
 export const SET_SHIPPING_INFORMATION_MUTATION = gql`
     mutation SetShippingInformation(
@@ -24,10 +25,12 @@ export const SET_SHIPPING_INFORMATION_MUTATION = gql`
             cart {
                 id
                 ...ShippingInformationFragment
+                ...ShippingMethodsFragment
             }
         }
     }
     ${ShippingInformationFragment}
+    ${ShippingMethodsFragment}
 `;
 
 export default {
