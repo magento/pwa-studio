5e1263a719f9b414789bc8ee0df380423e5c7acd
Stephen <sirugh@users.noreply.github.com>
2020-04-15 14:22:07 -0500

    Update packages/venia-ui/lib/components/MiniCart/cartOptions.gql.js
    Co-Authored-By: Andy Terranova <13182778+supernova-at@users.noreply.github.com>

--- packages/venia-ui/lib/components/MiniCart/cartOptions.gql.js
+++ packages/venia-ui/lib/components/MiniCart/cartOptions.gql.js
@@ -3,7 +3,7 @@ import gql from 'graphql-tag';
 import { CartPageFragment } from '../CartPage/cartPageFragments.gql';
 
 export const REMOVE_ITEM_MUTATION = gql`
-    mutation remoteItem($cartId: String!, $itemId: Int!) {
+    mutation removeItem($cartId: String!, $itemId: Int!) {
         removeItemFromCart(input: { cart_id: $cartId, cart_item_id: $itemId })
             @connection(key: "removeItemFromCart") {
             cart {
