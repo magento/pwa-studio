0bb9e65e4e8d045bcd11add88eeec195c5a73e18
sirugh <rugh@adobe.com>
2020-04-16 12:24:04 -0500

    Forgot some cleanup on checkout page
    Signed-off-by: sirugh <rugh@adobe.com>

--- packages/venia-ui/lib/components/CheckoutPage/checkoutPage.gql.js
+++ packages/venia-ui/lib/components/CheckoutPage/checkoutPage.gql.js
@@ -12,7 +12,7 @@ export const CREATE_CART = gql`
 
 export const PLACE_ORDER = gql`
     mutation placeOrder($cartId: String!) {
-        placeOrder(input: { cart_id: $cartId }) {
+        placeOrder(input: { cart_id: $cartId }) @connection(key: "placeOrder") {
             order {
                 order_number
             }
