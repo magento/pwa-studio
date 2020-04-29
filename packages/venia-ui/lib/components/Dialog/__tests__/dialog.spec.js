72ef4b55637e7d924937976fb187a89c412ce728
Andrew Terranova <aterrano@adobe.com>
2020-04-29 15:01:51 -0500

    Adds unit tests for Dialog

---
+++ packages/venia-ui/lib/components/Dialog/__tests__/dialog.spec.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { createTestInstance } from '@magento/peregrine';
+
+import Dialog from '../dialog';
+
+jest.mock('../../../classify');
+jest.mock('../../Modal', () => ({
+    Modal: props => <modal-mock>{props.children}</modal-mock>
+}));
+
+const props = {
+    areButtonsDisabled: false,
+    isModal: true,
+    isOpen: true,
+    title: 'Unit Test Dialog Title'
+};
+
+test('renders a basic Dialog', () => {
+    // Act.
+    const wrapper = createTestInstance(<Dialog {...props} />);
+
+    // Assert.
+    expect(wrapper.toJSON()).toMatchSnapshot();
+});
+
+test('renders a Dialog with disabled buttons', () => {
+    // Arrange.
+    const myProps = {
+        ...props,
+        areButtonsDisabled: true
+    };
+
+    // Act.
+    const wrapper = createTestInstance(<Dialog {...myProps} />);
+
+    // Assert.
+    expect(wrapper.toJSON()).toMatchSnapshot();
+});
+
+test('supports modifying title and button texts', () => {
+    // Arrange.
+    const myProps = {
+        ...props,
+        cancelText: 'Unit Test Cancel',
+        confirmText: 'Unit Test Confirm',
+        title: 'Unit Test Title'
+    };
+
+    // Act.
+    const wrapper = createTestInstance(<Dialog {...myProps} />);
+
+    // Assert.
+    expect(wrapper.toJSON()).toMatchSnapshot();
+});
