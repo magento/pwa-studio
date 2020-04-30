947b3c1b029a61284316e608a2c447a8e4e8bcd0
Stephen <sirugh@users.noreply.github.com>
2020-04-30 10:47:23 -0500

    [bug] Use gql persistence for checkout step between refreshes. (#2354)
    * Prevent cart id persistence in cache in gift options
    
    * Use resolver to handle local checkoutStep state on Cart type
    
    Signed-off-by: sirugh <rugh@adobe.com>
    
    * Cleanup
    
    Signed-off-by: sirugh <rugh@adobe.com>
    
    * Safely create resolvers for apollo client
    
    Signed-off-by: sirugh <rugh@adobe.com>
    
    * Move persistor check within async function
    
    Signed-off-by: sirugh <rugh@adobe.com>
    
    * Disable template strings for client directives
    
    Signed-off-by: sirugh <rugh@adobe.com>
    
    * Also delete inmemory persisted cache items
    
    Signed-off-by: sirugh <rugh@adobe.com>
    
    * prettier
    
    Signed-off-by: sirugh <rugh@adobe.com>
    
    * Protects against case where address not yet provided
    
    Signed-off-by: sirugh <rugh@adobe.com>
    
    * Simplify to single regex for cache entry deletion
    
    Signed-off-by: sirugh <rugh@adobe.com>
    
    * Feedback
    
    Signed-off-by: sirugh <rugh@adobe.com>
    
    Co-authored-by: Devagouda <40405790+dpatil-magento@users.noreply.github.com>

--- packages/peregrine/lib/talons/CheckoutPage/useCheckoutPage.js
+++ packages/peregrine/lib/talons/CheckoutPage/useCheckoutPage.js
@@ -8,7 +8,7 @@ import {
 import { useAppContext } from '../../context/app';
 import { useUserContext } from '../../context/user';
 import { useCartContext } from '../../context/cart';
-import { deleteCacheEntry } from '../../Apollo/deleteCacheEntry';
+import { clearCartDataFromCache } from '../../Apollo/clearCartDataFromCache';
 
 export const CHECKOUT_STEP = {
     SHIPPING_ADDRESS: 1,
@@ -20,16 +20,13 @@ export const CHECKOUT_STEP = {
 export const useCheckoutPage = props => {
     const {
         mutations: { createCartMutation, placeOrderMutation },
-        queries: {
-            getCheckoutDetailsQuery,
-            getCheckoutStepQuery,
-            getOrderDetailsQuery
-        }
+        queries: { getCheckoutDetailsQuery, getOrderDetailsQuery }
     } = props;
 
     const [reviewOrderButtonClicked, setReviewOrderButtonClicked] = useState(
         false
     );
+
     const apolloClient = useApolloClient();
     const [isUpdating, setIsUpdating] = useState(false);
 
@@ -59,31 +56,33 @@ export const useCheckoutPage = props => {
 
     const [
         getCheckoutDetails,
-        { data: checkoutData, loading: checkoutLoading }
-    ] = useLazyQuery(getCheckoutDetailsQuery, {
-        // TODO: Purposely overfetch and hit the network until all components
-        // are correctly updating the cache. Will be fixed by PWA-321.
-        fetchPolicy: 'cache-and-network'
-    });
+        {
+            data: checkoutData,
+            called: checkoutCalled,
+            client,
+            loading: checkoutLoading
+        }
+    ] = useLazyQuery(getCheckoutDetailsQuery);
 
-    const [getCheckoutStep, { data: stepData, client }] = useLazyQuery(
-        getCheckoutStepQuery
-    );
+    const checkoutStep = checkoutData && checkoutData.cart.checkoutStep;
 
     const setCheckoutStep = useCallback(
         step => {
+            const { cart: previousCart } = client.readQuery({
+                query: getCheckoutDetailsQuery
+            });
+
             client.writeQuery({
-                query: getCheckoutStepQuery,
+                query: getCheckoutDetailsQuery,
                 data: {
                     cart: {
-                        __typename: 'Cart',
-                        id: cartId,
+                        ...previousCart,
                         checkoutStep: step
                     }
                 }
             });
         },
-        [cartId, client, getCheckoutStepQuery]
+        [client, getCheckoutDetailsQuery]
     );
 
     const handleSignIn = useCallback(() => {
@@ -99,18 +98,23 @@ export const useCheckoutPage = props => {
         setReviewOrderButtonClicked(false);
     }, [setReviewOrderButtonClicked]);
 
-    const setShippingInformationDone = useCallback(
-        () => setCheckoutStep(CHECKOUT_STEP.SHIPPING_METHOD),
-        [setCheckoutStep]
-    );
-    const setShippingMethodDone = useCallback(
-        () => setCheckoutStep(CHECKOUT_STEP.PAYMENT),
-        [setCheckoutStep]
-    );
-    const setPaymentInformationDone = useCallback(
-        () => setCheckoutStep(CHECKOUT_STEP.REVIEW),
-        [setCheckoutStep]
-    );
+    const setShippingInformationDone = useCallback(() => {
+        if (checkoutStep === CHECKOUT_STEP.SHIPPING_ADDRESS) {
+            setCheckoutStep(CHECKOUT_STEP.SHIPPING_METHOD);
+        }
+    }, [checkoutStep, setCheckoutStep]);
+
+    const setShippingMethodDone = useCallback(() => {
+        if (checkoutStep === CHECKOUT_STEP.SHIPPING_METHOD) {
+            setCheckoutStep(CHECKOUT_STEP.PAYMENT);
+        }
+    }, [checkoutStep, setCheckoutStep]);
+
+    const setPaymentInformationDone = useCallback(() => {
+        if (checkoutStep === CHECKOUT_STEP.PAYMENT) {
+            setCheckoutStep(CHECKOUT_STEP.REVIEW);
+        }
+    }, [checkoutStep, setCheckoutStep]);
 
     const handlePlaceOrder = useCallback(async () => {
         await getOrderDetails({
@@ -127,8 +131,7 @@ export const useCheckoutPage = props => {
 
         await removeCart();
 
-        // Delete stale cart data from apollo
-        await deleteCacheEntry(apolloClient, key => key.match(/^Cart/));
+        await clearCartDataFromCache(apolloClient);
 
         await createCart({
             fetchCartId
@@ -143,24 +146,15 @@ export const useCheckoutPage = props => {
         removeCart
     ]);
 
-    const checkoutStep = (stepData && stepData.cart.checkoutStep) || 1;
-
     useEffect(() => {
         if (cartId) {
-            getCheckoutStep({
-                variables: {
-                    cartId
-                }
-            });
-
-            // And fetch any details for this page
             getCheckoutDetails({
                 variables: {
                     cartId
                 }
             });
         }
-    }, [cartId, getCheckoutDetails, getCheckoutStep, setCheckoutStep]);
+    }, [cartId, getCheckoutDetails]);
 
     return {
         checkoutStep,
@@ -170,7 +164,7 @@ export const useCheckoutPage = props => {
         hasError: !!placeOrderError,
         isCartEmpty: !(checkoutData && checkoutData.cart.total_quantity),
         isGuestCheckout: !isSignedIn,
-        isLoading: checkoutLoading,
+        isLoading: !checkoutCalled || (checkoutCalled && checkoutLoading),
         isUpdating,
         orderDetailsData,
         orderDetailsLoading,
