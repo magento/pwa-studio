c8bb3acc5f64baa6616225e3863085abcb696a0e
Tommy Wiebell <twiebell@adobe.com>
2020-05-04 17:04:20 -0500

    Run linter

--- packages/venia-ui/lib/components/CheckoutPage/checkoutPage.js
+++ packages/venia-ui/lib/components/CheckoutPage/checkoutPage.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { AlertCircle as AlertCircleIcon } from 'react-feather';
 
 import { useWindowSize, useToasts } from '@magento/peregrine';
