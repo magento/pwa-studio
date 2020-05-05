c6c604a7b8fabd9cbf47de13cab4bb17e4e11012
sirugh <rugh@adobe.com>
2020-05-05 12:59:26 -0500

    Fix lint
    Signed-off-by: sirugh <rugh@adobe.com>

--- packages/peregrine/lib/talons/CartPage/PriceAdjustments/useCouponCode.js
+++ packages/peregrine/lib/talons/CartPage/PriceAdjustments/useCouponCode.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo } from 'react';
+import { useCallback, useEffect } from 'react';
 import { useLazyQuery, useMutation } from '@apollo/react-hooks';
 import { useCartContext } from '@magento/peregrine/lib/context/cart';
 
