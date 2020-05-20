3780ba3b9e29f03384cb1f04e231ef15b5e45ecf
Kristof Ringleff, Fooman <kristof@fooman.co.nz>
2020-05-21 07:39:36 +1200

    Remove canUseDom

--- packages/venia-ui/lib/components/Head/headProvider.js
+++ packages/venia-ui/lib/components/Head/headProvider.js
@@ -2,11 +2,7 @@ import React from 'react';
 import {HelmetProvider as HeadProvider} from 'react-helmet-async';
 
 const VeniaHeadProvider = props => {
-    if (VeniaHeadProvider.canUseDom !== null) {
-        HeadProvider.canUseDOM = VeniaHeadProvider.canUseDom;
-    }
     return ( <HeadProvider> {props.children} </HeadProvider>);
 };
 
-VeniaHeadProvider.canUseDom = null;
 export default VeniaHeadProvider;
