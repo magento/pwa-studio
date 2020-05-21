ca2106bbc9f8cf8a40611989e712777367424675
Kristof Ringleff, Fooman <kristof@fooman.co.nz>
2020-05-21 12:38:26 +1200

    Everything is fixable

--- packages/venia-ui/lib/components/Head/headProvider.js
+++ packages/venia-ui/lib/components/Head/headProvider.js
@@ -2,9 +2,7 @@ import React from 'react';
 import { HelmetProvider as HeadProvider } from 'react-helmet-async';
 
 const VeniaHeadProvider = props => {
-    return (
-        <HeadProvider>{props.children}</HeadProvider>
-    );
+    return <HeadProvider>{props.children}</HeadProvider>;
 };
 
 export default VeniaHeadProvider;
