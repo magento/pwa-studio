e5d4c42855627c4365fb3313f52837945a30aeb4
James Zetlen <jzetlen@adobe.com>
2020-06-02 17:53:20 -0500

    [PWA-355]: Add richer extension points and documentation (#2451)
    * fix: resolve symlinked packages during configureWebpack
    
    * chore(debugging): improve logging and debuggability of BuildBus
    
    * chore: reorganize target impls into lib folders, not root
    
    * feat(extensibility): codemod targets and peregrine talon targets
    
    * chore(ci): upgrade jest to v25 and adjust tests for it
    
    * feat(extensibility): buildpack test helpers for extension devs
    
    * docs: targets tutorial
    
    * fixup jest tolerate slow ci
    
    * fixup remove unnecessary alias hack
    
    * fixup lint error
    
    * docs: add note on intercept filename
    
    * docs: reorganize contributing section
    
    * chore: upgrade to jest 26
    
    * docs: more detail and test helpers
    
    * fix better var names
    
    * fixup run serviceworker through buildbus as well
    
    * Update packages/peregrine/lib/targets/peregrine-declare.js
    
    Co-authored-by: Stephen <sirugh@users.noreply.github.com>
    
    * fixup var names i missed
    
    * fixup update snapshots after merge
    
    * fixup watch server regression
    
    * fixup add local intercept back
    
    * fixup: QA feedback for isEE and walkthrough
    
    Co-authored-by: Stephen <sirugh@users.noreply.github.com>
    Co-authored-by: Devagouda <40405790+dpatil-magento@users.noreply.github.com>

--- packages/pwa-buildpack/lib/WebpackTools/MagentoResolver.js
+++ packages/pwa-buildpack/lib/WebpackTools/MagentoResolver.js
@@ -1,11 +1,70 @@
-const optionsValidator = require('../util/options-validator');
-const validateConfig = optionsValidator('MagentoResolver', {
-    'paths.root': 'string'
-});
-module.exports = {
-    validateConfig,
-    async configure(options) {
-        const { isEE, ...restOptions } = options;
+/**
+ * Create a Webpack configuration object customized for your project.
+ * @module Buildpack/WebpackTools
+ */
+
+const fs = require('fs');
+const { CachedInputFileSystem, ResolverFactory } = require('enhanced-resolve');
+
+/**
+ * @typedef {Object} Buildpack/WebpackTools~MagentoResolverOptions
+ * @module MagentoResolver
+ * @property {boolean} isEE Resolve Magento Commerce (`*.ee.js`) modules instead of Magento Open Source `*.ce.js` modules
+ * @property {Object} paths Filesystem paths to resolve from
+ */
+
+/**
+ * Wrapper for an
+ * [enhanced-resolver](https://github.com/webpack/enhanced-resolve/) which can
+ * resolve paths according to Webpack rules before the Webpack compiler has
+ * been constructed.
+ *
+ * @class Buildpack/WebpackTools~MagentoResolver
+ */
+class MagentoResolver {
+    /**
+     * Legacy method for returning Webpack `resolve` config options as before
+     *
+     * @deprecated Use `new MagentoResolver(options).config` instead
+     * @static
+     * @param {Buildpack/WebpackTools~MagentoResolverOptions} options
+     * @returns {webpack~WebpackResolveOptions}
+     */
+    static async configure(options) {
+        const resolver = new MagentoResolver(options);
+        return resolver.config;
+    }
+    /**
+     *
+     * Lazy load an EnhancedResolver instance with a cached file system,
+     * configured from our constructor options.
+     *
+     * @ignore
+     * @private
+     * @readonly
+     */
+    get myResolver() {
+        if (!this._resolver) {
+            this._resolver = ResolverFactory.createResolver({
+                // Typical usage will consume the `fs` + `CachedInputFileSystem`, which wraps Node.js `fs` to add caching.
+                fileSystem: new CachedInputFileSystem(fs, 4000),
+                ...this.config
+            });
+        }
+        return this._resolver;
+    }
+    /**
+     * A MagentoResolver can asynchronously resolve `require` and `import`
+     * strings the same way the built PWA will.
+     * @param {Buildpack/WebpackTools~MagentoResolverOptions} options
+     */
+    constructor(options) {
+        const { isEE, paths, ...restOptions } = options;
+        if (!paths || typeof paths.root !== 'string') {
+            throw new Error(
+                'new MagentoResolver(options) requires "options.paths.root" to be a string'
+            );
+        }
         const extensions = [
             '.wasm',
             '.mjs',
@@ -15,13 +74,50 @@ module.exports = {
             '.json',
             '.graphql'
         ];
-        validateConfig('.configure()', restOptions);
-        return {
+        /** @ignore */
+        this._root = paths.root;
+
+        /** @type {webpack~WebpackResolveOptions} */
+        this.config = {
             alias: {},
-            modules: [options.paths.root, 'node_modules'],
+            modules: [this._root, 'node_modules'],
             mainFiles: ['index'],
             mainFields: ['esnext', 'es2015', 'module', 'browser', 'main'],
-            extensions
+            extensions,
+            ...restOptions
         };
+        /** @ignore */
+        this._context = {};
+        /** @ignore */
+        this._requestContext = {};
     }
-};
+    /**
+     * Asynchronously resolve a path the same way Webpack would given the
+     * current configuration.
+     * @async
+     * @param {string} request A module name or path, as in `require('<request>')` or `import foo from '<request>'`.
+     * @returns {Promise<string>} Absolute filesystem location.
+     */
+    async resolve(request) {
+        return new Promise((res, rej) => {
+            try {
+                this.myResolver.resolve(
+                    this._context,
+                    this._root,
+                    request,
+                    this._requestContext,
+                    (err, filepath) => {
+                        if (err) {
+                            return rej(err);
+                        }
+                        res(filepath);
+                    }
+                );
+            } catch (e) {
+                rej(e);
+            }
+        });
+    }
+}
+
+module.exports = MagentoResolver;
