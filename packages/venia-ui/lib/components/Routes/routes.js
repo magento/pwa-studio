205655510236e70f77000d176bfbb2e7c8e5a0e7
Jimmy Sanford <jimbo@users.noreply.github.com>
2020-06-08 11:55:05 -0700

    Reimport home page styles

--- packages/venia-ui/lib/components/Routes/routes.js
+++ packages/venia-ui/lib/components/Routes/routes.js
@@ -2,10 +2,9 @@ import React, { Suspense } from 'react';
 import { Route, Switch, useLocation } from 'react-router-dom';
 
 import { fullPageLoadingIndicator } from '../LoadingIndicator';
+import HomePage from '../HomePage';
 import MagentoRoute from '../MagentoRoute';
 import { useScrollTopOnChange } from '@magento/peregrine/lib/hooks/useScrollTopOnChange';
-
-
 
 const Routes = () => {
     const { pathname } = useLocation();
@@ -16,6 +15,7 @@ const Routes = () => {
             <Switch>
                 <Route>
                     <MagentoRoute />
+                    <HomePage />
                 </Route>
             </Switch>
         </Suspense>
