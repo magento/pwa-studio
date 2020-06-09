f3f6819f368091fe9ef23cff56d120ef0ef26b60
Dave Macaulay <macaulay@adobe.com>
2020-06-09 12:42:20 -0500

    PWA-560: Fix gift card flash on error
    - Fix lint issue

--- packages/peregrine/lib/talons/CartPage/GiftCards/useGiftCards.js
+++ packages/peregrine/lib/talons/CartPage/GiftCards/useGiftCards.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useLazyQuery, useMutation } from '@apollo/react-hooks';
 
 import { useCartContext } from '@magento/peregrine/lib/context/cart';
