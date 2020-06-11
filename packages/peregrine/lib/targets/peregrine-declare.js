7697af71d8bd1d5a84e4b20797b6a80c184e968d
James Calcaben <calcaben@adobe.com>
2020-06-11 16:03:15 -0500

    Revise JSDocs in peregrine targets declaration

--- packages/peregrine/lib/targets/peregrine-declare.js
+++ packages/peregrine/lib/targets/peregrine-declare.js
@@ -3,46 +3,65 @@
  *
  * Their implementations are found in `./peregrine-intercept.js`.
  *
- * @module Peregrine/Targets
  */
 module.exports = targets => {
     targets.declare({
         /**
-         * @callback talonIntercept
-         * @param {Peregrine/Targets.TalonWrapperConfig} talons - Registry of talon namespaces, talons, and Sets of interceptors
-         * @returns {undefined} - Interceptors of `talons` should add to the
-         * sets on passed TalonWrapperConfig instance. Any returned value will
-         * be ignored.
-         */
-
-        /**
-         * Collects requests to intercept and "wrap" individual Peregrine
-         * talons in decorator functions. Use it to add your own code to run
-         * when Peregrine talons are invoked, and/or to modify the behavior and
-         * output of those talons.
+         * Provides access to Peregrine talon wrappers.
+         * 
+         * This target collects requests to intercept and "wrap" individual Peregrine
+         * talons in decorator functions. It is a convenience wrapper around the
+         * `transformModules` target in `@magento/pwa-buildpack`, but instead of decorating
+         * files, it exposes the talons as functions to wrap.
+         * 
+         * Use this target to run custom code whenever the application calls a
+         * Peregrine talon.
+         * You can also use this target to modify the behavior or output returned by
+         * a talon.
          *
-         * This target is a convenience wrapper around the
-         * `@magento/pwa-buildpack` target `transformModules`. That target uses
-         * filenames, which are not guaranteed to be semantically versioned or
-         * to be readable APIs.
-         * This target publishes talons as functions to wrap, rather than as
-         * files to decorate.
+         * @typedef {talons}
+         * @member {tapable.SyncHook}
+         * 
+         * @see [`talons.tap()`]{@link talons.tap}
+         * @see [Intercept function signature]{@link talonInterceptFunction}
          *
-         * @type {tapable.SyncHook}
-         * @param {talonIntercept}
-         *
-         * @example <caption>Log whenever the `useApp()` hook runs.</caption>
-         * targets.of('@magento/peregrine').talons.tap(talons => {
-         *   talons.App.useApp.wrapWith('./log-wrapper');
+         * @example <caption>Access the tapable object</caption>
+         * const peregrineTargets = targets.of('@magento/peregrine');
+         * const talonsTarget = peregrineTargets.talons;
+         * 
+         * @example <caption>Wrap the `useApp()` hook  with a logging extension</caption>
+         * 
+         * talonsTarget.tap(talons => {
+         *   talons.App.useApp.wrapWith('@my-extensions/log-wrapper');
          * })
-         * // log-wrapper.js:
-         * export default function wrapUseApp(original) {
-         *   return function useApp(...args) {
-         *     console.log('calling useApp with', ...args);
-         *     return original(...args);
-         *   }
-         * }
          */
         talons: new targets.types.Sync(['talons'])
     });
 };
+
+/** Type definitions related to: talons */
+
+/**
+ * The `tap()` function gives you access to the talon registry through a 
+ * [callback function]{@link talonIntercept} you define in your project.
+ * 
+ * @function talons.tap
+ * 
+ * @param {talonInterceptFunction} intercept A callback function
+ * 
+ * @example
+ * const talons = peregrineTargets.talons;
+ * 
+ * talons.tap(talons => {
+ *   // Logic for wrapping specific talons
+ * })
+ */
+
+/**
+ * The talon intercept function is a callback defined in your intercept file.
+ * It has access to the talon registry when passed into the
+ * [`talons.tap()`]{@link talons.tap} function.
+ * 
+ * @callback talonInterceptFunction
+ * @param {Peregrine/Targets.TalonWrapperConfig} talons Registry of talon namespaces, talons, and Sets of interceptors
+ */
--- pwa-devdocs/src/peregrine/reference/targets/index.md
+++ pwa-devdocs/src/peregrine/reference/targets/index.md
@@ -1,6 +1,20 @@
 ---
 title: Peregrine Extensibility Targets
 ---
+<!-- TODO: Remove the following note after this feature is released in an official version -->
+
+{: .bs-callout .bs-callout-info}
+These targets are currently only available in the `develop` branch of the PWA Studio project.
+
+This page lists the extensibility targets declared in the Peregrine package.
+Access these in your intercept files by specifying `@magento/peregine` from the TargetProvider object.
+
+```js
+/* my-custom-interceptors.js */
+module.exports = targets => {
+    const peregrineTargets = targets.of('@magento/peregrine')
+}
+```
 
 <!--
 The reference doc content is generated automatically from the source code.
