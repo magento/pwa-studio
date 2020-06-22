4280d6fa359ba38d5f1f29ee8751c1bc27e2f876
christopher daniel <christopherdaniel13397@gmail.com>
2020-06-22 22:03:14 +0530

    Merge Carts on log in (#2377)
    * pwa-2190 merge carts
    
    * added merge cart support in create user component and fixed review comments
    
    * pwa-2190 merge carts fix cart trigger inconsistency
    
    * pwa-2190 tests fixed
    
    * Update cache after merge cart result
    
    Signed-off-by: sirugh <rugh@adobe.com>
    
    * Fix tests
    
    Signed-off-by: sirugh <rugh@adobe.com>
    
    * Use talon logic to merge instead of redux action
    
    Signed-off-by: sirugh <rugh@adobe.com>
    
    * Also update order confirmation create account
    
    Signed-off-by: sirugh <rugh@adobe.com>
    
    * Updates imports
    
    * No need to merge on order conf account creation since cart will be empty guest cart
    
    * Only wipe old data once sign in has completed
    
    * fix checkout page tests
    
    Signed-off-by: sirugh <rugh@adobe.com>
    
    * Update packages/peregrine/lib/talons/CheckoutPage/OrderConfirmationPage/useCreateAccount.js
    
    Co-authored-by: Andy Terranova <13182778+supernova-at@users.noreply.github.com>
    
    * Remove unnecessary call to clear customer data on checkout
    
    Signed-off-by: sirugh <rugh@adobe.com>
    
    Co-authored-by: sirugh <rugh@adobe.com>
    Co-authored-by: Stephen <sirugh@users.noreply.github.com>
    Co-authored-by: Andy Terranova <13182778+supernova-at@users.noreply.github.com>
    Co-authored-by: Devagouda <40405790+dpatil-magento@users.noreply.github.com>

--- packages/peregrine/lib/talons/Header/useCartTrigger.js
+++ packages/peregrine/lib/talons/Header/useCartTrigger.js
@@ -1,9 +1,5 @@
 import { useCallback, useEffect } from 'react';
-import {
-    useApolloClient,
-    useLazyQuery,
-    useMutation
-} from '@apollo/react-hooks';
+import { useApolloClient, useQuery, useMutation } from '@apollo/react-hooks';
 import { useCartContext } from '@magento/peregrine/lib/context/cart';
 import { useAppContext } from '@magento/peregrine/lib/context/app';
 import { useAwaitQuery } from '@magento/peregrine/lib/hooks/useAwaitQuery';
@@ -18,9 +14,14 @@ export const useCartTrigger = props => {
     const [, { toggleDrawer }] = useAppContext();
     const [{ cartId }, { getCartDetails }] = useCartContext();
 
-    const [getItemCount, { data }] = useLazyQuery(getItemCountQuery, {
-        fetchPolicy: 'cache-and-network'
+    const { data } = useQuery(getItemCountQuery, {
+        fetchPolicy: 'cache-and-network',
+        variables: {
+            cartId
+        },
+        skip: !cartId
     });
+
     const [fetchCartId] = useMutation(createCartMutation);
     const fetchCartDetails = useAwaitQuery(getCartDetailsQuery);
 
@@ -32,18 +33,9 @@ export const useCartTrigger = props => {
         getCartDetails({ apolloClient, fetchCartId, fetchCartDetails });
     }, [apolloClient, fetchCartDetails, fetchCartId, getCartDetails]);
 
-    useEffect(() => {
-        if (cartId) {
-            getItemCount({
-                variables: {
-                    cartId
-                }
-            });
-        }
-    }, [cartId, getItemCount]);
-
     const handleClick = useCallback(async () => {
         toggleDrawer('cart');
+        // TODO: Cart details should be fetched by MiniCart.
         await getCartDetails({
             fetchCartId,
             fetchCartDetails
