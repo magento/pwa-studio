018543e3c1f64e7b011228ea48436387c16e86a8
Revanth Kumar <revanth0212@gmail.com>
2020-07-06 15:40:13 -0500

    Created mini cart fragment.

--- packages/venia-ui/lib/components/MiniCart/miniCart.gql.js
+++ packages/venia-ui/lib/components/MiniCart/miniCart.gql.js
@@ -2,15 +2,22 @@ import gql from 'graphql-tag';
 
 import { ProductListFragment } from './ProductList/productList.gql';
 
+export const MiniCartFragment = gql`
+    fragment MiniCartFragment on Cart {
+        id
+        total_quantity
+        ...ProductListFragment
+    }
+    ${ProductListFragment}
+`;
+
 export const MINI_CART_QUERY = gql`
     query MiniCartQuery($cartId: String!) {
         cart(cart_id: $cartId) @connection(key: "Cart") {
-            id
-            total_quantity
-            ...ProductListFragment
+            ...MiniCartFragment
         }
     }
-    ${ProductListFragment}
+    ${MiniCartFragment}
 `;
 
 export const REMOVE_ITEM_MUTATION = gql`
@@ -18,13 +25,11 @@ export const REMOVE_ITEM_MUTATION = gql`
         removeItemFromCart(input: { cart_id: $cartId, cart_item_id: $itemId })
             @connection(key: "removeItemFromCart") {
             cart {
-                id
-                total_quantity
-                ...ProductListFragment
+                ...MiniCartFragment
             }
         }
     }
-    ${ProductListFragment}
+    ${MiniCartFragment}
 `;
 
 export default {
