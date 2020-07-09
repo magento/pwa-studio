f051e346acf889b74cf19928f7e896a030175b51
Revanth Kumar <revanth0212@gmail.com>
2020-07-09 14:33:53 -0500

    Minor.

--- packages/venia-ui/lib/components/MiniCart/miniCart.gql.js
+++ packages/venia-ui/lib/components/MiniCart/miniCart.gql.js
@@ -2,7 +2,6 @@ import gql from 'graphql-tag';
 
 import { ProductListFragment } from './ProductList/productList.gql';
 import { CartPageFragment } from '../CartPage/cartPageFragments.gql';
-import { CheckoutPageFragment } from '../CheckoutPage/checkoutPageFragments.gql';
 
 export const MiniCartFragment = gql`
     fragment MiniCartFragment on Cart {
@@ -31,13 +30,11 @@ export const REMOVE_ITEM_MUTATION = gql`
                 id
                 ...MiniCartFragment
                 ...CartPageFragment
-                ...CheckoutPageFragment
             }
         }
     }
     ${MiniCartFragment}
     ${CartPageFragment}
-    ${CheckoutPageFragment}
 `;
 
 export default {
