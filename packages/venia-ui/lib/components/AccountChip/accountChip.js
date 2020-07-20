6ad68a5f77b97c21d543e391e258d8686fbc5409
Andrew Terranova <aterrano@adobe.com>
2020-07-20 11:46:42 -0500

    Adds JSDoc comments to AccountChip component and talon

--- packages/venia-ui/lib/components/AccountChip/accountChip.js
+++ packages/venia-ui/lib/components/AccountChip/accountChip.js
@@ -8,6 +8,17 @@ import { mergeClasses } from '@magento/venia-ui/lib/classify';
 import Icon from '../Icon';
 import defaultClasses from './accountChip.css';
 
+/**
+ * The AccountChip component shows an icon next to some text.
+ * Sometimes the text is static, sometimes it is dynamic based on the user's name,
+ * and it can also be a loading icon to indicate that we're fetching the user's name.
+ *
+ * @param {Object} props
+ * @param {String} props.fallbackText - The text to display when the user is not signed in
+ *  or when we're loading details but don't want to show a loading icon.
+ * @param {Boolean} props.shouldIndicateLoading - Whether we should show a loading icon or
+ *  not when the user is signed in but we don't have their details (name) yet.
+ */
 const AccountChip = props => {
     const { fallbackText, shouldIndicateLoading } = props;
 
