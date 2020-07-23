9a0ce8454f7ef7b7370daec843e3ad3a74aa6034
Tommy Wiebell <twiebell@adobe.com>
2020-07-23 10:15:22 -0500

    Implement PR suggestions

--- packages/pwa-buildpack/lib/cli/create-project.js
+++ packages/pwa-buildpack/lib/cli/create-project.js
@@ -219,16 +219,6 @@ module.exports.handler = async function buildpackCli(argv) {
         prettyLogger.success(`Installed dependencies for '${name}' project`);
     }
 
-    if (process.env.DEBUG_PROJECT_CREATION) {
-        prettyLogger.info('Debug: Removing generated tarballs');
-        const pkgDir = require('pkg-dir');
-        const packagesDirectory = resolve(pkgDir.sync(__dirname), '../');
-        prettyLogger.info(
-            execa.shellSync('find . -name "*.tgz" -delete -print', {
-                cwd: packagesDirectory
-            }).stdout
-        );
-    }
     const showCommand = command =>
         ' - ' + chalk.whiteBright(`${params.npmClient} ${command}`);
     const buildpackPrefix = params.npmClient === 'npm' ? ' --' : '';
