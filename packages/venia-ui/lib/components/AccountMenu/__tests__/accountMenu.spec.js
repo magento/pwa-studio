d9ace4341f958bedced1d3e9457e8e71fce81111
Revanth Kumar <revanth0212@gmail.com>
2020-07-29 16:08:48 -0500

    Tests update.

--- packages/venia-ui/lib/components/AccountMenu/__tests__/accountMenu.spec.js
+++ packages/venia-ui/lib/components/AccountMenu/__tests__/accountMenu.spec.js
@@ -8,7 +8,14 @@ jest.mock('../accountMenuItems', () => 'AccountMenuItems');
 const props = {
     handleSignOut: jest.fn().mockName('handleSignOut'),
     isOpen: true,
-    isUserSignedIn: true
+    isUserSignedIn: true,
+    VIEWS: {
+        SIGNIN: 'SINGIN',
+        FORGOT_PASSWORD: 'FORGOT_PASSWORD',
+        CREATE_ACCOUNT: 'CREATE_ACCOUNT',
+        ACCOUNT: 'ACCOUNT'
+    },
+    view: 'ACCOUNT'
 };
 
 test('it renders AccountMenuItems when the user is signed in', () => {
