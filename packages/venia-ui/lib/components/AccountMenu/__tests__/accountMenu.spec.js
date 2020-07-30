5c7a95e48ff316bec7464eb5b83c51b61acc01c0
Revanth Kumar <revanth0212@gmail.com>
2020-07-30 15:47:55 -0500

    Updated account menu tests.

--- packages/venia-ui/lib/components/AccountMenu/__tests__/accountMenu.spec.js
+++ packages/venia-ui/lib/components/AccountMenu/__tests__/accountMenu.spec.js
@@ -2,17 +2,58 @@ import React from 'react';
 import { createTestInstance } from '@magento/peregrine';
 
 import AccountMenu from '../accountMenu';
+import { VIEWS } from '../../Header/accountTrigger';
 
 jest.mock('../accountMenuItems', () => 'AccountMenuItems');
+jest.mock('../../SignIn/signIn', () => 'Singin Component');
 
-const props = {
+const defaultProps = {
     handleSignOut: jest.fn().mockName('handleSignOut'),
     isOpen: true,
     isUserSignedIn: true,
-    view: 'ACCOUNT'
+    view: VIEWS.ACCOUNT
 };
 
 test('it renders AccountMenuItems when the user is signed in', () => {
+    // Act.
+    const instance = createTestInstance(<AccountMenu {...defaultProps} />);
+
+    // Assert.
+    expect(instance.toJSON()).toMatchSnapshot();
+});
+
+test('it renders SignIn component when the view is SIGNIN', () => {
+    const props = {
+        ...defaultProps,
+        view: VIEWS.SIGNIN
+    };
+
+    // Act.
+    const instance = createTestInstance(<AccountMenu {...props} />);
+
+    // Assert.
+    expect(instance.toJSON()).toMatchSnapshot();
+});
+
+test('it renders forgot password component when the view is FORGOT_PASSWORD', () => {
+    const props = {
+        ...defaultProps,
+        view: VIEWS.FORGOT_PASSWORD
+    };
+
+    // Act.
+    const instance = createTestInstance(<AccountMenu {...props} />);
+
+    // Assert.
+    expect(instance.toJSON()).toMatchSnapshot();
+});
+
+test('it renders forgot password component when the view is CREATE_ACCOUNT', () => {
+    const props = {
+        ...defaultProps,
+        view: VIEWS.CREATE_ACCOUNT
+    };
+
     // Act.
     const instance = createTestInstance(<AccountMenu {...props} />);
 
