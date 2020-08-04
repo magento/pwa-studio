8fc82a33bf535eb2efc9a78994a20810d1fb998d
James Calcaben <jcalcaben@users.noreply.github.com>
2020-08-04 11:59:29 -0500

    [Doc] buildpack ref docs (#2508)
    * Add buildpack configuration file to reference doc generator script
    
    * Add topic for buildpack target ref docs
    
    * Add module transform config ref docs and add to nav
    
    * Revise js docs
    
    * Add other reference topics
    
    * Add initial files for the new ref topics
    
    * Revise jsdocs for BuildBus and TargetProvider
    
    * Revise JSDocs for the mapHooksToTarget file
    
    * Revise JSDocs on the Target file
    
    * Revise JSDocs in TargetProvider
    
    * Minor fixes
    
    * Apply suggestions from code review
    
    Co-authored-by: James Zetlen <jzetlen@adobe.com>
    
    * Revise JSDoc based on feedback
    
    * Apply suggestions from code review
    
    Co-authored-by: James Zetlen <jzetlen@adobe.com>
    
    * [Doc] Suggested additions to buildpack ref docs (#2575)
    
    * docs: elaborate on envVarDefinitions API
    
    * Apply suggestions from code review
    
    Co-authored-by: James Zetlen <jzetlen@adobe.com>
    
    * Update pwa-devdocs/src/pwa-buildpack/reference/environment-variables/index.md
    
    Co-authored-by: James Calcaben <jcalcaben@users.noreply.github.com>
    
    * Update pwa-devdocs/src/pwa-buildpack/reference/environment-variables/index.md
    
    Co-authored-by: James Calcaben <jcalcaben@users.noreply.github.com>
    
    * docs: elaborate on envVarDefinitions API
    
    Co-authored-by: James Calcaben <jcalcaben@users.noreply.github.com>
    
    * Make changes based on feedback and removed 'in develop' notice
    
    * Revise new environment variables topic
    
    * [DOC] Suggested reorg of Buildpack reference doc content (#2579)
    
    * bluh
    
    * docs: reorg buildpack ref path
    
    * Fix links in doc blocks
    
    * doc: fix typo in Target docblock
    
    * docs: revise buildpack devdocs (#2598)
    
    * Fix links
    
    Co-authored-by: James Zetlen <jzetlen@adobe.com>
    Co-authored-by: Devagouda <40405790+dpatil-magento@users.noreply.github.com>

--- packages/pwa-buildpack/lib/WebpackTools/ModuleTransformConfig.js
+++ packages/pwa-buildpack/lib/WebpackTools/ModuleTransformConfig.js
@@ -1,38 +1,79 @@
-/**
- * @module Buildpack/WebpackTools
- */
 const path = require('path');
 
 /**
- * @typedef {Object} TransformRequest
- * Instruction to the Webpack transform loader to pass a given file through a
- * transform function implemented in a given Node module, with an optional set
- * of configuration values that will be passed to the transform function.
- * @prop {string} type - Type of transform. `'babel'` expects a Babel plugin as the `transformModule`. `"source"` expects a Webpack loader.
- * @prop {string} requestor - Name of the file doing the requesting.
- * @prop {string} fileToTransform - Relative path to the file in this module
- *   to be transformed when it is loaded by the compilation.
- * @prop {string} transformModule - The Node module that exports the transform
- *   function to use. Node will resolve this relative to project root.
- * @prop {object} [options] - Config values to send to the transform function.
- *   _Note: Options should be serializable to JSON as Webpack loader options._
+ * @typedef {function(TransformRequest)} addTransform
+ * Add a request to transform a file in the build. This function is passed as
+ * the first argument to an interceptor of the `transformModules` target.
+ *
+ * @param {TransformRequest} req - Instruction object for the requested
+ * transform, including the transform to apply, the target source code, and
+ * other options.
+ *
+ * @returns null
  */
 
+/** @enum {string} */
+const TransformType = {
+    /**
+     * Process the _source code_ of `fileToTransform` through the
+     * `transformModule` as text. When applying a `source` TransformRequest,
+     * Buildpack will use the `transformModule` as a [Webpack
+     * loader](https://v4.webpack.js.org/api/loaders/), so it must implement
+     * that interface. Any Webpack loader can be used as a `transformModule`
+     * for `source` TransformRequests.
+     *
+     * `source` transforms are fast and can run on source code of any language,
+     * but they aren't as precise and safe as AST-type transforms when modifying
+     * code.
+     */
+    source: 'source',
+    /**
+     * Process the _abstract syntax tree_ of the ES module specified by `fileToTransform` through the `transformModule` as a [Babel AST](https://github.com/babel/babel/blob/master/packages/babel-parser/ast/spec.md).
+     * When applying a `babel` TransformRequest, Buildpack will use the
+     * `transformModule` as a [Babel plugin](https://github.com/jamiebuilds/babel-handbook), so it must implement that interface. Any Babel plugin can be used as a `transformModule` for `babel` TransformRequests.
+     *
+     * `babel` transforms are powerful and versatile, giving the transformer
+     * much more insight into the structure of the source code to modify.
+     * However, they are slower than `source` transforms, and they can only work
+     * on ES Modules.
+     */
+    babel: 'babel'
+};
+
 /**
- * @typedef {Object.<string, FileExportWrappers>} configSerialized
- * A map of filenames of modules to be wrapped, to FileExportWrappers declaring
- * which exports of those modules will be wrapped with what.
+ * @typedef {Object} TransformRequest
+ * Instruction for configuring Webpack to apply custom transformations to one
+ * particular file. The [`configureWebpack()` function]{@link /pwa-buildpack/reference/configure-webpack/}
+ * gathers TransformRequests from all interceptors of the `transformModules`
+ * target and turns them into a configuration of Webpack [module
+ * rules](https://v4.webpack.js.org/configuration/module/#modulerules).
+ *
+ * @prop {TransformType} type - The type of transformation to apply.
+ * @prop {string} requestor - Name of the module making this request. Used for debugging purposes.
+ * @prop {string} fileToTransform - Resolvable path to the file to be transformed itself, the same path that you'd use in `import` or `require()`.
+ * @prop {string} transformModule - Absolute path to the Node module that will actually be doing the transforming. This path may be resolved using different
+ * rules at different times, so it's best for this path to always be absolute.
+ * @prop {object} [options] - Config values to send to the transform function.
+ *   _Note: Options should be serializable to JSON as Webpack loader options
+ *   and/or Babel plugin options.._
  */
 
 /**
  * Configuration builder for module transforms. Accepts TransformRequests
  * and emits loader config objects for Buildpack's custom transform loaders.
- *
- * @param {MagentoResolver} resolver - Resolver to use when finding real paths of
- * modules requested.
- * @param {string} localProjectName - The name of the PWA project being built, taken from the package.json `name` field.
  */
 class ModuleTransformConfig {
+    /** @borrows TransformType as types */
+    static get types() {
+        return TransformType;
+    }
+    /**
+     * @private
+     * @constructs
+     * @param {MagentoResolver} resolver - Resolver to use when finding real paths of
+     * modules requested.
+     * @param {string} localProjectName - The name of the PWA project being built, taken from the package.json `name` field.
+     */
     constructor(resolver, localProjectName) {
         this._resolver = resolver;
         this._localProjectName = localProjectName;
@@ -72,10 +113,7 @@ class ModuleTransformConfig {
             : path.join(requestor, fileToTransform);
     }
     /**
-     *
-     * Add a request to transform a file in the build.
-     * @param {TransformRequest} req - Request object
-     * @memberof ModuleTransformConfig
+     * @borrows addTransform as add
      */
     add({ requestor, fileToTransform, transformModule, type, options }) {
         let absTransformModule;
@@ -113,6 +151,8 @@ class ModuleTransformConfig {
     }
     /**
      * Resolve paths and emit as JSON.
+     *
+     * @returns {object} Configuration object
      */
     async toLoaderOptions() {
         const byType = {
