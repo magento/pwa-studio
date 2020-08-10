c1c0abf44b8548594387664e7ad16a83e9e8d389
sirugh <rugh@adobe.com>
2020-08-10 11:59:31 -0500

    Only loop once over errors

--- packages/peregrine/lib/util/deriveErrorMessage.js
+++ packages/peregrine/lib/util/deriveErrorMessage.js
@@ -1,14 +1,27 @@
 /**
+ * Maps an error to a string message
+ *
+ * @param {Error} error the error to map
+ * @return {String} error message
+ */
+const toString = error => {
+    const { graphQLErrors, message } = error;
+
+    return graphQLErrors && graphQLErrors.length
+        ? graphQLErrors.map(({ message }) => message).join(', ')
+        : message;
+};
+
+/**
  * A function to derive an error string from an array of errors.
  */
 export const deriveErrorMessage = errors => {
-    const errorCollection = errors.filter(Boolean).map(error => {
-        const { graphQLErrors, message } = error;
-
-        return graphQLErrors && graphQLErrors.length
-            ? graphQLErrors.map(({ message }) => message).join(', ')
-            : message;
-    });
+    const errorCollection = [];
+    for (error of errors) {
+        if (error) {
+            errorCollection.push(toString(error));
+        }
+    }
 
     return errorCollection.join(', ');
 };
