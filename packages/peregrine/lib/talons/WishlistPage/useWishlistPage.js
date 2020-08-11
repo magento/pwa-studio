b0d4eedd09c98f0796b363f3df10de9f08eeab96
Tommy Wiebell <twiebell@adobe.com>
2020-08-11 15:27:45 -0500

    Stub out list creation a little more and give it a base style

--- packages/peregrine/lib/talons/WishlistPage/useWishlistPage.js
+++ packages/peregrine/lib/talons/WishlistPage/useWishlistPage.js
@@ -1,4 +1,5 @@
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import { useQuery } from '@apollo/react-hooks';
 import { useUserContext } from '../../context/user';
@@ -7,6 +8,7 @@ export const useWishlistPage = props => {
     const { queries } = props;
     const { getCustomerWishlistQuery } = queries;
 
+    const history = useHistory();
     const [{ isSignedIn }] = useUserContext();
 
     const { data } = useQuery(getCustomerWishlistQuery, { skip: !isSignedIn });
@@ -14,6 +16,12 @@ export const useWishlistPage = props => {
     const derivedWishlists = useMemo(() => {
         return (data && data.customer.wishlists) || [];
     }, [data]);
+
+    useEffect(() => {
+        if (!isSignedIn) {
+            history.push('/');
+        }
+    }, [history, isSignedIn]);
 
     return {
         wishlists: derivedWishlists
--- packages/venia-ui/lib/components/WishlistPage/createWishlist.ee.js
+++ packages/venia-ui/lib/components/WishlistPage/createWishlist.ee.js
@@ -1,3 +1,22 @@
-const CreateWishlist = () => '<CreateWishlist /> To be completed by PWA-629';
+import React from 'react';
+import { PlusSquare } from 'react-feather';
+
+import defaultClasses from './createWishlist.css';
+import { mergeClasses } from '../../classify';
+import Icon from '../Icon';
+
+const CreateWishlist = props => {
+    const classes = mergeClasses(defaultClasses, props.classes);
+    const createIcon = (
+        <Icon classes={{ icon: classes.icon }} src={PlusSquare} />
+    );
+
+    return (
+        <button className={classes.root}>
+            {createIcon}
+            <span>Create a list</span>
+        </button>
+    );
+};
 
 export default CreateWishlist;
