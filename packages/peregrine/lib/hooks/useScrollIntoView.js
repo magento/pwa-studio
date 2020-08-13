b518f7dda6caf3ed9dd8d93e2a95c9eb2c63fd66
sirugh <rugh@adobe.com>
2020-08-13 13:24:11 -0500

    feedback
    Signed-off-by: sirugh <rugh@adobe.com>

--- packages/peregrine/lib/hooks/useScrollIntoView.js
+++ packages/peregrine/lib/hooks/useScrollIntoView.js
@@ -1,27 +1,22 @@
 import { useEffect } from 'react';
 
+const OPTIONS_DEFAULTS = { behavior: 'smooth', block: 'center' };
+
 /**
  * Scrolls a ref into view on truthiness of a thing.
  *
  * @param {React.Ref} ref
- * @param {Any} thing triggers scrolling if truthy
- * @param {Object} options https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView
+ * @param {Boolean} shouldScroll allows scrolling if truthy
+ * @param {OPTIONS_DEFAULTS} options https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView
  */
 export const useScrollIntoView = (
     ref,
-    thing,
-    options = {
-        behavior: 'smooth',
-        block: 'center'
-    }
+    shouldScroll,
+    options = OPTIONS_DEFAULTS
 ) => {
     useEffect(() => {
-        if (
-            ref.current &&
-            typeof ref.current.scrollIntoView !== 'undefined' &&
-            !!thing
-        ) {
+        if (ref.current && ref.current instanceof HTMLElement && shouldScroll) {
             ref.current.scrollIntoView(options);
         }
-    }, [options, ref, thing]);
+    }, [options, ref, shouldScroll]);
 };
