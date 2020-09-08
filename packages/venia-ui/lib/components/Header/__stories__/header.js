6ac8327c1e1f48847de02844ee3f70530701761f
Andrew Terranova <aterrano@adobe.com>
2020-09-08 14:44:35 -0500

    Updates Storybook and unit test snapshots

--- packages/venia-ui/lib/components/Header/__stories__/header.js
+++ packages/venia-ui/lib/components/Header/__stories__/header.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { IntlProvider } from 'react-intl';
+
+import { ToastContextProvider } from '@magento/peregrine';
 
 import Header from '../header';
 import defaultClasses from '../header.css';
 
 const stories = storiesOf('Venia/Header', module);
 
-stories.add('Search Bar Closed', () => <Header classes={defaultClasses} />);
-
-stories.add('Search Bar Open', () => {
-    return <span>TBD</span>;
+stories.add('Default', () => {
+    return (
+        <IntlProvider locale="en-US">
+            <ToastContextProvider>
+                <Header classes={defaultClasses} />
+            </ToastContextProvider>
+        </IntlProvider>
+    );
 });
--- packages/venia-ui/lib/components/SearchBar/__stories__/searchBar.js
+++ packages/venia-ui/lib/components/SearchBar/__stories__/searchBar.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { IntlProvider } from 'react-intl';
+
 import SearchBar from '../searchBar';
 import defaultClasses from '../searchBar.css';
 
 const stories = storiesOf('Components/SearchBar', module);
 
 stories.add('Search Bar', () => (
-    <SearchBar classes={defaultClasses} isOpen={true} />
+    <IntlProvider locale="en-US">
+        <SearchBar classes={defaultClasses} isOpen={true} />
+    </IntlProvider>
 ));
