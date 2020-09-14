1694b1c389659e29323ada55f5dcb0cf2347d8c8
Huy Kon <huykon225@gmail.com>
2020-09-15 02:50:39 +0700

    [Fix bug] Category didn't refresh data when click back previous or next page of… (#2641)
    * Category didn't refresh data when click back previous or next page of browser
    
    * prettier code
    
    * replace useMemo by useEffect
    
    * lint code
    
    * Adds unit tests for usePagination hook
    
    * Fix tests
    
    * update initialPage
    
    * Sync pagination state to location immediately
    
    * remove useState unnecessary in test
    
    * Replace state instead of pushing it
    
    * Update comment
    
    Co-authored-by: Andrew Terranova <aterrano@adobe.com>
    Co-authored-by: Jimmy Sanford <jimbo@users.noreply.github.com>
    Co-authored-by: Devagouda <40405790+dpatil-magento@users.noreply.github.com>

--- packages/peregrine/lib/hooks/usePagination.js
+++ packages/peregrine/lib/hooks/usePagination.js
@@ -1,24 +1,24 @@
-import { useCallback, useMemo, useState } from 'react';
+import { useCallback, useMemo, useState, useEffect } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 
 import { getSearchParam } from './useSearchParam';
 
 /**
- * Sets a query parameter in history. Attempt to use React Router if provided
- * otherwise fallback to builtins.
+ * Sets a query parameter in history.
  *
  * @private
  */
-const setQueryParam = ({ history, location, parameter, value }) => {
+const setQueryParam = ({ history, location, parameter, replace, value }) => {
     const { search } = location;
     const queryParams = new URLSearchParams(search);
-    queryParams.set(parameter, value);
 
-    if (history.push) {
-        history.push({ search: queryParams.toString() });
+    queryParams.set(parameter, value);
+    const destination = { search: queryParams.toString() };
+
+    if (replace) {
+        history.replace(destination);
     } else {
-        // Use the native pushState. See https://developer.mozilla.org/en-US/docs/Web/API/History_API#The_pushState()_method
-        history.pushState('', '', `?${queryParams.toString()}`);
+        history.push(destination);
     }
 };
 
@@ -38,40 +38,42 @@ const defaultInitialPage = 1;
  * @param {String} config.namespace='' The namespace to append to config.parameter in the query. For example: ?namespace_parameter=value
  * @param {String} config.parameter='page' The name of the query parameter to use for page
  * @param {Number} config.initialPage The initial current page value
- * @param {Number} config.intialTotalPages=1 The total pages expected to be usable by this hook
+ * @param {Number} config.initialTotalPages=1 The total pages expected to be usable by this hook
  *
  * @return {Object[]} An array with two entries containing the following content: [ {@link PaginationState}, {@link API} ]
  */
 export const usePagination = (props = {}) => {
     const { namespace = '', parameter = 'page', initialTotalPages = 1 } = props;
-    const searchParam = namespace ? `${namespace}_${parameter}` : parameter;
 
     const history = useHistory();
     const location = useLocation();
-
-    // Fetch the initial page value from location to avoid initializing twice.
-    const initialPage =
-        props.initialPage ||
-        parseInt(getSearchParam(searchParam, location) || defaultInitialPage);
-
-    const [currentPage, setCurrentPage] = useState(initialPage);
     const [totalPages, setTotalPages] = useState(initialTotalPages);
 
-    const setPage = useCallback(
-        page => {
+    const searchParam = namespace ? `${namespace}_${parameter}` : parameter;
+    const initialPage = props.initialPage || defaultInitialPage;
+    const currentPage = parseInt(getSearchParam(searchParam, location));
+
+    // use the location to hold state
+    const setCurrentPage = useCallback(
+        (page, replace = false) => {
             // Update the query parameter.
             setQueryParam({
-                location,
                 history,
+                location,
                 parameter: searchParam,
+                replace,
                 value: page
             });
-
-            // Update the state object.
-            setCurrentPage(page);
         },
         [history, location, searchParam]
     );
+
+    // ensure the location contains a page number
+    useEffect(() => {
+        if (!currentPage) {
+            setCurrentPage(initialPage, true);
+        }
+    }, [currentPage, initialPage, setCurrentPage]);
 
     /**
      * The current pagination state
@@ -83,7 +85,10 @@ export const usePagination = (props = {}) => {
      * @property {Number} currentPage The current page number
      * @property {Number} totalPages The total number of pages
      */
-    const paginationState = { currentPage, totalPages };
+    const paginationState = {
+        currentPage: currentPage || initialPage,
+        totalPages
+    };
 
     /**
      * The API object used for modifying the PaginationState.
@@ -108,10 +113,10 @@ export const usePagination = (props = {}) => {
      */
     const api = useMemo(
         () => ({
-            setCurrentPage: setPage,
+            setCurrentPage,
             setTotalPages
         }),
-        [setPage, setTotalPages]
+        [setCurrentPage, setTotalPages]
     );
 
     return [paginationState, api];
