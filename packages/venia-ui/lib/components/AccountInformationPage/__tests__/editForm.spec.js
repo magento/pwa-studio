9e5d37f91edb86cd41bc0663cecf6195ae55cb8b
sirugh <rugh@adobe.com>
2020-09-14 10:38:09 -0500

    Update tests for editForm
    Signed-off-by: sirugh <rugh@adobe.com>

--- packages/venia-ui/lib/components/AccountInformationPage/__tests__/editForm.spec.js
+++ packages/venia-ui/lib/components/AccountInformationPage/__tests__/editForm.spec.js
@@ -5,7 +5,27 @@ import EditForm from '../editForm';
 
 jest.mock('../../../classify');
 
-test('renders form', () => {
-    const tree = createTestInstance(<EditForm />);
+const defaultProps = {
+    handleChangePassword: jest.fn(),
+    shouldShowNewPassword: false
+};
+
+test('renders form when shouldShowNewPassword is false', () => {
+    const props = {
+        ...defaultProps,
+        shouldShowNewPassword: false
+    };
+
+    const tree = createTestInstance(<EditForm {...props} />);
+    expect(tree.toJSON()).toMatchSnapshot();
+});
+
+test('renders form when shouldShowNewPassword is true ', () => {
+    const props = {
+        ...defaultProps,
+        shouldShowNewPassword: true
+    };
+
+    const tree = createTestInstance(<EditForm {...props} />);
     expect(tree.toJSON()).toMatchSnapshot();
 });
