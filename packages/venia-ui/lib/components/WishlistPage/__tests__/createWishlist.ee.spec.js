94b4a88089ae78cb41dd2c6dc0d8637febad70b3
Revanth Kumar <revanth0212@gmail.com>
2020-10-12 11:29:20 -0500

    Added create wish list tests.

--- packages/venia-ui/lib/components/WishlistPage/__tests__/createWishlist.ee.spec.js
+++ packages/venia-ui/lib/components/WishlistPage/__tests__/createWishlist.ee.spec.js
@@ -4,8 +4,22 @@ import { createTestInstance } from '@magento/peregrine';
 import CreateWishlist from '../createWishlist.ee';
 
 jest.mock('../../../classify');
+jest.mock('../../Dialog', () => props => (
+    <div componentName={'Dialog'} {...props} />
+));
+jest.mock(
+    '@magento/peregrine/lib/talons/WishlistPage/useCreateWishlist',
+    () => ({
+        useCreateWishlist: jest.fn().mockReturnValue({
+            createList: jest.fn(),
+            isModalOpen: false,
+            hideModal: jest.fn(),
+            showModal: jest.fn()
+        })
+    })
+);
 
-test('renders create button', () => {
+test('renders correctly', () => {
     const tree = createTestInstance(<CreateWishlist />);
 
     expect(tree.toJSON()).toMatchSnapshot();
