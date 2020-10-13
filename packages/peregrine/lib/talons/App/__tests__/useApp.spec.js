fdda72f3500b5c21dc2fdc46813f18c2d41bd9d6
James Calcaben <calcaben@adobe.com>
2020-10-13 16:39:04 -0500

    Add more tests

--- packages/peregrine/lib/talons/App/__tests__/useApp.spec.js
+++ packages/peregrine/lib/talons/App/__tests__/useApp.spec.js
@@ -1,27 +1,38 @@
 import React from 'react';
-import { createTestInstance } from '@magento/peregrine'
+import { createTestInstance } from '@magento/peregrine';
+import { act } from 'react-test-renderer';
 
-import { useApp } from '../useApp'
+import { useApp } from '../useApp';
 
-const handleError = jest.fn((error) => {})
-const handleIsOffline = jest.fn()
-const handleIsOnline = jest.fn()
-const handleHTMLUpdate = jest.fn()
-const markErrorHandled = jest.fn((error)=> {})
-const renderError = {}
-const unhandledErrors = []
+import { useAppContext } from '@magento/peregrine/lib/context/app';
 
+const handleError = jest.fn((error, id, loc, dismisser) => {
+    dismisser();
+});
+const handleIsOffline = jest.fn();
+const handleIsOnline = jest.fn();
+const handleHTMLUpdate = jest.fn();
+const markErrorHandled = jest.fn(error => {});
+const renderError = {};
+const unhandledErrors = [];
+
+const reload = jest.fn();
 
 jest.mock('@magento/peregrine/lib/context/app', () => {
     const state = {
-    }
+        drawerClosed: false
+    };
 
-    const api = {}
+    const api = {
+        closeDrawer: jest.fn(() => {
+            state.drawerClosed = true
+        })
+    };
 
-    const useAppContext = jest.fn(() => [state, api])
+    const useAppContext = jest.fn(() => [state, api]);
 
-    return { useAppContext }
-})
+    return { useAppContext };
+});
 
 const log = jest.fn();
 
@@ -30,7 +41,7 @@ const Component = props => {
     log(talonProps);
 
     return <i />;
-}
+};
 
 const mockProps = {
     handleError,
@@ -39,13 +50,77 @@ const mockProps = {
     handleHTMLUpdate,
     markErrorHandled,
     renderError,
-    unhandledErrors 
-}
+    unhandledErrors
+};
+
+// JSDom does not support navigation, so it needs to be mocked
+const mockWindowLocation = {
+    reload: reload
+};
+
+let oldWindowLocation;
+beforeEach(() => {
+    oldWindowLocation = window.location;
+    delete window.location;
+    window.location = mockWindowLocation;
+    mockWindowLocation.reload.mockClear();
+});
+afterEach(() => {
+    window.location = oldWindowLocation;
+});
 
 test('returns the correct shape', () => {
-    createTestInstance(<Component {...mockProps} />)
+    createTestInstance(<Component {...mockProps} />);
 
     const talonProps = log.mock.calls[0][0];
 
     expect(talonProps).toMatchSnapshot();
-})
+});
+
+test('handle render error', () => {
+    const error = new Error('Render error');
+
+    const props = {
+        ...mockProps,
+        renderError: error
+    };
+
+    createTestInstance(<Component {...props} />);
+
+    expect(handleError).toHaveBeenCalledTimes(1);
+    expect(reload).toHaveBeenCalledTimes(1);
+});
+
+test('handle render error in development environment', () => {
+    const error = new Error('Render error');
+
+    const props = {
+        ...mockProps,
+        renderError: error
+    };
+
+    process.env.NODE_ENV = 'development';
+
+    createTestInstance(<Component {...props} />);
+
+    expect(handleError).toHaveBeenCalledTimes(1);
+    expect(reload).toHaveBeenCalledTimes(0);
+});
+
+test('handle drawer closing', () => {
+    createTestInstance(<Component {...mockProps} />);
+
+    const talonProps = log.mock.calls[0][0];
+
+    const { handleCloseDrawer } = talonProps;
+
+    const [appState] = useAppContext();
+
+    expect(appState.drawerClosed).toBeFalsy();
+
+    act(() => {
+        handleCloseDrawer();
+    });
+
+    expect(appState.drawerClosed).toBeTruthy();
+});
