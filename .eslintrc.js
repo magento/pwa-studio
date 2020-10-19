c00ebe187386fee8c0be6aa976b268a5909f344f
sirugh <rugh@adobe.com>
2020-10-19 17:01:10 -0500

    Add no jsx-literal rule to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,18 @@
 const config = {
     parser: 'babel-eslint',
+    plugins: ['react'],
     extends: ['@magento'],
     rules: {
         'no-undef': 'off',
-        'no-useless-escape': 'off'
+        'no-useless-escape': 'off',
+        'react/jsx-no-literals': [
+            'error',
+            {
+                allowedStrings: [],
+                ignoreProps: true,
+                noStrings: true
+            }
+        ]
     }
 };
 
