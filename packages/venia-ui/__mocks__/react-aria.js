eef6826ab7a0e1afe40b16435f8d53f5e0567a1e
Jimmy Sanford <jimbo@users.noreply.github.com>
2020-10-28 14:00:31 -0700

    Lint

--- packages/venia-ui/__mocks__/react-aria.js
+++ packages/venia-ui/__mocks__/react-aria.js
@@ -1,9 +1,9 @@
-import { Fragment } from "react"
+import { Fragment } from 'react';
 
-const reactAria = jest.requireActual("react-aria")
-const FocusScope = Fragment
+const reactAria = jest.requireActual('react-aria');
+const FocusScope = Fragment;
 
 module.exports = {
     ...reactAria,
     FocusScope
-}
+};
