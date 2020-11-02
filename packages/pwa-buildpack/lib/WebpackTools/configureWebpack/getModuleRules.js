b3212ad8892a3456993e105cc93673d7951415f8
Andrew Terranova <aterrano@adobe.com>
2020-11-02 09:27:21 -0600

    Reverts change to enable Storybook as out of scope

--- packages/pwa-buildpack/lib/WebpackTools/configureWebpack/getModuleRules.js
+++ packages/pwa-buildpack/lib/WebpackTools/configureWebpack/getModuleRules.js
@@ -56,7 +56,7 @@ getModuleRules.js = async ({
     const astLoaders = [
         {
             // Use custom loader to enable warning reporting from Babel plugins
-            loader: 'babel-loader',
+            loader: 'buildbus-babel-loader',
             options: {
                 sourceMaps: mode === 'development' && 'inline',
                 envName: mode,
