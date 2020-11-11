d25c0f7fb80a7c745c76d301162e6963842171fd
Jimmy Sanford <jimbo@users.noreply.github.com>
2020-11-10 18:06:51 -0700

    Use Apollo for MagentoRoute

---
+++ packages/peregrine/lib/talons/MagentoRoute/magentoRoute.gql.js
@@ -0,0 +1,12 @@
+import { gql } from '@apollo/client';
+
+export const RESOLVE_URL = gql`
+    query ResolveURL($url: String!) {
+        urlResolver(url: $url) {
+            id
+            relative_url
+            redirectCode
+            type
+        }
+    }
+`;
--- packages/peregrine/lib/talons/MagentoRoute/useMagentoRoute.js
+++ packages/peregrine/lib/talons/MagentoRoute/useMagentoRoute.js
@@ -1,52 +1,33 @@
 import { useEffect, useRef, useState } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
-import { useApolloClient, useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 
 import getRouteComponent from './getRouteComponent';
+import { RESOLVE_URL } from './magentoRoute.gql';
 
 const CODE_PERMANENT_REDIRECT = 301;
 const CODE_TEMPORARY_REDIRECT = 302;
-const REDIRECT_CODES = [CODE_PERMANENT_REDIRECT, CODE_TEMPORARY_REDIRECT];
+const REDIRECT_CODES = new Set()
+    .add(CODE_PERMANENT_REDIRECT)
+    .add(CODE_TEMPORARY_REDIRECT);
 
 const talonResponses = {
     ERROR: routeError => ({ hasError: true, routeError }),
     LOADING: { isLoading: true },
     NOT_FOUND: { isNotFound: true },
-    FOUND: (component, id, type, store) => ({ component, id, type, store }),
+    FOUND: (component, id, type) => ({ component, id, type }),
     REDIRECT: relativeUrl => ({ isRedirect: true, relativeUrl })
 };
 
-const shouldFetch = (data, store) => {
-    // Should fetch if we don't have any data.
-    if (!data) return true;
-
-    // Should fetch again following a prior failure.
-    if (data.isNotFound && navigator.onLine) {
-        return true;
-    }
-
-    // If we have data for the route, but the stores don't match fetch the correct route
-    return !!(store && data.id && data.store !== store);
-};
+const getRouteKey = (pathname, store) => `[${store}]--${pathname}`;
 
 export const useMagentoRoute = props => {
     const { getStoreCode } = props;
-    const [componentMap, setComponentMap] = useState(new Map());
-    const { apiBase } = useApolloClient();
     const history = useHistory();
     const { pathname } = useLocation();
     const isMountedRef = useRef(false);
-    const routeData = componentMap.get(pathname);
+    const [responses, setResponses] = useState(new Map());
 
-    const { data } = useQuery(getStoreCode, {
-        fetchPolicy: 'cache-and-network',
-        nextFetchPolicy: 'cache-first'
-    });
-
-    const store = data && data.storeConfig.code;
-
-    // Keep track of whether we have been mounted yet.
-    // Note that we are not unmounted on page transitions.
     useEffect(() => {
         isMountedRef.current = true;
 
@@ -55,49 +36,69 @@ export const useMagentoRoute = props => {
         };
     }, []);
 
-    // If the entry for this pathname is a redirect, perform the redirect.
+    const storeCodeResult = useQuery(getStoreCode, {
+        fetchPolicy: 'cache-and-network',
+        nextFetchPolicy: 'cache-first'
+    });
+
+    const { data: storeCodeData } = storeCodeResult;
+    const store = storeCodeData && storeCodeData.storeConfig.code;
+    const routeKey = getRouteKey(pathname, store);
+    const routeData = responses.get(routeKey);
+
+    const urlResult = useQuery(RESOLVE_URL, {
+        fetchPolicy: 'cache-and-network',
+        nextFetchPolicy: 'cache-first',
+        variables: { url: pathname }
+    });
+
+    // fetch a component if necessary
+    useEffect(() => {
+        const { data, error, loading } = urlResult;
+
+        // exit if we've unmounted, or if we don't have data
+        if (!isMountedRef.current || loading || !data || !store) return;
+
+        // the result may be null or incomplete, amounting to a 404
+        const { urlResolver } = data;
+        const { id, redirectCode, relative_url, type } = urlResolver || {};
+        const isNotFound = !urlResolver || !id || !type || id === -1;
+        const isRedirect = REDIRECT_CODES.has(redirectCode);
+
+        getRouteComponent(type, id)
+            .then(component => {
+                // throw Apollo errors to the catch block
+                if (error) throw error;
+
+                return isNotFound
+                    ? talonResponses.NOT_FOUND
+                    : isRedirect
+                    ? talonResponses.REDIRECT(relative_url)
+                    : talonResponses.FOUND(component, id, type);
+            })
+            .catch(error => {
+                // catch errors from url resolution and component fetching
+                return talonResponses.ERROR(error);
+            })
+            .then(nextValue => {
+                if (!isMountedRef.current || !nextValue) return;
+
+                // set the response in state, even if it's an error
+                setResponses(prevMap =>
+                    new Map(prevMap).set(
+                        getRouteKey(pathname, store),
+                        nextValue
+                    )
+                );
+            });
+    }, [pathname, setResponses, store, urlResult]);
+
+    // perform a redirect if necesssary
     useEffect(() => {
         if (routeData && routeData.isRedirect) {
             history.replace(routeData.relativeUrl);
         }
-    }, [componentMap, history, pathname, routeData]);
-
-    // ask Magento for a RootComponent that matches the current pathname
-    useEffect(() => {
-        // Avoid setting state if unmounted.
-        if (!isMountedRef.current) {
-            return;
-        }
-
-        if (shouldFetch(routeData, store)) {
-            getRouteComponent(apiBase, pathname, store).then(
-                ({
-                    component,
-                    id,
-                    pathname,
-                    redirectCode,
-                    relativeUrl,
-                    routeError,
-                    type
-                }) => {
-                    // Update our Map in local state for this path.
-                    setComponentMap(prevMap => {
-                        const nextMap = new Map(prevMap);
-
-                        const nextValue = routeError
-                            ? talonResponses.ERROR(routeError)
-                            : id === -1
-                            ? talonResponses.NOT_FOUND
-                            : REDIRECT_CODES.includes(redirectCode)
-                            ? talonResponses.REDIRECT(relativeUrl)
-                            : talonResponses.FOUND(component, id, type, store);
-
-                        return nextMap.set(pathname, nextValue);
-                    });
-                }
-            );
-        }
-    }, [apiBase, componentMap, history, pathname, routeData, store]);
+    }, [history, pathname, routeData]);
 
     return routeData || talonResponses.LOADING;
 };
