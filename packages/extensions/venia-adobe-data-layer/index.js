5c82e47b71e7fc23662722381a5fe466748a4923
Jimmy Sanford <jimbo@users.noreply.github.com>
2020-11-18 12:49:06 -0700

    Move definition to the effect

--- packages/extensions/venia-adobe-data-layer/index.js
+++ packages/extensions/venia-adobe-data-layer/index.js
@@ -2,6 +2,10 @@ const { useEffect } = require('react');
 
 module.exports = original => props => {
     useEffect(() => {
+        // define the global as soon as possible
+        globalThis.adobeDataLayer = globalThis.adobeDataLayer || [];
+
+        // import the library as late as possible
         import(/* webpackChunkName: "acdl" */
         /* webpackMode: "lazy" */
         /* webpackPrefetch: true */
