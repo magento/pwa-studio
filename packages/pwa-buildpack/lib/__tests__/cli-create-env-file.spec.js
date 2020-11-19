f04d371e31fd6e5ae7a296ca090825874f2193e4
Revanth Kumar <revanth0212@gmail.com>
2020-11-19 12:38:34 -0600

    Updated tests to use snapshots.

--- packages/pwa-buildpack/lib/__tests__/cli-create-env-file.spec.js
+++ packages/pwa-buildpack/lib/__tests__/cli-create-env-file.spec.js
@@ -1,11 +1,32 @@
 jest.mock('fs');
-const { resolve } = require('path');
+
 const { writeFileSync } = require('fs');
 const dotenv = require('dotenv');
 jest.mock('../Utilities/getEnvVarDefinitions', () => () =>
     require('../../envVarDefinitions.json')
 );
 const createEnvCliBuilder = require('../cli/create-env-file');
+
+// jest.mock('path', () => {
+//     const path = jest.requireActual('path');
+
+//     return {
+//         ...path,
+//         resolve: jest.fn().mockReturnValue()
+//     };
+// });
+
+const RealToISOString = Date.prototype.toISOString;
+
+beforeAll(() => {
+    global.Date.prototype.toISOString = jest
+        .fn()
+        .mockReturnValue('2019-04-22T10:20:30Z');
+});
+
+afterAll(() => {
+    global.Date.prototype.toISOString = RealToISOString;
+});
 
 beforeEach(() => {
     jest.spyOn(console, 'warn').mockImplementation(() => {});
@@ -26,27 +47,21 @@ test('is a yargs builder', () => {
 test('creates and writes file', async () => {
     process.env.MAGENTO_BACKEND_URL = 'https://example.com/';
     await createEnvCliBuilder.handler({
-        directory: process.cwd() || '/'
+        directory: process.cwd()
     });
-    expect(writeFileSync).toHaveBeenCalledWith(
-        resolve(process.cwd() || '/', '.env'),
-        expect.stringContaining('https://example.com/'),
-        'utf8'
-    );
+    expect(writeFileSync).toHaveBeenCalled();
+    expect(writeFileSync.mock.calls[0]).toMatchSnapshot();
     expect(console.warn).toHaveBeenCalledWith(expect.stringContaining('wrote'));
 });
 
 test('creates and writes file with examples', async () => {
     process.env.MAGENTO_BACKEND_URL = 'https://example.com/';
     await createEnvCliBuilder.handler({
-        directory: process.cwd() || '/',
+        directory: process.cwd(),
         useExamples: true
     });
-    expect(writeFileSync).toHaveBeenCalledWith(
-        resolve(process.cwd() || '/', '.env'),
-        expect.stringContaining('https://example.com/'),
-        'utf8'
-    );
+    expect(writeFileSync).toHaveBeenCalled();
+    expect(writeFileSync.mock.calls[0]).toMatchSnapshot();
     expect(console.warn).toHaveBeenCalledWith(expect.stringContaining('wrote'));
     const writtenFile = writeFileSync.mock.calls[0][1];
     expect(dotenv.parse(writtenFile)).toMatchObject({
