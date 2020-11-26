342e5e75e38e8477bd5536db1b0e86726b0039a1
Ankur Raiyani <ankurvr7003@gmail.com>
2020-11-26 10:52:57 +0530

    Updated unit test

--- packages/peregrine/lib/talons/CheckoutPage/__tests__/useCheckoutPage.spec.js
+++ packages/peregrine/lib/talons/CheckoutPage/__tests__/useCheckoutPage.spec.js
@@ -592,15 +592,6 @@ test('resetReviewOrderButtonClicked should set reviewOrderButtonClicked to false
 });
 
 test('check availablePaymentMethods, if not implemented then show page level message', () => {
-    placeOrderMutationResult.mockReturnValueOnce([
-        () => {},
-        {
-            data: null,
-            loading: false,
-            error: null
-        }
-    ]);
-
     const { talonProps } = getTalonProps(props);
 
     expect(talonProps.availablePaymentMethods).toBeNull();
