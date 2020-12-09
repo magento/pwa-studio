33f0b908a33f0764d434a17093eb3ffc0e4c1c53
Jimmy Sanford <jimbo@users.noreply.github.com>
2020-12-09 12:27:00 -0700

    Augment persistence API

--- packages/peregrine/lib/store/middleware/auth.js
+++ packages/peregrine/lib/store/middleware/auth.js
@@ -2,7 +2,7 @@ import BrowserPersistence from '../../util/simplePersistence';
 import userActions, { signOut } from '../actions/user';
 
 const timers = new Map();
-const { KEY } = BrowserPersistence;
+const storage = new BrowserPersistence();
 const SET_TOKEN = userActions.setToken.toString();
 const CLEAR_TOKEN = userActions.clearToken.toString();
 const GET_DETAILS = userActions.getDetails.request.toString();
@@ -22,7 +22,7 @@ const scheduleSignOut = store => next => action => {
     if (isSigningIn(action.type)) {
         // `BrowserPersistence.getItem()` only returns the value
         // but we need the full item with timestamp and ttl
-        const item = localStorage.getItem(`${KEY}__signin_token`);
+        const item = storage.getRawItem('signin_token');
 
         // exit if there's nothing in storage
         if (!item) return next(action);
@@ -30,7 +30,7 @@ const scheduleSignOut = store => next => action => {
         const { timeStored, ttl, value } = JSON.parse(item);
         const parsedValue = JSON.parse(value);
         const elapsed = Date.now() - timeStored;
-        const delay = Math.min(ttl * 1000 - elapsed, 0);
+        const delay = Math.max(ttl * 1000 - elapsed, 0);
 
         // only set one timer per token
         if (!timers.has(parsedValue)) {
