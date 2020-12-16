71a4f64413686dbb42c9066b326e5dc4b1d739c0
Revanth Kumar <revanth0212@gmail.com>
2020-12-16 11:07:01 -0600

    Minor.

--- packages/venia-ui/lib/components/SavedPaymentsPage/paymentCard.js
+++ packages/venia-ui/lib/components/SavedPaymentsPage/paymentCard.js
@@ -6,6 +6,12 @@ import paymentCardMapper from './savedPaymentTypes';
 const PaymentCard = props => {
     const PaymentComponent = paymentCardMapper[props.payment_method_code];
 
+    if (!PaymentComponent) {
+        /**
+         * Will be handled in https://jira.corp.magento.com/browse/PWA-1202
+         */
+    }
+
     return <PaymentComponent {...props} />;
 };
 
