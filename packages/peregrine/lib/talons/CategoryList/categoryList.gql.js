d8c5b8bc04131ab23b20c6493c6558baab9d1eba
huykon225 <huykon225@gmail.com>
2020-12-27 20:59:43 +0700

    move category params query to handle by CategoryTree fragment

--- packages/peregrine/lib/talons/CategoryList/categoryList.gql.js
+++ packages/peregrine/lib/talons/CategoryList/categoryList.gql.js
@@ -4,20 +4,22 @@ export const GET_CATEGORY_LIST = gql`
     query GetCategoryList($id: Int!) {
         category(id: $id) {
             id
-            children {
-                id
-                name
-                url_key
-                url_path
-                url_suffix
-                children_count
-                path
-                image
-                productImagePreview: products(pageSize: 1) {
-                    items {
-                        id
-                        small_image {
-                            url
+            ... on CategoryTree {
+                children {
+                    id
+                    name
+                    url_key
+                    url_path
+                    url_suffix
+                    children_count
+                    path
+                    image
+                    productImagePreview: products(pageSize: 1) {
+                        items {
+                            id
+                            small_image {
+                                url
+                            }
                         }
                     }
                 }
--- packages/peregrine/lib/talons/CategoryTree/categoryTree.gql.js
+++ packages/peregrine/lib/talons/CategoryTree/categoryTree.gql.js
@@ -4,19 +4,21 @@ export const GET_NAVIGATION_MENU = gql`
     query GetNavigationMenu($id: Int!) {
         category(id: $id) {
             id
-            name
-            children {
-                children_count
-                id
-                include_in_menu
+            ... on CategoryTree {
                 name
-                position
+                children {
+                    children_count
+                    id
+                    include_in_menu
+                    name
+                    position
+                    url_path
+                    url_suffix
+                }
+                include_in_menu
                 url_path
                 url_suffix
             }
-            include_in_menu
-            url_path
-            url_suffix
         }
     }
 `;
