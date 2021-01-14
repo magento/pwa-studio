68d00e468809ab6ff337c3f22844ab196c078bc9
Anthoula Wojczak <awojczak@adobe.com>
2021-01-14 16:45:25 -0600

    PWA-1065: Media Url generation should use store code header but have fallback to default like other store config queries
    - use global value from AVAILABLE_STORE_VIEWS for mediaBackend default

--- packages/venia-ui/lib/util/makeUrl.js
+++ packages/venia-ui/lib/util/makeUrl.js
@@ -6,9 +6,16 @@ const storage = new BrowserPersistence();
 const htmlDataset = document.querySelector('html').dataset;
 const { imageOptimizingOrigin } = htmlDataset;
 // Protect against potential falsy values for `mediaBackend`.
+const storeCode = storage.getItem('store_view_code') || STORE_VIEW_CODE;
+const storeSecureBaseMediaUrl = {};
+
+AVAILABLE_STORE_VIEWS.forEach(store => {
+    storeSecureBaseMediaUrl[store.code] = store.secure_base_media_url;
+});
+
 let mediaBackend =
     storage.getItem('store_view_secure_base_media_url') ||
-    htmlDataset.mediaBackend;
+    storeSecureBaseMediaUrl[storeCode];
 if (!mediaBackend) {
     console.warn('A media backend URL should be defined in your config.');
     mediaBackend = 'https://backend.test/media/';
