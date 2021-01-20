aed7252d3642fc7c8e24184ab7420cb7467d6193
James Delorey <delorey@adobe.com>
2021-01-20 11:45:50 -0600

    #2953 initialize acdl array as early as possible

--- packages/extensions/venia-adobe-data-layer/index.js
+++ packages/extensions/venia-adobe-data-layer/index.js
@@ -1,10 +1,9 @@
 const { useEffect } = require('react');
 
 module.exports = original => props => {
+    // define the global as soon as possible
+    globalThis.adobeDataLayer = globalThis.adobeDataLayer || [];
     useEffect(() => {
-        // define the global as soon as possible
-        globalThis.adobeDataLayer = globalThis.adobeDataLayer || [];
-
         // import the library as late as possible
         import(/* webpackChunkName: "acdl" */
         /* webpackMode: "lazy" */
