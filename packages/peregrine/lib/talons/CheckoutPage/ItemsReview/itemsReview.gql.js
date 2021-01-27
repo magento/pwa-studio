111ef663f1aa61f685cb595b7b38aa96e8adbef6
sirugh <rugh@adobe.com>
2021-01-27 10:49:50 -0600

    Re-add lost query

--- packages/peregrine/lib/talons/CheckoutPage/ItemsReview/itemsReview.gql.js
+++ packages/peregrine/lib/talons/CheckoutPage/ItemsReview/itemsReview.gql.js
@@ -1,5 +1,7 @@
 import { gql } from '@apollo/client';
 
+import { ItemsReviewFragment } from './itemsReviewFragments.gql';
+
 export const GET_CONFIGURABLE_THUMBNAIL_SOURCE = gql`
     query getConfigurableThumbnailSource {
         storeConfig {
@@ -9,6 +11,19 @@ export const GET_CONFIGURABLE_THUMBNAIL_SOURCE = gql`
     }
 `;
 
+
+export const LIST_OF_PRODUCTS_IN_CART_QUERY = gql`
+    query getItemsInCart($cartId: String!) {
+        cart(cart_id: $cartId) {
+            id
+            ...ItemsReviewFragment
+        }
+    }
+
+    ${ItemsReviewFragment}
+`;
+
 export default {
-    getConfigurableThumbnailSource: GET_CONFIGURABLE_THUMBNAIL_SOURCE
+    getConfigurableThumbnailSource: GET_CONFIGURABLE_THUMBNAIL_SOURCE,
+    getItemsInCart: LIST_OF_PRODUCTS_IN_CART_QUERY
 };
