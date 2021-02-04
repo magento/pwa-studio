9d4a9e05c7002fb07bce4545b242992c456566e1
Andrew Terranova <aterrano@adobe.com>
2021-02-04 12:34:24 -0600

    Initial implementation. Removes matches from inactive caches

--- packages/peregrine/lib/Apollo/deleteCacheEntry.js
+++ packages/peregrine/lib/Apollo/deleteCacheEntry.js
@@ -6,6 +6,11 @@
  * @param {Function} predicate a matching function
  */
 export const deleteCacheEntry = async (client, predicate) => {
+    await deleteActiveCacheEntry(client, predicate);
+    await deleteInactiveCacheEntry(predicate);
+};
+
+const deleteActiveCacheEntry = async (client, predicate) => {
     // If there is no client or cache then just back out since it doesn't matter :D
     if (
         !client ||
@@ -21,14 +26,48 @@ export const deleteCacheEntry = async (client, predicate) => {
         return;
     }
 
+    // Remove from the active cache.
     Object.keys(client.cache.data.data).forEach(key => {
         if (predicate(key)) {
             client.cache.data.delete(key);
         }
     });
 
-    // Immediately persist the cache changes.
+    // Immediately persist the cache changes to the active cache storage.
     if (client.persistor) {
         await client.persistor.persist();
     }
+};
+
+const deleteInactiveCacheEntry = async (client, predicate) => {
+    const activeApolloCacheLocalStorageKey =
+        client.persistor.persistor.storage.key;
+
+    const isAnInactiveApolloCache = ([key]) => {
+        // TODO: how do we keep this in sync with the storefront?
+        return (
+            key.startsWith('apollo-cache-persist') &&
+            key !== activeApolloCacheLocalStorageKey
+        );
+    };
+
+    Object.entries(localStorage)
+        .filter(isAnInactiveApolloCache)
+        .forEach(([inactiveCacheKey, inactiveCacheValue]) => {
+            const inactiveApolloCache = JSON.parse(inactiveCacheValue);
+
+            Object.keys(inactiveApolloCache).forEach(key => {
+                if (predicate(key)) {
+                    delete inactiveApolloCache[key];
+                }
+            });
+
+            // We're done deleting keys that match the predicate,
+            // but we've only mutated the object in memory.
+            // Write the updated inactive cache back out to localStorage.
+            localStorage.setItem(
+                inactiveCacheKey,
+                JSON.stringify(inactiveApolloCache)
+            );
+        });
 };
