348a00287875b60c3a4342f96362f687f28bfe0d
Ryan Castañeda <castaneda.ry@gmail.com>
2021-02-18 08:27:07 +0800

    Null check for element styling
    Fixes for https://github.com/magento/pwa-studio/issues/3013

--- packages/pagebuilder/lib/parseStorageHtml.js
+++ packages/pagebuilder/lib/parseStorageHtml.js
@@ -107,7 +107,10 @@ const convertToInlineStyles = document => {
 
     Object.keys(styles).map(selector => {
         const element = document.querySelector(selector);
-
+        if (!element) {
+            return;
+        }
+        
         styles[selector].map(style => {
             element.setAttribute(
                 'style',
