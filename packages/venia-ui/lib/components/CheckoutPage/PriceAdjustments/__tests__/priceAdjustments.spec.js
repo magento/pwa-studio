9df6fd82a05ce5d3e054dfeb90bad03761af4722
Tommy Wiebell <twiebell@adobe.com>
2021-02-22 09:22:15 -0600

    Bail on component manual mocks for now until warning can be suppressed

--- packages/venia-ui/lib/components/CheckoutPage/PriceAdjustments/__tests__/priceAdjustments.spec.js
+++ packages/venia-ui/lib/components/CheckoutPage/PriceAdjustments/__tests__/priceAdjustments.spec.js
@@ -4,6 +4,10 @@ import { createTestInstance } from '@magento/peregrine';
 import PriceAdjustments from '../priceAdjustments';
 
 jest.mock('../../../../classify');
+jest.mock('../../../Accordion', () => ({
+    Accordion: props => <mock-Accordion {...props} />,
+    Section: props => <mock-Section {...props} />
+}));
 jest.mock('../../../CartPage/PriceAdjustments/CouponCode', () => 'CouponCode');
 jest.mock(
     '../../../CartPage/PriceAdjustments/giftCardSection',
@@ -13,7 +17,6 @@ jest.mock(
     '../../../CartPage/PriceAdjustments/GiftOptions',
     () => 'GiftOptions'
 );
-jest.mock('../../../Accordion');
 
 test('renders price adjustments', () => {
     const tree = createTestInstance(
