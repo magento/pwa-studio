b52ff86ea458acaf2c48c8c3902c209ab1ba3eac
Dev Patil <depatil@adobe.com>
2021-03-03 12:31:25 -0600

    mock images

--- venia-e2e-tests/cypress/integration/page-builder/home-page-mock-data-spec.js
+++ venia-e2e-tests/cypress/integration/page-builder/home-page-mock-data-spec.js
@@ -1,12 +1,17 @@
 /// <reference types="cypress" />
 
-const requiredExample = require('../../fixtures/venia-home-page')
+
 
 context('Assert Venia Home Page pagebuilder content', () => {
     describe('Assert banner element exists with slider', () => {
         it('.should() - make an assertion about the current subject', () => {
-            cy.intercept('GET', 'https://integration-5ojmyuq-jnz3dtiuj77ca.us-4.magentosite.cloud/graphql?query=query+GetCmsPage*', { fixture: 'venia-home-page.json' }).as('getCMSMockData')
-            cy.visit('https://integration-5ojmyuq-jnz3dtiuj77ca.us-4.magentosite.cloud/')
+
+            cy.intercept('GET', '**/venia-hero1.jpg*', { fixture: 'mock-image-data/venia-hero1.jpg' }).as('getCMSMockImage')
+            cy.intercept('GET', '**/venia-hero2.jpg*', { fixture: 'mock-image-data/venia-hero2.jpg' }).as('getCMSMockImage2')
+            cy.intercept('GET', '**/graphql?query=query+GetCmsPage*', { fixture: 'mock-json-data/venia-home-page.json' }).as('getCMSMockData')
+            cy.visit('/')
+            cy.wait(['@getCMSMockImage']).its('response.body')
+            cy.wait(['@getCMSMockImage2']).its('response.body')
             cy.wait(['@getCMSMockData']).its('response.body')
             cy.get('.venia-home-slider').invoke("text").should('contain', 'Shop the NewOuterwear Collection')
             cy.get('[class^="slider-bannerWrapper-"]').eq(0).invoke('attr', 'style').should('contain', '/media/venia-hero1.jpg?auto=webp&format=pjpg&quality=85")')
@@ -20,8 +25,8 @@ context('Assert Venia Home Page pagebuilder content', () => {
 
     describe('Assert Shop the look button on Home page is Functioning', () => {
         it('.should() - make an assertion about button functionality', () => {
-            //force click to avoid my account element interuption
-            cy.get('a[href*="/shop-the-look.html"]').eq(1).click({ force: true })
+            // force click to avoid my account element interuption
+            // cy.get('a[href*="/shop-the-look.html"]').eq(1).click({ force: true })
             // cy.url().should('include', '/shop-the-look.html?page=1')
             // cy.get('div[class^="category-categoryTitle-"]').invoke("text").should('contain', 'Shop The Look')
         })
