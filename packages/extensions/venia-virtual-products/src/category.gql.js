aaa04d0206d16c2c61074dc5fc13a65791e4a12a
Tommy Wiebell <twiebell@adobe.com>
2021-03-04 12:15:08 -0600

    Break queries into fragments so extension is less fragile to breakage

--- packages/extensions/venia-virtual-products/src/category.gql.js
+++ packages/extensions/venia-virtual-products/src/category.gql.js
@@ -1,5 +1,10 @@
 import { gql } from '@apollo/client';
 
+import {
+    CategoryFragment,
+    ProductsFragment
+} from '@magento/peregrine/lib/talons/RootComponents/Category/categoryFragments.gql';
+
 export const GET_CATEGORY = gql`
     query GetCategoriesWithSubType(
         $id: Int!
@@ -10,12 +15,7 @@ export const GET_CATEGORY = gql`
     ) {
         category(id: $id) {
             id
-            description
-            name
-            product_count
-            meta_title
-            meta_keywords
-            meta_description
+            ...CategoryFragment
         }
         products(
             pageSize: $pageSize
@@ -24,31 +24,12 @@ export const GET_CATEGORY = gql`
             sort: $sort
         ) {
             items {
-                # id is always required, even if the fragment includes it.
                 id
-                # TODO: Once this issue is resolved we can use a
-                # GalleryItemFragment here:
-                # https://github.com/magento/magento2/issues/28584
-                name
-                price {
-                    regularPrice {
-                        amount {
-                            currency
-                            value
-                        }
-                    }
-                }
-                small_image {
-                    url
-                }
-                url_key
-                url_suffix
                 sub_type
             }
-            page_info {
-                total_pages
-            }
-            total_count
+            ...ProductsFragment
         }
     }
+    ${CategoryFragment}
+    ${ProductsFragment}
 `;
--- packages/peregrine/lib/talons/RootComponents/Category/category.gql.js
+++ packages/peregrine/lib/talons/RootComponents/Category/category.gql.js
@@ -1,5 +1,7 @@
 import { gql } from '@apollo/client';
 
+import { CategoryFragment, ProductsFragment } from './categoryFragments.gql';
+
 export const GET_CATEGORY = gql`
     query GetCategories(
         $id: Int!
@@ -10,12 +12,7 @@ export const GET_CATEGORY = gql`
     ) {
         category(id: $id) {
             id
-            description
-            name
-            product_count
-            meta_title
-            meta_keywords
-            meta_description
+            ...CategoryFragment
         }
         products(
             pageSize: $pageSize
@@ -23,33 +20,11 @@ export const GET_CATEGORY = gql`
             filter: $filters
             sort: $sort
         ) {
-            items {
-                # id is always required, even if the fragment includes it.
-                id
-                # TODO: Once this issue is resolved we can use a
-                # GalleryItemFragment here:
-                # https://github.com/magento/magento2/issues/28584
-                name
-                price {
-                    regularPrice {
-                        amount {
-                            currency
-                            value
-                        }
-                    }
-                }
-                small_image {
-                    url
-                }
-                url_key
-                url_suffix
-            }
-            page_info {
-                total_pages
-            }
-            total_count
+            ...ProductsFragment
         }
     }
+    ${CategoryFragment}
+    ${ProductsFragment}
 `;
 
 export const GET_FILTER_INPUTS = gql`
