c19e153de5ffcf649b6de106871998ebf83c3716
himmer <himmer@himmers-MacBook-Pro.local>
2021-03-15 16:15:17 -0500

    updating test

--- packages/peregrine/lib/talons/Gallery/__tests__/useGalleryItem.spec.js
+++ packages/peregrine/lib/talons/Gallery/__tests__/useGalleryItem.spec.js
@@ -1,8 +1,7 @@
 import { useGalleryItem } from '../useGalleryItem';
 test('returns object handelLinkClick as a function', () => {
-    const expectedReturnValue = {
-        handleLinkClick: () => {}
-    };
     const props = useGalleryItem({});
-    expect(JSON.stringify(props)).toBe(JSON.stringify(expectedReturnValue));
+    expect(props).toEqual(
+        expect.objectContaining({ handleLinkClick: expect.any(Function) })
+    );
 });
