917aec15032f2b9a6f6714e93e7c054b091c1b1c
sirugh <rugh@adobe.com>
2021-03-18 10:44:05 -0500

    Remove unused prop
    Signed-off-by: sirugh <rugh@adobe.com>

--- packages/peregrine/lib/talons/ProductFullDetail/useProductFullDetail.js
+++ packages/peregrine/lib/talons/ProductFullDetail/useProductFullDetail.js
@@ -374,7 +374,6 @@ export const useProductFullDetail = props => {
             isMissingOptions ||
             isAddConfigurableLoading ||
             isAddSimpleLoading,
-        isSignedIn,
         mediaGalleryEntries,
         shouldShowWishlistButton:
             isSignedIn &&
