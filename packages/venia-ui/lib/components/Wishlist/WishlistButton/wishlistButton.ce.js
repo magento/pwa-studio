f53e537ae35b34546742348c89c95c74d93590c7
sirugh <rugh@adobe.com>
2021-03-22 16:48:49 -0500

    Test wishlistButton.ce
    Signed-off-by: sirugh <rugh@adobe.com>

--- packages/venia-ui/lib/components/Wishlist/WishlistButton/wishlistButton.ce.js
+++ packages/venia-ui/lib/components/Wishlist/WishlistButton/wishlistButton.ce.js
@@ -9,7 +9,7 @@ import { mergeClasses } from '@magento/venia-ui/lib/classify';
 import Icon from '@magento/venia-ui/lib/components/Icon';
 
 import defaultClasses from './wishlistButton.css';
-import { arrayOf, bool, number, shape, string } from 'prop-types';
+import { arrayOf, number, shape, string } from 'prop-types';
 
 const ErrorIcon = <Icon src={AlertCircle} attrs={{ width: 18 }} />;
 
@@ -90,7 +90,6 @@ WishlistButton.propTypes = {
     classes: shape({
         wishlistButton: string
     }),
-    disabled: bool,
     itemOptions: shape({
         entered_options: arrayOf(
             shape({
