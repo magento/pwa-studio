8c0391abbd48a52f396454d4b3475d44ed744926
Revanth Kumar <revanth0212@gmail.com>
2021-04-06 17:23:06 -0500

    Updated tests.

--- packages/peregrine/lib/talons/CartPage/PriceAdjustments/ShippingMethods/__tests__/useShippingMethods.spec.js
+++ packages/peregrine/lib/talons/CartPage/PriceAdjustments/ShippingMethods/__tests__/useShippingMethods.spec.js
@@ -62,7 +62,7 @@ const mockData = {
                 },
                 postcode: '78725',
                 region: {
-                    id: 'TX'
+                    code: 'TX'
                 },
                 selected_shipping_method: {
                     carrier_code: 'tablerate',
