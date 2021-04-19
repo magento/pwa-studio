18207c30c999b2ea06bdc41f827779a659c70f7d
Dev Patil <depatil@adobe.com>
2021-04-19 10:19:38 -0500

    wip

--- venia-integration-tests/src/actions/accountAccess/index.js
+++ venia-integration-tests/src/actions/accountAccess/index.js
@@ -33,11 +33,52 @@ export const login = (username, password) => {
  * Utility function to open the login dialog
  */
 export const openLoginDialog = () => {
-    cy.visit('/');
-
-    cy.wait(500);
-
     // open the signin dialog
-    cy.get(loginTriggerField).click();
+    cy.get(accountTriggerButton).click();
 };
 
+/**
+ * Utility function to create account in Venia using the
+ * username and password provided.
+ *
+ * @param {String} firstName firstName to createAccount
+ * @param {String} lastName lastName to createAccount
+ * @param {String} accountEmail accountEmail to createAccount
+ * @param {String} accountPassword accountPassword to createAccount
+ */
+export const createAccount = (firstName, lastName, accountEmail, accountPassword) => {
+    // Enter password into the password field
+    cy.get(createAccountInitiateButton).click;
+
+    // Enter username into the username field
+    cy.get(firstNameTextField).type(firstName);
+
+    // Enter password into the password field
+    cy.get(lastNameTextField).type(lastName);
+
+    // Enter username into the username field
+    cy.get(createAccountEmailTextField).type(accountEmail);
+
+    // Enter password into the password field
+    cy.get(createAccountPasswordTextField).type(accountPassword);
+
+    // Enter password into the password field
+    cy.get(createAccountSubmitButton).click;
+};
+
+export const assertCreateAccount = (firstName, lastName, accountEmail, accountPassword) => {
+    // Enter username into the username field
+    cy.get(firstNameTextField).type(firstName);
+
+    // Enter password into the password field
+    cy.get(lastNameTextField).type(lastName);
+
+    // Enter username into the username field
+    cy.get(createAccountEmailTextField).type(accountEmail);
+
+    // Enter password into the password field
+    cy.get(createAccountPasswordTextField).type(accountPassword);
+
+    // Enter password into the password field
+    cy.get(createAccountSubmitButton).click;
+};
