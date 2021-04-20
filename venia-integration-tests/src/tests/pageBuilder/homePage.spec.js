4e48d03de21dc084de6a20ffbf3c46351744eca3
Revanth Kumar <revanth0212@gmail.com>
2021-04-20 16:36:58 -0500

    Added logic to capture multiple snapshots of full page.

--- venia-integration-tests/src/tests/pageBuilder/homePage.spec.js
+++ venia-integration-tests/src/tests/pageBuilder/homePage.spec.js
@@ -1,34 +1,49 @@
-
 /// <reference types="cypress" />
 
+const captureSnapshots = (index, num, options) => {
+    if (!num) {
+        return;
+    }
 
+    const { scrollByPx, testName } = options;
+    const snapName = `${testName} - Screenshot ${index}`;
+
+    cy.screenshot(snapName, {
+        capture: 'viewport'
+    })
+        .toMatchImageSnapshot()
+        .then(() => {
+            cy.scrollTo('0px', `${scrollByPx * index}px`);
+
+            cy.wait(1000);
+
+            captureSnapshots(index + 1, num - 1, options);
+        });
+};
+
+const captureFullPageSnapshot = testName => {
+    cy.document().then(doc => {
+        const docElm = doc.documentElement;
+        const { scrollHeight, clientHeight } = docElm;
+        const numOfScrolls = Math.ceil(scrollHeight / clientHeight);
+
+        captureSnapshots(1, numOfScrolls, {
+            scrollByPx: clientHeight,
+            testName
+        });
+    });
+};
 
 context('Assert Venia Home Page pagebuilder content', () => {
-    describe('Assert banner element exists with slider', () => {
-        it('.should() - make an assertion about the current subject', () => {
+    describe('Assert Page Builder content', () => {
+        it('should render page builder content correctly', () => {
+            cy.visit('/default/venia-tops.html?page=1');
 
-            // cy.intercept('GET', '**/venia-hero1.jpg*', { fixture: 'mock-image-data/venia-hero1.jpg' }).as('getCMSMockImage')
-            // cy.intercept('GET', '**/venia-hero2.jpg*', { fixture: 'mock-image-data/venia-hero2.jpg' }).as('getCMSMockImage2')
-            // cy.intercept('GET', '**/graphql?query=query+GetCmsPage*', { fixture: 'mock-json-data/venia-home-page.json' }).as('getCMSMockData')
-            cy.visit('/default/venia-tops.html?page=1')
-            // cy.wait(['@getCMSMockImage']).its('response.body')
-            // cy.wait(['@getCMSMockImage2']).its('response.body')
-            // cy.wait(['@getCMSMockData']).its('response.body')
-            cy.wait(5000);
-            // cy.get('.venia-home-slider').invoke("text").should('contain', 'Shop the NewOuterwear Collection')
-            // cy.get('[class^="slider-bannerWrapper-"]').eq(0).invoke('attr', 'style').should('contain', '/media/venia-hero1.jpg?auto=webp&format=pjpg&quality=85")')
-            // cy.get('[class^="slider-bannerWrapper-"]').parents('.slick-active').should('have.attr', 'data-index', '0').and('have.attr', 'aria-hidden', 'false')
-            // cy.get('ul[class="slick-dots"] > li').eq(1).click()
-            // cy.get('*[class^="slider-bannerWrapper-"]').eq(1).invoke('attr', 'style').should('contain', '/media/venia-hero2.jpg?auto=webp&format=pjpg&quality=85")')
-            // cy.get('[class^="slider-bannerWrapper-"]').parents('.slick-active').should('have.attr', 'data-index', '1').and('have.attr', 'aria-hidden', 'false')
-            // cy.get('ul[class="slick-dots"] > li').eq(1).should('have.class', 'slick-active')
-            // cy.matchImageSnapshot('homepage');
-            // cy.get('[class^="richContent-root-"]').eq(0).toMatchImageSnapshot('homepage');
-            // cy.get('[class^="richContent-root-"]').eq(1).toMatchImageSnapshot('homepage2');
-            // cy.get('[class^="richContent-root-"]').eq(2).toMatchImageSnapshot('homepage3');
-            // cy.get('[class^="richContent-root-"]').eq(3).toMatchImageSnapshot('homepage4');
-            // cy.get('[class^="richContent-root-"]').eq(4).toMatchImageSnapshot('homepage5');
-            cy.document().toMatchImageSnapshot('homepage7');
-        })
-    })
-})
+            cy.wait(3000);
+
+            captureFullPageSnapshot(
+                'should render page builder content correctly'
+            );
+        });
+    });
+});
