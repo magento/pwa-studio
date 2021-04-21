9fe9f710ebe5daf888442655502219b1825305d6
Tommy Wiebell <twiebell@adobe.com>
2021-04-21 17:25:54 -0500

    Break up frontend component code as well

--- packages/venia-ui/lib/components/Wishlist/GalleryButton/galleryButton.ee.js
+++ packages/venia-ui/lib/components/Wishlist/GalleryButton/galleryButton.ee.js
@@ -1,17 +1,14 @@
-import React, { Fragment, useEffect } from 'react';
-import { AlertCircle, Check, Heart, Info } from 'react-feather';
-import { useToasts } from '@magento/peregrine';
+import React, { Fragment } from 'react';
+import { Heart } from 'react-feather';
 import mergeClasses from '@magento/peregrine/lib/util/shallowMerge';
 import { useGalleryButton } from '@magento/peregrine/lib/talons/Wishlist/GalleryButton/useGalleryButton';
 
 import Icon from '../../Icon';
 import WishlistDialog from '../WishlistDialog';
 import defaultClasses from './galleryButton.css';
+import { useCommonToasts } from './useCommonToasts';
 
-const CheckIcon = <Icon size={20} src={Check} />;
-const ErrorIcon = <Icon size={20} src={AlertCircle} />;
 const HeartIcon = <Icon size={20} src={Heart} />;
-const InfoIcon = <Icon size={20} src={Info} />;
 
 const GalleryButton = props => {
     const talonProps = useGalleryButton(props);
@@ -24,32 +21,13 @@ const GalleryButton = props => {
         successToastProps
     } = talonProps;
 
-    const classes = mergeClasses(defaultClasses, props.classes);
-
-    const [, { addToast }] = useToasts();
-
-    useEffect(() => {
-        if (loginToastProps) {
-            addToast({ ...loginToastProps, icon: InfoIcon });
-        }
-    }, [addToast, loginToastProps]);
-
-    useEffect(() => {
-        if (successToastProps) {
-            addToast({ ...successToastProps, icon: CheckIcon });
-        }
-    }, [addToast, successToastProps]);
-
-    useEffect(() => {
-        if (errorToastProps) {
-            addToast({ ...errorToastProps, icon: ErrorIcon });
-        }
-    }, [addToast, errorToastProps]);
+    useCommonToasts({ errorToastProps, loginToastProps, successToastProps });
 
     const multipleWishlistDialog = modalProps ? (
         <WishlistDialog {...modalProps} />
     ) : null;
 
+    const classes = mergeClasses(defaultClasses, props.classes);
     const buttonClass = isSelected ? classes.root_selected : classes.root;
 
     return (
