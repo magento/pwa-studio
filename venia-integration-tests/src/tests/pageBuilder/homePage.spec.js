cf746e474634f602903ede6ad04680643ce78af1
Revanth Kumar <revanth0212@gmail.com>
2021-04-22 13:30:29 -0500

    Updated snapshot.

--- venia-integration-tests/src/tests/pageBuilder/homePage.spec.js
+++ venia-integration-tests/src/tests/pageBuilder/homePage.spec.js
@@ -3,7 +3,7 @@
 context('Assert Venia Home Page pagebuilder content', () => {
     describe('Assert Page Builder content', () => {
         it('should render page builder content correctly', () => {
-            cy.visit('/default/venia-tops.html?page=1');
+            cy.visit('/');
 
             cy.wait(3000);
 
