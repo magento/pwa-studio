622a2bb352d2f84da20fcad86c3a93951019d308
Revanth Kumar <revanth0212@gmail.com>
2021-05-17 14:48:55 -0500

    Added routes commands.

--- venia-integration-tests/src/support/index.js
+++ venia-integration-tests/src/support/index.js
@@ -2,6 +2,7 @@ import './visitPageCommand';
 import './fullPageScreenshotCommand';
 import './loadFullPageCommand';
 import './accountCommands';
+import './routesCommands';
 
 import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
 
