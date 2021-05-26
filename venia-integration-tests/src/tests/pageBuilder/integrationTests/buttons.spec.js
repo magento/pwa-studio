146da2e26e2c36a6eee808435515908cd497db60
Ievgen Kolesov <ikolesov@adobe.com>
2021-05-26 09:33:41 -0500

    PWA-1542: Verify Buttons

---
+++ venia-integration-tests/src/tests/pageBuilder/integrationTests/buttons.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe('verify pagebuilder buttons content is rendered correctly', () => {
+    it('verify buttons content', () => {
+        cy.intercept('GET', '**/graphql?query=query+GetCmsPage*', {fixture: 'pageBuilder/buttons/buttons.json'}).as('getCMSMockData')
+        cy.visit('/');
+        cy.wait(['@getCMSMockData']).its('response.body');
+        cy.loadFullPage().then(() => {
+            cy.captureFullPageScreenshot({
+                name: 'Page Builder Buttons Page',
+                timeout: 60000
+            });
+        });
+    });
+});
