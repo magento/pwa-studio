d60fa2c497a0c9092460e5531c1353a6ce34cc82
sirugh <rugh@adobe.com>
2021-05-26 14:20:10 -0500

    updates acording to dev

--- venia-integration-tests/src/tests/pageBuilder/integrationTests/image.spec.js
+++ venia-integration-tests/src/tests/pageBuilder/integrationTests/image.spec.js
@@ -1,11 +1,12 @@
+import { homePage } from '../../../fixtures/homePage/index';
+
 describe('pagebuilder > image', () => {
     it('renders properly', () => {
         cy.intercept('GET', '**/graphql?query=query+GetCmsPage*', {
             fixture: 'pageBuilder/image/image.json'
         }).as('getCMSMockData');
-        cy.visit('/');
+        cy.visit(homePage);
         cy.wait(['@getCMSMockData']).its('response.body');
-        cy.wait(5000);
         cy.loadFullPage().then(() => {
             cy.captureFullPageScreenshot({
                 name: 'Page Builder Image Snapshot',
