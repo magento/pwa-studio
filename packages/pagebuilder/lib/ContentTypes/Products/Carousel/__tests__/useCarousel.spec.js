0cca87fca1b959c172ee4cb67a21f032dc82f481
sirugh <rugh@adobe.com>
2021-05-28 10:36:48 -0500

    test carousel ce

--- packages/pagebuilder/lib/ContentTypes/Products/Carousel/__tests__/useCarousel.spec.js
+++ packages/pagebuilder/lib/ContentTypes/Products/Carousel/__tests__/useCarousel.spec.js
@@ -4,8 +4,16 @@ import { MockedProvider } from '@apollo/client/testing';
 import { renderHook } from '@testing-library/react-hooks';
 
 import typePolicies from '@magento/peregrine/lib/Apollo/policies';
-import { mockGetWishlistConfig } from '../__fixtures__/apolloMocks';
+import {
+    mockGetWishlistConfigCE,
+    mockGetWishlistConfigEE
+} from '../__fixtures__/apolloMocks';
+
 import { useCarousel } from '../useCarousel';
+
+import defaultOperations from '../carousel.gql';
+import ceOperations from '../carousel.gql.ce';
+import eeOperations from '../carousel.gql.ee';
 
 jest.mock(
     '@magento/peregrine/lib/hooks/useCustomerWishlistSkus/useCustomerWishlistSkus',
@@ -20,7 +28,7 @@ const cache = new InMemoryCache({
 
 const renderHookWithProviders = ({
     renderHookOptions = {},
-    mocks = [mockGetWishlistConfig]
+    mocks = []
 } = {}) => {
     const wrapper = ({ children }) => (
         <MockedProvider mocks={mocks} addTypename={true} cache={cache}>
@@ -31,8 +39,13 @@ const renderHookWithProviders = ({
     return renderHook(useCarousel, { wrapper, ...renderHookOptions });
 };
 
-test('returns store config', async () => {
-    const { result, waitForNextUpdate } = renderHookWithProviders();
+test('returns store config EE', async () => {
+    defaultOperations.getWishlistConfigQuery =
+        eeOperations.getWishlistConfigQuery;
+
+    const { result, waitForNextUpdate } = renderHookWithProviders({
+        mocks: [mockGetWishlistConfigEE]
+    });
 
     expect(result.current).toMatchInlineSnapshot(`
         Object {
@@ -46,6 +59,37 @@ test('returns store config', async () => {
         Object {
           "storeConfig": Object {
             "enable_multiple_wishlists": "1",
+            "id": 1,
+            "magento_wishlist_general_is_enabled": "1",
+          },
+        }
+    `);
+});
+
+test('returns store config CE', async () => {
+    defaultOperations.getWishlistConfigQuery =
+        ceOperations.getWishlistConfigQuery;
+
+    const { result, waitForNextUpdate } = renderHookWithProviders({
+        mocks: [mockGetWishlistConfigCE]
+    });
+
+    // I am unsure why this test renders without a loading state whereas the EE
+    // test renders with a loading (null) state.
+    expect(result.current).toMatchInlineSnapshot(`
+        Object {
+          "storeConfig": Object {
+            "id": 1,
+            "magento_wishlist_general_is_enabled": "1",
+          },
+        }
+    `);
+
+    await waitForNextUpdate();
+
+    expect(result.current).toMatchInlineSnapshot(`
+        Object {
+          "storeConfig": Object {
             "id": 1,
             "magento_wishlist_general_is_enabled": "1",
           },
