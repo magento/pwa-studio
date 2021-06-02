e1ebeb66f3bea5c3669b5039bb9e6c63d4a35d18
Dev Patil <depatil@adobe.com>
2021-06-02 15:01:28 -0500

    wip

--- venia-integration-tests/src/fixtures/wishlist/index.js
+++ venia-integration-tests/src/fixtures/wishlist/index.js
@@ -4,3 +4,7 @@ export const wishlistRemove = 'Remove';
 
 export const removeProductMessage =
     'Are you sure you want to delete this product from the list?';
+
+export const singleWishlistHeading = 'Remove';
+
+export const multipleWishlistHeading = 'Remove';
---
+++ venia-integration-tests/src/tests/wishList/integrationTests/verifyMultipleWishlistFeatures.spec.js
@@ -0,0 +1,64 @@
+import {
+    accountAccess as accountAccessFixtures,
+    homePage as homePageFixtures
+} from '../../../fixtures';
+import {
+    myAccountMenu as myAccountMenuAssertions
+} from '../../../assertions';
+
+const {
+    firstName,
+    lastName,
+    accountEmail,
+    accountPassword
+} = accountAccessFixtures;
+const { homePage } = homePageFixtures;
+
+
+const { assertCreateAccount } = myAccountMenuAssertions;
+
+
+// TODO add tags CE, EE to test to filter and run tests as needed
+describe('verify single wishlist basic features', () => {
+    it('user should be able to add and remove products from wishlist', () => {
+        cy.visitPage(homePage);
+
+        cy.openLoginDialog();
+        cy.createAccount(
+            accountAccessFixtures.firstName,
+            lastName,
+            accountEmail,
+            accountPassword
+        );
+
+        assertCreateAccount(firstName);
+
+        cy.intercept('GET', '**/graphql?query=query+getMultipleWishlistsEnabled*', {
+            fixture: 'wishlist/multipleWishlist/defaultPage.json'
+        }).as('getDefaultPage');
+        cy.visitPage(wishistRoute);
+        cy.wait(5000);
+        cy.wait(['@getDefaultPage']).its('response.body');
+
+        cy.intercept('POST', '**/graphql', {
+            fixture: 'wishlist/multipleWishlist/createWishlist.json'
+        }).as('createWishlist');
+        cy.intercept('GET', '**/graphql?query=query+GetCustomerWishlis*', {
+            fixture: 'wishlist/multipleWishlist/oneWishlistPage.json'
+        }).as('getCustomerWishlist');
+        createWishlist
+        cy.wait(['@createWishlist']).its('response.body');
+        cy.wait(['@getCustomerWishlist']).its('response.body');
+
+        cy.visitPage(categorySweaters);
+        addProductToWishlistFromCategoryPage(productCarinaCardigan);
+        cy.intercept('POST', '**/graphql', {
+            fixture: 'wishlist/multipleWishlist/createWishlist1.json'
+        }).as('createWishlist1');
+        createWishlistViaDialauge
+        cy.wait(['@createWishlist1']).its('response.body');
+
+        cy.visitPage(wishistRoute);
+        
+    });
+});
