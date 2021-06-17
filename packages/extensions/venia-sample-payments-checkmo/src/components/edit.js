2c1be239d5169098d6bedea99b87a442d89665fe
Andrew Terranova <aterrano@adobe.com>
2021-06-17 08:02:56 -0400

    Edit and Summary views

--- packages/extensions/venia-sample-payments-checkmo/src/components/edit.js
+++ packages/extensions/venia-sample-payments-checkmo/src/components/edit.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { shape, string, bool, func } from 'prop-types';
 
 import { mergeClasses } from '@magento/venia-ui/lib/classify';
 
 import CheckMo from './checkmo';
 import defaultClasses from './edit.css';
 
+/**
+ * The edit view for the Checkmo payment method.
+ */
 const EditCheckMo = props => {
     const {
         onPaymentReady,
@@ -30,3 +34,14 @@ const EditCheckMo = props => {
 };
 
 export default EditCheckMo;
+
+EditCheckMo.propTypes = {
+    classes: shape({
+        root: string
+    }),
+    onPaymentReady: func.isRequired,
+    onPaymentSuccess: func.isRequired,
+    onPaymentError: func.isRequired,
+    resetShouldSubmit: func.isRequired,
+    shouldSubmit: bool
+};
