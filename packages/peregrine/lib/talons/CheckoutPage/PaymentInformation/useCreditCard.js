285b02b3d07b4aeae762d936ee05c6d805202042
Lars Roettig <l.roettig@techdivision.com>
2021-06-24 13:14:10 +0200

    clean up

--- packages/peregrine/lib/talons/CheckoutPage/PaymentInformation/useCreditCard.js
+++ packages/peregrine/lib/talons/CheckoutPage/PaymentInformation/useCreditCard.js
@@ -34,8 +34,6 @@ export const mapAddressData = rawAddressData => {
             region
         } = rawAddressData;
 
-        debugger;
-
         return {
             firstName,
             lastName,
