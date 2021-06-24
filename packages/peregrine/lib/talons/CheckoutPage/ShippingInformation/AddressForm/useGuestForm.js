0379f8845da7db7b805d0a5248040216099c508e
Lars Roettig <l.roettig@techdivision.com>
2021-06-24 13:15:54 +0200

    clean up

--- packages/peregrine/lib/talons/CheckoutPage/ShippingInformation/AddressForm/useGuestForm.js
+++ packages/peregrine/lib/talons/CheckoutPage/ShippingInformation/AddressForm/useGuestForm.js
@@ -45,7 +45,7 @@ export const useGuestForm = props => {
                         address: {
                             ...address,
                             // region_id is used for field select and region is used for field input
-                            region: region.region_id || region.label,
+                            region: region.region_id || region.label || region.code,
                             country_code: country
                         }
                     }
