1c073f6a91d7d16d0f0134b6a3a70bdbe5a3fec6
Sofia Hernandez <sofiaguah@gmail.com>
2021-07-12 12:58:23 -0600

    gql mutation

--- packages/peregrine/lib/talons/Gallery/addToCart.gql.js
+++ packages/peregrine/lib/talons/Gallery/addToCart.gql.js
@@ -1,17 +1,6 @@
 import gql from 'graphql-tag';
 
 const ADD_PRODUCT_TO_CART = gql`
-mutation AddItemToCart(
-    $cartId: String!
-    $cartItem: CartItemInput!
-)
-{
-    addProductsToCart(cartId: $cartId, cartItems: [$cartItem]) {
-        cart {
-            id
-        }
-    }
-
 products(search: $searchString) {
     items {
       id
@@ -24,6 +13,19 @@ products(search: $searchString) {
     }
 `;
 
+const ADD_ITEM = gql `
+mutation AddItemToCart(
+    $cartId: String!
+    $cartItem: CartItemInput!
+)
+{
+    addProductsToCart(cartId: $cartId, cartItems: [$cartItem]) {
+        cart {
+            id
+        }
+    }
+`;
 export default {
-    ADD_PRODUCT_TO_CART
+    ADD_PRODUCT_TO_CART,
+    ADD_ITEM
 };
