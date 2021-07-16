68613f9278a6719409465c41d0aab97dc1734890
Andrew Terranova <aterrano@adobe.com>
2021-07-16 14:22:49 -0500

    Reverts accidental config change

--- jest.config.js
+++ jest.config.js
@@ -317,7 +317,7 @@ const jestConfig = {
     ],
     // Include files with zero tests in overall coverage analysis by specifying
     // coverage paths manually.
-    collectCoverage: false,
+    collectCoverage: true,
     collectCoverageFrom: [
         // Code directories
         'packages/*/{src,lib,_buildpack}/**/*.js',
