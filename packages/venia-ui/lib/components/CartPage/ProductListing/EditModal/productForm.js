b1e5b08333b4a667d25f33f0aa15e61cb67e3c75
Tommy Wiebell <twiebell@adobe.com>
2021-07-27 15:04:09 -0500

    Fix test so it correctly fails when the bug is present

--- packages/venia-ui/lib/components/CartPage/ProductListing/EditModal/productForm.js
+++ packages/venia-ui/lib/components/CartPage/ProductListing/EditModal/productForm.js
@@ -191,14 +191,6 @@ export const UPDATE_CONFIGURABLE_OPTIONS_MUTATION = gql`
         $variantSku: String!
         $quantity: Float!
     ) {
-        removeItemFromCart(
-            input: { cart_id: $cartId, cart_item_id: $cartItemId }
-        ) @connection(key: "removeItemFromCart") {
-            cart {
-                id
-            }
-        }
-
         addConfigurableProductsToCart(
             input: {
                 cart_id: $cartId
@@ -210,6 +202,14 @@ export const UPDATE_CONFIGURABLE_OPTIONS_MUTATION = gql`
                 ]
             }
         ) @connection(key: "addConfigurableProductsToCart") {
+            cart {
+                id
+            }
+        }
+
+        removeItemFromCart(
+            input: { cart_id: $cartId, cart_item_id: $cartItemId }
+        ) @connection(key: "removeItemFromCart") {
             cart {
                 id
                 ...CartPageFragment
