83cebfdaeb573a0894313957636d5b15a70eef68
Alexander Kras'ko <0m3r.mail@gmail.com>
2021-08-04 15:54:50 +0300

    Fixed the function paramater name

--- packages/pagebuilder/lib/ContentTypes/CustomContentTypeList.js
+++ packages/pagebuilder/lib/ContentTypes/CustomContentTypeList.js
@@ -21,11 +21,11 @@ class CustomContentTypeList {
         });
     }
     /**
-     * Add a rendering strategy to the Pagebuilder component.
+     * Add a rendering type to the Pagebuilder component.
      *
-     * @param {Object} strategy - Describes the rich content type to include
-     * @param {string} strategy.importPath - Import path to the CustomContentTypeRenderer module. Should be package-absolute.
-     * @param {string} strategy.contentType - Name that will be given to the imported renderer in generated code. This is used for debugging purposes.
+     * @param {Object} type - Describes the rich content type to include
+     * @param {string} type.importPath - Import path to the CustomContentTypeRenderer module. Should be package-absolute.
+     * @param {string} type.contentType - Name that will be given to the imported renderer in generated code. This is used for debugging purposes.
      */
     add(type) {
         if (
@@ -35,8 +35,8 @@ class CustomContentTypeList {
             !type.importPath
         ) {
             throw new Error(
-                `customContentTypes target: Argument is not a valid rich content type strategy.` +
-                    ` A valid strategy must have a JSX element name as "contentType" and a resolvable path to the renderer module as "importPath".`
+                `customContentTypes target: Argument is not a valid rich content type.` +
+                    ` A valid type must have a JSX element name as "contentType" and a resolvable path to the renderer module as "importPath".`
             );
         }
         const componentName = `${type.contentType}ContentType`;
