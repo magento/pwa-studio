96eaabc8dc8172272e463cde10c0fe77594bef39
Lars Roettig <l.roettig@techdivision.com>
2021-08-11 16:27:35 +0200

    PWA-1880: Add tailwind

---
+++ packages/venia-concept/tailwind.config.js
@@ -0,0 +1,19 @@
+const venia = {
+    mode: 'jit',
+    purge: {
+        content: ['./src/**/*.css'],
+        extractors: [
+            {
+                extractor: content => content.match(matcher) || [],
+                extensions: ['css']
+            }
+        ]
+    },
+    separator: '_'
+};
+
+const config = {
+    presets: [venia]
+};
+
+module.exports = config;
