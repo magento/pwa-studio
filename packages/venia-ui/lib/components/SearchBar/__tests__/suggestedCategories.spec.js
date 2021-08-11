d31ab81400634e6d229377f2556cb77598c5963c
Justin Conabree <jconabree@absolunet.com>
2021-08-11 17:36:10 -0400

    PWA-1974 fixing lint

--- packages/venia-ui/lib/components/SearchBar/__tests__/suggestedCategories.spec.js
+++ packages/venia-ui/lib/components/SearchBar/__tests__/suggestedCategories.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useHistory, useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { createTestInstance } from '@magento/peregrine';
 import SuggestedCategories from '../suggestedCategories';
 
