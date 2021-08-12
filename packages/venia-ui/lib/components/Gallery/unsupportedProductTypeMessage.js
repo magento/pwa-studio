76ae91ddf344c98b8decb3bbf32769027bcdbd50
Sofia Hernandez <sofiaguah@gmail.com>
2021-08-12 13:00:15 -0600

    prettify

--- packages/venia-ui/lib/components/Gallery/unsupportedProductTypeMessage.js
+++ packages/venia-ui/lib/components/Gallery/unsupportedProductTypeMessage.js
@@ -2,24 +2,16 @@ import React from 'react';
 import { Info } from 'react-feather';
 import { FormattedMessage } from 'react-intl';
 
-import { useAddToCartButton } from '@magento/peregrine/lib/talons/Gallery/useAddToCartButton';
-
+import { useAddToCartButton } from '@magento/peregrine/lib/talons/Gallery/useUnsupportedProductTypeMessage';
 
 const InfoIcon = <Icon size={20} src={Info} />;
 
-
 const UnsupportedProductTypeMessage = props => {
     const talonProps = useUnsupportedProductTypeMessage(props);
-    const {
-        isUnsupportedProductType
-        
-    } = talonProps;
+    const { isUnsupportedProductType} = talonProps;
     const classes = useStyle(defaultClasses, props.classes);
 
-    return (
-        < Icon className={classes}/>
-        
-    );
+    return <Icon className={classes} />;
 };
 
 export default UnsupportedProductTypeMessage;
@@ -28,7 +20,4 @@ UnsupportedProductTypeMessage.defaultProps = {
     icon: InfoIcon
 };
 
-UnsupportedProductTypeMessage.propTypes = {
-
-    
-};
+UnsupportedProductTypeMessage.propTypes = {};
