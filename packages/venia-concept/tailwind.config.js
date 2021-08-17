82666ca58500edf3d6fe21b44c74ef568967cd11
Lars Roettig <l.roettig@techdivision.com>
2021-08-17 13:32:59 +0200

    Update packages/venia-concept/tailwind.config.js
    Co-authored-by: Jimmy Sanford <jimbo@users.noreply.github.com>

--- packages/venia-concept/tailwind.config.js
+++ packages/venia-concept/tailwind.config.js
@@ -13,6 +13,10 @@ const venia = {}
 };
 
 const config = {
+    mode: 'jit',
+    purge: { /* ... */ },
+    separator: '_',
+    // if you created a theme, apply it here
     presets: [venia]
 };
 
