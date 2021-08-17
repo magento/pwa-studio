6f41361eaca9106206d4cecf08dcc143b948d0fe
mikhaelbois <mbois@absolunet.com>
2021-08-17 09:54:47 -0400

    PWA-1555 Update verify row video background test

--- venia-integration-tests/src/tests/integrationTests/pageBuilder/row.spec.js
+++ venia-integration-tests/src/tests/integrationTests/pageBuilder/row.spec.js
@@ -50,6 +50,42 @@ describe('verify pagebuilder row content', () => {
         }).as('getCMSMockData');
         cy.visitHomePage();
         cy.wait(['@getCMSMockData']).its('response.body');
+
+        // Row with youtube video in viewport
+        cy.get('div[class^="richContent-root"]')
+            .eq(0)
+            .find('iframe')
+            .should('exist')
+            .and('have.attr', 'src')
+            .and('contain', 'youtube.com');
+
+        // Row with youtube video outside viewport
+        cy.get('div[class^="richContent-root"]')
+            .eq(8)
+            .find('div[class^="richContent-root"]')
+            .eq(0)
+            .find('iframe')
+            .should('not.exist');
+
+        // Scroll to element to test iframe lazy load
+        cy.get('div[class^="richContent-root"]')
+            .eq(8)
+            .find('div[class^="richContent-root"]')
+            .eq(0)
+            .scrollIntoView({ duration: 2000 })
+            .find('iframe')
+            .should('exist');
+
+        // Scroll to bottom of the page to load all iframes
+        cy.scrollTo('bottom', { duration: 2000 });
+
+        // Hide iframes to prevent capturing moving images
+        cy.get('iframe').invoke(
+            'attr',
+            'style',
+            'visibility: hidden !important'
+        );
+
         cy.loadFullPage().then(() => {
             cy.captureFullPageScreenshot({
                 name: 'Page Builder Row Video Background',
