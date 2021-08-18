ca8be6a23001e658e9827160d7c6d66ff3cb0f86
Justin Conabree <jconabree@absolunet.com>
2021-08-18 14:34:51 -0400

    PWA-1204 adding test coverage

--- packages/venia-ui/lib/RootComponents/Product/__tests__/product.spec.js
+++ packages/venia-ui/lib/RootComponents/Product/__tests__/product.spec.js
@@ -1,24 +1,47 @@
 import React from 'react';
-import ShallowRenderer from 'react-test-renderer/shallow';
+import { act, create } from 'react-test-renderer';
 import { useProduct } from '@magento/peregrine/lib/talons/RootComponents/Product/useProduct';
 import Product from '../product';
+import ProductShimmer from '../product.shimmer';
 import mockData from '../mockData';
 
 jest.mock('@magento/peregrine/lib/talons/RootComponents/Product/useProduct');
+jest.mock('@magento/venia-ui/lib/components/ErrorView', () => 'ErrorView');
+jest.mock('@magento/venia-ui/lib/components/Head', () => ({
+    __esModule: true,
+    StoreTitle: jest.fn(() => 'StoreTitle'),
+    Meta: jest.fn(() => 'Meta')
+}));
+jest.mock('@magento/venia-ui/lib/components/ProductFullDetail', () => 'ProductFullDetail');
+jest.mock('@magento/venia-ui/lib/util/mapProduct');
+jest.mock('react-intl', () => ({
+    FormattedMessage: jest.fn(({ defaultMessage }) => defaultMessage)
+}));
+jest.mock('../product.shimmer', () => {
+    return jest.fn(() => 'ProductShimmer');
+});
 
-const renderer = new ShallowRenderer();
+const render = async (props = {}) => {
+    let tree;
 
-test('renders product page correctly', () => {
+    await act(() => {
+        tree = create(<Product {...props} />);
+    });
+
+    return tree;
+};
+
+test('renders product page correctly', async () => {
     useProduct.mockReturnValueOnce({
         product: mockData
     });
 
-    const tree = renderer.render(<Product />);
+    const tree = await render();
 
     expect(tree).toMatchSnapshot();
 });
 
-test('renders product page correctly when error and product data exist', () => {
+test('renders product page correctly when error and product data exist', async () => {
     useProduct.mockReturnValueOnce({
         product: mockData,
         error: {
@@ -26,19 +49,30 @@ test('renders product page correctly when error and product data exist', () => {
         }
     });
 
-    const tree = renderer.render(<Product />);
+    const tree = await render();
 
     expect(tree).toMatchSnapshot();
 });
 
-test('renders product page failed when error data', () => {
+test('renders product page failed when error data', async () => {
     useProduct.mockReturnValueOnce({
         error: {
             message: 'Fetch failed'
         }
     });
 
-    const tree = renderer.render(<Product />);
+    const tree = await render();
 
     expect(tree).toMatchSnapshot();
+});
+
+test('renders product page shimmer when loading data', async () => {
+    useProduct.mockReturnValueOnce({
+        product: undefined,
+        loading: true
+    });
+
+    await render({ '__typename': 'SimpleProduct' });
+
+    expect(ProductShimmer).toHaveBeenCalled();
 });
