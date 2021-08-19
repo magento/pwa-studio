e8b31bb4defa0f27ae42b44b821be88e24740d67
Jimmy Sanford <jimbo@users.noreply.github.com>
2021-08-19 13:01:04 -0700

    Update Tailwind config

--- packages/venia-concept/tailwind.config.js
+++ packages/venia-concept/tailwind.config.js
@@ -2,12 +2,38 @@ const venia = {};
 
 const config = {
     mode: 'jit',
+    // Include your custom theme here.
+    presets: [venia],
+    // Configure how Tailwind statically analyzes your code here.
+    // Note that the Tailwind's `jit` mode doesn't actually use PurgeCSS.
     purge: {
-        /* ... */
+        // Include paths to every file that may refer to Tailwind classnames.
+        // Classnames not found in these files will be excluded at build time.
+        content: ['@magento/venia-ui/lib/**/*.css', './src/**/*.css'],
+        // Extract Tailwind classnames from source files.
+        // Our default matcher only matches targets of CSS Modules' `composes`,
+        // not classnames included directly in HTML or JS!
+        extractors: [
+            {
+                extensions: ['css'],
+                extractor: content => content.match(matcher) || []
+            }
+        ]
     },
-    separator: '_',
-    // if you created a theme, apply it here
-    presets: [venia]
+    // Set the character Tailwind uses when prefixing classnames with variants.
+    // CSS Modules doesn't like Tailwind's default `:`, so we use `_`.
+    separator: '_'
 };
 
 module.exports = config;
+
+/**
+ * Matches declarations that contain tailwind classnames.
+ * Only classnames matched by this expression will be included in the build.
+ *
+ * @example
+ * .foo {
+ *   composes: mx-auto from global;
+ * }
+ */
+const matcher = /(?<=composes:.*)(\b\S+\b)(?=.*from global;)/g;
