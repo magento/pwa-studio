71b4aada4f0891e8e3fc2ecd86299ff885896173
Bruce Denham <bdenham@adobe.com>
2021-08-28 11:01:20 -0500

    fix: removed double commentting

--- packages/venia-ui/lib/components/Mask/mask.js
+++ packages/venia-ui/lib/components/Mask/mask.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { useStyle } from '../../classify';
 import defaultClasses from './mask.css';
 
-// /**
-//  * A component that masks content.
-//  *
-//  * @class Mask
-//  * @extends {Component}
-//  *
-//  * @typedef Mask
-//  * @kind class component
-//  *
-//  * @returns {React.Element} A React component that will mask content.
-//  */
+/**
+ * A component that masks content.
+ *
+ * @class Mask
+ * @extends {Component}
+ *
+ * @typedef Mask
+ * @kind class component
+ *
+ * @returns {React.Element} A React component that will mask content.
+ */
 const Mask = props => {
     const { dismiss, isActive } = props;
     const classes = useStyle(defaultClasses, props.classes);
