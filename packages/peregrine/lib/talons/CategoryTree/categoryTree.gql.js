07e671f3ee0fbcb70969ba4c5a1f48a08d874b6a
Pedro M. Chiossi <pchiossi@gmail.com>
2021-09-01 15:02:00 -0400

    update  graphql queries

--- packages/peregrine/lib/talons/CategoryTree/categoryTree.gql.js
+++ packages/peregrine/lib/talons/CategoryTree/categoryTree.gql.js
@@ -1,5 +1,14 @@
 import { gql } from '@apollo/client';
 
+export const GET_CATEGORY_URL_SUFFIX = gql`
+    query GetStoreConfigData {
+        storeConfig {
+            id
+            category_url_suffix
+        }
+    }
+`;
+
 export const GET_NAVIGATION_MENU = gql`
     query GetNavigationMenu($id: Int!) {
         category(id: $id) {
@@ -16,11 +25,11 @@ export const GET_NAVIGATION_MENU = gql`
             }
             include_in_menu
             url_path
-            url_suffix
         }
     }
 `;
 
 export default {
-    getNavigationMenuQuery: GET_NAVIGATION_MENU
+    getNavigationMenuQuery: GET_NAVIGATION_MENU,
+    getCategoryUrlSuffix: GET_CATEGORY_URL_SUFFIX
 };
