a28812a5a371558ede1d5c6842f19af12d1e767c
James Calcaben <calcaben@adobe.com>
2021-09-03 11:40:44 -0500

    Add tests getColors()

---
+++ packages/pwa-theme-venia/lib/__tests__/colors.spec.js
@@ -0,0 +1,48 @@
+const { getColors } = require('../colors');
+
+describe('getColors function', () => {
+    test('returns the correct values with default parameters', () => {
+        const colorConfig = getColors();
+
+        expect(colorConfig).toMatchSnapshot();
+    });
+
+    describe('returns property functions which return the correct format with', () => {
+        const data = {
+            brand: {
+                400: '38 128 235',
+                500: '20 115 230',
+                600: '13 102 208',
+                700: '9 90 186'
+            }
+        };
+
+        const colorConfig = getColors(data);
+        const { brand } = colorConfig;
+
+        test('no opacity args', () => {
+            const opacityArgs = {};
+            expect(brand[400](opacityArgs)).toMatchInlineSnapshot(
+                `"rgb(var(--color-brand-400))"`
+            );
+        });
+
+        test('opacity value given', () => {
+            const opacityArgs = {
+                opacityValue: 0.5
+            };
+            expect(brand[500](opacityArgs)).toMatchInlineSnapshot(
+                `"rgb(var(--color-brand-500) / 0.5)"`
+            );
+        });
+
+        test('opacity variable given', () => {
+            const opacityArgs = {
+                opacityVariable: '--my-opacity'
+            };
+            expect(brand[600](opacityArgs)).toMatchInlineSnapshot(
+                `"rgb(var(--color-brand-600) / var(--my-opacity, 1))"`
+            );
+        });
+    });
+});
--- packages/pwa-theme-venia/lib/colors.js
+++ packages/pwa-theme-venia/lib/colors.js
@@ -6,7 +6,7 @@ const PREFIX = '--color';
  * create color-weight functions for export to `tailwind.preset.js`
  * these functions *read* values from custom properties
  */
-const getColors = (colors = COLORS, prefix = PREFIX) => {
+const getColors = (data = COLORS, prefix = PREFIX) => {
     const colors = {};
 
     for (const [color, weights] of Object.entries(data)) {
