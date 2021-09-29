1441837d1653b6b86685cea13851e9be16740210
Bruce Denham <bdenham@adobe.com>
2021-09-29 10:33:59 -0500

    Updated jsdocs for Mask

--- packages/venia-ui/lib/components/Mask/mask.js
+++ packages/venia-ui/lib/components/Mask/mask.js
@@ -5,13 +5,12 @@ import defaultClasses from './mask.css';
 /**
  * A component that masks content.
  *
- * @class Mask
- * @extends {Component}
- *
- * @typedef Mask
- * @kind class component
- *
+ * @param {props} props React component props
+ * @param {Object} props.classes - CSS classes to override element styles.
+ * @param {callback} props.dismiss Handler for the onClick() event
+ * @param {callback} props.isActive True if the mask is in an active state. False otherwise.
  * @returns {React.Element} A React component that will mask content.
+ *
  */
 const Mask = props => {
     const { dismiss, isActive } = props;
