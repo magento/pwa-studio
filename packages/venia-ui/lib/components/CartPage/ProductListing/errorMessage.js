0891678493c2bd0a7c537457f790a94196b957a0
Justin Conabree <jconabree@absolunet.com>
2021-09-30 10:33:07 -0400

    PWA-2133 fix lint

--- packages/venia-ui/lib/components/CartPage/ProductListing/errorMessage.js
+++ packages/venia-ui/lib/components/CartPage/ProductListing/errorMessage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
 import { shape, string } from 'prop-types';
 import { useStyle } from '../../../classify';
 import defaultClasses from './errorMessage.css';
