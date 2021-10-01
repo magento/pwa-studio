3e1d11352a631c6815630c0cc117861df8637a66
Revanth Kumar <revanth0212@gmail.com>
2021-09-30 22:19:13 -0500

    Prettier update

--- venia-integration-tests/run-tests.js
+++ venia-integration-tests/run-tests.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-const glob = require('glob')
-const { exec } = require('child_process')
+const glob = require('glob');
+const { exec } = require('child_process');
 
 var argv = require('yargs/yargs')(process.argv.slice(2))
     .option('url', {
@@ -32,81 +32,91 @@ var argv = require('yargs/yargs')(process.argv.slice(2))
     })
     .option('h', {
         alias: 'help',
-        describe: 'Show run-tests help',
+        describe: 'Show run-tests help'
     })
     .help('h', 'Show run-tests help')
-    .version(false)
-    .argv
+    .version(false).argv;
 
-const { baseUrl, parallel: parallelRuns, update, spec } = argv
+const { baseUrl, parallel: parallelRuns, update, spec } = argv;
 
 if (!baseUrl) {
-    console.error('Missing baseUrl. Please provide a baseUrl using the --baseUrl arg')
+    console.error(
+        'Missing baseUrl. Please provide a baseUrl using the --baseUrl arg'
+    );
 
     return;
 }
 
-const files = spec ? spec.split(',') : glob.sync('./src/tests/**/*.spec.js')
+const files = spec ? spec.split(',') : glob.sync('./src/tests/**/*.spec.js');
 
 if (files.length < parallelRuns) {
-    console.error('Can not have more parallel runs than tests.')
+    console.error('Can not have more parallel runs than tests.');
 
     return;
 }
 
-const testsPerRun = files.length / parallelRuns
-const dockerRuns = {}
+const testsPerRun = files.length / parallelRuns;
+const dockerRuns = {};
 
-const port = new URL(baseUrl).port
+const port = new URL(baseUrl).port;
 
-let dockerCommand = null
+let dockerCommand = null;
 
 if (port) {
     // run docker on local instance
-    console.log(`Running tests on local instance ${baseUrl}`)
+    console.log(`Running tests on local instance ${baseUrl}`);
 
-    dockerCommand = `docker run --rm -v ${process.env.PWD}:/venia-integration-tests -w /venia-integration-tests --entrypoint=cypress cypress/included:8.3.1 run --browser chrome --config baseUrl=https://host.docker.internal:${port},screenshotOnRunFailure=false --config-file cypress.config.json --env updateSnapshots=${update} --headless`
+    dockerCommand = `docker run --rm -v ${
+        process.env.PWD
+    }:/venia-integration-tests -w /venia-integration-tests --entrypoint=cypress cypress/included:8.3.1 run --browser chrome --config baseUrl=https://host.docker.internal:${port},screenshotOnRunFailure=false --config-file cypress.config.json --env updateSnapshots=${update} --headless`;
 } else {
     // run docker on remote instance
-    console.log(`Running tests on remote instance ${baseUrl}`)
+    console.log(`Running tests on remote instance ${baseUrl}`);
 
-    dockerCommand = `docker run --rm -v ${process.env.PWD}:/venia-integration-tests -w /venia-integration-tests --entrypoint=cypress cypress/included:8.3.1 run --browser chrome --config baseUrl=${baseUrl},screenshotOnRunFailure=false --config-file cypress.config.json --env updateSnapshots=${update} --headless`
+    dockerCommand = `docker run --rm -v ${
+        process.env.PWD
+    }:/venia-integration-tests -w /venia-integration-tests --entrypoint=cypress cypress/included:8.3.1 run --browser chrome --config baseUrl=${baseUrl},screenshotOnRunFailure=false --config-file cypress.config.json --env updateSnapshots=${update} --headless`;
 }
 
-const start = process.hrtime()
+const start = process.hrtime();
 
 for (let i = 0; i < parallelRuns; i++) {
-    const filesToTest = files.slice(testsPerRun * (i), testsPerRun * (i + 1))
+    const filesToTest = files.slice(testsPerRun * i, testsPerRun * (i + 1));
 
-    const commandWithSpecFiles = `${dockerCommand} --spec ${filesToTest.join(',')}`
+    const commandWithSpecFiles = `${dockerCommand} --spec ${filesToTest.join(
+        ','
+    )}`;
 
-    console.log(`Running ${commandWithSpecFiles} \n`)
+    console.log(`Running ${commandWithSpecFiles} \n`);
 
-    const run = exec(commandWithSpecFiles)
+    const run = exec(commandWithSpecFiles);
 
-    run.stdout.on('data', (data) => {
+    run.stdout.on('data', data => {
         if (data !== '\n' || data !== '\r' || data.trim() !== '') {
             console.log(`docker run ${i + 1} => ${data}`);
         }
     });
 
-    run.stderr.on('data', (data) => {
+    run.stderr.on('data', data => {
         console.error(`docker run ${i + 1} => ${data}`);
     });
 
-    run.on('close', (code) => {
-        dockerRuns[i].completed = true
+    run.on('close', code => {
+        dockerRuns[i].completed = true;
 
-        const timeTaken = process.hrtime(dockerRuns[i].started)[0]
+        const timeTaken = process.hrtime(dockerRuns[i].started)[0];
 
-        console.log(`docker run ${i + 1} exited with ${code} code in ${timeTaken} seconds`);
+        console.log(
+            `docker run ${i +
+                1} exited with ${code} code in ${timeTaken} seconds`
+        );
 
         if (Object.values(dockerRuns).every(r => r.completed)) {
-            const totalTime = process.hrtime(start)[0]
+            const totalTime = process.hrtime(start)[0];
 
-            console.log(`\nAll runs completed in ${totalTime}\n`)
+            console.log(`\nAll runs completed in ${totalTime} seconds\n`);
 
-            process.exit()
+            process.exit();
         }
     });
 
@@ -114,11 +124,13 @@ for (let i = 0; i < parallelRuns; i++) {
         process: run,
         completed: false,
         started: process.hrtime()
-    }
+    };
 }
 
-process.on('SIGINT', function () {
+process.on('SIGINT', function() {
     console.log('Received kill signal. Killing all cypress tests. \n');
 
-    exec("docker ps -a | awk '{ print $1,$2 }' | grep cypress/included | awk '{print $1 }' | xargs -I {} docker kill {}")
+    exec(
+        "docker ps -a | awk '{ print $1,$2 }' | grep cypress/included | awk '{print $1 }' | xargs -I {} docker kill {}"
+    );
 });
