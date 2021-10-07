d0e8112e52736cd8ae0a5cc7c9f15d745ce4c226
mikhaelbois <mbois@absolunet.com>
2021-10-07 10:31:20 -0400

    PWA-2008 Clean and migrate RichContent CSS to Tailwind classes + Copy plugin for custom colors

--- packages/pwa-theme-venia/tailwind.preset.js
+++ packages/pwa-theme-venia/tailwind.preset.js
@@ -1,12 +1,15 @@
 const { getColors } = require('./lib/colors');
+const veniaPlugin = require('./plugins');
+
 /**
  * This is the main tailwindcss theme file for the Venia theme
  */
 
 module.exports = {
     theme: {
-        backgroundColor: {},
-        borderColor: {},
+        plugins: [veniaPlugin],
+        backgroundColor: theme => theme('colors'),
+        borderColor: theme => theme('colors'),
         colors: getColors(),
         extend: {
             gridTemplateColumns: {},
@@ -15,7 +18,11 @@ module.exports = {
             maxHeight: {},
             maxWidth: {}
         },
-        screens: {},
+        screens: {
+            tablet: '640px',
+            laptop: '1024px',
+            desktop: '1280px'
+        },
         transitionDuration: {}
     }
 };
