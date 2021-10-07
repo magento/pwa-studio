da7395818c53cb4d18975b00e6feab049ea333ca
James Calcaben <calcaben@adobe.com>
2021-10-07 10:57:27 -0500

    Remove custom breakpoints and use tailwind presets

--- packages/pwa-theme-venia/tailwind.preset.js
+++ packages/pwa-theme-venia/tailwind.preset.js
@@ -36,16 +36,6 @@ module.exports = {
                 desktop: '1440px'
             }
         },
-        screens: {
-            tablet: '640px',
-            // => @media (min-width: 640px) { ... }
-
-            laptop: '1024px',
-            // => @media (min-width: 1024px) { ... }
-
-            desktop: '1280px'
-            // => @media (min-width: 1280px) { ... }
-        },
         transitionDuration: {}
     }
 };
