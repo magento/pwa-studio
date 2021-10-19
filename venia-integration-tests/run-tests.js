4c3a16f83556b7b626c2ee2d64c853a2ee364c53
Revanth Kumar <revanth0212@gmail.com>
2021-10-19 13:04:33 -0500

    Updated tester file with new reporting logic

--- venia-integration-tests/run-tests.js
+++ venia-integration-tests/run-tests.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const glob = require('glob');
-const { exec } = require('child_process');
+const { exec, execSync } = require('child_process');
 
 var argv = require('yargs/yargs')(process.argv.slice(2))
     .option('url', {
@@ -66,19 +66,20 @@ if (port) {
     // run docker on local instance
     console.log(`Running tests on local instance ${baseUrl}`);
 
-    dockerCommand = `docker run --rm -v ${
-        process.env.PWD
-    }:/venia-integration-tests -w /venia-integration-tests --entrypoint=cypress cypress/included:8.3.1 run --browser chrome --config baseUrl=https://host.docker.internal:${port},screenshotOnRunFailure=false --config-file cypress.config.json --env updateSnapshots=${update} --headless`;
+    dockerCommand = `docker run --rm -v ${process.env.PWD
+        }:/venia-integration-tests -w /venia-integration-tests --entrypoint=cypress cypress/included:8.3.1 run --browser chrome --config baseUrl=https://host.docker.internal:${port},screenshotOnRunFailure=false --config-file cypress.config.json --env updateSnapshots=${update} --headless --reporter mochawesome --reporter-options reportDir=cypress/results,overwrite=false,html=false,json=true`;
 } else {
     // run docker on remote instance
     console.log(`Running tests on remote instance ${baseUrl}`);
 
-    dockerCommand = `docker run --rm -v ${
-        process.env.PWD
-    }:/venia-integration-tests -w /venia-integration-tests --entrypoint=cypress cypress/included:8.3.1 run --browser chrome --config baseUrl=${baseUrl},screenshotOnRunFailure=false --config-file cypress.config.json --env updateSnapshots=${update} --headless`;
+    dockerCommand = `docker run --rm -v ${process.env.PWD
+        }:/venia-integration-tests -w /venia-integration-tests --entrypoint=cypress cypress/included:8.3.1 run --browser chrome --config baseUrl=${baseUrl},screenshotOnRunFailure=false --config-file cypress.config.json --env updateSnapshots=${update} --headless --reporter mochawesome --reporter-options reportDir=cypress/results,overwrite=false,html=false,json=true`;
 }
 
 const start = process.hrtime();
+
+// remove old test results
+execSync('rimraf cypress/results/*');
 
 for (let i = 0; i < threads; i++) {
     const filesToTest = files.slice(testsPerRun * i, testsPerRun * (i + 1));
@@ -108,10 +109,13 @@ for (let i = 0; i < threads; i++) {
 
         console.log(
             `docker run ${i +
-                1} exited with ${code} code in ${timeTaken} seconds`
+            1} exited with ${code} code in ${timeTaken} seconds`
         );
 
         if (Object.values(dockerRuns).every(r => r.completed)) {
+            // build final results json
+            execSync('mochawesome-merge cypress/results/*.json -o cypress-test-results.json')
+
             const totalTime = process.hrtime(start)[0];
 
             console.log(`\nAll runs completed in ${totalTime} seconds\n`);
@@ -127,7 +131,7 @@ for (let i = 0; i < threads; i++) {
     };
 }
 
-process.on('SIGINT', function() {
+process.on('SIGINT', function () {
     console.log('Received kill signal. Killing all cypress tests. \n');
 
     exec(
