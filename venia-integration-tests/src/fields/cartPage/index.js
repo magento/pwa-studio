879ff86cae376cba69eca641cc550bf2f082ba7c
mikhaelbois <mbois@absolunet.com>
2021-10-22 13:05:57 -0400

    PWA-2206 Add test for user can remove product from cart

--- venia-integration-tests/src/fields/cartPage/index.js
+++ venia-integration-tests/src/fields/cartPage/index.js
@@ -1,8 +1,16 @@
-export const cartPageRoot = 'div[class^="cartPage-root-"]';
+export const cartPageRoot = '[data-cy="CartPage-root"]';
 
-export const productName =
+export const productListingProduct =
+    '[data-cy="CartPage-root"] [data-cy="ProductListing-root"] [data-cy="Product-root"]';
+
+export const productListingProductName =
     '[data-cy="CartPage-root"] [data-cy="ProductListing-root"] [data-cy="Product-name"]';
 
 export const kebabMenuButton = 'button[class^="kebab-kebab-"]';
 
 export const saveForLaterButton = 'button[class^="product-addToListButton-"]';
+
+export const productKebab = '[data-cy="LegacyMiniCart-Kebab-button"]';
+
+export const productSectionRemoveFromCartButton =
+    '[data-cy="Product-Section-removeFromCart"] [data-cy="LegacyMiniCart-Section-button"]';
