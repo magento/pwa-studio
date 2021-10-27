68dc51de75f67ecadd7e52745b4b4c8737e4539d
Pedro M. Chiossi <pchiossi@gmail.com>
2021-10-27 16:57:57 -0400

    PWA-2201: Add necessary assertions

--- venia-integration-tests/src/assertions/cartPage/index.js
+++ venia-integration-tests/src/assertions/cartPage/index.js
@@ -1,4 +1,11 @@
-import { productName } from '../../fields/cartPage';
+import {
+    productName,
+    kebabMenuButton,
+    cartPageRoot,
+    cartPageProductOption,
+    cartPageProductQuantity,
+    cartPageProductPrice
+} from '../../fields/cartPage';
 
 /**
  * Utility function to assert Product is in Cart Page
@@ -7,4 +14,37 @@ import { productName } from '../../fields/cartPage';
  */
 export const assertProductInCartPage = name => {
     cy.get(productName).should('contain', name);
+};
+
+export const assertKebabMenuExists = productName => {
+    const itemToCheck = cy.get(cartPageRoot).contains('li', productName);
+
+    itemToCheck.get(kebabMenuButton).should('exist');
+};
+
+export const assertUpdatedProductColorExists = (productName, color) => {
+    const itemToCheck = cy.get(cartPageRoot).contains('li', productName);
+
+    itemToCheck.get(cartPageProductOption).should('contain', color);
+};
+
+export const assertUpdatedProductSizeExists = (productName, size) => {
+    const itemToCheck = cy.get(cartPageRoot).contains('li', productName);
+
+    itemToCheck.get(cartPageProductOption).should('contain', size);
+};
+
+export const assertUpdatedProductQuantityExists = (productName, qty) => {
+    const itemToCheck = cy.get(cartPageRoot).contains('li', productName);
+
+    itemToCheck.get(cartPageProductQuantity).should('have.value', qty);
+};
+
+export const assertUpdatedProductPriceExists = (productName, price) => {
+    const itemToCheck = cy.get(cartPageRoot).contains('li', productName);
+
+    itemToCheck
+        .get(cartPageProductPrice)
+        .contains('span', price)
+        .should('exist');
 };
