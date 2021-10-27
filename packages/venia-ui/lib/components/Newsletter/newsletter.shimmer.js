4b2703dd9340af424375624339eaeb6816c1f579
Justin Conabree <jconabree@absolunet.com>
2021-10-27 12:19:40 -0400

    PWA-2007 changing inline style to classes, changing translation

--- packages/venia-ui/lib/components/Newsletter/newsletter.shimmer.js
+++ packages/venia-ui/lib/components/Newsletter/newsletter.shimmer.js
@@ -3,24 +3,31 @@ import React from 'react';
 import { useStyle } from '../../classify';
 import Shimmer from '../Shimmer';
 import defaultClasses from './newsletter.module.css';
+import defaultShimmerClasses from './newsletter.shimmer.module.css';
 
 const NewsletterShimmer = props => {
-    const classes = useStyle(defaultClasses, props.classes);
+    const classes = useStyle(defaultClasses, defaultShimmerClasses, props.classes);
 
     return (
         <div className={classes.root}>
             <Shimmer
                 key="title"
-                style={{ display: 'block', marginBottom: '1rem' }}
+                classes={{
+                    root_rectangle: classes.shimmerItem
+                }}
             />
             <Shimmer
                 key="text"
-                style={{ display: 'block', marginBottom: '0.5rem' }}
+                classes={{
+                    root_rectangle: classes.shimmerParagraphLine
+                }}
             />
             <Shimmer
                 key="text2"
                 width="50%"
-                style={{ display: 'block', marginBottom: '1rem' }}
+                classes={{
+                    root_rectangle: classes.shimmerItem
+                }}
             />
             <div className={classes.form}>
                 <Shimmer type="textInput" />
