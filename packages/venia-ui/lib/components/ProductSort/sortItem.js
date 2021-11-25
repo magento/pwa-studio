66113a4b395cecd3987a22416df4adaa7ac0541a
Pedro M. Chiossi <pchiossi@gmail.com>
2021-11-25 12:42:04 -0300

    Revert "Use react-aria useButton on sortItem"
    This reverts commit 62f3f66f3e6f7d3e77f0210f0e795cab12255e44.

--- packages/venia-ui/lib/components/ProductSort/sortItem.js
+++ packages/venia-ui/lib/components/ProductSort/sortItem.js
@@ -1,8 +1,7 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Check } from 'react-feather';
 import { bool, func, shape, string } from 'prop-types';
-import { useButton } from 'react-aria';
 
 import { useStyle } from '../../classify';
 import Icon from '../Icon/icon';
@@ -15,18 +14,22 @@ const SortItem = props => {
     const handleClick = useCallback(() => {
         onClick(sortItem);
     }, [sortItem, onClick]);
-    const buttonRef = useRef();
 
-    const { buttonProps } = useButton({ onPress: handleClick }, buttonRef);
+    const handleKeyDown = e => {
+        if (e.key === 'Enter' || e.key === ' ') {
+            e.preventDefault();
+            handleClick();
+        }
+    };
 
     const activeIcon = active ? <Icon size={20} src={Check} /> : null;
 
     return (
         <button
-            ref={buttonRef}
             className={classes.root}
             data-cy={active ? 'SortItem-activeButton' : 'SortItem-button'}
-            {...buttonProps}
+            onMouseDown={handleClick}
+            onKeyDown={handleKeyDown}
         >
             <span className={classes.content}>
                 <span className={classes.text}>
