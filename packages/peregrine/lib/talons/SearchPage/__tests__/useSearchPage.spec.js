fee875e1752cfc2b2465e845a962853844bfa1bc
itsYanik <allan.yanik@gmail.com>
2021-12-03 11:15:39 -0300

    Fixes useSearchPage test.

--- packages/peregrine/lib/talons/SearchPage/__tests__/useSearchPage.spec.js
+++ packages/peregrine/lib/talons/SearchPage/__tests__/useSearchPage.spec.js
@@ -1,37 +1,17 @@
 import React, { useEffect } from 'react';
 import { act } from 'react-test-renderer';
-
-import { useSearchPage } from '../useSearchPage';
-
+import { useLazyQuery } from '@apollo/client';
 import createTestInstance from '../../../util/createTestInstance';
 import { getFiltersFromSearch } from '../../FilterModal/helpers';
-import { useLazyQuery } from '@apollo/client';
-
-jest.mock('react-router-dom', () => ({
-    useHistory: jest.fn(() => ({ push: jest.fn() })),
-    useLocation: jest.fn(() => ({ pathname: '', search: '' }))
-}));
-
-jest.mock('../../FilterModal/helpers', () => {
-    return {
-        ...jest.requireActual('../../FilterModal/helpers'),
-        getFiltersFromSearch: jest.fn(() => new Map())
-    };
-});
-
-jest.mock('@magento/peregrine/lib/hooks/useScrollTopOnChange');
-
-jest.mock('../../../context/app', () => {
-    const state = {};
-    const api = {
-        toggleDrawer: jest.fn(),
-        actions: { setPageLoading: jest.fn() }
-    };
-    const useAppContext = jest.fn(() => [state, api]);
-
-    return { useAppContext };
-});
-
+import { useSearchPage } from '../useSearchPage';
+const log = jest.fn();
+const Component = props => {
+    const talonProps = useSearchPage({ ...props });
+    useEffect(() => {
+        log(talonProps);
+    }, [talonProps]);
+    return <i talonProps={talonProps} />;
+};
 const mockUseSort = jest
     .fn()
     .mockReturnValue([
@@ -43,9 +23,40 @@ const mockUseSort = jest
         jest.fn()
     ])
     .mockName('mockUseSort');
-
 const mockSetCurrentPage = jest.fn().mockName('mockSetCurrentPage');
-
+const mockAvailableSortMethods = {
+    products: {
+        sort_fields: {
+            options: [
+                {
+                    label: 'Position',
+                    value: 'position'
+                }
+            ]
+        }
+    }
+};
+const mockGetSearchAvailableSortMethods = jest.fn();
+jest.mock('react-router-dom', () => ({
+    useHistory: jest.fn(() => ({ push: jest.fn() })),
+    useLocation: jest.fn(() => ({ pathname: '', search: '' }))
+}));
+jest.mock('../../FilterModal/helpers', () => {
+    return {
+        ...jest.requireActual('../../FilterModal/helpers'),
+        getFiltersFromSearch: jest.fn(() => new Map())
+    };
+});
+jest.mock('@magento/peregrine/lib/hooks/useScrollTopOnChange');
+jest.mock('../../../context/app', () => {
+    const state = {};
+    const api = {
+        toggleDrawer: jest.fn(),
+        actions: { setPageLoading: jest.fn() }
+    };
+    const useAppContext = jest.fn(() => [state, api]);
+    return { useAppContext };
+});
 jest.mock('../../../hooks/usePagination', () => ({
     usePagination: jest.fn(() => [
         {
@@ -62,11 +73,9 @@ jest.mock('../../../hooks/usePagination', () => ({
         }
     ])
 }));
-
 jest.mock('../../../hooks/useSort', () => ({
     useSort: jest.fn().mockImplementation(() => mockUseSort())
 }));
-
 jest.mock('@apollo/client', () => {
     const apolloClient = jest.requireActual('@apollo/client');
     const useQuery = jest.fn().mockReturnValue({
@@ -94,154 +103,48 @@ jest.mock('@apollo/client', () => {
         loading: false
     };
     const useLazyQuery = jest.fn(() => [runQuery, queryResult]);
-
     return { ...apolloClient, useLazyQuery, useQuery };
 });
-
-const mockAvailableSortMethods = {
-    products: {
-        sort_fields: {
-            options: [
-                {
-                    label: 'Position',
-                    value: 'position'
-                }
-            ]
-        }
-    }
-};
-
-const mockGetSearchAvailableSortMethods = jest.fn();
-
-const Component = props => {
-    const talonProps = useSearchPage(props);
-    return <i talonProps={talonProps} />;
-};
-
-const mockProps = { queries: {} };
-const tree = createTestInstance(<Component {...mockProps} />);
-
+const initialProps = { queries: {} };
 useLazyQuery.mockReturnValueOnce([
     mockGetSearchAvailableSortMethods,
     { data: mockAvailableSortMethods }
 ]);
-
-test('returns the correct shape', () => {
-    const { root } = tree;
-    const { talonProps } = root.findByType('i').props;
-
-    expect(talonProps).toMatchSnapshot();
-});
-
-const testCases = [
-    [
-        'sortText does not reset',
-        {
-            sortText: 'Changed',
-            sortAttribute: 'relevance',
-            sortDirection: 'DESC'
-        },
-        0
-    ],
-    [
-        'sortAttribute resets',
-        {
-            sortText: 'Best Match',
-            sortAttribute: 'Changed',
-            sortDirection: 'DESC'
-        },
-        1
-    ],
-    [
-        'sortDirection resets',
-        {
-            sortText: 'Best Match',
-            sortAttribute: 'relevance',
-            sortDirection: 'Changed'
-        },
-        1
-    ]
-];
-
-test.each(testCases)(
-    'Changing %s current page to 1.',
-    (description, sortParams, expected) => {
-        mockUseSort.mockReturnValueOnce([sortParams, jest.fn()]);
-        act(() => {
-            tree.update(<Component {...mockProps} />);
-        });
-
-        expect(mockSetCurrentPage).toHaveBeenCalledTimes(expected);
-    }
-);
-
-test('preserve history when search term changes on Search Page', () => {
-    mockUseSort.mockReturnValueOnce([
-        {
-            sortText: 'Best Match',
-            sortAttribute: 'Changed',
-            sortDirection: 'DESC'
-        },
-        jest.fn()
-    ]);
-    expect(mockSetCurrentPage).not.toHaveBeenCalledWith(1, true);
-    act(() => {
-        tree.update(<Component {...mockProps} />);
-    });
-
-    expect(mockSetCurrentPage).toHaveBeenCalledWith(1, true);
-});
-
 describe('searchCategory', () => {
-    const log = jest.fn();
-    const Component = props => {
-        const talonProps = useSearchPage({ ...props });
-
-        useEffect(() => {
-            log(talonProps);
-        }, [talonProps]);
-
-        return <i talonProps={talonProps} />;
-    };
-
+    test('returns the correct shape', () => {
+        createTestInstance(<Component {...initialProps} />);
+        const talonProps = log.mock.calls[0][0];
+        expect(talonProps).toMatchSnapshot();
+    });
     test('is null when no filters exist', () => {
         // Arrange.
         getFiltersFromSearch.mockReturnValueOnce(new Map());
-
         // Act.
-        createTestInstance(<Component {...mockProps} />);
-
+        createTestInstance(<Component {...initialProps} />);
         // Assert.
         const { searchCategory } = log.mock.calls[0][0];
         expect(searchCategory).toBeNull();
     });
-
     test('is null when category_id doesnt exist', () => {
         // Arrange.
         const map = new Map().set('not_category_id', 'unit test');
         getFiltersFromSearch.mockReturnValueOnce(map);
-
         // Act.
-        createTestInstance(<Component {...mockProps} />);
-
+        createTestInstance(<Component {...initialProps} />);
         // Assert.
         const { searchCategory } = log.mock.calls[0][0];
         expect(searchCategory).toBeNull();
     });
-
     test('is correct when a single category filter exists', () => {
         // Arrange.
         const map = new Map().set('category_id', new Set(['Bottoms,11']));
         getFiltersFromSearch.mockReturnValueOnce(map);
-
         // Act.
-        createTestInstance(<Component {...mockProps} />);
-
+        createTestInstance(<Component {...initialProps} />);
         // Assert.
         const { searchCategory } = log.mock.calls[0][0];
         expect(searchCategory).toEqual('Bottoms');
     });
-
     test('is correct when multiple category filters exist', () => {
         // Arrange.
         const map = new Map().set(
@@ -249,12 +152,66 @@ describe('searchCategory', () => {
             new Set(['Bottoms,11', 'Skirts,12'])
         );
         getFiltersFromSearch.mockReturnValueOnce(map);
-
         // Act.
-        createTestInstance(<Component {...mockProps} />);
-
+        createTestInstance(<Component {...initialProps} />);
         // Assert.
         const { searchCategory } = log.mock.calls[0][0];
         expect(searchCategory).toEqual('Bottoms, Skirts');
+    });
+    const testCases = [
+        [
+            'sortText does not reset',
+            {
+                sortText: 'Changed',
+                sortAttribute: 'relevance',
+                sortDirection: 'DESC'
+            },
+            0
+        ],
+        [
+            'sortAttribute resets',
+            {
+                sortText: 'Best Match',
+                sortAttribute: 'Changed',
+                sortDirection: 'DESC'
+            },
+            1
+        ],
+        [
+            'sortDirection resets',
+            {
+                sortText: 'Best Match',
+                sortAttribute: 'relevance',
+                sortDirection: 'Changed'
+            },
+            1
+        ]
+    ];
+    test.each(testCases)(
+        'Changing %s current page to 1.',
+        (description, sortParams, expected) => {
+            mockUseSort.mockReturnValueOnce([sortParams, jest.fn()]);
+            const tree = createTestInstance(<Component {...initialProps} />);
+            act(() => {
+                tree.update(<Component {...initialProps} />);
+            });
+            expect(mockSetCurrentPage).toHaveBeenCalledTimes(expected);
+        }
+    );
+    test('preserve history when search term changes on Search Page', () => {
+        mockUseSort.mockReturnValueOnce([
+            {
+                sortText: 'Best Match',
+                sortAttribute: 'Changed',
+                sortDirection: 'DESC'
+            },
+            jest.fn()
+        ]);
+        const tree = createTestInstance(<Component {...initialProps} />);
+        expect(mockSetCurrentPage).not.toHaveBeenCalledWith(1, true);
+        act(() => {
+            tree.update(<Component {...initialProps} />);
+        });
+        expect(mockSetCurrentPage).toHaveBeenCalledWith(1, true);
     });
 });
