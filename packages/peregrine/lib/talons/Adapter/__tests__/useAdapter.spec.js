a36c2c61684868bcbe872a4375853f699d51912f
hwyu@adobe.com <hwyu@adobe.com>
2021-12-09 17:05:14 -0600

    PWA-2313: Update StoreConfig.id GraphQL field to store_code
    - Added Apollo policies

--- packages/peregrine/lib/talons/Adapter/__tests__/useAdapter.spec.js
+++ packages/peregrine/lib/talons/Adapter/__tests__/useAdapter.spec.js
@@ -72,11 +72,11 @@ const givenDefaultValues = () => {
     globalThis.localStorage.setItem('store_view_code', 'store_view_code');
     globalThis.localStorage.setItem(
         `${CACHE_PERSIST_PREFIX}-default`,
-        '{"StoreConfig:1":{"store_code":"default","__typename":"StoreConfig","code":"default"}}'
+        '{"StoreConfig:{"store_code":"default"}":{"store_code":"default","__typename":"StoreConfig","code":"default"}}'
     );
     globalThis.localStorage.setItem(
         `${CACHE_PERSIST_PREFIX}-french`,
-        '{"StoreConfig:2":{"store_code":"fr","__typename":"StoreConfig","code":"french"}}'
+        '{"StoreConfig:{"store_code":"fr"}":{"store_code":"fr","__typename":"StoreConfig","code":"french"}}'
     );
 };
 
