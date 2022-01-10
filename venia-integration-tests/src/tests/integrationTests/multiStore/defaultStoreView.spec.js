df9a2ccac10ddf380fbcfc3719fa9a9ba6573ad3
James Calcaben <calcaben@adobe.com>
2022-01-10 16:27:36 -0600

    Create initial test file for default store view

---
+++ venia-integration-tests/src/tests/integrationTests/multiStore/defaultStoreView.spec.js
@@ -0,0 +1,5 @@
+describe('PWA-1408: Verify data displayed is from Default Root Category', () => {
+    it('displays subcategories from the default root category', () => {
+        cy.visitPage('/');
+    });
+});
