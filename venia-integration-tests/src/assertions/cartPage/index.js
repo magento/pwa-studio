8bf706f08495b01b0a364bc0aab104c8f5cb5ba4
Mikhaël Bois <mbois@absolunet.com>
2022-01-20 15:52:05 -0500

    [PWA-1947] Gift options on Order Level (Commerce only) (#3540)
    * PWA-1947 First draft of gift options
    
    * PWA-1947 Add Gift Options Section + Update Gift Options + Update tests
    
    * PWA-1947 Fix data issue + Move Cypres Gift Options in mocked data test
    
    * PWA-1947 Add ce file extension for better testing + Add tests
    
    * PWA-1947 Remove faulty test
    
    * PWA-1947 Save data in cache and save to cart only when Review Order button is clicked
    
    * PWA-1947 Add form errors + Update doc blocks + Update tests
    
    * PWA-1947 Remove mutation from Cypress test + Fix ContactPage Cypress test
    
    * PWA-1947 Check if form data has changed before submitting
    
    * PWA-1947 Simplify Spinner component
    
    * PWA-1947 POC for better Gift Options
    
    * PWA-1947 Update Form UX + Tests
    
    * PWA-1947 Fix lint
    
    * PWA-1947 Fix states and update test
    
    * PWA-1947 Update Cypress test
    
    * PWA-1947 Update labels and styles
    
    * PWA-1947 Update cypress fixtures
    
    * PWA-1947 Clean code
    
    * PWA-1947 Fix race condition bug with checkboxes
    
    * PWA-1947 Empty object fallback
    
    * PWA-1947 Adjust test and move condition to return default data in initial values
    
    Co-authored-by: Pankhuri Goel <goelpankhuri105@gmail.com>
    Co-authored-by: Justin Conabree <68294336+JustinConabreeAbsolunet@users.noreply.github.com>
    Co-authored-by: Pedro Chiossi <36369921+pedrochiossi@users.noreply.github.com>
    Co-authored-by: Jeremy Charron <jcharron@absolunet.com>

--- venia-integration-tests/src/assertions/cartPage/index.js
+++ venia-integration-tests/src/assertions/cartPage/index.js
@@ -6,7 +6,15 @@ import {
     cartPageProductPrice,
     productListingProductName,
     cartPagePriceSummaryDiscountSummary,
-    cartPagePriceSummaryGiftCardSummary
+    cartPagePriceSummaryGiftCardSummary,
+    giftOptionsSection,
+    giftOptionsGiftMessageResult,
+    giftOptionsIncludeGiftReceiptCheckbox,
+    giftOptionsIncludeGiftMessageCheckbox,
+    giftOptionsIncludePrintedCardCheckbox,
+    giftOptionsCardToInput,
+    giftOptionsCardFromInput,
+    giftOptionsCardMessageTextarea
 } from '../../fields/cartPage';
 
 /**
@@ -78,4 +86,140 @@ export const assertAppliedGiftCardInCartPage = () => {
  */
 export const assertNoGiftCardInCartPage = () => {
     cy.get(cartPagePriceSummaryGiftCardSummary).should('not.exist');
+};
+
+/**
+ * Utility function to assert Gift Options Section is not available
+ */
+export const assertNoGiftOptionsSection = () => {
+    cy.get(giftOptionsSection).should('not.exist');
+};
+
+/**
+ * Utility function to assert Gift Message form is available
+ */
+export const assertGiftMessage = () => {
+    cy.get(giftOptionsCardToInput).should('exist');
+    cy.get(giftOptionsCardFromInput).should('exist');
+    cy.get(giftOptionsCardMessageTextarea).should('exist');
+};
+
+/**
+ * Utility function to assert Gift Receipt checkbox is not available
+ */
+export const assertNoGiftReceipt = () => {
+    cy.get(giftOptionsIncludeGiftReceiptCheckbox).should('not.exist');
+};
+
+/**
+ * Utility function to assert Gift Receipt checkbox is available
+ */
+export const assertGiftReceipt = () => {
+    cy.get(giftOptionsIncludeGiftReceiptCheckbox).should('exist');
+};
+
+/**
+ * Utility function to assert Printed Card checkbox is not available
+ */
+export const assertNoPrintedCard = () => {
+    cy.get(giftOptionsIncludePrintedCardCheckbox).should('not.exist');
+};
+
+/**
+ * Utility function to assert Printed Card checkbox is available
+ */
+export const assertPrintedCard = () => {
+    cy.get(giftOptionsIncludePrintedCardCheckbox).should('exist');
+};
+
+/**
+ * Utility function assert Gift Options from Cart
+ *
+ * @param {Object} data gift options data
+ * @param {Boolean} [data.includeGiftReceipt] include gift receipt
+ * @param {Boolean} [data.includeGiftMessage] include gift message
+ * @param {Boolean} [data.includePrintedCard] include printed card
+ * @param {String} [data.cardTo] card to
+ * @param {String} [data.cardFrom] card from
+ * @param {String} [data.cardMessage] card message
+ */
+export const assertCartGiftOptions = ({
+    includeGiftReceipt = null,
+    includeGiftMessage = null,
+    includePrintedCard = null,
+    cardTo,
+    cardFrom,
+    cardMessage
+}) => {
+    if (includeGiftReceipt !== null) {
+        if (includeGiftReceipt) {
+            cy.get(giftOptionsIncludeGiftReceiptCheckbox)
+                .should('exist')
+                .and('be.checked');
+        } else {
+            cy.get(giftOptionsIncludeGiftReceiptCheckbox)
+                .should('exist')
+                .and('not.be.checked');
+        }
+    }
+
+    if (includeGiftMessage !== null) {
+        if (includeGiftMessage) {
+            cy.get(giftOptionsIncludeGiftMessageCheckbox)
+                .should('exist')
+                .and('be.checked');
+
+            if (cardTo) {
+                cy.get(giftOptionsGiftMessageResult)
+                    .should('exist')
+                    .and('contain', cardTo);
+            }
+
+            if (cardFrom) {
+                cy.get(giftOptionsGiftMessageResult)
+                    .should('exist')
+                    .and('contain', cardFrom);
+            }
+
+            if (cardMessage) {
+                cy.get(giftOptionsGiftMessageResult)
+                    .should('exist')
+                    .and('contain', cardMessage);
+            }
+        } else {
+            cy.get(giftOptionsIncludeGiftMessageCheckbox)
+                .should('exist')
+                .and('not.be.checked');
+
+            if (cardTo) {
+                cy.get(giftOptionsCardToInput)
+                    .should('exist')
+                    .and('contain.value', cardTo);
+            }
+
+            if (cardFrom) {
+                cy.get(giftOptionsCardFromInput)
+                    .should('exist')
+                    .and('contain.value', cardFrom);
+            }
+
+            if (cardMessage) {
+                cy.get(giftOptionsCardMessageTextarea)
+                    .should('exist')
+                    .and('contain.value', cardMessage);
+            }
+        }
+    }
+
+    if (includePrintedCard !== null) {
+        if (includePrintedCard) {
+            cy.get(giftOptionsIncludePrintedCardCheckbox)
+                .should('exist')
+                .and('be.checked');
+        } else {
+            cy.get(giftOptionsIncludePrintedCardCheckbox)
+                .should('exist')
+                .and('not.be.checked');
+        }
+    }
 };
