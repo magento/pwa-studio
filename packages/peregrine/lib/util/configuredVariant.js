14abfe9e51e9deed330339501e17514e94fd8abf
Ryan Walter <r.walter215@gmail.com>
2022-02-03 15:48:02 -0600

    updated unit tests, jsDocs

--- packages/peregrine/lib/util/configuredVariant.js
+++ packages/peregrine/lib/util/configuredVariant.js
@@ -1,9 +1,17 @@
-// Resolves configured variant for configurable cart item.
-// This field is proposed in the https://github.com/magento/magento2/pull/30817
+/**
+ * This function resolves configured variant for configurable cart item.
+ * This field is proposed in the https://github.com/magento/magento2/pull/30817
+ * @param {Object} configured_options An array of product variant options
+ * @param {Object} product A configurable product object with a variants property.
+ *
+ * @returns {Object | undefined}
+ */
 export default function configuredVariant(configured_options, product) {
-    if (!configured_options || !product.variants) return;
+    if (!configured_options || !product?.variants) return;
     const optionUids = configured_options
-        .map(option => option.configurable_product_option_value_uid)
+        .map(option => {
+            return option.configurable_product_option_value_uid;
+        })
         .sort()
         .toString();
 
