3d3fdfe7d2d170ff189f8c8174f28fbc0bee8d56
Ryan Walter <r.walter215@gmail.com>
2022-02-18 15:45:25 -0600

    PWA-2415: Configurable images are not shown in Edit Cart Item menu (#3695)
    * ProductDetail to show configured variant image
    
    * prettier
    
    * updated unit tests
    
    Co-authored-by: Mikhaël Bois <mbois@absolunet.com>

---
+++ packages/peregrine/lib/talons/CartPage/ProductListing/EditModal/__fixtures__/configurableThumbnailSource.js
@@ -0,0 +1,8 @@
+export const configurableThumbnailSourceResponse = {
+    data: {
+        storeConfig: {
+            store_code: 1,
+            configurable_thumbnail_source: 'parent'
+        }
+    }
+};
