623228ab5979edc0341d6fddc69b1c33796c2f11
Hwashiang (Michael) Yu <hwyu@adobe.com>
2022-04-13 13:21:33 -0500

    PWA-1673: View Product Attributes values on PDP - Content
    - Updated plain html renderer to modify img src

---
+++ packages/peregrine/lib/util/htmlStringImgUrlConverter.js
@@ -0,0 +1,21 @@
+import makeUrl from "./makeUrl";
+
+/**
+ * Modifies html string images to use makeUrl as source.
+ *
+ * @param {string} htmlString - the html string to be updated
+ * @return {string}
+ */
+const htmlStringImgUrlConverter = (htmlString) => {
+    const temporaryElement = document.createElement('div');
+    temporaryElement.innerHTML = htmlString;
+    for(const imgElement of temporaryElement.getElementsByTagName('img')) {
+        imgElement.src = makeUrl(imgElement.src, {
+            type: 'image-wysiwyg',
+            quality: 85
+        });
+    }
+    return temporaryElement.innerHTML;
+}
+
+export default htmlStringImgUrlConverter;
