4e953d3c60faf2d18f5780a0e3cc199c2326df7e
Dirk Rudolph <diru91@gmail.com>
2022-04-13 08:49:12 +0200

    Update packages/peregrine/lib/talons/Adapter/useAdapter.js
    Co-authored-by: Jimmy Sanford <jimbo@users.noreply.github.com>

--- packages/peregrine/lib/talons/Adapter/useAdapter.js
+++ packages/peregrine/lib/talons/Adapter/useAdapter.js
@@ -20,7 +20,7 @@ import { BrowserPersistence } from '@magento/peregrine/lib/util';
 import shrinkQuery from '@magento/peregrine/lib/util/shrinkQuery';
 
 export const useAdapter = props => {
-    const { origin, endpoint, store, styles } = props;
+    const { endpoint, origin, store, styles } = props;
     const storeCode = storage.getItem('store_view_code') || STORE_VIEW_CODE;
     const basename = urlHasStoreCode ? `/${storeCode}` : null;
     const [initialized, setInitialized] = useState(false);
