995818fa2738aa8cf21c1a8cceb687f896c157ab
Jimmy Sanford <jimbo@users.noreply.github.com>
2022-04-20 11:22:49 -0700

    Add eventing context

---
+++ packages/extensions/venia-sample-eventing/main.js
@@ -0,0 +1,16 @@
+import { useEffect } from "react"
+import { useEventingContext } from "@magento/peregrine/lib/context/eventing";
+
+export default original => props => {
+    const [observable, { dispatch }] = useEventingContext()
+
+    useEffect(() => {
+        observable.subscribe(event => {
+            console.log("Logging event:", event)
+        })
+
+        dispatch("hello world")
+    }, [dispatch, observable])
+
+    return original(props);
+};
---
+++ packages/peregrine/lib/context/eventing.js
@@ -0,0 +1,44 @@
+import React, { createContext, useCallback, useContext, useMemo, useRef } from "react"
+import { Observable } from "zen-observable-ts"
+
+const DEFAULT_EVENT_NAME = "beacon"
+const EventingContext = createContext()
+
+const EventingContextProvider = props => {
+	const { children, eventName = DEFAULT_EVENT_NAME } = props
+	const cacheRef = useRef([])
+
+	const state = useMemo(() => new Observable(observer => {
+		for (const element of cacheRef.current) {
+			observer.next(element)
+		}
+
+		document.addEventListener(eventName, event => {
+			observer.next(event.detail)
+		})
+	}), [])
+
+	const dispatch = useCallback(detail => {
+		const event = new CustomEvent(eventName, { detail })
+
+		cacheRef.current.push(detail)
+		document.dispatchEvent(event)
+	})
+
+	const contextValue = useMemo(() => [
+		state,
+		{
+			dispatch,
+			subscribe: state.subscribe
+		}
+	], [dispatch, state])
+
+	return (
+		<EventingContext.Provider value={contextValue}>
+			{children}
+		</EventingContext.Provider>
+	)
+}
+
+export default EventingContextProvider
+export const useEventingContext = () => useContext(EventingContext)
