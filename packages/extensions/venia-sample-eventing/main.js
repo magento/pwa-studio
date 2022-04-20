bfd253be4b001fced05d55ffbb37f776bc347c7d
Jimmy Sanford <jimbo@users.noreply.github.com>
2022-04-20 11:29:14 -0700

    Fixup

--- packages/extensions/venia-sample-eventing/main.js
+++ packages/extensions/venia-sample-eventing/main.js
@@ -1,16 +1,16 @@
-import { useEffect } from "react"
-import { useEventingContext } from "@magento/peregrine/lib/context/eventing";
+import { useEffect } from 'react';
+import { useEventingContext } from '@magento/peregrine/lib/context/eventing';
 
 export default original => props => {
-    const [observable, { dispatch }] = useEventingContext()
+    const [observable, { dispatch }] = useEventingContext();
 
     useEffect(() => {
         observable.subscribe(event => {
-            console.log("Logging event:", event)
-        })
+            console.log('Logging event:', event);
+        });
 
-        dispatch("hello world")
-    }, [dispatch, observable])
+        dispatch('hello world');
+    }, [dispatch, observable]);
 
     return original(props);
 };
