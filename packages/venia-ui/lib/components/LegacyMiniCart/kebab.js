d3be5d08d40b450ab41d9d9c63fc865025e75e4f
Rajesh Kumar <glo71317@adobe.com>
2022-04-26 16:34:10 +0530

    AC-2791::Current state of custom controls not announced. (Shopping Bag)

--- packages/venia-ui/lib/components/LegacyMiniCart/kebab.js
+++ packages/venia-ui/lib/components/LegacyMiniCart/kebab.js
@@ -17,6 +17,7 @@ const Kebab = props => {
     return (
         <div className={classes.root}>
             <button
+                aria-expanded={isOpen}
                 className={classes.kebab}
                 data-cy="Kebab-button"
                 onClick={handleKebabClick}
