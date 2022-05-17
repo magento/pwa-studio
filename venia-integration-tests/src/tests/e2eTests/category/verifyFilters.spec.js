47c98914834860932fa363b3397b34dfbc1677d3
Dev Patil <depatil@adobe.com>
2022-05-17 09:13:08 +0530

    remove skip

--- venia-integration-tests/src/tests/e2eTests/category/verifyFilters.spec.js
+++ venia-integration-tests/src/tests/e2eTests/category/verifyFilters.spec.js
@@ -258,223 +258,201 @@ describe(
 
             assertPaginationActivePage(1);
         });
-        it(
-            'user should be able to use radio-boolean filter results in Category and Search pages',
-            { tags: ['@skip'] },
-            () => {
-                cy.intercept('GET', getCategoriesCall).as(
-                    'gqlGetCategoriesQuery'
-                );
-                cy.intercept('GET', getCategoryDataCall).as(
-                    'gqlGetCategoryDataQuery'
-                );
-                cy.intercept('GET', getProductFiltersByCategoryCall).as(
-                    'gqlGetProductFiltersByCategoryQuery'
-                );
-                cy.intercept('GET', getProductFiltersBySearchCall).as(
-                    'gqlGetProductFiltersBySearchQuery'
-                );
-                cy.intercept('GET', getProductSearchCall).as(
-                    'gqlGetProductSearchQuery'
-                );
+        it('user should be able to use radio-boolean filter results in Category and Search pages', () => {
+            cy.intercept('GET', getCategoriesCall).as('gqlGetCategoriesQuery');
+            cy.intercept('GET', getCategoryDataCall).as(
+                'gqlGetCategoryDataQuery'
+            );
+            cy.intercept('GET', getProductFiltersByCategoryCall).as(
+                'gqlGetProductFiltersByCategoryQuery'
+            );
+            cy.intercept('GET', getProductFiltersBySearchCall).as(
+                'gqlGetProductFiltersBySearchQuery'
+            );
+            cy.intercept('GET', getProductSearchCall).as(
+                'gqlGetProductSearchQuery'
+            );
 
-                // Test - Add simple product to cart from Product Page
-                cy.visit(categoryAccessories.url);
+            // Test - Add simple product to cart from Product Page
+            cy.visit(categoryAccessories.url);
 
-                cy.wait(
-                    [
-                        '@gqlGetCategoriesQuery',
-                        '@gqlGetCategoryDataQuery',
-                        '@gqlGetProductFiltersByCategoryQuery'
-                    ],
-                    {
-                        timeout: 60000
-                    }
-                );
-
-                assertCategoryTitle(categoryAccessories.name);
-
-                // Test - Desktop - Add and clear Has Video filter
-                let isMobile = false;
-                // Add price filter to keep filter list in place
-                selectFilterFromList(
-                    filtersData.price.name,
-                    filtersData.price.otherOption,
-                    isMobile
-                );
-
-                cy.wait(['@gqlGetCategoriesQuery'], {
+            cy.wait(
+                [
+                    '@gqlGetCategoriesQuery',
+                    '@gqlGetCategoryDataQuery',
+                    '@gqlGetProductFiltersByCategoryQuery'
+                ],
+                {
                     timeout: 60000
-                });
+                }
+            );
 
-                assertBooleanFilterUnselectedInputState(
-                    filtersData.hasVideo.name,
-                    isMobile
-                );
-                selectFilterFromList(
-                    filtersData.hasVideo.name,
-                    'No',
-                    isMobile,
-                    true
-                );
+            assertCategoryTitle(categoryAccessories.name);
 
-                cy.wait(['@gqlGetCategoriesQuery'], {
+            // Test - Desktop - Add and clear Has Video filter
+            let isMobile = false;
+            // Add price filter to keep filter list in place
+            selectFilterFromList(
+                filtersData.price.name,
+                filtersData.price.otherOption,
+                isMobile
+            );
+
+            cy.wait(['@gqlGetCategoriesQuery'], {
+                timeout: 60000
+            });
+
+            assertBooleanFilterUnselectedInputState(
+                filtersData.hasVideo.name,
+                isMobile
+            );
+            selectFilterFromList(
+                filtersData.hasVideo.name,
+                'No',
+                isMobile,
+                true
+            );
+
+            cy.wait(['@gqlGetCategoriesQuery'], {
+                timeout: 60000
+            });
+            assertCurrentFilter(filtersData.hasVideo.noLabel, isMobile);
+            assertNotInCurrentFilter(filtersData.hasVideo.yesLabel, isMobile);
+            assertNumberOfProductsInResults(11);
+            assertBooleanFilterInputState(
+                filtersData.hasVideo.name,
+                isMobile,
+                false
+            );
+
+            selectFilterFromList(
+                filtersData.hasVideo.name,
+                'Yes',
+                isMobile,
+                true
+            );
+
+            cy.wait(['@gqlGetCategoriesQuery'], {
+                timeout: 60000
+            });
+
+            assertCurrentFilter(filtersData.hasVideo.yesLabel, isMobile);
+            assertNotInCurrentFilter(filtersData.hasVideo.noLabel, isMobile);
+            assertNumberOfProductsInResults(4);
+            assertBooleanFilterInputState(
+                filtersData.hasVideo.name,
+                isMobile,
+                true
+            );
+
+            clearFilters(isMobile);
+
+            cy.wait(['@gqlGetCategoriesQuery'], {
+                timeout: 60000
+            });
+
+            assertNumberOfProductsInResults(24);
+            assertBooleanFilterUnselectedInputState(
+                filtersData.hasVideo.name,
+                isMobile
+            );
+            // Test - Mobile - Add and clear Has Video filter
+            isMobile = true;
+            cy.viewport(375, 812);
+            cy.visit(categoryAccessories.url);
+
+            cy.wait(
+                [
+                    '@gqlGetCategoriesQuery',
+                    '@gqlGetCategoryDataQuery',
+                    '@gqlGetProductFiltersByCategoryQuery'
+                ],
+                {
                     timeout: 60000
-                });
-                assertCurrentFilter(filtersData.hasVideo.noLabel, isMobile);
-                assertNotInCurrentFilter(
-                    filtersData.hasVideo.yesLabel,
-                    isMobile
-                );
-                assertNumberOfProductsInResults(11);
-                assertBooleanFilterInputState(
-                    filtersData.hasVideo.name,
-                    isMobile,
-                    false
-                );
+                }
+            );
 
-                selectFilterFromList(
-                    filtersData.hasVideo.name,
-                    'Yes',
-                    isMobile,
-                    true
-                );
+            toggleFilterModal();
+            // Add price filter to keep filter list in place
+            selectFilterFromList(
+                filtersData.price.name,
+                filtersData.price.otherOption,
+                isMobile
+            );
+            assertNotInCurrentFilter(filtersData.hasVideo.yesLabel);
+            assertNotInCurrentFilter(filtersData.hasVideo.noLabel);
+            assertBooleanFilterUnselectedInputState(
+                filtersData.hasVideo.name,
+                isMobile
+            );
+            selectFilterFromList(filtersData.hasVideo.name, 'No', true, true);
+            assertNotInCurrentFilter(filtersData.hasVideo.yesLabel);
+            assertCurrentFilter(filtersData.hasVideo.noLabel);
+            assertBooleanFilterInputState(
+                filtersData.hasVideo.name,
+                isMobile,
+                false
+            );
+            applyFiltersFromFilterModal();
 
-                cy.wait(['@gqlGetCategoriesQuery'], {
-                    timeout: 60000
-                });
+            cy.wait(['@gqlGetCategoriesQuery'], {
+                timeout: 60000
+            });
 
-                assertCurrentFilter(filtersData.hasVideo.yesLabel, isMobile);
-                assertNotInCurrentFilter(
-                    filtersData.hasVideo.noLabel,
-                    isMobile
-                );
-                assertNumberOfProductsInResults(4);
-                assertBooleanFilterInputState(
-                    filtersData.hasVideo.name,
-                    isMobile,
-                    true
-                );
+            assertCategoryTitle(categoryAccessories.name);
+            assertNumberOfProductsInResults(11);
 
-                clearFilters(isMobile);
+            toggleFilterModal();
+            assertNotInCurrentFilter(filtersData.hasVideo.yesLabel);
+            assertCurrentFilter(filtersData.hasVideo.noLabel);
+            assertBooleanFilterInputState(
+                filtersData.hasVideo.name,
+                isMobile,
+                false
+            );
+            selectFilterFromList(filtersData.hasVideo.name, 'Yes', true, true);
+            assertCurrentFilter(filtersData.hasVideo.yesLabel);
+            assertNotInCurrentFilter(filtersData.hasVideo.noLabel);
+            assertBooleanFilterInputState(
+                filtersData.hasVideo.name,
+                isMobile,
+                true
+            );
+            applyFiltersFromFilterModal();
 
-                cy.wait(['@gqlGetCategoriesQuery'], {
-                    timeout: 60000
-                });
+            cy.wait(['@gqlGetCategoriesQuery'], {
+                timeout: 60000
+            });
 
-                assertNumberOfProductsInResults(24);
-                assertBooleanFilterUnselectedInputState(
-                    filtersData.hasVideo.name,
-                    isMobile
-                );
-                // Test - Mobile - Add and clear Has Video filter
-                isMobile = true;
-                cy.viewport(375, 812);
-                cy.visit(categoryAccessories.url);
+            assertCategoryTitle(categoryAccessories.name);
+            assertNumberOfProductsInResults(4);
 
-                cy.wait(
-                    [
-                        '@gqlGetCategoriesQuery',
-                        '@gqlGetCategoryDataQuery',
-                        '@gqlGetProductFiltersByCategoryQuery'
-                    ],
-                    {
-                        timeout: 60000
-                    }
-                );
+            toggleFilterModal();
+            clearFilter(filtersData.hasVideo.yesLabel);
+            assertNotInCurrentFilter(filtersData.hasVideo.yesLabel);
+            assertNotInCurrentFilter(filtersData.hasVideo.noLabel);
+            assertBooleanFilterUnselectedInputState(
+                filtersData.hasVideo.name,
+                isMobile
+            );
 
-                toggleFilterModal();
-                // Add price filter to keep filter list in place
-                selectFilterFromList(
-                    filtersData.price.name,
-                    filtersData.price.otherOption,
-                    isMobile
-                );
-                assertNotInCurrentFilter(filtersData.hasVideo.yesLabel);
-                assertNotInCurrentFilter(filtersData.hasVideo.noLabel);
-                assertBooleanFilterUnselectedInputState(
-                    filtersData.hasVideo.name,
-                    isMobile
-                );
-                selectFilterFromList(
-                    filtersData.hasVideo.name,
-                    'No',
-                    true,
-                    true
-                );
-                assertNotInCurrentFilter(filtersData.hasVideo.yesLabel);
-                assertCurrentFilter(filtersData.hasVideo.noLabel);
-                assertBooleanFilterInputState(
-                    filtersData.hasVideo.name,
-                    isMobile,
-                    false
-                );
-                applyFiltersFromFilterModal();
-
-                cy.wait(['@gqlGetCategoriesQuery'], {
-                    timeout: 60000
-                });
-
-                assertCategoryTitle(categoryAccessories.name);
-                assertNumberOfProductsInResults(11);
-
-                toggleFilterModal();
-                assertNotInCurrentFilter(filtersData.hasVideo.yesLabel);
-                assertCurrentFilter(filtersData.hasVideo.noLabel);
-                assertBooleanFilterInputState(
-                    filtersData.hasVideo.name,
-                    isMobile,
-                    false
-                );
-                selectFilterFromList(
-                    filtersData.hasVideo.name,
-                    'Yes',
-                    true,
-                    true
-                );
-                assertCurrentFilter(filtersData.hasVideo.yesLabel);
-                assertNotInCurrentFilter(filtersData.hasVideo.noLabel);
-                assertBooleanFilterInputState(
-                    filtersData.hasVideo.name,
-                    isMobile,
-                    true
-                );
-                applyFiltersFromFilterModal();
-
-                cy.wait(['@gqlGetCategoriesQuery'], {
-                    timeout: 60000
-                });
-
-                assertCategoryTitle(categoryAccessories.name);
-                assertNumberOfProductsInResults(4);
-
-                toggleFilterModal();
-                clearFilter(filtersData.hasVideo.yesLabel);
-                assertNotInCurrentFilter(filtersData.hasVideo.yesLabel);
-                assertNotInCurrentFilter(filtersData.hasVideo.noLabel);
-                assertBooleanFilterUnselectedInputState(
-                    filtersData.hasVideo.name,
-                    isMobile
-                );
-
-                applyFiltersFromFilterModal();
+            applyFiltersFromFilterModal();
 
             cy.wait(['@gqlGetCategoriesQuery'], {
                 timeout: 60000
             });
             assertNumberOfProductsInResults(18);
 
-                //Clean Up
-                toggleFilterModal();
-                clearFilters();
-                applyFiltersFromFilterModal();
+            //Clean Up
+            toggleFilterModal();
+            clearFilters();
+            applyFiltersFromFilterModal();
 
-                cy.wait(['@gqlGetCategoriesQuery'], {
-                    timeout: 60000
-                });
+            cy.wait(['@gqlGetCategoriesQuery'], {
+                timeout: 60000
+            });
 
-                assertNumberOfProductsInResults(24);
-            }
-        );
+            assertNumberOfProductsInResults(24);
+        });
     }
 );
