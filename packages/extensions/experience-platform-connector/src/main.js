38746d5637018c34e8d622a1c8d31a906de365fb
James Calcaben <calcaben@adobe.com>
2022-05-31 09:53:24 -0500

    Integrate the storefront events sdk and collector libraries into the extension

--- packages/extensions/experience-platform-connector/src/main.js
+++ packages/extensions/experience-platform-connector/src/main.js
@@ -1,18 +1,45 @@
 import { useEventingContext } from '@magento/peregrine/lib/context/eventing';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default original => props => {
     const [observable] = useEventingContext();
 
-    useEffect(() => {
-        const sub = observable.subscribe(async event => {
-          // Add event handlers here
-        });
+    const [sdk, setSdk] = useState();
 
-        return () => {
-            sub.unsubscribe();
-        };
-    },[observable]);
+    useEffect(() => {
+        import('@adobe/magento-storefront-events-sdk').then(mse => {
+            if (!window.magentoStorefrontEvents) {
+                window.magentoStorefrontEvents = mse;
+            }
+
+            // TODO: Update this once we are able to get the id values from graphql
+            mse.context.setAEP({
+                imsOrgId: process.env.IMS_ORG_ID,
+                datastreamId: process.env.DATASTREAM_ID
+            });
+
+            mse.context.setEventForwarding({
+                aep: true
+            });
+
+            import('@adobe/magento-storefront-event-collector').then(msec => {
+                msec;
+                setSdk(mse);
+            });
+        });
+    }, [setSdk]);
+
+    useEffect(() => {
+        if (sdk) {
+            const sub = observable.subscribe(async event => {
+                // Add event handlers here
+            });
+
+            return () => {
+                sub.unsubscribe();
+            };
+        }
+    }, [sdk, observable]);
 
     return original(props);
 };
