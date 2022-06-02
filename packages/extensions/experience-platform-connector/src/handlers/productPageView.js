d7e0f4d9d7f5409807cd183f2e80bdbc76302865
James Calcaben <calcaben@adobe.com>
2022-06-02 12:56:33 -0500

    Add product page view event handling

--- packages/extensions/experience-platform-connector/src/handlers/productPageView.js
+++ packages/extensions/experience-platform-connector/src/handlers/productPageView.js
@@ -1,13 +1,42 @@
-const canHandle = event => {
-  // TODO: Return true if this module's handler can handle the event
-  return;
-}
+const canHandle = event => event.type === 'PRODUCT_PAGE_VIEW';
 
 const handle = (sdk, event) => {
-  // TODO: Handler logic
-}
+    const { payload } = event;
+
+    const { name, id, currency_code, price_range, sku, url_key } = payload;
+
+    const pageContext = {
+        pageType: 'PDP',
+        pageName: name,
+        eventType: 'visibilityHidden',
+        maxXOffset: 0,
+        maxYOffset: 0,
+        minXOffset: 0,
+        minYOffset: 0
+    };
+
+    sdk.context.setPage(pageContext);
+
+    sdk.publish.pageView();
+
+    const productContext = {
+        productId: id,
+        name: name,
+        sku: sku,
+        pricing: {
+            currencyCode: currency_code,
+            maximalPrice: price_range.maximum_price.final_price
+        },
+        canonicalUrl: url_key
+    };
+
+    sdk.context.setProduct(productContext);
+
+    // TODO: Handler logic
+    sdk.publish.productPageView();
+};
 
 export default {
-  canHandle,
-  handle
-}
+    canHandle,
+    handle
+};
