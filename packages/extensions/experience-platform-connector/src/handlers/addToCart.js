986a9b5121f083a0fb748aa7e495e866380fb33a
James Calcaben <calcaben@adobe.com>
2022-06-07 15:08:15 -0500

    Add support for add to cart events

--- packages/extensions/experience-platform-connector/src/handlers/addToCart.js
+++ packages/extensions/experience-platform-connector/src/handlers/addToCart.js
@@ -1,13 +1,60 @@
-const canHandle = event => {
-  // TODO: Return true if this module's handler can handle the event
-  return;
-}
+const canHandle = event => event.type === 'CART_ADD_ITEM';
 
 const handle = (sdk, event) => {
-  // TODO: Handler logic
-}
+    const { payload } = event;
+
+    const {
+        cartId,
+        currencyCode,
+        priceTotal,
+        quantity,
+        name,
+        sku,
+        selectedOptions
+    } = payload;
+
+    const configurableOptions = selectedOptions
+        ? [
+              {
+                  optionLabel: selectedOptions.attribute,
+                  valueLabel: selectedOptions.value
+              }
+          ]
+        : null;
+
+    const cartItemContext = {
+        id: cartId,
+        prices: {
+            subtotalExcludingTax: {
+                value: priceTotal * quantity,
+                currency: currencyCode
+            }
+        },
+        items: [
+            {
+                product: {
+                    name: name,
+                    sku: sku,
+                    configurableOptions: configurableOptions
+                },
+                prices: {
+                    price: {
+                        value: priceTotal,
+                        currency: currencyCode
+                    }
+                }
+            }
+        ],
+        possibleOnepageCheckout: false,
+        giftMessageSelected: false,
+        giftWrappingSelected: false
+    };
+
+    sdk.context.setShoppingCart(cartItemContext);
+    sdk.publish.addToCart();
+};
 
 export default {
-  canHandle,
-  handle
-}
+    canHandle,
+    handle
+};
