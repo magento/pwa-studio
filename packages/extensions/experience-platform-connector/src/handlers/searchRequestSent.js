4ec22756bfaafa6a2992447209c84a5310bffd8c
James Calcaben <calcaben@adobe.com>
2022-06-13 13:46:04 -0500

    Add support for search requests

--- packages/extensions/experience-platform-connector/src/handlers/searchRequestSent.js
+++ packages/extensions/experience-platform-connector/src/handlers/searchRequestSent.js
@@ -1,13 +1,37 @@
-const canHandle = event => {
-  // TODO: Return true if this module's handler can handle the event
-  return;
-}
+const canHandle = event => event.type.in(['SEARCH_REQUEST', 'SEARCHBAR_REQUEST']);
 
 const handle = (sdk, event) => {
-  // TODO: Handler logic
-}
+    const { payload } = event;
+
+    const { query, pageSize, currentPage, refinements, sort } = payload;
+
+    const filter = refinements.map(refinement => {
+        const { attribute, value } = refinement;
+        return {
+            attribute: attribute,
+            in: Array.from(value.values())
+        };
+    });
+
+    const requestContext = {
+        units: [
+            {
+                searchUnitId: 'productPage',
+                queryTypes: ['products'],
+                phrase: query,
+                pageSize: pageSize,
+                currentPage: currentPage,
+                filter: filter,
+                sort: [{ attribute: sort.attribute, direction: sort.order }]
+            }
+        ]
+    };
+
+    sdk.context.setSearchInput(requestContext);
+    sdk.publish.searchRequestSent();
+};
 
 export default {
-  canHandle,
-  handle
-}
+    canHandle,
+    handle
+};
