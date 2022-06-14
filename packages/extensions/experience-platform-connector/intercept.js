6039f1fe202c7bc23fc1e484149f6b9dbf6c533d
Anthoula Wojczak <awojczak@adobe.com>
2022-06-14 16:55:23 -0500

    PWA-2879: Custom Beacon extension event
    - add wrapper for useAutocomplete
    - add dispatch event to wrapper
    - publish Search Request Response in handler

--- packages/extensions/experience-platform-connector/intercept.js
+++ packages/extensions/experience-platform-connector/intercept.js
@@ -12,10 +12,13 @@ module.exports = targets => {
         };
     });
 
-    talons.tap(({ App, Header }) => {
+    talons.tap(({ App, Header, SearchBar }) => {
         App.useApp.wrapWith('@magento/experience-platform-connector');
         Header.useAccountMenu.wrapWith(
             '@magento/experience-platform-connector/src/wrappers/wrapUseAccountMenu'
+        );
+        SearchBar.useAutocomplete.wrapWith(
+            '@magento/experience-platform-connector/src/wrappers/wrapUseAutocomplete'
         );
     });
 };
