b1ec13d85d4ff445063d7acb2e9244d4dd63b7af
Yousef Yasin <yyasin@orienteed.com>
2022-06-20 15:01:44 +0200

    overwrite order details for order history page

---
+++ packages/venia-concept/src/components/OrderHistoryPage/OrderDetails/item.js
@@ -0,0 +1,106 @@
+import React, { useMemo } from 'react';
+import { shape, string, number, arrayOf } from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+import { Link } from 'react-router-dom';
+import { useOrderHistoryContext } from '@magento/peregrine/lib/talons/OrderHistoryPage/orderHistoryContext';
+
+import { useStyle } from '@magento/venia-ui/lib/classify';
+import Button from '@magento/venia-ui/lib/components/Button';
+import ProductOptions from '@magento/venia-ui/lib/components/LegacyMiniCart/productOptions';
+import PlaceholderImage from '@magento/venia-ui/lib/components/Image/placeholderImage';
+import Image from '@magento/venia-ui/lib/components/Image';
+import Price from '@magento/venia-ui/lib/components/Price';
+import defaultClasses from './item.module.css';
+const Item = props => {
+    const {
+        index,
+        product_name,
+        product_sale_price,
+        product_url_key,
+        quantity_ordered,
+        selected_options,
+        thumbnail
+    } = props;
+
+    const orderHistoryState = useOrderHistoryContext();
+    const { productURLSuffix } = orderHistoryState;
+    const itemLink = `${product_url_key}${productURLSuffix}`;
+    const mappedOptions = useMemo(
+        () => selected_options.map(option => option.value),
+        [selected_options]
+    );
+    const classes = useStyle(defaultClasses, props.classes);
+
+    const thumbnailProps = {
+        alt: product_name,
+        classes: { root: classes.thumbnail },
+        width: 87,
+        height: 70
+    };
+    const thumbnailElement = thumbnail ? (
+        <Image {...thumbnailProps} resource={thumbnail.url} />
+    ) : (
+        <PlaceholderImage {...thumbnailProps} />
+    );
+
+    return (
+        <>
+            <tr className={classes.tableRow}>
+                <td>{index + 1}</td>
+                <td>
+                    <Link className={classes.thumbnailContainer} to={itemLink}>
+                        <span className={classes.nameProduct}>
+                            {thumbnailElement}
+                            {product_name} {mappedOptions.join(', ')}
+                        </span>
+                    </Link>
+                </td>
+                <td>{mappedOptions[0]}</td>
+                <td>{quantity_ordered} items</td>
+                <td>
+                    <Price
+                        value={product_sale_price.value}
+                        currencyCode={product_sale_price.currency}
+                    />{' '}
+                </td>
+                <td className={classes.grossPrice}>
+                    <Price
+                        value={product_sale_price.value}
+                        currencyCode={product_sale_price.currency}
+                    />{' '}
+                </td>
+            </tr>
+        </>
+    );
+};
+
+export default Item;
+
+Item.propTypes = {
+    classes: shape({
+        root: string,
+        thumbnailContainer: string,
+        thumbnail: string,
+        name: string,
+        options: string,
+        quantity: string,
+        price: string,
+        buyAgainButton: string
+    }),
+    product_name: string.isRequired,
+    product_sale_price: shape({
+        currency: string,
+        value: number
+    }).isRequired,
+    product_url_key: string.isRequired,
+    quantity_ordered: number.isRequired,
+    selected_options: arrayOf(
+        shape({
+            label: string,
+            value: string
+        })
+    ).isRequired,
+    thumbnail: shape({
+        url: string
+    })
+};
