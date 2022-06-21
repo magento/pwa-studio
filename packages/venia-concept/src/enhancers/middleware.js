dae52a6c8c9f5816ac806d74dbe654e5931d2469
JESUS <jiasenzaniro@orienteed.com>
2022-06-21 12:52:44 +0200

    add the redirection functionality

--- packages/venia-concept/src/enhancers/middleware.js
+++ packages/venia-concept/src/enhancers/middleware.js
@@ -1,7 +1,14 @@
 import { applyMiddleware } from 'redux';
 
+import auth from '@magento/peregrine/lib/store/middleware/auth';
+import log from '@magento/peregrine/lib/store/middleware/log';
+import thunk from '@magento/peregrine/lib/store/middleware/thunk';
 import restrictedAuthPage from '../middleware/restrictedAuthPage';
 
-const middleware = [restrictedAuthPage];
+const middleware = [thunk, auth, restrictedAuthPage];
+
+if (process.env.NODE_ENV !== 'production') {
+    middleware.push(log);
+}
 
 export default applyMiddleware(...middleware);
--- packages/venia-concept/src/middleware/restrictedAuthPage.js
+++ packages/venia-concept/src/middleware/restrictedAuthPage.js
@@ -1,28 +1,14 @@
 import { BrowserPersistence } from '@magento/peregrine/lib/util';
 const storage = new BrowserPersistence();
-const unAuthPath = [
-    'sign-in',
-    'create-account',
-    'forgot-password',
-    'create-account-be-customer',
-    'create-account-non-customer',
-    'customer/account/createPassword'
-];
 
 const restrictedAuthPage = store => next => action => {
+    const unAuthPath = ['sign-in', 'create-account', 'forgot-password'];
     const currentPath = window.location.pathname;
-
-    let storeConfigRequiredLogin = storage.getItem('is_required_login');
-
-    if (!storeConfigRequiredLogin) {
-        return next(action);
-    }
-
     const signin_token = storage.getItem('signin_token');
 
-    if (signin_token == undefined) {
+    if (!signin_token) {
         let found = false;
-        unAuthPath.forEach(function(value) {
+        unAuthPath.forEach(value => {
             if (currentPath.indexOf(value) > -1) {
                 found = true;
             }
@@ -30,18 +16,6 @@ const restrictedAuthPage = store => next => action => {
 
         if (!found) {
             history.pushState({}, '', '/sign-in');
-            history.go(0);
-        }
-    } else {
-        let found = false;
-        unAuthPath.forEach(function(value) {
-            if (currentPath.indexOf(value) > -1) {
-                found = true;
-            }
-        });
-
-        if (found) {
-            history.pushState({}, '', '/');
             history.go(0);
         }
     }
