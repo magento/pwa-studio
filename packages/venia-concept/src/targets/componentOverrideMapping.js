f4ac4bcb1d427ec594e62c8dc1db7ab919bff85c
JESUS <jiasenzaniro@orienteed.com>
2022-06-22 13:33:54 +0200

    add the vat_id to the graphql in the addressBookFragment

--- packages/venia-concept/src/targets/componentOverrideMapping.js
+++ packages/venia-concept/src/targets/componentOverrideMapping.js
@@ -155,5 +155,8 @@ module.exports = componentOverrideMapping = {
     [`@magento/venia-ui/lib/components/WishlistPage/wishlist.module.css`]: './src/components/WishlistPage/wishlist.module.css',
     [`@magento/venia-ui/lib/components/WishlistPage/wishlistItems.module.css`]: './src/components/WishlistPage/wishlistItems.module.css',
     [`@magento/peregrine/lib/talons/WishlistPage/useWishlistItem.js`]: './src/talons/WishlistPage/useWishlistItem.js',
-    [`@magento/venia-ui/lib/components/CheckoutPage/ItemsReview/showAllButton.module.css`]: './src/components/CheckoutPage/ItemsReview/showAllButton.module.css'
+    [`@magento/venia-ui/lib/components/CheckoutPage/ItemsReview/showAllButton.module.css`]: './src/components/CheckoutPage/ItemsReview/showAllButton.module.css',
+
+    //new overwrite
+    [`@magento/peregrine/lib/talons/AddressBookPage/addressBookFragments.gql.js`]: './src/talons/AddresBookPage/addressBookFragments.gql.js'
 };
