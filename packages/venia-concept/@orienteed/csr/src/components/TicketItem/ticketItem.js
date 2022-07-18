894c6ea8ecddac5c2bb4f4a373c7a4a1c52b2344
jalvarez00 <jalvarez@orienteed.com>
2022-07-18 16:55:10 +0200

    Fix date

--- packages/venia-concept/@orienteed/csr/src/components/TicketItem/ticketItem.js
+++ packages/venia-concept/@orienteed/csr/src/components/TicketItem/ticketItem.js
@@ -54,10 +54,10 @@ const TicketItem = props => {
 
         const date = new Date(isoDate);
         const day = date
-            .getDay()
+            .getDate()
             .toString()
             .padStart(2, '0');
-        const month = months[date.getMonth() - 1];
+        const month = months[date.getMonth()];
         const year = date.getFullYear();
         return `${day} ${month} ${year}`;
     };
