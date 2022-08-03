3be95509b8be723cdc81fba923808a64611cc47d
Yousef Yasin <yyasin@orienteed.com>
2022-08-03 15:50:28 +0200

    fix product table

--- packages/venia-concept/@orienteed/customComponents/components/comparePage/components/ProductsTable.js
+++ packages/venia-concept/@orienteed/customComponents/components/comparePage/components/ProductsTable.js
@@ -22,56 +22,62 @@ const ProductsTable = ({ productsItems, deleteProduct }) => {
                 </thead>
                 <tbody>
                     <tr>
-                        <th scope="row" className={classes.cell}>
-                            <span>
-                                <FormattedMessage id={'compareProducts.product'} defaultMessage="Product" />
-                            </span>
-                        </th>
+                        {productsItems.length > 0 && (
+                            <th scope="row" className={classes.cell}>
+                                <span>
+                                    <FormattedMessage id={'compareProducts.product'} defaultMessage="Product" />
+                                </span>
+                            </th>
+                        )}
                         {productsItems?.map(item => (
                             <td key={item.product.sku + 'tbody1'} className={classes.cell}>
                                 <ProductsCard deleteProduct={deleteProduct} key={item.id} item={item} />
                             </td>
                         ))}
                     </tr>
-                    {productsItems[0].attributes.map(attribute => {
-                        let attributeProducts = productsItems.map(({ attributes }) =>
-                            attributes.find(({ code }) => code === attribute.code)
-                        );
-                        let inValidValues = attributeProducts.every(({ value }) => value === 'N/A');
-                        if (!inValidValues) {
-                            return (
-                                <tr>
-                                    <th className={classes.cell}>
-                                        <span>
-                                            {attribute.code
-                                                .split('_')
-                                                .join(' ')
-                                                .replace(/(^[a-z])/i, (str, firstLetter) => firstLetter.toUpperCase())}
-                                        </span>
-                                    </th>
-                                    {productsItems?.map(product => (
-                                        <td key={product.uid + 'tbody3'} className={classes.cell}>
-                                            <span>
-                                                {attribute.code === 'description' ? (
-                                                    <span className={classes.description}>
-                                                        <RichText
-                                                            content={
-                                                                product.attributes.find(
-                                                                    ({ code }) => code === attribute.code
-                                                                ).value
-                                                            }
-                                                        />
-                                                    </span>
-                                                ) : (
-                                                    product.attributes.find(({ code }) => code === attribute.code).value
-                                                )}
-                                            </span>
-                                        </td>
-                                    ))}
-                                </tr>
+                    {productsItems.length > 0 &&
+                        [0].attributes.map(attribute => {
+                            let attributeProducts = productsItems.map(({ attributes }) =>
+                                attributes.find(({ code }) => code === attribute.code)
                             );
-                        }
-                    })}
+                            let inValidValues = attributeProducts.every(({ value }) => value === 'N/A');
+                            if (!inValidValues) {
+                                return (
+                                    <tr>
+                                        <th className={classes.cell}>
+                                            <span>
+                                                {attribute.code
+                                                    .split('_')
+                                                    .join(' ')
+                                                    .replace(/(^[a-z])/i, (str, firstLetter) =>
+                                                        firstLetter.toUpperCase()
+                                                    )}
+                                            </span>
+                                        </th>
+                                        {productsItems?.map(product => (
+                                            <td key={product.uid + 'tbody3'} className={classes.cell}>
+                                                <span>
+                                                    {attribute.code === 'description' ? (
+                                                        <span className={classes.description}>
+                                                            <RichText
+                                                                content={
+                                                                    product.attributes.find(
+                                                                        ({ code }) => code === attribute.code
+                                                                    ).value
+                                                                }
+                                                            />
+                                                        </span>
+                                                    ) : (
+                                                        product.attributes.find(({ code }) => code === attribute.code)
+                                                            .value
+                                                    )}
+                                                </span>
+                                            </td>
+                                        ))}
+                                    </tr>
+                                );
+                            }
+                        })}
                 </tbody>
             </table>
         </div>
