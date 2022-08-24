8a13f6dfca24efb81fc5607232fafd2e15f3dc8d
glo82145 <glo82145@adobe.com>
2022-08-24 16:39:04 +0530

    AC-2488::'Has Video' checkboxes do not provide descriptive labels. (Search Results)

--- packages/venia-ui/lib/components/FilterModal/FilterList/filterItemRadio.js
+++ packages/venia-ui/lib/components/FilterModal/FilterList/filterItemRadio.js
@@ -43,6 +43,7 @@ const FilterItemRadio = props => {
             id={`item-${group}-${value}`}
             label={title}
             value={value}
+            aria-label={title}
             onChange={handleOnchange}
             data-cy="FilterDefault-radio"
             ariaLabel={ariaLabel}
