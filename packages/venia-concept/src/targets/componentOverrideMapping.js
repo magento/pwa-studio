bcb95e2f2243b99ee5bf669046f9d4e9ab9726ae
Yousef Yasin <yyasin@orienteed.com>
2022-11-01 09:56:28 +0100

    overWrite category tree

--- packages/venia-concept/src/targets/componentOverrideMapping.js
+++ packages/venia-concept/src/targets/componentOverrideMapping.js
@@ -190,5 +190,9 @@ module.exports = componentOverrideMapping = {
     [`@magento/venia-ui/lib/index.module.css`]: './src/css/index.module.css',
     [`@magento/venia-ui/lib/tokens.module.css`]: './src/css/tokens.module.css',
     [`@orienteed/customComponents/css/forms.module.css`]: './src/css/forms.module.css', // replace directly
-    [`@orienteed/reorder/components/ReOrderBtn/reOrderBtn.module.css`]: './src/components/ReOrderBtn/reOrderBtn.module.css' // replace directly
+    [`@orienteed/reorder/components/ReOrderBtn/reOrderBtn.module.css`]: './src/components/ReOrderBtn/reOrderBtn.module.css', // replace directly
+
+    //new OverWrite 
+    [`@magento/venia-ui/lib/components/CategoryTree/categoryTree.js`]: './src/components/CategoryTree/categoryTree.js',
+    [`@magento/venia-ui/lib/components/CategoryTree/categoryTree.module.css`]: './src/components/CategoryTree/categoryTree.module.css',
 };
