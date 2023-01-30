10470d8c448a6a9039217ae2fa5403fef8581620
jalvarez00 <jalvarez@orienteed.com>
2023-01-30 10:47:09 +0100

    fx getConfigDetails

--- app/packages/peregrine/lib/talons/RequestQuote/requestQuote.gql.js
+++ app/packages/peregrine/lib/talons/RequestQuote/requestQuote.gql.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export const GET_CONFIG_DETAILS = gql`
-    query getConfigDetailsRequestQuote {
+    query getConfigDetailsForRequestQuote {
         mpQuoteConfig {
             allow_category
             category
