7878fc6e72796401fb1882b14888b4374454d5d0
Yousef Yasin <yyasin@orienteed.com>
2023-02-07 15:50:00 +0100

    add next and prev btn in PDP

--- app/packages/venia-ui/lib/components/Breadcrumbs/breadcrumbs.js
+++ app/packages/venia-ui/lib/components/Breadcrumbs/breadcrumbs.js
@@ -1,13 +1,16 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { Fragment, useMemo } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { string } from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import { useBreadcrumbs } from '@magento/peregrine/lib/talons/Breadcrumbs/useBreadcrumbs';
 import resourceUrl from '@magento/peregrine/lib/util/makeUrl';
 import { useStyle } from '../../classify';
 import Shimmer from './breadcrumbs.shimmer';
 import defaultClasses from './breadcrumbs.module.css';
+import Icon from '@magento/venia-ui/lib/components/Icon';
+import { ChevronLeft as ChevronLeftIcon, ChevronRight as ChevronRightIcon } from 'react-feather';
 
 const DELIMITER = '/';
 /**
@@ -19,18 +22,53 @@ const DELIMITER = '/';
 const Breadcrumbs = props => {
     const classes = useStyle(defaultClasses, props.classes);
 
-    const { categoryId, currentProduct } = props;
+    const { categoryId, currentProduct, url_keys } = props;
+
+    const history = useHistory();
+
+    const urlKeysHistory = history.location?.state?.urlKeys;
+    const currentUrlKeys = urlKeysHistory ? urlKeysHistory : url_keys;
 
     const talonProps = useBreadcrumbs({ categoryId });
 
-    const {
-        currentCategory,
-        currentCategoryPath,
-        hasError,
-        isLoading,
-        normalizedData,
-        handleClick
-    } = talonProps;
+    const { currentCategory, currentCategoryPath, hasError, isLoading, normalizedData, handleClick } = talonProps;
+
+    const names = currentUrlKeys?.items?.map(ele => ele?.name);
+    const index = names?.indexOf(currentProduct);
+    const productLink = product => resourceUrl(`/${product.url_key}${product.url_suffix || ''}`);
+
+    const moveToOtherProcuct = type => {
+        const configrableProducts = currentUrlKeys?.items.filter(
+            ({ __typename }) => __typename === 'ConfigurableProduct'
+        );
+        if (type === 'next' && index === configrableProducts?.length - 1) {
+            const product = configrableProducts[0];
+            history.push({
+                pathname: productLink(product),
+                state: { urlKeys: currentUrlKeys }
+            });
+        } else if (type === 'prev' && index === 0) {
+            const product = configrableProducts[configrableProducts?.length - 1];
+            history.push({
+                pathname: productLink(product),
+                state: { urlKeys: currentUrlKeys }
+            });
+        } else if (index < configrableProducts?.length) {
+            if (type === 'prev') {
+                const prevProduct = configrableProducts[index - 1];
+                history.push({
+                    pathname: productLink(prevProduct),
+                    state: { urlKeys: currentUrlKeys }
+                });
+            } else if (type === 'next') {
+                const nextProduct = configrableProducts[index + 1];
+                history.push({
+                    pathname: productLink(nextProduct),
+                    state: { urlKeys: currentUrlKeys }
+                });
+            }
+        }
+    };
 
     // For all links generate a fragment like "/ Text"
     const links = useMemo(() => {
@@ -38,11 +76,7 @@ const Breadcrumbs = props => {
             return (
                 <Fragment key={text}>
                     <span className={classes.divider}>{DELIMITER}</span>
-                    <Link
-                        className={classes.link}
-                        to={resourceUrl(path)}
-                        onClick={handleClick}
-                    >
+                    <Link className={classes.link} to={resourceUrl(path)} onClick={handleClick}>
                         {text}
                     </Link>
                 </Fragment>
@@ -56,24 +90,14 @@ const Breadcrumbs = props => {
 
     // Don't display anything but the empty, static height div when there's an error.
     if (hasError) {
-        return (
-            <div
-                className={classes.root}
-                aria-live="polite"
-                aria-busy="false"
-            />
-        );
+        return <div className={classes.root} aria-live="polite" aria-busy="false" />;
     }
 
     // If we have a "currentProduct" it means we're on a PDP so we want the last
     // category text to be a link. If we don't have a "currentProduct" we're on
     // a category page so it should be regular text.
     const currentCategoryLink = currentProduct ? (
-        <Link
-            className={classes.link}
-            to={resourceUrl(currentCategoryPath)}
-            onClick={handleClick}
-        >
+        <Link className={classes.link} to={resourceUrl(currentCategoryPath)} onClick={handleClick}>
             {currentCategory}
         </Link>
     ) : (
@@ -89,13 +113,26 @@ const Breadcrumbs = props => {
 
     return (
         <div className={classes.root} aria-live="polite" aria-busy="false">
-            <Link className={classes.link} to="/">
-                <FormattedMessage id={'global.home'} defaultMessage={'Home'} />
-            </Link>
-            {links}
-            <span className={classes.divider}>{DELIMITER}</span>
-            {currentCategoryLink}
-            {currentProductNode}
+            <div className={classes.leftNav} aria-live="polite" aria-busy="false">
+                <Link className={classes.link} to="/">
+                    <FormattedMessage id={'global.home'} defaultMessage={'Home'} />
+                </Link>
+                {links}
+                <span className={classes.divider}>{DELIMITER}</span>
+                {currentCategoryLink}
+                {currentProductNode}
+            </div>
+
+            {(urlKeysHistory || currentUrlKeys) && (
+                <div className={classes.rightNav}>
+                    <button onClick={() => moveToOtherProcuct('prev')}>
+                        <Icon src={ChevronLeftIcon} />
+                    </button>
+                    <button onClick={() => moveToOtherProcuct('next')}>
+                        <Icon src={ChevronRightIcon} />
+                    </button>
+                </div>
+            )}
         </div>
     );
 };
