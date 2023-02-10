a76c0d07778382223e5bb3fc90eaee4167d67cf7
smartin00 <smartin@orienteed.com>
2023-02-10 12:52:31 +0100

    modify css variables in SVGs

--- app/packages/venia-ui/lib/assets/compareIcon.js
+++ app/packages/venia-ui/lib/assets/compareIcon.js
@@ -18,15 +18,15 @@ export const CompareIcon = () => {
             <g>
                 <path
                     d="M0.168,9.709v28.374h6.248c1.104,0,2-0.896,2-2V7.709H2.168C1.064,7.709,0.168,8.605,0.168,9.709z"
-                    fill="var(--b2bstore-compareIcon-color)"
+                    fill="var(--b2bstore-global-color-orange)"
                 />
                 <path
                     d="M22.776,36.083V15.3h-6.248c-1.105,0-2,0.896-2,2v20.783h6.248C21.88,38.083,22.776,37.188,22.776,36.083z"
-                    fill="var(--b2bstore-compareIcon-color)"
+                    fill="var(--b2bstore-global-color-orange)"
                 />
                 <path
                     d="M37.915,36.083V0h-6.248c-1.104,0-2,0.896-2,2v36.083h6.248C37.018,38.083,37.915,37.188,37.915,36.083z"
-                    fill="var(--b2bstore-compareIcon-color)"
+                    fill="var(--b2bstore-global-color-orange)"
                 />
             </g>
         </svg>
--- app/packages/venia-ui/lib/assets/downloadIcon.js
+++ app/packages/venia-ui/lib/assets/downloadIcon.js
@@ -5,9 +5,9 @@ export const DownloadIcon = () => {
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
                 d="M11.1582 0V16.7594L5.45144 10.7895L4.2334 11.9474L12.0003 20.0752L19.7672 11.9474L18.5492 10.782L12.8424 16.7519V0H11.1582Z"
-                fill='var(--b2bstore-global-color-orange-SVG)'
+                fill='var(--b2bstore-global-color-orange)'
             />
-            <path d="M4 22.0752H20V23.7594H4V22.0752Z" fill='var(--b2bstore-global-color-orange-SVG)' />
+            <path d="M4 22.0752H20V23.7594H4V22.0752Z" fill='var(--b2bstore-global-color-orange)' />
         </svg>
     );
 };
