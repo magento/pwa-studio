0c9edb0b054958fd81c1f89936f05b16aaef1593
JESUS <jiasenzaniro@orienteed.com>
2023-02-14 14:37:47 +0100

    add the styles

--- app/packages/venia-ui/lib/components/StoreLocator/Map/Map.js
+++ app/packages/venia-ui/lib/components/StoreLocator/Map/Map.js
@@ -2,17 +2,25 @@ import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import LocationPin from '../LocationPin/LocationPin';
 
-const Map = ({ location, zoomLevel }) => {
+import { useStyle } from '../../../classify';
+import defaultClasses from './Map.module.css';
+
+const Map = props => {
+    const { location, zoomLevel } = props;
+    const classes = useStyle(defaultClasses, props.classes);
+    const googleApiKey = process.env.GOOGLE_API_KEY;
+
     return (
-        <div>
-            <GoogleMapReact
-                style={{ width: '100px' }}
-                bootstrapURLKeys={{ key: process.env.GOOGLE_API_KEY }} // we have to create an env variable
-                defaultCenter={location}
-                defaultZoom={zoomLevel}
-            >
-                <LocationPin lat={location.lat} lng={location.lng} text={location.address} />
-            </GoogleMapReact>
+        <div className={classes.googleMapContainer}>
+            <article>
+                <GoogleMapReact
+                    bootstrapURLKeys={{ key: googleApiKey }} // we have to create an env variable
+                    defaultCenter={location}
+                    defaultZoom={zoomLevel}
+                >
+                    <LocationPin lat={location.lat} lng={location.lng} text={location.address} />
+                </GoogleMapReact>
+            </article>
         </div>
     );
 };
