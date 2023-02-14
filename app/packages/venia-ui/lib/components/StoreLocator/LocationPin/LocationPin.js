9e4e1e8f6c0318ead43a8a308ca5dc0760db2f8d
JESUS <jiasenzaniro@orienteed.com>
2023-02-14 14:38:10 +0100

    add the icon to the locator pin

--- app/packages/venia-ui/lib/components/StoreLocator/LocationPin/LocationPin.js
+++ app/packages/venia-ui/lib/components/StoreLocator/LocationPin/LocationPin.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { MapPin } from 'react-feather';
+import Icon from '../../Icon';
 
 const LocationPin = () => {
-    return <div>LocationPin</div>;
+    return (
+        <div>
+            <Icon src={MapPin} />
+        </div>
+    );
 };
 
 export default LocationPin;
