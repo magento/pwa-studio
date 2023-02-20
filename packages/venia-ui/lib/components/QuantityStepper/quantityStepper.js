17aedbe0ec774307d97c7d8fb36f66b1f4eafd8d
sahil <sahil.kumar.sharma@gds.ey.com>
2023-02-20 22:42:12 +0530

    Prettier

--- packages/venia-ui/lib/components/QuantityStepper/quantityStepper.js
+++ packages/venia-ui/lib/components/QuantityStepper/quantityStepper.js
@@ -14,7 +14,7 @@ const QuantityStepper = props => {
     const { formatMessage } = useIntl();
     const classes = useStyle(defaultClasses, props.classes);
     const iconClasses = { root: classes.icon };
-    const handleFocus = (event) => event.target.select();
+    const handleFocus = event => event.target.select();
 
     const talonProps = useQuantityStepper({
         initialValue,
