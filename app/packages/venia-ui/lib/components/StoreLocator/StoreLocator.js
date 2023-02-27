2981d480025b12c6714b3bf3783071fec58c60b1
JESUS <jiasenzaniro@orienteed.com>
2023-02-27 13:30:39 +0100

    add the store locator component

--- app/packages/venia-ui/lib/components/StoreLocator/StoreLocator.js
+++ app/packages/venia-ui/lib/components/StoreLocator/StoreLocator.js
@@ -1,28 +1,13 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
-import { arrayOf, node, shape, string } from 'prop-types';
-// import { useStockStatusMessage } from '@magento/peregrine/lib/talons/StockStatusMessage/useStockStatusMessage';
-
 import { useStyle } from '../../classify';
-// import defaultClasses from './stockStatusMessage.module.css';
-import { useStoreLocatorContext } from './StoreLocatorProvider/StoreLocatorProvider';
-import Map from './Map/Map';
-
-const location = {
-    address: '1600 Amphitheatre Parkway, Mountain View, california.',
-    lat: 37.42216,
-    lng: -122.08427
-};
+import MapContainer from './Map/MapContainer';
+import defaultClasses from './Map/MapContainer.module.css';
 
 const StoreLocator = props => {
-    const { hello } = useStoreLocatorContext();
+    const classes = useStyle(defaultClasses, props.classes);
 
-    return (
-        <div>
-            <div>hello</div>
-            <Map location={location} zoomLevel={15} />
-        </div>
-    );
+    return <MapContainer />;
 };
 
 export default StoreLocator;
