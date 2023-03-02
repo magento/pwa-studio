e499c3d6a1a836a687454515b54bf9ee2b1365b3
jalvarez00 <jalvarez@orienteed.com>
2023-03-02 11:30:46 +0100

    update useAdapter hook

--- app/packages/peregrine/lib/hooks/useAdapter.js
+++ app/packages/peregrine/lib/hooks/useAdapter.js
@@ -1,68 +1,24 @@
-import { useApolloClient } from '@apollo/client';
-import { Magento2 } from '../RestApi';
+import mergeOperations from '../util/shallowMerge';
+import { Magento2 } from '../../lib/RestApi';
+import { useAwaitQuery } from '../../lib/hooks/useAwaitQuery';
+import { useLazyQuery, useMutation, useQuery, useSubscription } from '@apollo/client';
 
-import {getResolver} from '@b2bstore/boilerplate-adapter';
-
-    const Client = boilerplate.Client.getInstance();
-    Client.setFetcher('')
-
-    const boilerplate = getResolver(Client);
+import { getResolvers } from '@b2bstore/boilerplate-adapter';
 
 export const useAdapter = () => {
-    /*
-        {
-            getProducts,
-            getProductById
-        }
-    */
+    const { request: restClient } = Magento2;
+
+    const resolvers = getResolvers({
+        mergeOperations,
+        restClient,
+        useAwaitQuery,
+        useLazyQuery,
+        useMutation,
+        useQuery,
+        useSubscription
+    });
 
     return {
-        ...boilerplate
+        ...resolvers
     };
 };
-
-// Hook
-const { getProducts } = useAdapter();
-getProducts()
-
-const potencia = (a,b) => a**b
-
-generadorPotencia = a => b => b**a
-generadorPotencia(3)(4)
-// currificar
-console.log(generadorPotencia(3)) // f
-
-const cuadrado = generdorPotencia(2);
-cuadrado(4)
-cuadrado(5)
-
-
-// VISTA -> HOOK -> ADAPTER -> LIBRERIA
-
-
-// Libreria
-const getProducts = (Client) => () => {
-    // const Client = boilerplate.Client.getInstance();
-    let fetcher = Client.getFetcher()
-
-    fetcher.getProducts
-
-    return DataTransfer
-}
-
-const getProductById = (Client) => (id) => {
-    // const Client = boilerplate.Client.getInstance();
-    let fetcher = Client.getFetcher()
-
-    fetcher.getProducts(id)
-
-    return DataTransfer
-}
-
-
-const getResolver = (Client) => {
-    return {
-        getProducts: getProducts(Client),
-        getProductById: getProducts(Client)
-    }
-}
