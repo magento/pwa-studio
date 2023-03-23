e2ac5e5704b7dacd3838b01c9b103cb4ea05b88e
smartin00 <smartin@orienteed.com>
2023-03-23 15:42:10 +0100

    fix: compare icon color and remove console logs

--- app/packages/venia-ui/lib/assets/compareIcon.js
+++ app/packages/venia-ui/lib/assets/compareIcon.js
@@ -3,30 +3,20 @@ import React from 'react';
 export const CompareIcon = () => {
     return (
         <svg
-            version="1.1"
-            id="Capa_1"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlnsXlink="http://www.w3.org/1999/xlink"
-            x="0px"
-            y="0px"
             width="20px"
             height="20px"
             viewBox="0 0 38.083 38.083"
             style={{ enableBackground: 'new 0 0 38.083 38.083' }}
-            xmlSpace="preserve"
         >
             <g>
                 <path
                     d="M0.168,9.709v28.374h6.248c1.104,0,2-0.896,2-2V7.709H2.168C1.064,7.709,0.168,8.605,0.168,9.709z"
-                    fill="var(--b2bstore-global-color-orange)"
                 />
                 <path
                     d="M22.776,36.083V15.3h-6.248c-1.105,0-2,0.896-2,2v20.783h6.248C21.88,38.083,22.776,37.188,22.776,36.083z"
-                    fill="var(--b2bstore-global-color-orange)"
                 />
                 <path
                     d="M37.915,36.083V0h-6.248c-1.104,0-2,0.896-2,2v36.083h6.248C37.018,38.083,37.915,37.188,37.915,36.083z"
-                    fill="var(--b2bstore-global-color-orange)"
                 />
             </g>
         </svg>
