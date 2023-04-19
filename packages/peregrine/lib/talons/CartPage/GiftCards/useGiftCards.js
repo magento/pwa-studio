b66fe14f030ff062457e7d8f1df35577cc9533fd
glo80771 <glo80771@adobe.com>
2023-04-19 18:32:55 +0530

    PWA-3112::Updated unit test cases

--- packages/peregrine/lib/talons/CartPage/GiftCards/useGiftCards.js
+++ packages/peregrine/lib/talons/CartPage/GiftCards/useGiftCards.js
@@ -117,7 +117,6 @@ export const useGiftCards = props => {
         };
     }, [checkGiftCardBalance]);
 
-
     const removeGiftCard = useCallback(
         async giftCardCode => {
             setMostRecentAction(actions.REMOVE);
