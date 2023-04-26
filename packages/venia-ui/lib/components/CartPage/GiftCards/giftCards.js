3c9e996b1d48dcdfc80f4c49c5dfc021cf0cceb1
glo80771 <glo80771@adobe.com>
2023-04-26 19:00:52 +0530

    PWA-3122::Formatted the code using prettier

--- packages/venia-ui/lib/components/CartPage/GiftCards/giftCards.js
+++ packages/venia-ui/lib/components/CartPage/GiftCards/giftCards.js
@@ -186,8 +186,7 @@ const GiftCards = props => {
                         validate={isRequired}
                     />
                 </div>
-                <span aria-live="polite"
-                >{cardBalance}</span>
+                <span aria-live="polite">{cardBalance}</span>
             </Field>
             <Field
                 classes={{
