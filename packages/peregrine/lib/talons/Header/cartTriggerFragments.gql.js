dbfed1bd805c1b1ad83588df93b0856de8fa85d2
glo82145 <glo82145@adobe.com>
2023-07-27 13:54:11 +0530

    PWA-3117::[PWA] display number of items in cart instead of quantities not working

--- packages/peregrine/lib/talons/Header/cartTriggerFragments.gql.js
+++ packages/peregrine/lib/talons/Header/cartTriggerFragments.gql.js
@@ -4,6 +4,6 @@ export const CartTriggerFragment = gql`
     fragment CartTriggerFragment on Cart {
         id
         total_quantity
-        
+        total_summary_quantity_including_config
     }
 `;
