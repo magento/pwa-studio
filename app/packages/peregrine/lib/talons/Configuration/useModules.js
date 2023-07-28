647ea837480ee1dce29944fad486866acf108317
smartin00 <smartin@orienteed.com>
2023-07-28 08:47:46 +0200

    Add env vars

--- app/packages/peregrine/lib/talons/Configuration/useModules.js
+++ app/packages/peregrine/lib/talons/Configuration/useModules.js
@@ -12,10 +12,15 @@ export const useModules = () => {
             this.chatbotEnabled = Boolean(tenantConfig.chatbot?.ENABLED === 'true' || tenantConfig.chatbot?.ENABLED === true);
             this.braintreeToken = tenantConfig.braintree?.CHECKOUT_BRAINTREE_TOKEN;
             this.googleAnalyticsTrackingId = tenantConfig.googleAnalytics?.GOOGLE_ANALYTICS_TRACKING_ID;
-            this.b2bProductDetailView = Boolean(
-                tenantConfig.b2b?.IS_B2B === 'true' || tenantConfig.b2b?.IS_B2B === true
-                );
+            this.b2bProductDetailView = Boolean(tenantConfig.b2b?.IS_B2B === 'true' || tenantConfig.b2b?.IS_B2B === true);
             this.GoogleMapApiKey = tenantConfig.googleMap?.GOOGLE_MAPS_API_KEY;
+            this.downloadCsv = Boolean(tenantConfig.downloadCsv?.DOWNLOAD_CSV === 'true' || tenantConfig.downloadCsv?.DOWNLOAD_CSV === true);
+            this.quickCart = Boolean(tenantConfig.quickCart?.QUICK_CART === 'true' || tenantConfig.quickCart?.QUICK_CART === true);
+            this.requestForQuote = Boolean(tenantConfig.requestForQuote?.REQUEST_FOR_QUOTE === 'true' || tenantConfig.requestForQuote?.REQUEST_FOR_QUOTE === true);
+            this.stockVisibility = Boolean(tenantConfig.stockVisibility?.STOCK_VISIBILITY === 'true' || tenantConfig.stockVisibility?.STOCK_VISIBILITY === true);
+            this.addToCartFromSearch = Boolean(tenantConfig.addToCartFromSearch?.ADD_TO_CART_FROM_SEARCH === 'true' || tenantConfig.addToCartFromSearch?.ADD_TO_CART_FROM_SEARCH === true);
+            this.productComparator = Boolean(tenantConfig.productComparator?.PRODUCT_COMPARATOR === 'true' || tenantConfig.productComparator?.PRODUCT_COMPARATOR === true);
+            this.printPdfOfTheCart = Boolean(tenantConfig.printPdfOfTheCart?.PRINT_PDF_OF_THE_CART === 'true' || tenantConfig.printPdfOfTheCart?.PRINT_PDF_OF_THE_CART === true);
         }
     }
 
@@ -41,10 +46,37 @@ export const useModules = () => {
             },
             googleMap:{
                 GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY
+            },
+            backendTechnology: {
+                BACKEND_TECHNOLOGY: process.env.BACKEND_TECHNOLOGY
+            },
+            bigcommerceChannelId: {
+                BIGCOMMERCE_CHANNEL_ID: process.env.BIGCOMMERCE_CHANNEL_ID
+            },
+            downloadCsv: {
+                DOWNLOAD_CSV: process.env.DOWNLOAD_CSV
+            },
+            quickCart: {
+                QUICK_CART: process.env.QUICK_CART
+            },
+            requestForQuote: {
+                REQUEST_FOR_QUOTE: process.env.REQUEST_FOR_QUOTE
+            },
+            stockVisibility: {
+                STOCK_VISIBILITY: process.env.STOCK_VISIBILITY
+            },
+            addToCartFromSearch: {
+                ADD_TO_CART_FROM_SEARCH: process.env.ADD_TO_CART_FROM_SEARCH
+            },
+            productComparator: {
+                PRODUCT_COMPARATOR: process.env.PRODUCT_COMPARATOR
+            },
+            printPdfOfTheCart: {
+                PRINT_PDF_OF_THE_CART: process.env.PRINT_PDF_OF_THE_CART
             }
         };
 
-        return enabledModulesObj;
+            return enabledModulesObj;
     }
 
     const fetchTenantConfig = useCallback(
