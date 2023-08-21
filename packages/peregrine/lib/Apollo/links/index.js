e440f4aea43d499576b3a882f4c2bc9a7c01bb4a
glo82145 <glo82145@adobe.com>
2023-08-21 15:16:22 +0530

    PWA-3154::Dont Merge

--- packages/peregrine/lib/Apollo/links/index.js
+++ packages/peregrine/lib/Apollo/links/index.js
@@ -51,27 +51,16 @@ const getLinks = apiBase => {
 
     // preserve this array order, it's important
     // as the terminating link, `httpLink` must be last
-    if ( !gqlCacheLink ) { 
-        const links = new Map()
-            .set('MUTATION_QUEUE', mutationQueueLink)
-            .set('RETRY', retryLink)
-            .set('AUTH', authLink)
-            .set('STORE', storeLink)
-            .set('ERROR', errorLink)
-            .set('HTTP', httpLink);
-        
-        return links;
-    } else { 
-        const links = new Map()
-            .set('MUTATION_QUEUE', mutationQueueLink)
-            .set('RETRY', retryLink)
-            .set('GQL_CACHE', gqlCacheLink)
-            .set('STORE', storeLink)
-            .set('ERROR', errorLink)
-            .set('HTTP', httpLink);
-        return links;
-    }
-    
+    const links = new Map()
+        .set('MUTATION_QUEUE', mutationQueueLink)
+        .set('RETRY', retryLink)
+        .set('AUTH', authLink)
+        .set('GQL_CACHE', gqlCacheLink)
+        .set('STORE', storeLink)
+        .set('ERROR', errorLink)
+        .set('HTTP', httpLink);
+
+    return links;
 };
 
 export default getLinks;
