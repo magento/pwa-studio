7646e578473777dac2e087b58dd982a9007c74d1
glo80771 <glo80771@adobe.com>
2023-08-28 14:24:03 +0530

    PWA-3154::changes

--- packages/peregrine/lib/Apollo/links/authLink.js
+++ packages/peregrine/lib/Apollo/links/authLink.js
@@ -2,7 +2,7 @@ import { ApolloLink } from '@apollo/client';
 import { BrowserPersistence } from '@magento/peregrine/lib/util';
 
 const storage = new BrowserPersistence();
-const token = storage.getItem('signin_token') || null;
+var token = storage.getItem('signin_token') || null;
 export class MagentoGQLCacheLink extends ApolloLink {
     // The token get reinstantiated on refresh.
     // If we have an existing token value from a previous browsing session, use it.
@@ -17,6 +17,7 @@ export class MagentoGQLCacheLink extends ApolloLink {
         operation.setContext(previousContext => {
             const { headers } = previousContext;
             // return the headers to the context so httpLink can read them
+            // console.log(token);
             return {
                 headers: {
                     ...headers,
@@ -26,14 +27,15 @@ export class MagentoGQLCacheLink extends ApolloLink {
         });
 
         // Update the token from each response.
-        const updateToken = data => { console.log(token);
+        const updateToken = data => {
             const context = operation.getContext();
             const { response } = context;
 
-            if (response.headers.get('Pragma') != 'no-cache') {
+            
+            if (response.headers.get('Pragma') != 'cache') {
                 this.settoken(null);
             }
-
+            console.log(token);
             // Purposefully don't modify the result,
             // no other link needs to know about the cache id.
             return data;
