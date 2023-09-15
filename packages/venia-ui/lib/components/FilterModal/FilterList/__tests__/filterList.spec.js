47d0ec56fa07da038766393f5861cb6a74fc9df8
glo80771 <glo80771@adobe.com>
2023-09-15 13:24:07 +0530

    PWA-3162-V2::Updated unit test case

--- packages/venia-ui/lib/components/FilterModal/FilterList/__tests__/filterList.spec.js
+++ packages/venia-ui/lib/components/FilterModal/FilterList/__tests__/filterList.spec.js
@@ -34,11 +34,6 @@ jest.mock('@magento/peregrine/lib/talons/FilterModal', () => ({
 }));
 
 jest.mock('react-router-dom', () => ({
-    useHistory: jest.fn(),
-    useLocation: jest.fn()
-}));
-
-jest.mock('react-router-dom', () => ({
     useHistory: jest.fn(() => ({ push: jest.fn() })),
     useLocation: jest.fn(() => ({ pathname: '', search: '' }))
 }));
