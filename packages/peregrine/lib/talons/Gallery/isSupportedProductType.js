09808cdbab6502159051e70b6fc9ffdc50f2cc10
glo82145 <glo82145@adobe.com>
2024-06-25 19:25:47 +0530

    Product recommendation

---
+++ packages/peregrine/lib/talons/Gallery/isSupportedProductType.js
@@ -0,0 +1,5 @@
+const SUPPORTED_PRODUCT_TYPES = ['SimpleProduct', 'ConfigurableProduct', 'configurable', 'simple'];
+
+export const isSupportedProductType = productType => {
+    return SUPPORTED_PRODUCT_TYPES.includes(productType);
+};
