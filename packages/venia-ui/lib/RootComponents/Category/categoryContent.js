4448d78223883ad70f97664ec5f8ae789d3a28e4
glo82145 <glo82145@adobe.com>
2024-07-19 14:22:34 +0530

    PWA-3307::[Cloud] Language translating giving errors

--- packages/venia-ui/lib/RootComponents/Category/categoryContent.js
+++ packages/venia-ui/lib/RootComponents/Category/categoryContent.js
@@ -101,15 +101,18 @@ const CategoryContent = props => {
 
     const categoryResultsHeading =
         totalCount > 0 ? (
-         <div>  <FormattedMessage
-                id={'categoryContent.resultCount'}
-                values={{
-                    count: totalCount
-                }}
-                defaultMessage={'{count} Results'}
-            /> </div> 
+            <div>
+                {' '}
+                <FormattedMessage
+                    id={'categoryContent.resultCount'}
+                    values={{
+                        count: totalCount
+                    }}
+                    defaultMessage={'{count} Results'}
+                />{' '}
+            </div>
         ) : isLoading ? (
-           <Shimmer width={5} />
+            <Shimmer width={5} />
         ) : null;
 
     const categoryDescriptionElement = categoryDescription ? (
@@ -152,45 +155,48 @@ const CategoryContent = props => {
     return (
         <Fragment>
             <div>
-            <Breadcrumbs categoryId={categoryId} />
-            <StoreTitle>{categoryName}</StoreTitle>
-            <article className={classes.root} data-cy="CategoryContent-root">
-                <div className={classes.categoryHeader}>
-                    <h1 aria-live="polite" className={classes.title}>
-                        <div
-                            className={classes.categoryTitle}
-                            data-cy="CategoryContent-categoryTitle"
-                        >
-                           <div>{categoryTitle}</div> 
-                        </div>
-                    </h1>
-                    {categoryDescriptionElement}
-                </div>
-                <div className={classes.contentWrapper}>
-                    <div ref={sidebarRef} className={classes.sidebar}>
-                        <Suspense fallback={<FilterSidebarShimmer />}>
-                            {shouldRenderSidebarContent ? sidebar : null}
-                        </Suspense>
-                    </div>
-                    <div className={classes.categoryContent}>
-                        <div className={classes.heading}>
+                <Breadcrumbs categoryId={categoryId} />
+                <StoreTitle>{categoryName}</StoreTitle>
+                <article
+                    className={classes.root}
+                    data-cy="CategoryContent-root"
+                >
+                    <div className={classes.categoryHeader}>
+                        <h1 aria-live="polite" className={classes.title}>
                             <div
-                                data-cy="CategoryContent-categoryInfo"
-                                className={classes.categoryInfo}
+                                className={classes.categoryTitle}
+                                data-cy="CategoryContent-categoryTitle"
                             >
-                                {categoryResultsHeading}
+                                <div>{categoryTitle}</div>
                             </div>
-                            <div className={classes.headerButtons}>
-                                {maybeFilterButtons}
-                                {maybeSortButton}
-                            </div>
-                            {maybeSortContainer}
-                        </div>
-                        {content}
-                        <Suspense fallback={null}>{filtersModal}</Suspense>
+                        </h1>
+                        {categoryDescriptionElement}
                     </div>
-                </div>
-            </article>
+                    <div className={classes.contentWrapper}>
+                        <div ref={sidebarRef} className={classes.sidebar}>
+                            <Suspense fallback={<FilterSidebarShimmer />}>
+                                {shouldRenderSidebarContent ? sidebar : null}
+                            </Suspense>
+                        </div>
+                        <div className={classes.categoryContent}>
+                            <div className={classes.heading}>
+                                <div
+                                    data-cy="CategoryContent-categoryInfo"
+                                    className={classes.categoryInfo}
+                                >
+                                    {categoryResultsHeading}
+                                </div>
+                                <div className={classes.headerButtons}>
+                                    {maybeFilterButtons}
+                                    {maybeSortButton}
+                                </div>
+                                {maybeSortContainer}
+                            </div>
+                            {content}
+                            <Suspense fallback={null}>{filtersModal}</Suspense>
+                        </div>
+                    </div>
+                </article>
             </div>
         </Fragment>
     );
