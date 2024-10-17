6d696f3c5063e9cc6b1c1c122883c9cabf9ef649
Bharathidasan Elangovan <del22123@adobe.com>
2024-10-17 10:44:18 +0530

    Added code to display error message from grapqhl response in form

--- packages/peregrine/lib/talons/FormError/useFormError.js
+++ packages/peregrine/lib/talons/FormError/useFormError.js
@@ -14,7 +14,27 @@ export const useFormError = props => {
                   defaultMessage:
                       'An error has occurred. Please check the input and try again.'
               });
-        return deriveErrorMessage(errors, defaultErrorMessage);
+
+        const firstError = errors
+        .filter(error => error !== null||undefined)
+        .map(error => Array.isArray(error) ? error[0] : error)
+        .find(message => message);
+        var graphqlErrorMessage;
+
+        if (firstError) {
+            var category = firstError.graphQLErrors?.find(extensions => extensions).extensions?.category;
+            category = category ? category
+            .split('-')
+            .map((word, index) => index === 0 ? word : word.charAt(0).toUpperCase() + word.slice(1))
+            .join('') : null;
+            const errorId = category ? 'formError.'+category : 'formError.responseError';
+            graphqlErrorMessage = formatMessage({
+                id: errorId,
+                defaultMessage: firstError.message
+            });
+        }
+
+        return (graphqlErrorMessage) ? deriveErrorMessage(errors, graphqlErrorMessage) : deriveErrorMessage(errors, defaultErrorMessage);
     }, [errors, formatMessage, allowErrorMessages]);
 
     return {
