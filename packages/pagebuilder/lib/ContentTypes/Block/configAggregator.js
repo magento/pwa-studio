10b60cee12b7706ef67c55b7894fe3a7b273dba3
Shivam7-1 <55046031+Shivam7-1@users.noreply.github.com>
2024-12-06 21:47:15 +0530

    Update configAggregator.js

--- packages/pagebuilder/lib/ContentTypes/Block/configAggregator.js
+++ packages/pagebuilder/lib/ContentTypes/Block/configAggregator.js
@@ -1,8 +1,16 @@
+import DOMPurify from 'dompurify';
 import { getAdvanced } from '../../utils';
 
 export default node => {
+    // Get the raw HTML content from the first child node
+    const rawHTML = node.childNodes[0] ? node.childNodes[0].innerHTML : '';
+
+    // Sanitize the raw HTML using DOMPurify
+    const sanitizedHTML = DOMPurify.sanitize(rawHTML);
+
     return {
-        richContent: node.childNodes[0] ? node.childNodes[0].innerHTML : '',
+        // Return the sanitized HTML content, along with the result from getAdvanced
+        richContent: sanitizedHTML,
         ...getAdvanced(node)
     };
 };
