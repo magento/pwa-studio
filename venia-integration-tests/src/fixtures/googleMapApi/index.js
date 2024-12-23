b05f8cc8e43a8ba109f783760caefef400360313
Shivam7-1 <55046031+Shivam7-1@users.noreply.github.com>
2024-12-23 11:39:41 +0000

    Fix

--- venia-integration-tests/src/fixtures/googleMapApi/index.js
+++ venia-integration-tests/src/fixtures/googleMapApi/index.js
@@ -86,7 +86,9 @@ export const createGoogleMapApi = currentMapApi => {
 
                 open(map) {
                     map.infoWindowContainer.style.maxWidth = this.maxWidth;
-                    map.infoWindowContainer.innerHTML = DOMPurify.sanitize(this.content);
+                    map.infoWindowContainer.innerHTML = DOMPurify.sanitize(
+                        this.content
+                    );
                 }
 
                 close() {
