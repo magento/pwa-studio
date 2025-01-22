561e4806e985cd4545dd154daac9a33156fd529a
Bharathidasan Elangovan <del22123@adobe.com>
2025-01-22 16:25:06 +0530

    Fixed graphql blocker issue for this ticket

--- packages/peregrine/lib/talons/CmsDynamicBlock/cmsDynamicBlock.gql.js
+++ packages/peregrine/lib/talons/CmsDynamicBlock/cmsDynamicBlock.gql.js
@@ -2,16 +2,12 @@ import { gql } from '@apollo/client';
 
 export const GET_CMS_DYNAMIC_BLOCKS = gql`
     query GetCmsDynamicBlocks(
-        $cartId: String
-        $productId: ID
         $type: DynamicBlockTypeEnum!
         $locations: [DynamicBlockLocationEnum]
         $uids: [ID]!
     ) {
         dynamicBlocks(
             input: {
-                cart_id: $cartId
-                product_uid: $productId
                 dynamic_block_uids: $uids
                 locations: $locations
                 type: $type
