e1041825192344932d773175001914ef456b8d07
Aanchal Pawar <97873570+glo82145@users.noreply.github.com>
2025-01-24 18:03:01 +0530

    Pwa 2968::Gomage/plugin braintree three d secure (#4378)
    * The current problem is that the 3D Secure feature is not working. Whenever you are trying to run a transaction, you get an error (see image), and there seems to be no API call to process this request.
    
    * [PWA Studio] - Test Braintree #D Secure fix for Venia
    
    * [PWA Studio] - Add to frame-src  *.cardinalcommerce.com (for test only)
    
    * [PWA Studio] - Add to frame-src  *.cardinalcommerce.com (for test only)
    
    * PWA-2968::[Issue] Gomage/plugin braintree three d secure
    
    * PWA-2968::[Issue] Gomage/plugin braintree three d secure
    
    * Resolve clean issue
    
    * PWA-2968:updating version
    
    * Adding license field  else showing error during yarn clean:all
    
    * Gomage/plugin braintree three d secure
    
    ---------
    
    Co-authored-by: rtsehynka <rtsehynka@gomage.com>
    Co-authored-by: rtsehynka-gm <59726618+rtsehynka-gm@users.noreply.github.com>
    Co-authored-by: Devagouda <40405790+dpatil-magento@users.noreply.github.com>
    Co-authored-by: chetana11 <cjain11.cj@gmail.com>

---
+++ packages/extensions/gomage-plugin-braintree-three-d-secure/intercept.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * @param targets
+ */
+function localIntercept(targets) {
+    require('@gomage/plugin-braintree-three-d-secure/Intercepts/brainTreeDropIn')(
+        targets
+    );
+}
+
+module.exports = localIntercept;
