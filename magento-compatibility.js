a0ad6e1eaba8d2a49e7e260ce388890c3070ea82
Aanchal Pawar <97873570+glo82145@users.noreply.github.com>
2025-03-04 15:53:46 +0530

    Merge conflict (#4433)
    * adding release 14.1.0 change.log md
    
    * adding compatibility.js
    
    * Removed npm yml
    
    * v14.2.0
    
    * v14.2.0
    
    ---------
    
    Co-authored-by: arpit.khare <glo42707@adobe.com>
    Co-authored-by: magento-github-admin-beta <119524626+magento-github-admin-beta@users.noreply.github.com>
    Co-authored-by: devops-pwa-codebuild <magento-devops-github-pwa-int@adobe.com>

--- magento-compatibility.js
+++ magento-compatibility.js
@@ -42,7 +42,7 @@ var magentoVersion = '2.4.7-p1';
 // React version compatibility
 var reactVersion = '17.0.2';
 
-// Fornt End dependencies
+// Front end dependencies
 var forntEndDependencies = {
     '@magento/babel-preset-peregrine': '1.3.3',
     '@magento/create-pwa': '2.4.6',
