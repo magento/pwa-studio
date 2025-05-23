a38fe8cb59a64856168e8e830b8ae79a70cb04ea
del15881 <del15881@adobe.com>
2025-05-23 14:58:57 +0530

    Adding Eventing Changes , Added new Queries, Hooks, Wrappers to trigger tp2 events

---
+++ packages/extensions/venia-pwa-live-search/src/queries/eventing/getPageType.gql.js
@@ -0,0 +1,13 @@
+import { gql } from '@apollo/client';
+
+export const RESOLVE_PAGE_TYPE = gql`
+  query ResolveURL($url: String!) {
+    urlResolver(url: $url) {
+      type
+    }
+  }
+`;
+
+export default {
+  resolvePagetypeQuery: RESOLVE_PAGE_TYPE,
+};
---
+++ packages/extensions/venia-pwa-live-search/src/utils/eventing/getPageType.js
@@ -0,0 +1,26 @@
+import { useQuery } from '@apollo/client';
+import query from '../../queries/eventing/getPageType.gql';
+
+const pagetypeMap = {
+  CMS_PAGE: 'CMS',
+  CATEGORY: 'Category',
+  PRODUCT: 'Product',
+  '/cart': 'Cart',
+  '/checkout': 'Checkout',
+};
+
+export const getPagetype = ({ pathname }) => {
+  if (pathname) {
+    const queryResult = useQuery(query.resolvePagetypeQuery, {
+      fetchPolicy: 'cache-and-network',
+      nextFetchPolicy: 'cache-first',
+      variables: { url: pathname },
+    });
+    const { data } = queryResult || {};
+    const { urlResolver } = data || {};
+    const { type } = urlResolver || {};
+    // use pagetype from graphql, if it doesn't match, check pathname, if it doesn't match, return undefined.
+    const pagetype = pagetypeMap[type] || pagetypeMap[pathname];
+    return pagetype;
+  }
+};
