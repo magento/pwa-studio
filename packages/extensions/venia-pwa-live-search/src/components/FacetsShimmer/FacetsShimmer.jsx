174f8923ea5cd8c78ebb5b6fc5aaf9389176ac36
del15881 <del15881@adobe.com>
2025-06-05 18:10:09 +0530

    Fixing Issues with Live search Loading/Shimmer Affect

--- packages/extensions/venia-pwa-live-search/src/components/FacetsShimmer/FacetsShimmer.jsx
+++ packages/extensions/venia-pwa-live-search/src/components/FacetsShimmer/FacetsShimmer.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import './FacetsShimmer.css';
+import styles from './FacetsShimmer.css';
 
 export const FacetsShimmer = () => {
   return (
     <>
-      <div className="ds-sdk-input ds-sdk-input--loading">
-        <div className="ds-sdk-input__content">
-          <div className="ds-sdk-input__header">
-            <div className="ds-sdk-input__title shimmer-animation-facet" />
+      <div className={`${styles['ds-sdk-input']} ${styles['ds-sdk-input--loading']}`}>
+        <div className={styles['ds-sdk-input__content']}>
+          <div className={styles['ds-sdk-input__header']}>
+            <div className={`${styles['ds-sdk-input__title']} ${styles['shimmer-animation-facet']}`} />
           </div>
-          <div className="ds-sdk-input__list">
-            <div className="ds-sdk-input__item shimmer-animation-facet" />
-            <div className="ds-sdk-input__item shimmer-animation-facet" />
-            <div className="ds-sdk-input__item shimmer-animation-facet" />
-            <div className="ds-sdk-input__item shimmer-animation-facet" />
+          <div className={styles['ds-sdk-input__list']}>
+            <div className={`${styles['ds-sdk-input__item']} ${styles['shimmer-animation-facet']}`} />
+            <div className={`${styles['ds-sdk-input__item']} ${styles['shimmer-animation-facet']}`} />
+            <div className={`${styles['ds-sdk-input__item']} ${styles['shimmer-animation-facet']}`} />
+            <div className={`${styles['ds-sdk-input__item']} ${styles['shimmer-animation-facet']}`} />
           </div>
         </div>
       </div>
-      <div className="ds-sdk-input__border border-t mt-md border-gray-200" />
+      <div className={`${styles['ds-sdk-input__border']} ${styles['border-t']} ${styles['mt-md']} ${styles['border-gray-200']}`} />
     </>
   );
 };
