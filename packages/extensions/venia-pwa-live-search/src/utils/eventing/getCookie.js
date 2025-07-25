bdaddcccb97f30b84886790f10dbdc1739f33e2f
Aanchal Pawar <97873570+glo82145@users.noreply.github.com>
2025-07-25 18:15:37 +0530

    Removed Live search (#4515)

--- packages/extensions/venia-pwa-live-search/src/utils/eventing/getCookie.js
+++
@@ -1,9 +0,0 @@
-export const getDecodedCookie = key => {
-    const encodedCookie = document.cookie
-        .split('; ')
-        .find(row => row.startsWith(key))
-        .split('=')[1];
-    const decodedCookie = decodeURIComponent(encodedCookie);
-    const value = JSON.parse(decodedCookie);
-    return value;
-};
