3d87fc5844d21cfd33b0d2bb45abecdcbd5177bc
glo82145 <glo82145@adobe.com>
2025-08-28 11:41:32 +0530

    PWA-3576::Video component issue in dynamic CMS block

--- packages/pagebuilder/lib/ContentTypes/Block/configAggregator.js
+++ packages/pagebuilder/lib/ContentTypes/Block/configAggregator.js
@@ -6,7 +6,21 @@ export default node => {
     const rawHTML = node.childNodes[0] ? node.childNodes[0].innerHTML : '';
 
     // Sanitize the raw HTML using DOMPurify
-    const sanitizedHTML = DOMPurify.sanitize(rawHTML);
+    const sanitizedHTML = DOMPurify.sanitize(rawHTML, {
+        ADD_TAGS: ["iframe"],
+        ADD_ATTR: [
+          "allow",
+          "allowfullscreen",
+          "frameborder",
+          "scrolling",
+          "src",
+          "title",
+          "width",
+          "height",
+          "sandbox"
+        ],
+        ALLOWED_URI_REGEXP: /^https?:\/\//i // allow only http/https sources
+    });
 
     return {
         // Return the sanitized HTML content, along with the result from getAdvanced
