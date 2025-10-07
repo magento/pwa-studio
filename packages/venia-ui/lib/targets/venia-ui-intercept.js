d3c8287974b9bcf29961707ec5c68277acde01c2
glo82145 <glo82145@adobe.com>
2025-10-07 19:31:56 +0530

    PWA-3610: Fixed targetables call

--- packages/venia-ui/lib/targets/venia-ui-intercept.js
+++ packages/venia-ui/lib/targets/venia-ui-intercept.js
@@ -1,7 +1,7 @@
 /**
  * @module VeniaUI/Targets
  */
-const { Targetables } = require('@magento/pwa-buildpack');
+
 const CategoryListProductAttributes = require('./CategoryListProductAttributes');
 const RichContentRendererList = require('./RichContentRendererList');
 const makeRoutesTarget = require('./makeRoutesTarget');
@@ -12,9 +12,7 @@ const SummaryPaymentTypes = require('./SummaryPaymentTypes');
 const RootShimmerTypes = require('./RootShimmerTypes');
 
 module.exports = veniaTargets => {
-    if (!veniaTargets) {
-        throw new Error('veniaTargets not provided — check local-intercept.js');
-    }
+    const { Targetables } = require('@magento/pwa-buildpack');
     const venia = Targetables.using(veniaTargets);
 
     venia.setSpecialFeatures(
