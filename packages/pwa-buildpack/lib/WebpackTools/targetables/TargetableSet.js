fefb5d63ffbcefad9b99f4c75489f88d07bdd121
del15881 <del15881@adobe.com>
2025-10-08 18:58:41 +0530

    Fixing Prettier Issues

--- packages/pwa-buildpack/lib/WebpackTools/targetables/TargetableSet.js
+++ packages/pwa-buildpack/lib/WebpackTools/targetables/TargetableSet.js
@@ -24,12 +24,13 @@ class TargetableSet {
     }
     /** @hideconstructor  */
     constructor(targetProvider) {
-        const isValidTargetProvider = targetProvider instanceof TargetProvider ||
+        const isValidTargetProvider =
+            targetProvider instanceof TargetProvider ||
             (targetProvider &&
-             typeof targetProvider === 'object' &&
-             typeof targetProvider.of === 'function' &&
-             typeof targetProvider.name === 'string' &&
-             targetProvider.constructor.name === 'TargetProvider');
+                typeof targetProvider === 'object' &&
+                typeof targetProvider.of === 'function' &&
+                typeof targetProvider.name === 'string' &&
+                targetProvider.constructor.name === 'TargetProvider');
 
         if (!isValidTargetProvider) {
             throw new Error(
