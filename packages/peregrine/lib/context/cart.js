e91a8c603fa385b7bb826cdf2f7d22e81e3fda53
Niki Tester <niki@tester-dev.com>
2025-12-15 11:46:03 +0000

    fix: restore cart items on page reload
    - Add useEffect to load cart from localStorage on component mount
    - When cardId exists in storage but Redux state is empty.different, trigger loadCart to restore persisted car items
    - Fixes #4585

--- packages/peregrine/lib/context/cart.js
+++ packages/peregrine/lib/context/cart.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useMemo, useCallback } from 'react';
+import React, { createContext, useContext, useMemo, useCallback, useEffect } from 'react';
 import { connect } from 'react-redux';
 import actions from '../store/actions/cart/actions';
 import * as asyncActions from '../store/actions/cart/asyncActions';
@@ -16,6 +16,15 @@ const getTotalQuantity = items =>
 
 const CartContextProvider = props => {
     const { actions, asyncActions, cartState, children } = props;
+
+    useEffect(() => {
+        const storage = new BrowserPersistence();
+        const cartId = storage.getItem('cartId');
+
+        if (cartId && (!cartState || cartState.cartId !== cartId)) {
+            asyncActions.loadCart(cartId);
+        }
+    }, [])
 
     // Make deeply nested details easier to retrieve and provide empty defaults
     const derivedDetails = useMemo(() => {
