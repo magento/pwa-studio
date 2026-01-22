2247becbfb1ad38a8448d08726b3b0ebc1512949
glo82145 <glo82145@adobe.com>
2026-01-22 13:36:55 +0530

    Fixed test issues

--- packages/pwa-buildpack/lib/WebpackTools/targetables/BabelModifyJSXPlugin/Operation.js
+++ packages/pwa-buildpack/lib/WebpackTools/targetables/BabelModifyJSXPlugin/Operation.js
@@ -43,7 +43,7 @@ class Operation {
                 continue;
             }
             const { name, value } = attr;
-            
+
             this.requiredAttributes.set(
                 this._getSource(name),
                 this._getSource(value)
