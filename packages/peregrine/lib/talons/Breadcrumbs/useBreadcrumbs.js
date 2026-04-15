454eb0393de25de095f743f460345039f76f0aee
Niki Tester <accounts@tester-dev.com>
2026-04-15 08:44:00 +0100

    Fix sorting logic in sortCrumbs function. closes #4591 (#4592)

--- packages/peregrine/lib/talons/Breadcrumbs/useBreadcrumbs.js
+++ packages/peregrine/lib/talons/Breadcrumbs/useBreadcrumbs.js
@@ -7,7 +7,7 @@ import mergeOperations from '../../util/shallowMerge';
 import DEFAULT_OPERATIONS from './breadcrumbs.gql';
 
 // Just incase the data is unsorted, lets sort it.
-const sortCrumbs = (a, b) => a.category_level > b.category_level;
+const sortCrumbs = (a, b) => a.category_level - b.category_level;
 
 // Generates the path for the category.
 const getPath = (path, suffix) => {
